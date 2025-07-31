57bf1b8c812c3e0df33f3e493f509080d2875eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 14:06:00 +0000

    Add package itacart/0.1.0a3/itacart-0.1.0a3-py3-none-any.whl

---
+++ packages/itacart/itacart-0.1.0a3-py3-none-any.whl/itacart/__init__.py
@@ -0,0 +1,11 @@
+# src/itacart/__init__.py
+
+"""
+ITACaRT: An Equal-Area Parallelogram Discrete Global Grid System.
+
+This package is a placeholder for the reference implementation of the ITACaRT DGGS,
+as described in the paper: 
+http://mtc-m16c.sid.inpe.br/col/sid.inpe.br/mtc-m16c/2025/06.04.13.53/doc/thisInformationItemHomePage.html
+"""
+
+__version__ = "0.1.0a3"
