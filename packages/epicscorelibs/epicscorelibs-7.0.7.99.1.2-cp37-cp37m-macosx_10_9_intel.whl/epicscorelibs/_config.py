e5bc2b66df5d20a1dc3cb8650db4409dba3444c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:04:48 +0000

    Add package epicscorelibs/7.0.7.99.1.2/epicscorelibs-7.0.7.99.1.2-cp37-cp37m-macosx_10_9_intel.whl

---
+++ packages/epicscorelibs/epicscorelibs-7.0.7.99.1.2-cp37-cp37m-macosx_10_9_intel.whl/epicscorelibs/_config.py
@@ -0,0 +1,20 @@
+# Generated file
+from collections import OrderedDict
+toolchain_macros = OrderedDict([('__cplusplus', '199711L'),
+             ('__GNUC__', '4'),
+             ('__GNUC_MINOR__', '2'),
+             ('__GNUC_PATCHLEVEL__', '1'),
+             ('__clang__', '1'),
+             ('__clang_version__', '"15.0.0 (clang-1500.1.0.2.5)"'),
+             ('_MSC_VER', None),
+             ('_MSC_FULL_VER', None),
+             ('__GLIBC__', None),
+             ('__GLIBCXX__', None),
+             ('_GLIBCXX_USE_CXX11_ABI', None),
+             ('__UCLIBC__', None),
+             ('__UCLIBC_MAJOR__', None),
+             ('__UCLIBC_MINOR__', None),
+             ('__UCLIBC_SUBLEVEL__', None),
+             ('_CPPLIB_VER', None),
+             ('_LIBCPP_VERSION', '160006')])
+cxxdefs = [('_GLIBCXX_USE_CXX11_ABI', '0')]
