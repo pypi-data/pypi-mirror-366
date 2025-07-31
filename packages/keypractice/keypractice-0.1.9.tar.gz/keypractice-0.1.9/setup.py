7e94ec4caa7a4c2d2d2892331f4b65f4646eb4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:54 +0000

    Add package keypractice/0.1.9/keypractice-0.1.9.tar.gz

---
+++ packages/keypractice/keypractice-0.1.9.tar.gz/keypractice-0.1.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Read the README file for long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='keypractice',
+    version='0.1.9',
+    description='A slim, cross-platform terminal-based typing trainer with YAML-based exercises and analytics.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Stephen',
+    packages=find_packages(),
+    install_requires=[
+        'PyYAML>=6.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'keypractice=keypractice.__main__:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        'keypractice': ['data/*.json', 'exercises/*.yaml'],
+    },
+    python_requires='>=3.7',
+) 
