a1382b6f5caf7fe1134f48ca085ae41add619d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:44 +0000

    Add package tesorotools-python/0.0.18/tesorotools_python-0.0.18.tar.gz

---
+++ packages/tesorotools-python/tesorotools_python-0.0.18.tar.gz/tesorotools_python-0.0.18/tesorotools/utils/globals.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+BASE_PATH: Path = Path(__file__).parent.parent
+
+DEBUG: Path = Path("debug")
+CONFIG: Path = Path("config")
+EXAMPLES: Path = Path("examples")
+
+# various assets for the plots
+ASSETS: Path = BASE_PATH / "assets"
+FONTS: Path = ASSETS / "fonts"
+STYLE_SHEET: Path = ASSETS / "tesoro.mplstyle"
+
+PLOT_CONFIG_FILE: Path = ASSETS / "plots.yaml"
