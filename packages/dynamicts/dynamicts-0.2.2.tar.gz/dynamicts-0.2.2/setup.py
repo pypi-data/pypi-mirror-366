c71c62df1e8a02b2e703d917ff7b96fa864124d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:33:18 +0000

    Add package dynamicts/0.2.2/dynamicts-0.2.2.tar.gz

---
+++ packages/dynamicts/dynamicts-0.2.2.tar.gz/dynamicts-0.2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+    
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+    
+__version__ = "0.2.2"
+REPO_NAME = "DynamicTS"
+PACKAGE_NAME = "dynamicts"
+ORGANISATION_USERNAME = "Chinar-Quantum-AI-Ltd"
+ORGANISATION_EMAIL = "engineering@chinarqai.com"
+
+
+setup(
+    name=PACKAGE_NAME,
+    version=__version__,
+    description='A library for time series analysis and preprocessing',
+    author=ORGANISATION_USERNAME,
+    author_email=ORGANISATION_EMAIL,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{ORGANISATION_USERNAME}/{REPO_NAME}",
+    project_urls={
+        "Issue Tracker": f"https://github.com/{ORGANISATION_USERNAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "."},
+    packages=find_packages(include=["dynamicts"]),
+)
