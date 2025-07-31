7db6e69b136c55835d59578bdf5e898d80080891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 14:06:55 +0000

    Add package boto3-stubs-full/1.39.17/boto3_stubs_full-1.39.17-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.39.17-py3-none-any.whl/mypy_boto3_sesv2/__init__.py
@@ -0,0 +1,31 @@
+"""
+Main interface for sesv2 service.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_sesv2/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_sesv2 import (
+        Client,
+        ListMultiRegionEndpointsPaginator,
+        SESV2Client,
+    )
+
+    session = Session()
+    client: SESV2Client = session.client("sesv2")
+
+    list_multi_region_endpoints_paginator: ListMultiRegionEndpointsPaginator = client.get_paginator("list_multi_region_endpoints")
+    ```
+"""
+
+from .client import SESV2Client
+from .paginator import ListMultiRegionEndpointsPaginator
+
+Client = SESV2Client
+
+
+__all__ = ("Client", "ListMultiRegionEndpointsPaginator", "SESV2Client")
