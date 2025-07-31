a3e4f05d370279c6a2749afc7480c91ea4ccb1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:05:12 +0000

    Add package azol/0.4.0b3/azol-0.4.0b3-py3-none-any.whl

---
+++ packages/azol/azol-0.4.0b3-py3-none-any.whl/azol/clients/__init__.py
@@ -0,0 +1,18 @@
+"""Module containing all http clients"""
+from azol.clients.arm_client import ArmClient
+from azol.clients.graph_client import GraphClient
+from azol.clients.key_vault_client import KeyVaultClient
+from azol.clients.oauth_http_client import OAuthHTTPClient
+from azol.clients.datafactory_client import DataFactoryClient
+from azol.clients.kudu_client import KuduClient
+from azol.clients.devops_agent_client import AzureDevOpsAgentClient
+
+__all__ = [
+    "ArmClient",
+    "GraphClient",
+    "KeyVaultClient",
+    "OAuthHTTPClient",
+    "DataFactoryClient",
+    "KuduClient",
+    "AzureDevOpsAgentClient"
+]
