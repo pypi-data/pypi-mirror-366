245d331e1a15769ae98fd3de1607a5725d0f3d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 17:06:56 +0000

    Add package causal-testing-framework/11.0.0/causal_testing_framework-11.0.0-py3-none-any.whl

---
+++ packages/causal-testing-framework/causal_testing_framework-11.0.0-py3-none-any.whl/causal_testing/testing/causal_test_case.py
@@ -0,0 +1,92 @@
+"""This module contains the CausalTestCase class, a class that holds the information required for a causal test"""
+
+import logging
+from typing import Any
+
+from causal_testing.specification.variable import Variable
+from causal_testing.testing.causal_effect import CausalEffect
+from causal_testing.testing.base_test_case import BaseTestCase
+from causal_testing.estimation.abstract_estimator import Estimator
+from causal_testing.testing.causal_test_result import CausalTestResult, TestValue
+
+
+logger = logging.getLogger(__name__)
+
+
+class CausalTestCase:
+    # pylint: disable=too-many-instance-attributes
+    """
+    A CausalTestCase extends the information held in a BaseTestCase. As well as storing the treatment and outcome
+    variables, a CausalTestCase stores the values of these variables. Also the outcome variable and value are
+    specified. The goal of a CausalTestCase is to test whether the intervention made to the control via the treatment
+               causes the model-under-test to produce the expected change.
+    """
+
+    def __init__(
+        # pylint: disable=too-many-arguments
+        self,
+        base_test_case: BaseTestCase,
+        expected_causal_effect: CausalEffect,
+        estimate_type: str = "ate",
+        estimate_params: dict = None,
+        effect_modifier_configuration: dict[Variable:Any] = None,
+        estimator: type(Estimator) = None,
+    ):
+        """
+        :param base_test_case: A BaseTestCase object consisting of a treatment variable, outcome variable and effect
+        :param expected_causal_effect: The expected causal effect (Positive, Negative, No Effect).
+        :param estimate_type: A string which denotes the type of estimate to return.
+        :param effect_modifier_configuration: The assignment of the effect modifiers to use for estimates.
+        :param estimator: An Estimator class object
+        """
+        self.base_test_case = base_test_case
+        self.expected_causal_effect = expected_causal_effect
+        self.outcome_variable = base_test_case.outcome_variable
+        self.treatment_variable = base_test_case.treatment_variable
+        self.estimate_type = estimate_type
+        self.estimator = estimator
+        if estimate_params is None:
+            self.estimate_params = {}
+
+        else:
+            self.estimate_params = estimate_params
+
+        self.effect = base_test_case.effect
+
+        if effect_modifier_configuration:
+            self.effect_modifier_configuration = effect_modifier_configuration
+        else:
+            self.effect_modifier_configuration = {}
+
+    def execute_test(self, estimator: type(Estimator) = None) -> CausalTestResult:
+        """
+        Execute a causal test case and return the causal test result.
+        :param estimator: An alternative estimator. Defaults to `self.estimator`. This parameter is useful when you want
+        to execute a test with different data or a different equational form, but don't want to redefine the whole test
+        case.
+
+        :return causal_test_result: A CausalTestResult for the executed causal test case.
+        """
+
+        if estimator is None:
+            estimator = self.estimator
+
+        if not hasattr(estimator, f"estimate_{self.estimate_type}"):
+            raise AttributeError(f"{estimator.__class__} has no {self.estimate_type} method.")
+        estimate_effect = getattr(estimator, f"estimate_{self.estimate_type}")
+        effect, confidence_intervals = estimate_effect(**self.estimate_params)
+        return CausalTestResult(
+            estimator=estimator,
+            test_value=TestValue(self.estimate_type, effect),
+            effect_modifier_configuration=self.effect_modifier_configuration,
+            confidence_intervals=confidence_intervals,
+        )
+
+    def __str__(self):
+        treatment_config = {self.treatment_variable.name: self.estimator.treatment_value}
+        control_config = {self.treatment_variable.name: self.estimator.control_value}
+        outcome_variable = {self.outcome_variable.name}
+        return (
+            f"Running {treatment_config} instead of {control_config} should cause the following "
+            f"changes to {outcome_variable}: {self.expected_causal_effect}."
+        )
---
+++ packages/causal-testing-framework/causal_testing_framework-11.0.0-py3-none-any.whl/causal_testing/testing/causal_test_result.py
@@ -0,0 +1,137 @@
+"""This module contains the CausalTestResult class, which is a container for the results of a causal test, and the
+TestValue dataclass.
+"""
+
+from typing import Any
+from dataclasses import dataclass
+import pandas as pd
+
+from causal_testing.estimation.abstract_estimator import Estimator
+from causal_testing.specification.variable import Variable
+
+
+@dataclass
+class TestValue:
+    """A dataclass to hold both the type and value of a causal test result"""
+
+    type: str
+    value: float
+
+
+class CausalTestResult:
+    """A container to hold the results of a causal test case. Every causal test case provides a point estimate of
+    the ATE, given a particular treatment, outcome, and adjustment set. Some but not all estimators can provide
+    confidence intervals."""
+
+    def __init__(
+        # pylint: disable=too-many-arguments
+        self,
+        estimator: Estimator,
+        test_value: TestValue,
+        confidence_intervals: [pd.Series, pd.Series] = None,
+        effect_modifier_configuration: {Variable: Any} = None,
+        adequacy=None,
+    ):
+        self.estimator = estimator
+        self.adequacy = adequacy
+        if estimator.adjustment_set:
+            self.adjustment_set = estimator.adjustment_set
+        else:
+            self.adjustment_set = set()
+        self.test_value = test_value
+        self.confidence_intervals = confidence_intervals
+
+        if effect_modifier_configuration is not None:
+            self.effect_modifier_configuration = effect_modifier_configuration
+        else:
+            self.effect_modifier_configuration = {}
+
+    def __str__(self):
+        def push(s, inc="  "):
+            return inc + str(s).replace("\n", "\n" + inc)
+
+        result_str = str(self.test_value.value)
+        if "\n" in result_str:
+            result_str = "\n" + push(self.test_value.value)
+        if isinstance(self.estimator.base_test_case.treatment_variable, list):
+            treatment = [x.name for x in self.estimator.base_test_case.treatment_variable]
+        else:
+            treatment = self.estimator.base_test_case.treatment_variable.name
+        base_str = (
+            f"Causal Test Result\n==============\n"
+            f"Treatment: {treatment}\n"
+            f"Control value: {self.estimator.control_value}\n"
+            f"Treatment value: {self.estimator.treatment_value}\n"
+            f"Outcome: {self.estimator.base_test_case.outcome_variable.name}\n"
+            f"Adjustment set: {self.adjustment_set}\n"
+        )
+        if hasattr(self.estimator, "formula"):
+            base_str += f"Formula: {self.estimator.formula}\n"
+        base_str += f"{self.test_value.type}: {result_str}\n"
+        confidence_str = ""
+        if self.confidence_intervals:
+            ci_str = " " + str(self.confidence_intervals)
+            if "\n" in ci_str:
+                ci_str = " " + push(pd.DataFrame(self.confidence_intervals).transpose().to_string(header=False))
+            confidence_str += f"Confidence intervals:{ci_str}\n"
+            confidence_str += f"Alpha:{self.estimator.alpha}\n"
+        adequacy_str = ""
+        if self.adequacy:
+            adequacy_str = str(self.adequacy)
+        return base_str + confidence_str + adequacy_str
+
+    def to_dict(self, json=False):
+        """Return result contents as a dictionary
+        :return: Dictionary containing contents of causal_test_result
+        """
+        base_dict = {
+            "treatment": (
+                self.estimator.base_test_case.treatment_variable.name
+                if self.estimator.base_test_case.treatment_variable is not None
+                else None
+            ),
+            "control_value": self.estimator.control_value,
+            "treatment_value": self.estimator.treatment_value,
+            "outcome": self.estimator.base_test_case.outcome_variable.name,
+            "adjustment_set": list(self.adjustment_set) if json else self.adjustment_set,
+            "effect_measure": self.test_value.type,
+            "effect_estimate": (
+                self.test_value.value.to_dict()
+                if json and hasattr(self.test_value.value, "to_dict")
+                else self.test_value.value
+            ),
+            "ci_low": self.ci_low().to_dict() if json and hasattr(self.ci_low(), "to_dict") else self.ci_low(),
+            "ci_high": self.ci_high().to_dict() if json and hasattr(self.ci_high(), "to_dict") else self.ci_high(),
+        }
+        if self.adequacy:
+            base_dict["adequacy"] = self.adequacy.to_dict()
+        return base_dict
+
+    def ci_low(self):
+        """Return the lower bracket of the confidence intervals."""
+        if self.confidence_intervals:
+            if isinstance(self.confidence_intervals[0], pd.Series):
+                return self.confidence_intervals[0].to_list()
+            return self.confidence_intervals[0]
+        return None
+
+    def ci_high(self):
+        """Return the higher bracket of the confidence intervals."""
+        if self.confidence_intervals:
+            if isinstance(self.confidence_intervals[1], pd.Series):
+                return self.confidence_intervals[1].to_list()
+            return self.confidence_intervals[1]
+        return None
+
+    def ci_valid(self) -> bool:
+        """Return whether or not the result has valid confidence invervals"""
+        return self.ci_low() and (not pd.isnull(self.ci_low())) and self.ci_high() and (not pd.isnull(self.ci_high()))
+
+    def summary(self):
+        """Summarise the causal test result as an intuitive sentence."""
+        treatment_variable = self.estimator.base_test_case.treatment_variable
+        print(
+            f"The causal effect of changing {treatment_variable.name} = {self.estimator.control_value} to "
+            f"{treatment_variable.name}' = {self.estimator.treatment_value} is {self.test_value.value}"
+            f"(95% confidence intervals: {self.confidence_intervals})."
+        )
