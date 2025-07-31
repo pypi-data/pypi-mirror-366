ab435db0ad7209bd02ca17c97f9a56fecc96ce36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:04:49 +0000

    Add package batch-tamarin/0.2.4/batch_tamarin-0.2.4.tar.gz

---
+++ packages/batch-tamarin/batch_tamarin-0.2.4.tar.gz/batch_tamarin-0.2.4/src/batch_tamarin/__init__.py
@@ -0,0 +1,8 @@
+"""Tamarin Python Wrapper - Run Tamarin Prover models with JSON recipes."""
+
+__version__ = "0.2.4"
+__author__ = "Luca Mandrelli <luca.mandrelli@icloud.com>"
+
+from .main import app
+
+__all__ = ["app"]
