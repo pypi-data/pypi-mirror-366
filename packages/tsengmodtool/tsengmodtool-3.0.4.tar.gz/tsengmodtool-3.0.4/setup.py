6cb2921158c24117bbf797f0854557e0efe7cb31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:05:17 +0000

    Add package tsengmodtool/3.0.4/tsengmodtool-3.0.4.tar.gz

---
+++ packages/tsengmodtool/tsengmodtool-3.0.4.tar.gz/tsengmodtool-3.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tsengmodtool',
+    version='3.0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    description='完整中文版《貓咪大戰爭》存檔修改工具，林安潔製作',
+)
