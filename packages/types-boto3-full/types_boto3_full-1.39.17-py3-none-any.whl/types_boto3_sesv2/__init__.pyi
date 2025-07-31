d84e54784c53167e3ee8d442c4831bdae9699095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 14:06:41 +0000

    Add package types-boto3-full/1.39.17/types_boto3_full-1.39.17-py3-none-any.whl

---
+++ packages/types-boto3-full/types_boto3_full-1.39.17-py3-none-any.whl/types_boto3_sesv2/__init__.pyi
@@ -0,0 +1,30 @@
+"""
+Main interface for sesv2 service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_sesv2/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_sesv2 import (
+        Client,
+        ListMultiRegionEndpointsPaginator,
+        SESV2Client,
+    )
+
+    session = Session()
+    client: SESV2Client = session.client("sesv2")
+
+    list_multi_region_endpoints_paginator: ListMultiRegionEndpointsPaginator = client.get_paginator("list_multi_region_endpoints")
+    ```
+"""
+
+from .client import SESV2Client
+from .paginator import ListMultiRegionEndpointsPaginator
+
+Client = SESV2Client
+
+__all__ = ("Client", "ListMultiRegionEndpointsPaginator", "SESV2Client")
