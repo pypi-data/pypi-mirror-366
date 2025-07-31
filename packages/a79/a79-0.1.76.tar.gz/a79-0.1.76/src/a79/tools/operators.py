5f7f80b0632f274070135c77d840746a1d624d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:18:00 +0000

    Add package a79/0.1.76/a79-0.1.76.tar.gz

---
+++ packages/a79/a79-0.1.76.tar.gz/a79-0.1.76/src/a79/tools/operators.py
@@ -0,0 +1,28 @@
+from ..client import A79Client
+from ..models.tools import DEFAULT
+from ..models.tools.operators_models import MergeContentInput, MergeContentOutput
+
+__all__ = ["MergeContentInput", "MergeContentOutput", "merge_content"]
+
+
+def merge_content(
+    *,
+    separator: str = DEFAULT,
+    content_headers: list[str] = DEFAULT,
+    content_texts: list[str] = DEFAULT,
+    indent_level: int = DEFAULT,
+    title: str | None = DEFAULT,
+) -> MergeContentOutput:
+    """
+    Merge content from multiple nodes into a single text with numbered section
+    headers.
+    """
+    kwargs = locals()
+    kwargs = {k: v for k, v in kwargs.items() if v is not DEFAULT}
+    input_model = MergeContentInput.model_validate(kwargs)
+
+    client = A79Client()
+    output_model = client.execute_tool(
+        package="operators", name="merge_content", input=input_model.model_dump()
+    )
+    return MergeContentOutput.model_validate(output_model)
