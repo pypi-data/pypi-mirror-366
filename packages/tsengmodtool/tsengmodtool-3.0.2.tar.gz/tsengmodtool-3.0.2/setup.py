14dbc32d48b5c1212d9e14bf1be50267284288b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:06:08 +0000

    Add package tsengmodtool/3.0.2/tsengmodtool-3.0.2.tar.gz

---
+++ packages/tsengmodtool/tsengmodtool-3.0.2.tar.gz/tsengmodtool-3.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tsengmodtool',
+    version='3.0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
