0f3d950ad0b4cd6abe1723cfe5459261bd715b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:18:24 +0000

    Add package dqbot/0.1.0/dqbot-0.1.0.tar.gz

---
+++ packages/dqbot/dqbot-0.1.0.tar.gz/dqbot-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dqbot",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "h5py",
+        "pyyaml",
+        "openpyxl",
+        "azure-storage-blob",
+        "argparse",
+    ],
+    entry_points={
+        "console_scripts": [
+            "dqbot=dqbot.cli:main",
+        ],
+    },
+)
