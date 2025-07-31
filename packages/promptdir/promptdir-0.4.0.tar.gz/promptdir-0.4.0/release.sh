f3dc71069923abd0992b0463ba80a6d7d2b142de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:04:52 +0000

    Add package promptdir/0.4.0/promptdir-0.4.0.tar.gz

---
+++ packages/promptdir/promptdir-0.4.0.tar.gz/promptdir-0.4.0/release.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+if ! [ "$(command -v threeflow)" ]; then
+  echo "Threeflow is not installed"
+  echo "Please install it with"
+  echo "- cargo install threeflow"
+  exit 1
+fi
+
+if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
+  echo "You have uncommitted changes."
+  return 1
+fi
+
+if [ -f dist ]; then
+  rm -r dist
+fi
+
+if [ -z "$PYPI_TOKEN" ]; then
+  echo "Please set token"
+fi
+
+function main() {
+  threeflow -r rs
+  uv version --bump minor
+  git commit -am "updating version"
+  uv build
+  uv publish --username __token__ --password "$PYPI_TOKEN"
+  threeflow -r rf
+}
+
+main
