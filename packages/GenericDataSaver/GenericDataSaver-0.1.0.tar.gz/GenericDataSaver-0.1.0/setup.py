f14d8c118a44fbba723b2f557905af33221959c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:06:47 +0000

    Add package GenericDataSaver/0.1.0/GenericDataSaver-0.1.0.tar.gz

---
+++ packages/GenericDataSaver/GenericDataSaver-0.1.0.tar.gz/GenericDataSaver-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GenericDataSaver',
+    version='0.1.0',
+    author='MondayStuff',
+    author_email='Hoangquocanhmon@icloud.com',
+    description='Saves Data',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
