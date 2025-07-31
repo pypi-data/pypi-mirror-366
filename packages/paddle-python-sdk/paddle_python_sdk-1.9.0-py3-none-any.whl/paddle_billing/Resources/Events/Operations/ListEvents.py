d8ff16b34849deae12f38642bfb1320d259036bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 17:06:38 +0000

    Add package paddle-python-sdk/1.9.0/paddle_python_sdk-1.9.0-py3-none-any.whl

---
+++ packages/paddle-python-sdk/paddle_python_sdk-1.9.0-py3-none-any.whl/paddle_billing/Resources/Events/Operations/ListEvents.py
@@ -0,0 +1,11 @@
+from paddle_billing.HasParameters import HasParameters
+
+from paddle_billing.Resources.Shared.Operations import Pager
+
+
+class ListEvents(HasParameters):
+    def __init__(self, pager: Pager | None = None):
+        self.pager = pager
+
+    def get_parameters(self) -> dict[str, str]:
+        return self.pager.get_parameters() if self.pager else {}
