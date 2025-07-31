bc923ca24a03bfef9b6a35640da5f48e6414fffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:04:40 +0000

    Add package Disc-Hub-DIA-FDR/0.3.2/disc_hub_dia_fdr-0.3.2.tar.gz

---
+++ packages/Disc-Hub-DIA-FDR/disc_hub_dia_fdr-0.3.2.tar.gz/disc_hub_dia_fdr-0.3.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Disc_Hub_DIA_FDR",
+    version="0.3.2",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["numpy","matplotlib","pandas", "pyarrow==19.0.0",
+                      "torch>=2.6.0","xgboost","scikit-learn"], #pip install torch==2.6.0+cu126 --extra-index-url https://download.pytorch.org/whl/cu126
+    author="Yiwen Yu",
+    description="A package for Disc Hub Data Integration Analysis",
+)
