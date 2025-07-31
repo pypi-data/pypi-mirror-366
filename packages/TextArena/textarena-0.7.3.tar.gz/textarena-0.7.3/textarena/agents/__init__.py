d53411fa15d1b008afc448b9ab127e7f726dc190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:05:04 +0000

    Add package TextArena/0.7.3/textarena-0.7.3.tar.gz

---
+++ packages/TextArena/textarena-0.7.3.tar.gz/textarena-0.7.3/textarena/agents/__init__.py
@@ -0,0 +1,4 @@
+# Agents
+from textarena.agents.basic_agents import HumanAgent, OpenRouterAgent, GeminiAgent, OpenAIAgent, HFLocalAgent, CerebrasAgent, AWSBedrockAgent, AnthropicAgent
+
+__all__ = ["HumanAgent", "OpenRouterAgent", "GeminiAgent", "OpenAIAgent", "HFLocalAgent", "CerebrasAgent", "AWSBedrockAgent", "AnthropicAgent"]
