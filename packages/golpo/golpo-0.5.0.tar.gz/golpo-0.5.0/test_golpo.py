e40adf84d3aedf7c8c11a5f9e89aafd4e08bb28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:05:45 +0000

    Add package golpo/0.5.0/golpo-0.5.0.tar.gz

---
+++ packages/golpo/golpo-0.5.0.tar.gz/golpo-0.5.0/test_golpo.py
@@ -0,0 +1,17 @@
+from golpo import Golpo
+import time 
+
+golpo_client = Golpo(api_key='96gkPmRejr1nLClgLpxHc3YlfpBP8IsW6Ptom6mT')
+
+start_time = time.time()
+podcast_url, podcast_script = golpo_client.create_podcast(
+    prompt="summarize",
+    uploads=['/Users/shreyas/Downloads/MSCS-2223-AI.pdf']
+)
+end_time = time.time()
+print(f'time elapsed {end_time - start_time}')
+print("******")
+print(podcast_url)
+print("********")
+print(podcast_script)
+print("**********")
