a845e305919c80ac5a227cebd2d26b9017788e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:06:31 +0000

    Add package vedro-essential/1.14.3.4/vedro_essential-1.14.3.4.tar.gz

---
+++ packages/vedro-essential/vedro_essential-1.14.3.4.tar.gz/vedro_essential-1.14.3.4/README.md
@@ -0,0 +1,10 @@
+# Vedro Essential
+
+Most common Vedro plugin for testing pipeline.
+
+# Versioning
+```
+{1.14.3}.{1}
+```
+`1.14.3` - vedro version base
+`1` - compose version
---
+++ packages/vedro-essential/vedro_essential-1.14.3.4.tar.gz/vedro_essential-1.14.3.4/setup.py
@@ -0,0 +1,43 @@
+import pathlib
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def find_required():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def get_version(filename='vedro_essential/version'):
+    return open(filename, "r").read().strip()
+
+
+HERE = pathlib.Path(__file__).parent
+README = open("README.md").read()
+setup(
+    name="vedro-essential",
+    version=get_version(),
+    description="Most common Vedro plugins and helpers",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Yuriy Sagitov",
+    author_email="pro100.ko10ok@gmail.com",
+    python_requires=">=3.10.0",
+    url="https://github.com/ko10ok/vedro-essential",
+    license="Apache-2.0",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=find_required(),
+    entry_points={},
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    package_data={
+        'vedro-essential': ['version'],
+    },
+)
