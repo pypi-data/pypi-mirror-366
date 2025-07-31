294011b14f7cf9288056a15a03cc3ef25acf85a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:18:20 +0000

    Add package markdown_convert/1.2.7/markdown_convert-1.2.7.tar.gz

---
+++ packages/markdown_convert/markdown_convert-1.2.7.tar.gz/markdown_convert-1.2.7/markdown_convert/__init__.py
@@ -0,0 +1,8 @@
+"""
+This file is used to import all the functions from the modules and make them
+available to the user.
+Author: @julynx
+"""
+
+from __main__ import *
+from .modules.convert import convert, live_convert, convert_text
