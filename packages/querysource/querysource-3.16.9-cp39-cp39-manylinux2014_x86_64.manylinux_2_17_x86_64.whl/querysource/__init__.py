2dfbce8ad35fd22e951c5d9f75a256b164203710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 14:06:22 +0000

    Add package querysource/3.16.9/querysource-3.16.9-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/querysource/querysource-3.16.9-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/querysource/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+QuerySource.
+
+Querysource is a tool for querying database with different syntax, no matter
+type of database (SQL, noSQL, etc).
+"""
+
+## more information
+__copyright__ = 'Copyright (c) 2020-2023 Jesus Lara'
+__license__ = 'BSD'
+__author__ = 'Jesus Lara <jesuslarag@gmail.com>'
