7d4733ca2cafd1ac187b956d90a815d5a352eb72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:17:50 +0000

    Add package shabti-api-client/0.1a6/shabti_api_client-0.1a6.tar.gz

---
+++ packages/shabti-api-client/shabti_api_client-0.1a6.tar.gz/shabti_api_client-0.1a6/src/shabti_api_client/__init__.py
@@ -0,0 +1,3 @@
+from .client import ShabtiClient as ShabtiClient
+from .auth_client import ShabtiAuthorizationClient as ShabtiAuthorizationClient
+from .base_client import BaseShabtiClient as BaseShabtiClient
