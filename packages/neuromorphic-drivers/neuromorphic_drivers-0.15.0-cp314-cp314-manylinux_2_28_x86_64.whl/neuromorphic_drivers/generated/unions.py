1ba35a0384b0856d6b601ed4e8dbe28fcd1d43e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:06:17 +0000

    Add package neuromorphic-drivers/0.15.0/neuromorphic_drivers-0.15.0-cp314-cp314-manylinux_2_28_x86_64.whl

---
+++ packages/neuromorphic-drivers/neuromorphic_drivers-0.15.0-cp314-cp314-manylinux_2_28_x86_64.whl/neuromorphic_drivers/generated/unions.py
@@ -0,0 +1,46 @@
+import typing
+
+from . import enums
+from .. import serde
+from .devices import inivation_davis346
+from .devices import prophesee_evk3_hd
+from .devices import prophesee_evk4
+
+
+Properties = typing.Union[
+    inivation_davis346.Properties,
+    prophesee_evk3_hd.Properties,
+    prophesee_evk4.Properties,
+]
+
+Configuration = typing.Union[
+    inivation_davis346.Configuration,
+    prophesee_evk3_hd.Configuration,
+    prophesee_evk4.Configuration,
+]
+
+UsbConfiguration = typing.Union[
+    inivation_davis346.UsbConfiguration,
+    prophesee_evk3_hd.UsbConfiguration,
+    prophesee_evk4.UsbConfiguration,
+]
+
+
+def name_to_properties(name: enums.Name) -> Properties:
+    if name == enums.Name.INIVATION_DAVIS346:
+        return inivation_davis346.Properties()
+    if name == enums.Name.PROPHESEE_EVK3_HD:
+        return prophesee_evk3_hd.Properties()
+    if name == enums.Name.PROPHESEE_EVK4:
+        return prophesee_evk4.Properties()
+    raise Exception(f"unknown name {name}")
+
+
+def deserialize_configuration(name: enums.Name, data: bytes) -> Configuration:
+    if name == enums.Name.INIVATION_DAVIS346:
+        return serde.bincode.deserialize(data, inivation_davis346.Configuration)[0]
+    if name == enums.Name.PROPHESEE_EVK3_HD:
+        return serde.bincode.deserialize(data, prophesee_evk3_hd.Configuration)[0]
+    if name == enums.Name.PROPHESEE_EVK4:
+        return serde.bincode.deserialize(data, prophesee_evk4.Configuration)[0]
+    raise Exception(f"unknown name {name}")
