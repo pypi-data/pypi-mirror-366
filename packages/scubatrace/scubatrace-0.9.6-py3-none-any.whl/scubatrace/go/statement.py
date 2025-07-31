0127b0b5669fc6a652daea8647b3686d6a1d8169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:45 +0000

    Add package scubatrace/0.9.6/scubatrace-0.9.6-py3-none-any.whl

---
+++ packages/scubatrace/scubatrace-0.9.6-py3-none-any.whl/scubatrace/go/statement.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from ..statement import BlockStatement, SimpleStatement
+
+
+class GoSimpleStatement(SimpleStatement): ...
+
+
+class GoBlockStatement(BlockStatement): ...
