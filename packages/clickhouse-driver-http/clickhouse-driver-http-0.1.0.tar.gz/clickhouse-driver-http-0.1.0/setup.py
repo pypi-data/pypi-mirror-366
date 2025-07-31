f25d2d301b3c43e6e9969ed756a2e7f85ba1c37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:38 +0000

    Add package clickhouse-driver-http/0.1.0/clickhouse-driver-http-0.1.0.tar.gz

---
+++ packages/clickhouse-driver-http/clickhouse-driver-http-0.1.0.tar.gz/clickhouse-driver-http-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="clickhouse-driver-http",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.3.0; extra == 'pandas'"
+    ],
+    extras_require={
+        "pandas": ["pandas>=1.3.0"],
+    },
+    author="Nikita Krachkovskiy",
+    author_email="sudokns@gmail.com",
+    description="Lightweight HTTP client for ClickHouse",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    keywords=["clickhouse", "http", "driver", "database"],
+)
