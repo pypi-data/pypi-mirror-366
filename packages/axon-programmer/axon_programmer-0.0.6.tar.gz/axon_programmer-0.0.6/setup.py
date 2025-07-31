eb8136658dca8493b7722262880ee02846b4346d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 14:06:43 +0000

    Add package axon-programmer/0.0.6/axon_programmer-0.0.6.tar.gz

---
+++ packages/axon-programmer/axon_programmer-0.0.6.tar.gz/axon_programmer-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="axon-programmer",
+    version="0.0.6",
+    description="Cross-platform tool for detecting and programming Axon servos",
+    long_description = open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Pranav Yerramaneni",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "hidapi",
+        "ttkthemes",
+        "PyQt6",
+        "PyQt6-Fluent-Widgets"
+    ],
+    entry_points={
+        "console_scripts": [
+            "axon-programmer=src.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+)
