fb24d0c95932f6069df15b981d118037f417bb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:33:01 +0000

    Add package countries-dictionary/2.0.1/countries_dictionary-2.0.1-py3-none-any.whl

---
+++ packages/countries-dictionary/countries_dictionary-2.0.1-py3-none-any.whl/countries_dictionary/quick_variables.py
@@ -0,0 +1,27 @@
+"""Some prepared variables"""
+
+from countries_dictionary import countries
+from countries_dictionary.russia import russia
+from countries_dictionary.vietnam import vietnam
+import json
+
+json_countries = json.dumps(countries, indent=4)
+"""A JSON string converted from the countries dictionary, having an indent level of 4"""
+
+json_russia = json.dumps(russia, indent=4)
+"""A JSON string converted from the Russia dictionary, having an indent level of 4"""
+
+json_vietnam = json.dumps(vietnam, indent=4)
+"""A JSON string converted from the Vietnam dictionary, having an indent level of 4"""
+
+countries_france_censored = countries
+countries_france_censored["Fr*nce"] = countries_france_censored.pop("France")
+countries_france_censored = dict(sorted(countries_france_censored.items()))
+"""The countries dictionary with the `France` key gets censored `Fr*nce`
+(This is only a joke, I don't support hate against France and French)"""
+
+countries_area_sorted = dict(sorted(countries.items(), key=lambda item: item[1]["area"], reverse=True))
+"""The countries dictionary sorted by area"""
+
+countries_population_sorted = dict(sorted(countries.items(), key=lambda item: item[1]["population"], reverse=True))
+"""The countries dictionary sorted by population"""
---
+++ packages/countries-dictionary/countries_dictionary-2.0.1-py3-none-any.whl/countries_dictionary/russia.py
@@ -0,0 +1,456 @@
+russia = {
+    "Adygea": {
+        "federal district": "Southern",
+        "economic region": "North Caucasus",
+        "area": 7792.0,
+        "population": 501038},
+    "Bashkortostan": {
+        "federal district": "Volga",
+        "economic region": "Ural",
+        "area": 142947.0,
+        "population": 4046094},
+    "Buryatia": {
+        "federal district": "Far Eastern",
+        "economic region": "East Siberian",
+        "area": 351334.0,
+        "population": 970679},
+    "Altai Republic": {
+        "federal district": "Siberian",
+        "economic region": "West Siberian",
+        "area": 92903.0,
+        "population": 210099},
+    "Dagestan": {
+        "federal district": "North Caucasian",
+        "economic region": "North Caucasus",
+        "area": 50270.0,
+        "population": 3258993},
+    "Ingushetia": {
+        "federal district": "North Caucasian",
+        "economic region": "North Caucasus",
+        "area": 3628.0,
+        "population": 534219},
+    "Kabardino-Balkaria": {
+        "federal district": "North Caucasian",
+        "economic region": "North Caucasus",
+        "area": 12470.0,
+        "population": 908090},
+    "Kalmykia": {
+        "federal district": "Southern",
+        "economic region": "Volga",
+        "area": 74731.0,
+        "population": 267376},
+    "Karachay-Cherkessia": {
+        "federal district": "North Caucasian",
+        "economic region": "North Caucasus",
+        "area": 14277.0,
+        "population": 468531},
+    "Karelia": {
+        "federal district": "Northwestern",
+        "economic region": "Northern",
+        "area": 180520.0,
+        "population": 518644},
+    "Komi Republic": {
+        "federal district": "Northwestern",
+        "economic region": "Northern",
+        "area": 416774.0,
+        "population": 714391},
+    "Mari El": {
+        "federal district": "Volga",
+        "economic region": "Volga-Vyatka",
+        "area": 23375.0,
+        "population": 665983},
+    "Mordovia": {
+        "federal district": "Volga",
+        "economic region": "Volga-Vyatka",
+        "area": 26128.0,
+        "population": 758390},
+    "Sakha": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 3083523.0,
+        "population": 1007058},
+    "North Ossetia–Alania": {
+        "federal district": "North Caucasian",
+        "economic region": "North Caucasus",
+        "area": 7987.0,
+        "population": 678454},
+    "Tatarstan": {
+        "federal district": "Volga",
+        "economic region": "Volga",
+        "area": 67847.0,
+        "population": 4016571},
+    "Tuva": {
+        "federal district": "Siberian",
+        "economic region": "East Siberian",
+        "area": 168604.0,
+        "population": 338341},
+    "Udmurtia": {
+        "federal district": "Volga",
+        "economic region": "Ural",
+        "area": 42061.0,
+        "population": 1427018},
+    "Khakassia": {
+        "federal district": "Siberian",
+        "economic region": "East Siberian",
+        "area": 61569.0,
+        "population": 534795},
+    "Chechnya": {
+        "federal district": "North Caucasian",
+        "economic region": "North Caucasus",
+        "area": 16165.0,
+        "population": 1575819},
+    "Chuvashia": {
+        "federal district": "Volga",
+        "economic region": "Volga-Vyatka",
+        "area": 18343.0,
+        "population": 1159757},
+    "Altai Krai": {
+        "federal district": "Siberian",
+        "economic region": "West Siberian",
+        "area": 167996.0,
+        "population": 2098979},
+    "Krasnodar Krai": {
+        "federal district": "Southern",
+        "economic region": "North Caucasus",
+        "area": 75485.0,
+        "population": 5841846},
+    "Krasnoyarsk Krai": {
+        "federal district": "Siberian",
+        "economic region": "East Siberian",
+        "area": 2366797.0,
+        "population": 2837374},
+    "Primorsky Krai": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 164673.0,
+        "population": 1798047},
+    "Stavropol Krai": {
+        "federal district": "North Caucasian",
+        "economic region": "North Caucasus",
+        "area": 66160.0,
+        "population": 2883494},
+    "Khabarovsk Krai": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 787633.0,
+        "population": 1273093},
+    "Amur Oblast": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 361908.0,
+        "population": 750870},
+    "Arkhangelsk Oblast": {
+        "federal district": "Northwestern",
+        "economic region": "Northern",
+        "area": 413103.0,
+        "population": 947192},
+    "Astrakhan Oblast": {
+        "federal district": "Southern",
+        "economic region": "Volga",
+        "area": 49024.0,
+        "population": 945991},
+    "Belgorod Oblast": {
+        "federal district": "Central",
+        "economic region": "Central Black Earth",
+        "area": 27134.0,
+        "population": 1481098},
+    "Bryansk Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 34857.0,
+        "population": 1132475},
+    "Vladimir Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 29084.0,
+        "population": 1295930},
+    "Volgograd Oblast": {
+        "federal district": "Southern",
+        "economic region": "Volga",
+        "area": 112877.0,
+        "population": 2435355},
+    "Vologda Oblast": {
+        "federal district": "Northwestern",
+        "economic region": "Northern",
+        "area": 144527.0,
+        "population": 1114639},
+    "Voronezh Oblast": {
+        "federal district": "Central",
+        "economic region": "Central Black Earth",
+        "area": 52216.0,
+        "population": 2259610},
+    "Ivanovo Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 21437.0,
+        "population": 897869},
+    "Irkutsk Oblast": {
+        "federal district": "Siberian",
+        "economic region": "East Siberian",
+        "area": 774846.0,
+        "population": 2316571},
+    "Kaliningrad Oblast": {
+        "federal district": "Northwestern",
+        "economic region": "Kaliningrad",
+        "area": 15125.0,
+        "population": 1064747},
+    "Kaluga Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 29777.0,
+        "population": 1064747},
+    "Kamchatka Krai": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 464275.0,
+        "population": 287949},
+    "Kemerovo Oblast": {
+        "federal district": "Siberian",
+        "economic region": "West Siberian",
+        "area": 95725.0,
+        "population": 2526384},
+    "Kirov Oblast": {
+        "federal district": "Volga",
+        "economic region": "Volga-Vyatka",
+        "area": 120374.0,
+        "population": 1120178},
+    "Kostroma Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 60211.0,
+        "population": 560758},
+    "Kurgan Oblast": {
+        "federal district": "Ural",
+        "economic region": "Ural",
+        "area": 71488.0,
+        "population": 744197},
+    "Kursk Oblast": {
+        "federal district": "Central",
+        "economic region": "Central Black Earth",
+        "area": 29997.0,
+        "population": 1050134},
+    "Leningrad Oblast": {
+        "federal district": "Northwestern",
+        "economic region": "Northwestern",
+        "area": 83908.0,
+        "population": 2057708},
+    "Lipetsk Oblast": {
+        "federal district": "Central",
+        "economic region": "Central Black Earth",
+        "area": 24047.0,
+        "population": 1107812},
+    "Magadan Oblast": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 462464.0,
+        "population": 134202},
+    "Moscow Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 44329.0,
+        "population": 8766594},
+    "Murmansk Oblast": {
+        "federal district": "Northwestern",
+        "economic region": "Northern",
+        "area": 144902.0,
+        "population": 650920},
+    "Nizhny Novgorod Oblast": {
+        "federal district": "Volga",
+        "economic region": "Volga-Vyatka",
+        "area": 76624.0,
+        "population": 3037816},
+    "Novgorod Oblast": {
+        "federal district": "Northwestern",
+        "economic region": "Northwestern",
+        "area": 54501.0,
+        "population": 566745},
+    "Novosibirsk Oblast": {
+        "federal district": "Siberian",
+        "economic region": "West Siberian",
+        "area": 177756.0,
+        "population": 2784587},
+    "Omsk Oblast": {
+        "federal district": "Siberian",
+        "economic region": "West Siberian",
+        "area": 141140.0,
+        "population": 1805443},
+    "Orenburg Oblast": {
+        "federal district": "Volga",
+        "economic region": "Ural",
+        "area": 123702.0,
+        "population": 1815655},
+    "Oryol Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 24652.0,
+        "population": 685693},
+    "Penza Oblast": {
+        "federal district": "Volga",
+        "economic region": "Volga",
+        "area": 43352.0,
+        "population": 1225984},
+    "Perm Krai": {
+        "federal district": "Volga",
+        "economic region": "Ural",
+        "area": 160236.0,
+        "population": 2482080},
+    "Pskov Oblast": {
+        "federal district": "Northwestern",
+        "economic region": "Northwestern",
+        "area": 55399.0,
+        "population": 574199},
+    "Rostov Oblast": {
+        "federal district": "Southern",
+        "economic region": "North Caucasus",
+        "area": 100967.0,
+        "population": 4135018},
+    "Ryazan Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 39605.0,
+        "population": 1073981},
+    "Samara Oblast": {
+        "federal district": "Volga",
+        "economic region": "Volga",
+        "area": 53565.0,
+        "population": 3108944},
+    "Saratov Oblast": {
+        "federal district": "Volga",
+        "economic region": "Volga",
+        "area": 101240.0,
+        "population": 2368387},
+    "Sakhalin Oblast": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 87101.0,
+        "population": 456792},
+    "Sverdlovsk Oblast": {
+        "federal district": "Ural",
+        "economic region": "Ural",
+        "area": 194307.0,
+        "population": 4218204},
+    "Smolensk Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 49779.0,
+        "population": 857847},
+    "Tambov Oblast": {
+        "federal district": "Central",
+        "economic region": "Central Black Earth",
+        "area": 34462.0,
+        "population": 946010},
+    "Tver Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 84201.0,
+        "population": 1189685},
+    "Tomsk Oblast": {
+        "federal district": "Siberian",
+        "economic region": "West Siberian",
+        "area": 314391.0,
+        "population": 1039458},
+    "Tula Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 25679.0,
+        "population": 1455911},
+    "Tyumen Oblast": {
+        "federal district": "Ural",
+        "economic region": "West Siberian",
+        "area": 160122.0,
+        "population": 1625129},
+    "Ulyanovsk Oblast": {
+        "federal district": "Volga",
+        "economic region": "Volga",
+        "area": 37181.0,
+        "population": 1164837},
+    "Chelyabinsk Oblast": {
+        "federal district": "Ural",
+        "economic region": "Ural",
+        "area": 88529.0,
+        "population": 3383188},
+    "Zabaykalsky Krai": {
+        "federal district": "Far Eastern",
+        "economic region": "East Siberian",
+        "area": 431892.0,
+        "population": 982525},
+    "Yaroslavl Oblast": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 36177.0,
+        "population": 1179301},
+    "Moscow": {
+        "federal district": "Central",
+        "economic region": "Central",
+        "area": 2561.0,
+        "population": 13258262},
+    "Saint Petersburg": {
+        "federal district": "Northwestern",
+        "economic region": "Northwestern",
+        "area": 1403.0,
+        "population": 5645943},
+    "Jewish Autonomous Oblast": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 36271.0,
+        "population": 144389},
+    "Nenets Autonomous Okrug": {
+        "federal district": "Northwestern",
+        "economic region": "Northern",
+        "area": 176810.0,
+        "population": 41829},
+    "Khanty-Mansi Autonomous Okrug": {
+        "federal district": "Ural",
+        "economic region": "West Siberian",
+        "area": 534801.0,
+        "population": 1779510},
+    "Chukotka Autonomous Okrug": {
+        "federal district": "Far Eastern",
+        "economic region": "Far Eastern",
+        "area": 721481.0,
+        "population": 47902},
+    "Yamalo-Nenets Autonomous Okrug": {
+        "federal district": "Ural",
+        "economic region": "West Siberian",
+        "area": 769250.0,
+        "population": 521655},
+    # Occupied zones; internationally regconised as Ukraine's territories
+    "Republic of Crimea": {
+        "federal district": "Southern",
+        "economic region": "North Caucasus",
+        "area": 26081.0,
+        "population": 1934630},
+    "Sevastopol": {
+        "federal district": "Southern",
+        "economic region": "North Caucasus",
+        "area": 864.0,
+        "population": 547820},
+    "Donetsk People's Republic": {
+        "federal district": None,
+        "economic region": None,
+        "area": 26517.0,
+        "population": 4100280},
+    "Luhansk People's Republic": {
+        "federal district": None,
+        "economic region": None,
+        "area": 26684.0,
+        "population": 2121322},
+    "Zaporozhye Oblast": {
+        "federal district": None,
+        "economic region": None,
+        "area": 27183.0,
+        "population": 1666515},
+    "Kherson Oblast": {
+        "federal district": None,
+        "economic region": None,
+        "area": 28461.0,
+        "population": 1016707},
+    # Source: https://en.wikipedia.org/wiki/Federal_subjects_of_Russia
+}
+"""A dictionary contains all federal subjects of Russia and information about them:
+- Federal district
+- Economic region
+- Area
+- Population
+
+Note: the dictionary might contain inaccuracies"""
