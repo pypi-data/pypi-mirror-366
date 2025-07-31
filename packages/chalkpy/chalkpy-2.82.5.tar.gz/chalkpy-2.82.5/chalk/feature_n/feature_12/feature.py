b31913ac2c1e7dab433be5f5733fdc62564cf667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:49 +0000

    Add package chalkpy/2.82.5/chalkpy-2.82.5.tar.gz

---
+++ packages/chalkpy/chalkpy-2.82.5.tar.gz/chalkpy-2.82.5/chalk/feature_n/feature_12/feature.py
@@ -0,0 +1,27 @@
+# AUTO-GENERATED FILE.
+# Re-run /chalk/feature_n/codegen.py to regenerate contents.
+from typing import TypeVar, Generic, Optional, Dict
+from chalk.features.dataframe import DataFrameMeta
+
+
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+T3 = TypeVar("T3")
+T4 = TypeVar("T4")
+T5 = TypeVar("T5")
+T6 = TypeVar("T6")
+T7 = TypeVar("T7")
+T8 = TypeVar("T8")
+T9 = TypeVar("T9")
+T10 = TypeVar("T10")
+T11 = TypeVar("T11")
+T12 = TypeVar("T12")
+
+
+class Features(Generic[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12]):
+    pass
+
+
+class DataFrame(Generic[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12], metaclass=DataFrameMeta):
+    def __getitem__(self, item):
+        pass
