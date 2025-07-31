46a2c9cec9c6fd41818c5bf5a9640a1892e40ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:18:25 +0000

    Add package spankbang-api/1.4/spankbang_api-1.4.tar.gz

---
+++ packages/spankbang-api/spankbang_api-1.4.tar.gz/spankbang_api-1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spankbang_api",
+    version="1.4",
+    packages=find_packages(),
+    install_requires=["bs4", "eaf_base_api"],
+    entry_points=None,
+    author="Johannes Habel",
+    author_email="EchterAlsFake@proton.me",
+    description="A Python API for the Porn Site spankbang.com",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="LGPLv3",
+    url="https://github.com/EchterAlsFake/spankbang_api",
+    classifiers=[
+        # Classifiers help users find your project on PyPI
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python",
+    ],
+)
