357f763e9c0c8e9855ec78150b1026db85533259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:06:58 +0000

    Add package logie/0.1.4/logie-0.1.4.tar.gz

---
+++ packages/logie/logie-0.1.4.tar.gz/logie-0.1.4/test/test.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.append('/home/kimyh/library/loggsy')
+import loggsy as lo
+
+def main():
+    log = lo.get_logger()
+    pass
+
+if __name__ == '__main__':
+    main()
