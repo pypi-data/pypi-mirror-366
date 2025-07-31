51a44b26b35f3b1050b9f53d60399ea6fc155ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:04:59 +0000

    Add package mem0ai-azure-mysql/0.1.115.1/mem0ai_azure_mysql-0.1.115.1-py3-none-any.whl

---
+++ packages/mem0ai-azure-mysql/mem0ai_azure_mysql-0.1.115.1-py3-none-any.whl/mem0/embeddings/azure_openai.py
@@ -0,0 +1,57 @@
+import os
+import logging
+from typing import Literal, Optional
+
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+    
+from mem0.configs.embeddings.base import BaseEmbedderConfig
+from mem0.embeddings.base import EmbeddingBase
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class AzureOpenAIEmbedding(EmbeddingBase):
+    def __init__(self, config: Optional[BaseEmbedderConfig] = None):
+        super().__init__(config)
+
+        api_key = self.config.azure_kwargs.api_key or os.getenv("EMBEDDING_AZURE_API_KEY")
+        azure_deployment = self.config.azure_kwargs.azure_deployment or os.getenv("EMBEDDING_AZURE_DEPLOYMENT")
+        azure_endpoint = self.config.azure_kwargs.azure_endpoint or os.getenv("EMBEDDING_AZURE_ENDPOINT")
+        api_version = self.config.azure_kwargs.api_version or os.getenv("EMBEDDING_AZURE_API_VERSION")
+        default_headers = self.config.azure_kwargs.default_headers
+
+        auth_kwargs = {}
+
+        if api_key:
+            logger.info(f"Using API key for Azure OpenAI embedding authentication. {api_key}")
+            auth_kwargs["azure_ad_token"] = api_key
+        else:
+            logger.info("Using Azure AD token provider for Azure OpenAI embedding authentication.")
+            credential = DefaultAzureCredential()
+            token_provider = get_bearer_token_provider(credential, "https://cognitiveservices.azure.com/.default")
+            auth_kwargs["azure_ad_token_provider"] = token_provider
+
+        self.client = AzureOpenAI(
+            azure_deployment=azure_deployment,
+            azure_endpoint=azure_endpoint,
+            api_version=api_version,
+            http_client=self.config.http_client,
+            default_headers=default_headers,
+            **auth_kwargs,
+        )
+
+    def embed(self, text, memory_action: Optional[Literal["add", "search", "update"]] = None):
+        """
+        Get the embedding for the given text using OpenAI.
+
+        Args:
+            text (str): The text to embed.
+            memory_action (optional): The type of embedding to use. Must be one of "add", "search", or "update". Defaults to None.
+        Returns:
+            list: The embedding vector.
+        """
+        text = text.replace("\n", " ")
+        return self.client.embeddings.create(input=[text], model=self.config.model).data[0].embedding
