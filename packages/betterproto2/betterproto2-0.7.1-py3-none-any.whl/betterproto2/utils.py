96bbe843a63300f8a96f1f6410f993bbb5331f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:32:56 +0000

    Add package betterproto2/0.7.1/betterproto2-0.7.1-py3-none-any.whl

---
+++ packages/betterproto2/betterproto2-0.7.1-py3-none-any.whl/betterproto2/utils.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import Any, Generic, TypeVar
+
+T_co = TypeVar("T_co")
+TT_co = TypeVar("TT_co", bound="type[Any]")
+
+
+class classproperty(Generic[TT_co, T_co]):
+    def __init__(self, func: Callable[[TT_co], T_co]):
+        self.__func__ = func
+
+    def __get__(self, instance: Any, type: TT_co) -> T_co:
+        return self.__func__(type)
+
+
+T = TypeVar("T")
+
+
+def unwrap(x: T | None) -> T:
+    """
+    Unwraps an optional value, returning the value if it exists, or raises a ValueError if the value is None.
+
+    Args:
+        value (Optional[T]): The optional value to unwrap.
+
+    Returns:
+        T: The unwrapped value if it exists.
+
+    Raises:
+        ValueError: If the value is None.
+    """
+    if x is None:
+        raise ValueError("Can't unwrap a None value")
+    return x
