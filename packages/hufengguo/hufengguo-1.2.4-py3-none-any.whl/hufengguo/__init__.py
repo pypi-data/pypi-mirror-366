f3f5e1ba73f81460768c27230824e90d13cda3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:05:50 +0000

    Add package hufengguo/1.2.4/hufengguo-1.2.4-py3-none-any.whl

---
+++ packages/hufengguo/hufengguo-1.2.4-py3-none-any.whl/hufengguo/__init__.py
@@ -0,0 +1,22 @@
+from .hufengguo import (
+    mysum,
+    isprime,
+    find_files_listdir,
+    find_files_walk,
+    my_path2path,
+
+    my_read_from_txtfile,
+    my_write_to_txtfile,
+    my_abspath,
+    my_desktop_path,
+    make_dir_exists,
+
+    seg_by_jieba,
+    segtag_by_jieba,
+    seg_str_by_jieba,
+    lseg_str_by_jieba,
+    segtag_str_by_jieba,
+    lsegtag_str_by_jieba,
+
+    remove_white_from_text,
+)
