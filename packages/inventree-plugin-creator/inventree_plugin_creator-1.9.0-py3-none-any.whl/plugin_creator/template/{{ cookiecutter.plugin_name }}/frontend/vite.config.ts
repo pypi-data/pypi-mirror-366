46983257601e848960e07eea3d7335ac3e5f2ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:33:01 +0000

    Add package inventree-plugin-creator/1.9.0/inventree_plugin_creator-1.9.0-py3-none-any.whl

---
+++ packages/inventree-plugin-creator/inventree_plugin_creator-1.9.0-py3-none-any.whl/plugin_creator/template/{{ cookiecutter.plugin_name }}/frontend/vite.config.ts
@@ -0,0 +1,68 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
+
+
+/**
+ * The following libraries are externalized to avoid bundling them with the plugin.
+ * These libraries are expected to be provided by the InvenTree core application.
+ */
+export const externalLibs : Record<string, string> = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'ReactDom': 'ReactDOM',
+  '@lingui/core': 'LinguiCore',
+  '@lingui/react': 'LinguiReact',
+  '@mantine/core': 'MantineCore',
+  "@mantine/notifications": 'MantineNotifications',
+};
+
+// Just the keys of the externalLibs object
+const externalKeys = Object.keys(externalLibs);
+
+/**
+ * Vite config to build the frontend plugin as an exported module.
+ * This will be distributed in the 'static' directory of the plugin.
+ */
+export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    }),
+    viteExternalsPlugin(externalLibs),
+  ],
+  esbuild: {
+    jsx: 'preserve',
+  },
+  build: {
+    // minify: false,
+    target: 'esnext',
+    cssCodeSplit: false,
+    manifest: true,
+    sourcemap: true,
+    rollupOptions: {
+      preserveEntrySignatures: "exports-only",
+      input: [
+        {% if cookiecutter.frontend.features.panel -%}
+        './src/Panel.tsx',
+        {%- endif %}
+        {% if cookiecutter.frontend.features.dashboard -%}
+        './src/Dashboard.tsx',
+        {%- endif %}
+        {% if cookiecutter.frontend.features.settings -%}
+        './src/Settings.tsx',
+        {%- endif %}
+      ],
+      output: {
+        dir: '../{{ cookiecutter.package_name }}/static',
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+        globals: externalLibs,
+      },
+      external: externalKeys,
+    }
+  },
+  optimizeDeps: {
+    exclude: externalKeys,
+  }
+})
