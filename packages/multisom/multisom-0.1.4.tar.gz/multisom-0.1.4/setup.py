f82484d4d109aee42b1bf4826923be1bb3e74c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:53 +0000

    Add package multisom/0.1.4/multisom-0.1.4.tar.gz

---
+++ packages/multisom/multisom-0.1.4.tar.gz/multisom-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multisom',
+    version='0.1.4',
+    author='E.Bringa-F.Aquistapace-SiMaF',
+    author_email='tatoaquista@gmail.com',
+    license='MIT',
+    description='Multilayer algorithm of Self Organising Maps (also known as Kohonen Networks) implemented in Python for clustering of atomistic samples through unsupervised learning. The program allows the user to select wich per-atom quantities to use for training and application of the network, this quantities must be specified in the LAMMPS input file that is being analysed. ',
+    url='https://github.com/SIMAF-MDZ/MultiSOM',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[ 'pandas','numpy','joblib '],
+    include_package_data=True,
+)
