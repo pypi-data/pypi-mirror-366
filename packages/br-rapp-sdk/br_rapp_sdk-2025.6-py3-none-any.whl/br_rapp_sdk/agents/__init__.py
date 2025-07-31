db0b961b84c77e145d4b8989da000568a1d460f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:06:37 +0000

    Add package br-rapp-sdk/2025.6/br_rapp_sdk-2025.6-py3-none-any.whl

---
+++ packages/br-rapp-sdk/br_rapp_sdk-2025.6-py3-none-any.whl/br_rapp_sdk/agents/__init__.py
@@ -0,0 +1,4 @@
+from .chat_model_config import ChatModelClientConfig
+from .chat_model_client import ChatModelClient
+from .tools import ToolCall
+from .agent import AgentApplication, AgentGraph, AgentTaskResult
