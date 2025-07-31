315bd8c6e6f72fcc1f5e7b29d3bf9203b455bc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:04:59 +0000

    Add package gtfs-station-stop/0.10.0/gtfs_station_stop-0.10.0.tar.gz

---
+++ packages/gtfs-station-stop/gtfs_station_stop-0.10.0.tar.gz/gtfs_station_stop-0.10.0/src/gtfs_station_stop/const.py
@@ -0,0 +1,6 @@
+"""Constants."""
+
+from datetime import timedelta
+
+GTFS_STATIC_CACHE = "gtfs_static_cache"
+GTFS_STATIC_CACHE_EXPIRY = timedelta(hours=1)
---
+++ packages/gtfs-station-stop/gtfs_station_stop-0.10.0.tar.gz/gtfs_station_stop-0.10.0/tests/test_schedule.py
@@ -0,0 +1,40 @@
+from dataclasses import asdict
+
+from gtfs_station_stop.route_info import RouteInfoDataset
+from gtfs_station_stop.schedule import GtfsSchedule, async_build_schedule
+from gtfs_station_stop.stop_times import StopTimesDataset
+
+
+async def test_async_build_schedule(mock_feed_server, snapshot):
+    schedule: GtfsSchedule = await async_build_schedule(
+        *[
+            url
+            for url in mock_feed_server.static_urls
+            if url.endswith("gtfs_static.zip")
+        ]
+    )
+    assert snapshot == asdict(schedule)
+    assert isinstance(schedule.stop_times_ds, StopTimesDataset)
+
+
+async def test_async_build_schedule_add_data_later(mock_feed_server, snapshot):
+    schedule: GtfsSchedule = await async_build_schedule(
+        *[
+            url
+            for url in mock_feed_server.static_urls
+            if url.endswith("gtfs_static.zip")
+        ]
+    )
+    orig_data = asdict(schedule)
+
+    await schedule.async_update_schedule(
+        *[
+            url
+            for url in mock_feed_server.static_urls
+            if url.endswith("gtfs_static_supl.zip")
+        ]
+    )
+    assert orig_data != asdict(schedule)
+    assert snapshot == asdict(schedule)
+    assert isinstance(schedule.stop_times_ds, StopTimesDataset)
+    assert isinstance(schedule.route_info_ds, RouteInfoDataset)
