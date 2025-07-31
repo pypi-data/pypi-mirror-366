718a5edb67efb059d12c863f1c7b7818ee16a9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:18:07 +0000

    Add package wsp-tasha-toolbox/0.1.1/wsp_tasha_toolbox-0.1.1.tar.gz

---
+++ packages/wsp-tasha-toolbox/wsp_tasha_toolbox-0.1.1.tar.gz/wsp_tasha_toolbox-0.1.1/src/wsp_tasha_toolbox/common/enums_microsim.py
@@ -0,0 +1,34 @@
+from enum import StrEnum
+
+
+class Activity(StrEnum):
+    PRIMARY_WORK = "PrimaryWork"  # The main work activity episode going to the assigned work location for the individual
+    SECONDARY_WORK = "SecondaryWork"  # A work activity episode not going to the primary work location
+    WORK_BASED_BUSINESS = "WorkBasedBusiness"  # A work location not going to the main work location, such as a business meeting
+    SCHOOL = "School"  # A school activity episode
+    MARKET = "Market"  # A market activity episode
+    JOINT_MARKET = "JointMarket"  # A market activity episode that is part of a joint tour
+    INDIVIDUAL_OTHER = "IndividualOther"  # An activity episode that is not work, school, or market
+    JOINT_OTHER = "JointOther"  # An activity episode that is not work, school, or market and is part of a joint tour
+    HOME = "Home"  # The home activity episode
+    RETURN_FROM_WORK = "ReturnFromWork"  # A purposeful return home activity episode from work, such as lunch
+
+
+class Mode(StrEnum):
+    AUTO = "Auto"
+    RIDESHARE = "RideShare"
+    VEHICLE_FOR_HIRE = "VFH"
+    PASSENGER = "Passenger"
+    CARPOOL = "Carpool"
+    WALK_ACCESS_TRANSIT = "WAT"
+    DRIVE_ACCESS_TRANSIT = "DAT"
+    PASSENGER_ACCESS_TRANSIT = "PAT"
+    PASSENGER_EGRESS_TRANSIT = "PET"
+    WALK = "Walk"
+    BIKE = "Bicycle"
+    SCHOOLBUS = "Schoolbus"
+
+
+class TransitDirection(StrEnum):
+    AUTO_TO_TRANSIT = "auto2transit"
+    TRANSIT_TO_AUTO = "transit2auto"
