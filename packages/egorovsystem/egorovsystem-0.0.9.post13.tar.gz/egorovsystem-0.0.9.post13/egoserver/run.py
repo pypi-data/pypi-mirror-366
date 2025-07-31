19cfbd221b46914026dfe9f069ce5db157cd54cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:04:37 +0000

    Add package egorovsystem/0.0.9.post13/egorovsystem-0.0.9.post13.tar.gz

---
+++ packages/egorovsystem/egorovsystem-0.0.9.post13.tar.gz/egorovsystem-0.0.9.post13/egoserver/run.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import argparse
+from YTools import python_run_file
+
+def init():
+	os.chdir(os.path.dirname(__file__))
+	sys.path.append(os.path.abspath("."))
+	
+	python_run_file("manage.py migrate")
+	python_run_file("manage.py createsuperuser")
+
+
+def run():
+	os.chdir(os.path.dirname(__file__))
+	sys.path.append(os.path.abspath("."))
+
+	C = argparse.ArgumentParser()
+	C.add_argument("--port" , type = int , default = 7999)
+	C = C.parse_args()
+
+	python_run_file("manage.py migrate")
+	python_run_file("manage.py collectstatic --noinput")
+	os.system(f"gunicorn --workers 3 egoserver.wsgi:application --bind 0.0.0.0:{C.port}")
