117892aac1c2c229c4540072e1767142a5516b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:17:49 +0000

    Add package adv-bible-search/0.1.0/adv_bible_search-0.1.0.tar.gz

---
+++ packages/adv-bible-search/adv_bible_search-0.1.0.tar.gz/adv_bible_search-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Define dependencies directly
+requirements = [
+    "rapidfuzz",
+    "sentence-transformers>=2.2.2",
+    "huggingface-hub<0.14.0",
+    "transformers<4.30.0",
+    "tqdm",
+    "numpy",
+    "pandas",
+    "torch",
+    "whoosh"
+]
+
+setup(
+    name="adv-bible-search",
+    version="0.1.0",
+    author="Mahboob",
+    author_email="siroosab@example.com",
+    description="Advanced Bible Search Library with fuzzy and semantic search capabilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/siroosab/bible-search",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "bible-search=bible_search.search_cli:main",
+        ],
+    },
+    include_package_data=True,
+)
