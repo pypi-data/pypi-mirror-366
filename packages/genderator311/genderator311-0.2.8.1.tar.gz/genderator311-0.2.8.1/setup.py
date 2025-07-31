c0733bfaeff19aa45d7975380a5547f969e1cffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 14:07:03 +0000

    Add package genderator311/0.2.8.1/genderator311-0.2.8.1.tar.gz

---
+++ packages/genderator311/genderator311-0.2.8.1.tar.gz/genderator311-0.2.8.1/setup.py
@@ -0,0 +1,47 @@
+#!/user/bin/env python
+
+import re, uuid
+from setuptools import setup, find_packages
+import io
+
+with io.open("genderator/__init__.py", "r", encoding="utf-8") as f:
+    init_content = f.read()
+
+match = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]", init_content, re.M)
+
+if not match:
+    raise RuntimeError("Could not find __version__ in genderator/__init__.py")
+
+version = match.group(1)
+
+
+with open("README.rst", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    install_reqs = [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+setup(name='genderator311',
+      version=version,
+      description='Python library to guess gender given a spanish full name for Python 3.11+',
+      long_description=long_description,
+      author='David Moreno-Garcia (mantained by Diego Delgado)',
+      author_email='ddelgado.1405@gmail.com',
+      license='MIT',
+      url='https://github.com/xumixprto/genderator311',
+      download_url=f"https://github.com/xumixprto/genderator311/archive/refs/tags/v{version}.tar.gz",
+      keywords=['gender', 'guess', 'spanish', 'name'],
+      packages=find_packages(exclude=['tests']),
+      install_requires=install_reqs,
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Topic :: Software Development :: Libraries',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+      ]
+      )
---
+++ packages/genderator311/genderator311-0.2.8.1.tar.gz/genderator311-0.2.8.1/tests/test_parser.py
@@ -0,0 +1,77 @@
+from unittest import TestCase
+
+import codecs
+import os
+import genderator
+
+path = os.path.dirname(__file__)
+
+DELTA = 0.01
+MAX_PERCENTAGE_ERROR = 0.03
+TEST_FILE_LINES = 100000
+
+
+class TestParser(TestCase):
+    __data = {}
+    __parser = genderator.Parser()
+
+    def remove_file_comments(self, relative_path):
+        """
+        Generator to remove comments from a file.
+
+        Params:
+            file: File to be processed.
+        """
+        with codecs.open(os.path.join(path, 'data', relative_path), 'r', 'UTF-8') as file:
+            for line in file:
+                line = line.strip()
+                if not line.startswith('#'):
+                    yield line
+
+    def test_gender_guessing(self):
+        """
+        Check if mistake percentage is lower than MAX_ERROR_PERCENTAGE while guessing gender.
+        """
+        mistakes = 0
+        for line in self.remove_file_comments('valid_names.tsv'):
+            (name, first_surname, second_surname, male_probability) = line.split('\t')
+            fullname = ' '.join([name, first_surname, second_surname])
+            answer = self.__parser.guess_gender(fullname)
+            if answer is None:
+                mistakes += 1
+            else:
+                confidence = float(answer['confidence'])
+                calculated_male_probability = confidence if answer['gender'] == 'Male' else 1 - confidence
+                if abs(calculated_male_probability - float(male_probability)) > DELTA:
+                    mistakes += 1
+
+            self.assertLess(mistakes / TEST_FILE_LINES * 100, MAX_PERCENTAGE_ERROR,
+                            'Mistakes percentage greater than ' + str(MAX_PERCENTAGE_ERROR))
+
+    def test_match(self):
+        """
+        Check if there is a match for every full name in the test file.
+        """
+        mistakes = 0
+        for line in self.remove_file_comments('valid_names.tsv'):
+            (name, first_surname, second_surname, male_probability) = line.split('\t')
+            fullname = ' '.join([name, first_surname, second_surname])
+            answer = self.__parser.guess_gender(fullname)
+            if answer is None:
+                mistakes += 1
+            self.assertLess(mistakes / TEST_FILE_LINES * 100, MAX_PERCENTAGE_ERROR,
+                            'Mistakes percentage greater than ' + str(MAX_PERCENTAGE_ERROR))
+
+    def test_name_guessing(self):
+        """
+        Check if mistake percentage is lower than MAX_ERROR_PERCENTAGE while guessing name.
+        """
+        mistakes = 0
+        for line in self.remove_file_comments('valid_names.tsv'):
+            (name, first_surname, second_surname, male_probability) = line.split('\t')
+            fullname = ' '.join([name, first_surname, second_surname])
+            answer = self.__parser.guess_gender(fullname)
+            if answer is None or answer['real_name'] != name:
+                mistakes += 1
+            self.assertLess(mistakes / TEST_FILE_LINES * 100, MAX_PERCENTAGE_ERROR,
+                            'Mistakes percentage greater than ' + str(MAX_PERCENTAGE_ERROR))
