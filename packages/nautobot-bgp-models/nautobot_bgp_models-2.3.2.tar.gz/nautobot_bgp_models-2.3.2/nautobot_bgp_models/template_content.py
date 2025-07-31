8a6857a87f4a7d72a6cc70fbbde1abe4c24a59a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 06:04:53 +0000

    Add package nautobot-bgp-models/2.3.2/nautobot_bgp_models-2.3.2.tar.gz

---
+++ packages/nautobot-bgp-models/nautobot_bgp_models-2.3.2.tar.gz/nautobot_bgp_models-2.3.2/nautobot_bgp_models/template_content.py
@@ -0,0 +1,89 @@
+"""Extensions of baseline Nautobot views."""
+
+from nautobot.extras.plugins import PluginTemplateExtension
+
+from .models import AddressFamily, AutonomousSystem, BGPRoutingInstance, PeerEndpoint
+
+
+class DevicePeerEndpoints(PluginTemplateExtension):  # pylint: disable=abstract-method
+    """Add PeerEndpoints to the right side of the Device page."""
+
+    model = "dcim.device"
+
+    def right_page(self):
+        """Add content to the right side of the Devices detail view."""
+        endpoints = PeerEndpoint.objects.filter(
+            routing_instance__device=self.context["object"],
+        )
+        if endpoints.exists():
+            return self.render(
+                "nautobot_bgp_models/inc/device_peer_endpoints.html",
+                extra_context={"endpoints": endpoints},
+            )
+        return ""
+
+
+class DeviceAddressFamilies(PluginTemplateExtension):  # pylint: disable=abstract-method
+    """Add AddressFamilies to the right side of the Device page."""
+
+    model = "dcim.device"
+
+    def right_page(self):
+        """Add content to the right side of the Device detail view."""
+        address_families = AddressFamily.objects.filter(
+            routing_instance__device=self.context["object"],
+        )
+        if address_families.exists():
+            return self.render(
+                "nautobot_bgp_models/inc/device_address_families.html",
+                extra_context={"address_families": address_families},
+            )
+        return ""
+
+
+class DeviceBgpRoutingInstances(PluginTemplateExtension):  # pylint: disable=abstract-method
+    """Add BGPRoutingInstance to the right side of the Device page."""
+
+    model = "dcim.device"
+
+    def right_page(self):
+        """Add content to the right side of the Device detail view."""
+        bgp_routing_instances = BGPRoutingInstance.objects.filter(
+            device=self.context["object"],
+        )
+        if bgp_routing_instances.exists():
+            return self.render(
+                "nautobot_bgp_models/inc/device_bgp_routing_instances.html",
+                extra_context={"bgp_routing_instances": bgp_routing_instances},
+            )
+        return ""
+
+
+class DeviceAutonomousSystems(PluginTemplateExtension):  # pylint: disable=abstract-method
+    """Add AutonomousSystems to the right side of the Device page."""
+
+    model = "dcim.device"
+
+    def right_page(self):
+        """Add content to the right side of the Device detail view."""
+        autonomous_systems = (
+            AutonomousSystem.objects.filter(
+                bgproutinginstance__device=self.context["object"],
+            )
+            .distinct()
+            .order_by("asn")
+        )
+        if autonomous_systems.exists():
+            return self.render(
+                "nautobot_bgp_models/inc/device_autonomous_systems.html",
+                extra_context={"autonomous_systems": autonomous_systems},
+            )
+        return ""
+
+
+template_extensions = [
+    DeviceAutonomousSystems,
+    DeviceBgpRoutingInstances,
+    DeviceAddressFamilies,
+    DevicePeerEndpoints,
+]
