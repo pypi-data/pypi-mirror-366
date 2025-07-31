85d1283dc33f580a68ce4e4eb406cbc492a0c559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 17:06:40 +0000

    Add package pip-tools/7.5.0/pip_tools-7.5.0.tar.gz

---
+++ packages/pip-tools/pip_tools-7.5.0.tar.gz/pip_tools-7.5.0/tests/test_logging.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from piptools.logging import LogContext
+
+
+def test_indentation(runner):
+    """
+    Test LogContext.indentation() context manager increases indentation.
+    """
+    log = LogContext(indent_width=2)
+
+    with runner.isolation() as streams:
+        stderr = streams[1]
+        log.log("Test message 1")
+        with log.indentation():
+            log.log("Test message 2")
+            with log.indentation():
+                log.log("Test message 3")
+            log.log("Test message 4")
+        log.log("Test message 5")
+
+    assert stderr.getvalue().decode().splitlines() == [
+        "Test message 1",
+        "  Test message 2",
+        "    Test message 3",
+        "  Test message 4",
+        "Test message 5",
+    ]
