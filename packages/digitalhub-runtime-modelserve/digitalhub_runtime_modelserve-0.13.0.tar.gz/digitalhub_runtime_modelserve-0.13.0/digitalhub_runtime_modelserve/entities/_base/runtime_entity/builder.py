06c92a9fa9c5a97c01ef13e71a237626b4862223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 17:06:51 +0000

    Add package digitalhub-runtime-modelserve/0.13.0/digitalhub_runtime_modelserve-0.13.0.tar.gz

---
+++ packages/digitalhub-runtime-modelserve/digitalhub_runtime_modelserve-0.13.0.tar.gz/digitalhub_runtime_modelserve-0.13.0/digitalhub_runtime_modelserve/entities/_base/runtime_entity/builder.py
@@ -0,0 +1,88 @@
+# SPDX-FileCopyrightText: © 2025 DSLab - Fondazione Bruno Kessler
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from digitalhub.entities._base.runtime_entity.builder import RuntimeEntityBuilder
+from digitalhub.entities.task._base.utils import build_task_actions
+
+from digitalhub_runtime_modelserve.entities._commons.enums import EntityKinds, TaskActions
+
+
+class RuntimeEntityBuilderMlflowserve(RuntimeEntityBuilder):
+    EXECUTABLE_KIND = EntityKinds.FUNCTION_MLFLOWSERVE.value
+    TASKS_KINDS = build_task_actions(
+        [
+            (
+                EntityKinds.TASK_MLFLOWSERVE_SERVE.value,
+                TaskActions.SERVE.value,
+            ),
+        ]
+    )
+    RUN_KIND = EntityKinds.RUN_MLFLOWSERVE.value
+
+
+class RuntimeEntityBuilderSklearnserve(RuntimeEntityBuilder):
+    EXECUTABLE_KIND = EntityKinds.FUNCTION_SKLEARNSERVE.value
+    TASKS_KINDS = build_task_actions(
+        [
+            (
+                EntityKinds.TASK_SKLEARNSERVE_SERVE.value,
+                TaskActions.SERVE.value,
+            ),
+        ]
+    )
+    RUN_KIND = EntityKinds.RUN_SKLEARNSERVE.value
+
+
+class RuntimeEntityBuilderHuggingfaceserve(RuntimeEntityBuilder):
+    EXECUTABLE_KIND = EntityKinds.FUNCTION_HUGGINGFACESERVE.value
+    TASKS_KINDS = build_task_actions(
+        [
+            (
+                EntityKinds.TASK_HUGGINGFACESERVE_SERVE.value,
+                TaskActions.SERVE.value,
+            ),
+        ]
+    )
+    RUN_KIND = EntityKinds.RUN_HUGGINGFACESERVE.value
+
+
+class RuntimeEntityBuilderKubeaiserve(RuntimeEntityBuilder):
+    EXECUTABLE_KIND = EntityKinds.FUNCTION_KUBEAISERVE.value
+    TASKS_KINDS = build_task_actions(
+        [
+            (
+                EntityKinds.TASK_KUBEAISERVE_SERVE.value,
+                TaskActions.SERVE.value,
+            ),
+        ]
+    )
+    RUN_KIND = EntityKinds.RUN_KUBEAISERVE.value
+
+
+class RuntimeEntityBuilderKubeaiserveSpeechtotext(RuntimeEntityBuilder):
+    EXECUTABLE_KIND = EntityKinds.FUNCTION_KUBEAISERVESPEECHTOTEXT.value
+    TASKS_KINDS = build_task_actions(
+        [
+            (
+                EntityKinds.TASK_KUBEAISERVESPEECHTOTEXT_SERVE.value,
+                TaskActions.SERVE.value,
+            ),
+        ]
+    )
+    RUN_KIND = EntityKinds.RUN_KUBEAISERVESPEECHTOTEXT.value
+
+
+class RuntimeEntityBuilderKubeaiserveText(RuntimeEntityBuilder):
+    EXECUTABLE_KIND = EntityKinds.FUNCTION_KUBEAISERVETEXT.value
+    TASKS_KINDS = build_task_actions(
+        [
+            (
+                EntityKinds.TASK_KUBEAISERVETEXT_SERVE.value,
+                TaskActions.SERVE.value,
+            ),
+        ]
+    )
+    RUN_KIND = EntityKinds.RUN_KUBEAISERVETEXT.value
