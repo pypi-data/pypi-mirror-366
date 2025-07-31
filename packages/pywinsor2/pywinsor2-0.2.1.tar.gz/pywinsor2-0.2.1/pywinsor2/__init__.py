addd9a40660fae65fd45147fa35b7a6fc013c928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 14:06:16 +0000

    Add package pywinsor2/0.2.1/pywinsor2-0.2.1.tar.gz

---
+++ packages/pywinsor2/pywinsor2-0.2.1.tar.gz/pywinsor2-0.2.1/pywinsor2/__init__.py
@@ -0,0 +1,19 @@
+"""
+pywinsor2: Python implementation of Stata's winsor2 command
+
+This package provides tools for winsorizing and trimming data,
+equivalent to Stata's winsor2 command functionality.
+"""
+
+__version__ = "0.2.0"
+__author__ = "Bryce Wang"
+__email__ = "brycew6m@stanford.edu"
+
+from .core import winsor2
+from .utils import compute_percentiles, validate_inputs
+
+__all__ = [
+    "winsor2",
+    "validate_inputs",
+    "compute_percentiles",
+]
