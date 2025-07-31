54a03f6a57368d5da4d6a972c00dceb0656043ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 06:04:30 +0000

    Add package logfmter/0.0.10/logfmter-0.0.10.tar.gz

---
+++ packages/logfmter/logfmter-0.0.10.tar.gz/logfmter-0.0.10/src/logfmter/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+from logfmter.formatter import Logfmter as Logfmter
+
+__version__ = "0.0.10"
---
+++ packages/logfmter/logfmter-0.0.10.tar.gz/logfmter-0.0.10/src/logfmter/formatter.py
@@ -0,0 +1,229 @@
+import logging
+import numbers
+import traceback
+from types import TracebackType
+from typing import Any, Dict, List, Optional, Tuple, Type, cast
+
+ExcInfo = Tuple[Type[BaseException], BaseException, TracebackType]
+
+# Reserved log record attributes cannot be overwritten. They
+# will not be included in the formatted log.
+#
+# https://docs.python.org/3/library/logging.html#logrecord-attributes
+RESERVED: Tuple[str, ...] = (
+    "args",
+    "asctime",
+    "created",
+    "exc_info",
+    "exc_text",
+    "filename",
+    "funcName",
+    "levelname",
+    "levelno",
+    "lineno",
+    "message",
+    "module",
+    "msecs",
+    "msg",
+    "name",
+    "pathname",
+    "process",
+    "processName",
+    "relativeCreated",
+    "stack_info",
+    "taskName",
+    "thread",
+    "threadName",
+)
+
+
+class Logfmter(logging.Formatter):
+    @classmethod
+    def format_string(cls, value: str) -> str:
+        """
+        Process the provided string with any necessary quoting and/or escaping.
+        """
+        needs_dquote_escaping = '"' in value
+        needs_newline_escaping = "\n" in value
+        needs_quoting = " " in value or "=" in value or needs_dquote_escaping
+        needs_backslash_escaping = "\\" in value and needs_quoting
+
+        if needs_backslash_escaping:
+            value = value.replace("\\", "\\\\")
+
+        if needs_dquote_escaping:
+            value = value.replace('"', '\\"')
+
+        if needs_newline_escaping:
+            value = value.replace("\n", "\\n")
+
+        if needs_quoting:
+            value = '"{}"'.format(value)
+
+        return value if value else ""
+
+    @classmethod
+    def format_value(cls, value) -> str:
+        """
+        Map the provided value to the proper logfmt formatted string.
+        """
+        if value is None:
+            return ""
+        elif isinstance(value, bool):
+            return "true" if value else "false"
+        elif isinstance(value, numbers.Number):
+            return str(value)
+
+        return cls.format_string(str(value))
+
+    @classmethod
+    def format_exc_info(cls, exc_info: ExcInfo) -> str:
+        """
+        Format the provided exc_info into a logfmt formatted string.
+
+        This function should only be used to format exceptions which are
+        currently being handled. Not with those exceptions which are
+        manually passed into the logger. For example:
+
+            try:
+                raise Exception()
+            except Exception:
+                logging.exception()
+        """
+        # Tracebacks have a single trailing newline that we don't need.
+        value = "".join(traceback.format_exception(*exc_info)).rstrip("\n")
+
+        return cls.format_string(value)
+
+    @classmethod
+    def format_params(cls, params: dict) -> str:
+        """
+        Return a string representing the logfmt formatted parameters.
+        """
+        return " ".join(
+            [
+                "{}={}".format(key, cls.format_value(value))
+                for key, value in params.items()
+            ]
+        )
+
+    @classmethod
+    def normalize_key(cls, key: str) -> str:
+        """
+        Return a string whereby any spaces are converted to underscores and
+        newlines are escaped.
+
+        If the provided key is empty, then return a single underscore. This
+        function is used to prevent any logfmt parameters from having invalid keys.
+
+        As a choice of implementation, we normalize any keys instead of raising an
+        exception to prevent raising exceptions during logging. The goal is to never
+        impede logging. This is especially important when logging in exception handlers.
+        """
+        if not key:
+            return "_"
+
+        return key.replace(" ", "_").replace("\n", "\\n")
+
+    @classmethod
+    def get_extra(cls, record: logging.LogRecord) -> dict:
+        """
+        Return a dictionary of logger extra parameters by filtering any reserved keys.
+        """
+        extras = {}
+
+        for key, value in record.__dict__.items():
+            key = cls.normalize_key(key)
+
+            if key in RESERVED:
+                continue
+
+            if isinstance(value, dict):
+                extras.update(cls.flatten_dict(value, key))
+            else:
+                extras[key] = value
+
+        return extras
+
+    @classmethod
+    def flatten_dict(cls, v: dict, root: str = "") -> dict[str, Any]:
+        """
+        Return a dictionary whereby the input dictionary is converted to
+        depth equal to one with keys that are joined via periods.
+        """
+        flattened = {}
+
+        for key, value in v.items():
+            key = f"{root}.{cls.normalize_key(key)}" if root else cls.normalize_key(key)
+
+            if isinstance(value, dict):
+                flattened.update(cls.flatten_dict(value, key))
+            else:
+                flattened[key] = value
+
+        return flattened
+
+    def __init__(
+        self,
+        keys: List[str] = ["at"],
+        mapping: Dict[str, str] = {"at": "levelname"},
+        datefmt: Optional[str] = None,
+    ):
+        self.keys = [self.normalize_key(key) for key in keys]
+        self.mapping = {
+            self.normalize_key(key): value for key, value in mapping.items()
+        }
+        self.datefmt = datefmt
+
+    def format(self, record: logging.LogRecord) -> str:
+        # If the 'asctime' attribute will be used, then generate it.
+        if "asctime" in self.keys or "asctime" in self.mapping.values():
+            record.asctime = self.formatTime(record, self.datefmt)
+
+        if isinstance(record.msg, dict):
+            params = self.flatten_dict(record.msg)
+        else:
+            params = {"msg": record.getMessage()}
+
+        params.update(self.get_extra(record))
+
+        tokens = []
+
+        # Add the initial tokens from the provided list of default keys.
+        #
+        # This supports parameters which should be included in every log message. The
+        # values for these keys must already exist on the log record. If they are
+        # available under a different attribute name, then the formatter's mapping will
+        # be used to lookup these attributes. e.g. 'at' from 'levelname'
+        for key in self.keys:
+            attribute = key
+
+            # If there is a mapping for this key's attribute, then use it to lookup
+            # the key's value.
+            if key in self.mapping:
+                attribute = self.mapping[key]
+
+            # If this key is in params, then skip it, because it was manually passed in
+            # will be added via the params system.
+            if attribute in params:
+                continue
+
+            # If the attribute doesn't exist on the log record, then skip it.
+            if not hasattr(record, attribute):
+                continue
+
+            value = getattr(record, attribute)
+
+            tokens.append("{}={}".format(key, self.format_value(value)))
+
+        formatted_params = self.format_params(params)
+        if formatted_params:
+            tokens.append(formatted_params)
+
+        if record.exc_info:
+            # Cast exc_info to its not null variant to make mypy happy.
+            exc_info = cast(ExcInfo, record.exc_info)
+
+            tokens.append("exc_info={}".format(self.format_exc_info(exc_info)))
+
+        return " ".join(tokens)
