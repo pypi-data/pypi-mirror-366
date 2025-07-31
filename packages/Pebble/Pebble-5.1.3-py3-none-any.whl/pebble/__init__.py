bbb370ec3f766d640dde359e464b0a1b3d456e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 06:04:22 +0000

    Add package Pebble/5.1.3/Pebble-5.1.3-py3-none-any.whl

---
+++ packages/Pebble/Pebble-5.1.3-py3-none-any.whl/pebble/__init__.py
@@ -0,0 +1,22 @@
+__author__ = 'Matteo Cafasso'
+__version__ = '5.1.3'
+__license__ = 'LGPL'
+
+
+__all__ = ['waitforthreads',
+           'waitforqueues',
+           'synchronized',
+           'sighandler',
+           'ProcessFuture',
+           'MapFuture',
+           'ProcessMapFuture',
+           'ProcessExpired',
+           'ProcessPool',
+           'ThreadPool']
+
+
+from pebble import concurrent, asynchronous
+from pebble.decorators import synchronized, sighandler
+from pebble.functions import waitforqueues, waitforthreads
+from pebble.common import ProcessExpired, ProcessFuture, CONSTS
+from pebble.pool import ThreadPool, ProcessPool, MapFuture, ProcessMapFuture
