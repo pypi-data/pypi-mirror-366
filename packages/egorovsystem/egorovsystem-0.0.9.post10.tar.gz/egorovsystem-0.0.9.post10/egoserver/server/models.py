27d1abb45f4d51fa32b6cb2a85ec6c4161aa56c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:06:08 +0000

    Add package egorovsystem/0.0.9.post10/egorovsystem-0.0.9.post10.tar.gz

---
+++ packages/egorovsystem/egorovsystem-0.0.9.post10.tar.gz/egorovsystem-0.0.9.post10/egoserver/server/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Variable(models.Model):
+    key = models.TextField(default = "", blank = True, null = True)
+    val = models.TextField(default = "", blank = True, null = True)
+
+    def __str__(self):
+        return str(self.key)
+    
+class Function(models.Model):
+    key = models.TextField(default = "", blank = True, null = True)
+    val = models.TextField(default = "", blank = True, null = True)
+    type = models.TextField(default = "cmd", blank = True, null = True)
+
+    def __str__(self):
+        return str(self.key)
+
+class Password(models.Model):
+    key = models.TextField(default = "", blank = True, null = True)
+    val = models.TextField(default = "", blank = True, null = True)
+
+    def __str__(self):
+        return str(self.key)
+
