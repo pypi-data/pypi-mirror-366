27f425e6b8167dbc0193d8089f4f6dee270f2b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 17:06:48 +0000

    Add package digitalhub-runtime-dbt/0.13.0/digitalhub_runtime_dbt-0.13.0.tar.gz

---
+++ packages/digitalhub-runtime-dbt/digitalhub_runtime_dbt-0.13.0.tar.gz/digitalhub_runtime_dbt-0.13.0/digitalhub_runtime_dbt/entities/_base/runtime_entity/builder.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: © 2025 DSLab - Fondazione Bruno Kessler
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from digitalhub.entities._base.runtime_entity.builder import RuntimeEntityBuilder
+from digitalhub.entities.task._base.utils import build_task_actions
+
+from digitalhub_runtime_dbt.entities._commons.enums import EntityKinds, TaskActions
+
+
+class RuntimeEntityBuilderDbt(RuntimeEntityBuilder):
+    EXECUTABLE_KIND = EntityKinds.FUNCTION_DBT.value
+    TASKS_KINDS = build_task_actions(
+        [
+            (
+                EntityKinds.TASK_DBT_TRANSFORM.value,
+                TaskActions.TRANSFORM.value,
+            )
+        ]
+    )
+    RUN_KIND = EntityKinds.RUN_DBT.value
