989b2a3e75eee723d48e8afbb458fa1dc8e77d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:15 +0000

    Add package oarepo-communities/5.4.3/oarepo_communities-5.4.3-py3-none-any.whl

---
+++ packages/oarepo-communities/oarepo_communities-5.4.3-py3-none-any.whl/oarepo_communities/ui/oarepo_communities/semantic-ui/js/oarepo_communities/search/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { parametrize } from "react-overridable";
+import { i18next } from "@translations/oarepo_communities";
+import {
+  createSearchAppsInit,
+  parseSearchAppConfigs,
+  SearchappSearchbarElement,
+  DynamicResultsListItem,
+  SearchAppLayoutWithSearchbarHOC,
+  SearchAppResultViewWithSearchbar,
+} from "@js/oarepo_ui";
+
+const [{ overridableIdPrefix }] = parseSearchAppConfigs();
+
+const SearchAppResultViewWithSearchbarWAppName = parametrize(
+  SearchAppResultViewWithSearchbar,
+  {
+    appName: overridableIdPrefix,
+  }
+);
+
+export const DashboardUploadsSearchLayout = SearchAppLayoutWithSearchbarHOC({
+  placeholder: i18next.t("Search inside the community..."),
+
+  appName: overridableIdPrefix,
+});
+export const componentOverrides = {
+  [`${overridableIdPrefix}.ResultsList.item`]: DynamicResultsListItem,
+  [`${overridableIdPrefix}.SearchBar.element`]: SearchappSearchbarElement,
+  [`${overridableIdPrefix}.SearchApp.results`]:
+    SearchAppResultViewWithSearchbarWAppName,
+  [`${overridableIdPrefix}.SearchApp.layout`]: DashboardUploadsSearchLayout,
+};
+
+createSearchAppsInit({ componentOverrides });
