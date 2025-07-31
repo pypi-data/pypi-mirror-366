986e885eb615a3f8bcc9e48df28c78106aae1fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:13 +0000

    Add package apifoncier2/0.1.1/apifoncier2-0.1.1-py3-none-any.whl

---
+++ packages/apifoncier2/apifoncier2-0.1.1-py3-none-any.whl/apifoncier/endpoints/__init__.py
@@ -0,0 +1,6 @@
+from .base import BaseEndpoint
+from .dvf_opendata import DVFOpenDataEndpoint
+from .cartofriches import CartofrichesEndpoint
+
+
+__all__ = ["BaseEndpoint", "DVFOpenDataEndpoint", "CartofrichesEndpoint"]
