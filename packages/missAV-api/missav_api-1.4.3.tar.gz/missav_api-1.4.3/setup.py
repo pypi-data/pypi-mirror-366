d054cdf8a12980f42fb9ed3f3831f2c1c0694825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:18:22 +0000

    Add package missAV-api/1.4.3/missav_api-1.4.3.tar.gz

---
+++ packages/missAV-api/missav_api-1.4.3.tar.gz/missav_api-1.4.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="missAV_api",
+    version="1.4.3",
+    packages=find_packages(),
+    install_requires=["eaf_base_api"],
+    author="Johannes Habel",
+    author_email="EchterAlsFake@proton.me",
+    description="A Python API for the Porn Site missav.ws",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="LGPLv3",
+    url="https://github.com/EchterAlsFake/missAV_api",
+    classifiers=[
+        # Classifiers help users find your project on PyPI
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python",
+    ],
+)
