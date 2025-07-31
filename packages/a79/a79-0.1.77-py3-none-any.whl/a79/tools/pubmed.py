e29f08a9ff458e9482f2bf57a5231e045fde7eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 17:06:51 +0000

    Add package a79/0.1.77/a79-0.1.77-py3-none-any.whl

---
+++ packages/a79/a79-0.1.77-py3-none-any.whl/a79/tools/pubmed.py
@@ -0,0 +1,22 @@
+from ..client import A79Client
+from ..models.tools import DEFAULT
+from ..models.tools.pubmed_models import (
+    ArticleRecord,
+    GetArticlesInput,
+    GetArticlesOutput,
+)
+
+__all__ = ["ArticleRecord", "GetArticlesInput", "GetArticlesOutput", "get_articles"]
+
+
+def get_articles(*, feed_url: str, limit: int = DEFAULT) -> GetArticlesOutput:
+    """Fetch articles from PubMed."""
+    kwargs = locals()
+    kwargs = {k: v for k, v in kwargs.items() if v is not DEFAULT}
+    input_model = GetArticlesInput.model_validate(kwargs)
+
+    client = A79Client()
+    output_model = client.execute_tool(
+        package="pubmed", name="get_articles", input=input_model.model_dump()
+    )
+    return GetArticlesOutput.model_validate(output_model)
