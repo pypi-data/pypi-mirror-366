29ed282b790963279fe8f84a3d29f2f46150092e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:05:03 +0000

    Add package parity-tensor/0.0.1a12/parity_tensor-0.0.1a12.tar.gz

---
+++ packages/parity-tensor/parity_tensor-0.0.1a12.tar.gz/parity_tensor-0.0.1a12/parity_tensor/__init__.py
@@ -0,0 +1,8 @@
+"""
+A parity tensor package.
+"""
+
+__all__ = ["__version__", "ParityTensor"]
+
+from .version import __version__
+from .parity_tensor import ParityTensor
---
+++ packages/parity-tensor/parity_tensor-0.0.1a12.tar.gz/parity_tensor-0.0.1a12/tests/import_test.py
@@ -0,0 +1,3 @@
+def test_import() -> None:
+    from parity_tensor import ParityTensor
+    assert isinstance(ParityTensor, type)
