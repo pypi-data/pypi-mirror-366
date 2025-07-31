84c9878fbc8e5f0418a135354ad0294e9c8d2869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 15:06:09 +0000

    Add package egorovsystem/0.0.9.post11/egorovsystem-0.0.9.post11.tar.gz

---
+++ packages/egorovsystem/egorovsystem-0.0.9.post11.tar.gz/egorovsystem-0.0.9.post11/egorovsystem/run_command.py
@@ -0,0 +1,24 @@
+from . import get_function
+import sys
+import os
+import tempfile
+from YTools import python_run
+
+def run():
+    argv = sys.argv
+    
+    if len(argv) == 1:
+        return 
+    
+    function = get_function(argv[1])
+
+    if function["type"] == "cmd":
+        os.system(function["content"])
+    if function["type"] == "python":
+        python_run(function["content"])
+
+if __name__ == "__main__":
+    sys.argv = ["", "test"]
+    run()    
+    sys.argv = ["", "test_python"]
+    run()
---
+++ packages/egorovsystem/egorovsystem-0.0.9.post11.tar.gz/egorovsystem-0.0.9.post11/egoserver/egoserver/secret.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'django-insecure-(sqtddlvbbbg9*s@($a*uubagzkve)8mx+3ol9wyj#lymw5g@f'
