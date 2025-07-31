b7c363490105e19f2464150339b9a1e3a0d725bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:19 +0000

    Add package maktab-dl/0.7.0/maktab_dl-0.7.0.tar.gz

---
+++ packages/maktab-dl/maktab_dl-0.7.0.tar.gz/maktab_dl-0.7.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="maktab-dl",
+    version="0.6.0",
+    description="A simple downloader videos from maktabkhooneh",
+    author="Mohammadreza Shaghouzi",
+    author_email="sh.mohammad66@gmail.com.com",
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+        "pydantic",
+        "lxml",
+        "tqdm",
+    ],
+    entry_points={
+        "console_scripts": [
+            "maktab-dl=maktab_dl.cli:main",
+        ],
+    },
+)
