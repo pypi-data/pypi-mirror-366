ef429e93ee3d36ad9b7a5c2fa08a1a21f942f5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:47 +0000

    Add package robotframework-visualizer/0.0.10/robotframework_visualizer-0.0.10-py3-none-any.whl

---
+++ packages/robotframework-visualizer/robotframework_visualizer-0.0.10-py3-none-any.whl/Visualizer/utils/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class GraphColor(Enum):
+    Red = "red"
+    Blue = "blue"
+    Green = "green"
+    Yellow = "yellow"
+    Black = "black"
+    White = "white"
+    Orange = "orange"
+    Grey = "grey"
