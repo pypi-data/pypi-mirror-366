f80342d89fc3c7c6632e807f91d802c82b5a28f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:32:48 +0000

    Add package php-framework-scaffolder/0.1.53/php_framework_scaffolder-0.1.53-py3-none-any.whl

---
+++ packages/php-framework-scaffolder/php_framework_scaffolder-0.1.53-py3-none-any.whl/php_framework_scaffolder/frameworks/popphp.py
@@ -0,0 +1,13 @@
+from php_framework_scaffolder.frameworks.base import BaseFrameworkSetup
+from php_framework_detector.core.models import FrameworkType
+from typing import List
+
+class PopPHPSetup(BaseFrameworkSetup):
+    def __init__(self):
+        super().__init__(FrameworkType.POPPHP)
+
+    def get_setup_commands(self) -> List[List[str]]:
+        raise NotImplementedError("Not implemented")
+    
+    def get_routes_command(self) -> List[str]:
+        raise NotImplementedError("Not implemented")
---
+++ packages/php-framework-scaffolder/php_framework_scaffolder-0.1.53-py3-none-any.whl/php_framework_scaffolder/models/progress.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class ImplementationProgress(Enum):
+    TODO = "todo"
+    WIP = "wip"
+    DONE = "done"
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return str(self)
