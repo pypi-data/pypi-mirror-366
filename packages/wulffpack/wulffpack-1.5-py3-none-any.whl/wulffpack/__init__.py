d9372d82ec65fc22eb035c2709390b1ce673c552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 05:09:45 +0000

    Add package wulffpack/1.5/wulffpack-1.5-py3-none-any.whl

---
+++ packages/wulffpack/wulffpack-1.5-py3-none-any.whl/wulffpack/__init__.py
@@ -0,0 +1,11 @@
+from .single_crystal import SingleCrystal
+from .decahedron import Decahedron
+from .icosahedron import Icosahedron
+from .winterbottom import Winterbottom
+
+__version__ = '1.5'
+
+__all__ = ['SingleCrystal',
+           'Decahedron',
+           'Icosahedron',
+           'Winterbottom']
