8e91227099108badb580460633d23e700de76eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 16:06:13 +0000

    Add package rdkit/2025.3.4/rdkit-2025.3.4-cp310-cp310-win_amd64.whl

---
+++ packages/rdkit/rdkit-2025.3.4-cp310-cp310-win_amd64.whl/rdkit-stubs/Chem/Descriptors3D.pyi
@@ -0,0 +1,29 @@
+"""
+ Descriptors derived from a molecule's 3D structure
+
+"""
+from __future__ import annotations
+from rdkit.Chem.Descriptors import _isCallable
+from rdkit.Chem import rdMolDescriptors
+__all__ = ['CalcMolDescriptors3D', 'descList', 'rdMolDescriptors']
+def CalcMolDescriptors3D(mol, confId = None):
+    """
+    
+        Compute all 3D descriptors of a molecule
+        
+        Arguments:
+        - mol: the molecule to work with
+        - confId: conformer ID to work with. If not specified the default (-1) is used
+        
+        Return:
+        
+        dict
+            A dictionary with decriptor names as keys and the descriptor values as values
+    
+        raises a ValueError 
+            If the molecule does not have conformers
+        
+    """
+def _setupDescriptors(namespace):
+    ...
+descList: list  # value = [('PMI1', <function <lambda> at 0x000002B491496D40>), ('PMI2', <function <lambda> at 0x000002B4995DF400>), ('PMI3', <function <lambda> at 0x000002B4995DF490>), ('NPR1', <function <lambda> at 0x000002B4995DF520>), ('NPR2', <function <lambda> at 0x000002B4995DF5B0>), ('RadiusOfGyration', <function <lambda> at 0x000002B4995DF640>), ('InertialShapeFactor', <function <lambda> at 0x000002B4995DF6D0>), ('Eccentricity', <function <lambda> at 0x000002B4995DF760>), ('Asphericity', <function <lambda> at 0x000002B4995DF7F0>), ('SpherocityIndex', <function <lambda> at 0x000002B4995DF880>), ('PBF', <function <lambda> at 0x000002B4995DF910>)]
