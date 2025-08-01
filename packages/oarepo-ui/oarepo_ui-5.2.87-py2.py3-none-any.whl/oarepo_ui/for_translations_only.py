7935e3efdb3f5dee1d0d1d4bb6bc8c31929af3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 11:05:24 +0000

    Add package oarepo-ui/5.2.87/oarepo_ui-5.2.87-py2.py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-5.2.87-py2.py3-none-any.whl/oarepo_ui/for_translations_only.py
@@ -0,0 +1,12 @@
+from gettext import gettext as _
+
+translated_strings = [
+    _("api.draft"),
+    _("api.latest"),
+    _("api.files"),
+    _("api.latest_html"),
+    _("api.publish"),
+    _("api.record"),
+    _("api.self_html"),
+    _("api.versions"),
+]
