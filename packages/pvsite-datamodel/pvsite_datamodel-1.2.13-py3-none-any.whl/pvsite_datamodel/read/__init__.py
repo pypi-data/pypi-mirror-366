0e3ace26801f263b1e2dbdbbdd409fe81d512794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 05:14:09 +0000

    Add package pvsite-datamodel/1.2.13/pvsite_datamodel-1.2.13-py3-none-any.whl

---
+++ packages/pvsite-datamodel/pvsite_datamodel-1.2.13-py3-none-any.whl/pvsite_datamodel/read/__init__.py
@@ -0,0 +1,27 @@
+"""
+Functions for reading from the PVSite database
+"""
+
+from .client import get_client_by_name
+from .forecast_value import get_forecast_values_fast, get_forecast_values_day_ahead_fast
+from .generation import get_pv_generation_by_sites, get_pv_generation_by_user_uuids
+from .latest_forecast_values import get_latest_forecast_values_by_site
+from .model import get_or_create_model
+from .site import (
+    get_all_sites,
+    get_site_by_client_site_id,
+    get_site_by_client_site_name,
+    get_site_by_uuid,
+    get_sites_by_client_name,
+    get_sites_by_country,
+    get_sites_from_user,
+)
+from .status import get_latest_status
+from .user import (
+    get_all_last_api_request,
+    get_all_site_groups,
+    get_all_users,
+    get_api_requests_for_one_user,
+    get_site_group_by_name,
+    get_user_by_email,
+)
---
+++ packages/pvsite-datamodel/pvsite_datamodel-1.2.13-py3-none-any.whl/pvsite_datamodel/read/site.py
@@ -0,0 +1,217 @@
+"""Functions for reading to pvsite db."""
+
+import logging
+
+from sqlalchemy.orm import Session
+
+from pvsite_datamodel.pydantic_models import LatitudeLongitudeLimits
+from pvsite_datamodel.sqlmodels import (
+    ClientSQL,
+    LocationGroupLocationSQL,
+    LocationGroupSQL,
+    LocationSQL,
+    UserSQL,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def get_site_by_uuid(session: Session, site_uuid: str) -> LocationSQL:
+    """Get site object from uuid.
+
+    Raise error if site does not exist.
+    :param session: database sessions
+    :param site_uuid: the site uuid
+    :return: the site object
+    """
+    query = session.query(LocationSQL)
+    query = query.filter(LocationSQL.location_uuid == site_uuid)
+    existing_site: LocationSQL | None = query.first()
+    if existing_site is None:
+        raise KeyError(f"Location uuid {site_uuid} not found in locations table")
+
+    return existing_site
+
+
+def get_site_by_client_site_id(
+    session: Session,
+    client_name: str,
+    client_site_id: int,
+) -> LocationSQL:
+    """Get site from client name and client site id.
+
+    :param session: database sessions
+    :param client_name: client name
+    :param client_site_id: client's id of site
+    :return: site object, or None
+    """
+    logger.debug(f"Getting {client_name}'s site {client_site_id}")
+
+    # start main query
+    query = session.query(LocationSQL)
+
+    # start main query
+    query = query.filter(LocationSQL.client_location_name == client_name)
+
+    # select the correct client site id
+    query = query.filter(LocationSQL.client_location_id == client_site_id)
+
+    # get first result (should only be one site)
+    site: LocationSQL | None = query.first()
+
+    if site is None:
+        raise KeyError(f"Could not find location {client_site_id} from client {client_name}")
+
+    return site
+
+
+def get_site_by_client_site_name(
+    session: Session,
+    client_name: str,
+    client_site_name: str,
+) -> LocationSQL:
+    """Get site from client name and client site id.
+
+    :param session: database sessions
+    :param client_name: client name
+    :param client_site_name: client's name of site
+    :return: site object, or None
+    """
+    logger.debug(f"Getting {client_name}'s site {client_site_name}")
+
+    # start main query
+    query = session.query(LocationSQL)
+
+    # select the correct client site name
+    query = query.filter(LocationSQL.client_location_name == client_site_name)
+
+    # get first result (should only be one site)
+    site: LocationSQL | None = query.first()
+
+    if site is None:
+        raise Exception(f"Could not find site {client_site_name} from client {client_name}")
+
+    return site
+
+
+def get_all_sites(session: Session) -> list[LocationSQL]:
+    """Get all sites from the sites table.
+
+    :param session: database session
+    :return: site object
+    """
+    logger.debug("Getting all sites")
+
+    # start main query
+    query = session.query(LocationSQL)
+
+    # order by uuuid
+    query = query.order_by(LocationSQL.location_uuid)
+
+    # get all results
+    sites = query.all()
+
+    logger.debug(f"Found {len(sites)} sites")
+
+    return sites
+
+
+def get_sites_by_country(
+    session: Session,
+    country: str,
+    client_name: str | None = None,
+) -> list[LocationSQL]:
+    """Get sites for specific country from the sites table.
+
+    :param session: database session
+    :param country: country name
+    :param client_name: optional client string, we search this in the 'client_site_name'.
+    :return: site object
+    """
+    logger.debug(f"Getting sites by country={country}")
+
+    # start main query
+    query = session.query(LocationSQL)
+
+    # filter by country
+    query = query.filter(LocationSQL.country == country)
+
+    # filter by client string
+    # This could cause an issue if client_a's name is in the site of clients_b.
+    # We could make a new Client table and join it with sites
+    # https://github.com/openclimatefix/pv-site-datamodel/issues/148
+    if client_name is not None:
+        query = query.filter(LocationSQL.client_location_name.like(f"%{client_name}%"))
+
+    # order by uuuid
+    query = query.order_by(LocationSQL.location_uuid)
+
+    # get all results
+    sites = query.all()
+
+    logger.debug(f"Found {len(sites)} sites")
+
+    return sites
+
+
+def get_sites_from_user(
+    session: Session,
+    user: UserSQL,
+    lat_lon_limits: LatitudeLongitudeLimits | None = None,
+) -> list[LocationSQL]:
+    """Get the sites for a user.
+
+    Option to filter on latitude longitude max and min
+    """
+    # get sites and filter if required
+    if lat_lon_limits is not None:
+        # make query
+        query = session.query(LocationSQL)
+
+        # join to Usersql through site groups
+        query = query.join(LocationGroupLocationSQL)
+        query = query.join(LocationGroupSQL)
+        query = query.join(UserSQL)
+
+        # filter on user on lat lon limits
+        query = query.filter(LocationSQL.latitude <= lat_lon_limits.latitude_max)
+        query = query.filter(LocationSQL.latitude >= lat_lon_limits.latitude_min)
+        query = query.filter(LocationSQL.longitude <= lat_lon_limits.longitude_max)
+        query = query.filter(LocationSQL.longitude >= lat_lon_limits.longitude_min)
+
+        # query db
+        sites = query.all()
+
+    else:
+        sites = user.location_group.locations
+    return sites
+
+
+def get_sites_by_client_name(session: Session, client_name: str) -> list[LocationSQL]:
+    """Get sites from client name.
+
+    :param session: database session
+    :param client_name: client name
+    :return: list of site objects
+    """
+    logger.debug(f"Getting {client_name}'s sites")
+
+    # start main query
+    query = session.query(LocationSQL)
+
+    # join the Client table
+    query = query.join(ClientSQL)
+
+    # order by uuuid
+    query = query.order_by(LocationSQL.location_uuid)
+
+    # select the sites related to the client name
+    query = query.filter(ClientSQL.client_name == client_name)
+
+    # get all results
+    sites: list[LocationSQL] | None = query.all()
+
+    if len(sites) == 0:
+        raise Exception(f"Could not find locations from client {client_name}")
+
+    return sites
