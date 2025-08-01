e606ab691db2239635a681641fbee13f727ac082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:27 +0000

    Add package bday-song/1.0.1/bday_song-1.0.1.tar.gz

---
+++ packages/bday-song/bday_song-1.0.1.tar.gz/bday_song-1.0.1/README.md
@@ -0,0 +1,12 @@
+# bday-song
+It *beeps* happy birthday to you using your internal speaker.
+
+Usage: `birthday Turtle` (normal) , `birthday` (no name), `birthday -f Turtle` (fast mode)
+
+Requirements: A PC with an internal beeper, Linux, and the beep package (use dnf/apt install).
+
+If you're using Linux, simply pip install it.
+If you're using Windows, first set up WSL and install this package in there, then run the command.
+If you're using MacOS, Sorry. Use a VM.
+
+Contact me at `contact@turtldev.online` for bug reports, feature requests, etc..
---
+++ packages/bday-song/bday_song-1.0.1.tar.gz/bday_song-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from pathlib import Path
+
+setup(
+    name="bday-song",
+    version="1.0.1",
+    description="Happy Birthday beep CLI tool for Linux",
+    author="Turtledevv",
+    packages=["birthday"],
+    entry_points={
+        'console_scripts': [
+            'birthday = birthday.__main__:main',
+        ],
+    },
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+	"Topic :: Multimedia :: Sound/Audio",
+	"License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.6'
+)
