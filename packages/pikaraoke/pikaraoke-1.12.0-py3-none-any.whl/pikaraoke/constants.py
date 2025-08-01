40d4bbc83add842c01360c6ff5577564ebf42143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:48 +0000

    Add package pikaraoke/1.12.0/pikaraoke-1.12.0-py3-none-any.whl

---
+++ packages/pikaraoke/pikaraoke-1.12.0-py3-none-any.whl/pikaraoke/constants.py
@@ -0,0 +1,9 @@
+LANGUAGES = {
+    "en": "English",
+    "es_VE": "Spanish (Venezuela)",
+    "fi_FI": "Finnish",
+    "zh_CN": "Chinese (Simplified)",
+    "zh_TW": "Chinese (Traditional)",
+    "pt_BR": "Brazilian Portuguese",
+    "it_IT": "Italian",
+}
