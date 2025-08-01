b2151428c181a84d68c8348042085a6a60f6bc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 03:39:36 +0000

    Add package elrahapi/1.2.0/elrahapi-1.2.0-py3-none-any.whl

---
+++ packages/elrahapi/elrahapi-1.2.0-py3-none-any.whl/elrahapi/authentication/token.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from enum import Enum
+class AccessToken(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class RefreshToken(BaseModel):
+    refresh_token: str
+    token_type: str
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+class TokenType(Enum):
+    ACCESS_TOKEN = "access_token"
+    REFRESH_TOKEN = "refresh_token"
