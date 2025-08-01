0d4e619cfed0cf803c5e756c704bcb8fb1527e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:05:06 +0000

    Add package basilisp-kernel/1.3.0/basilisp_kernel-1.3.0-py3-none-any.whl

---
+++ packages/basilisp-kernel/basilisp_kernel-1.3.0-py3-none-any.whl/basilisp_kernel/__init__.py
@@ -0,0 +1,5 @@
+"""The Basilisp Jupyter kernel"""
+
+__version__ = '1.3.0'
+
+from .kernel import BasilispKernel
---
+++ packages/basilisp-kernel/basilisp_kernel-1.3.0-py3-none-any.whl/basilisp_kernel/kernel.py
@@ -0,0 +1,83 @@
+from ipykernel.ipkernel import IPythonKernel
+
+from basilisp import cli
+from basilisp import main as basilisp
+from basilisp.lang import compiler as compiler
+from basilisp.lang import reader as reader
+from basilisp.lang import runtime as runtime
+from basilisp.lang import symbol as sym
+from basilisp.lang.exception import format_exception
+
+import re
+from typing import Any, Callable, Optional, Sequence, Type
+import sys
+
+opts = {}
+basilisp.init(opts)
+ctx = compiler.CompilerContext(filename="basilisp-kernel", opts=opts)
+eof = object()
+
+core_ns = runtime.Namespace.get(runtime.CORE_NS_SYM)
+ns_dyn_var = runtime.Var.find_safe(sym.symbol("*ns*", ns=runtime.CORE_NS))
+
+_DELIMITED_WORD_PATTERN = re.compile(r"[\[\](){\}\s]+")
+
+def do_execute(code):
+    with runtime.bindings({
+            runtime.Var.find_safe(sym.symbol("*out*", ns=runtime.CORE_NS)) : sys.stdout,
+            runtime.Var.find_safe(sym.symbol("*err*", ns=runtime.CORE_NS)) : sys.stderr
+    }):
+        try:
+            return cli.eval_str(code, ctx, ns_dyn_var.deref(), eof)
+        except reader.SyntaxError as e:
+            msg = "".join(format_exception(e, reader.SyntaxError, e.__traceback__))
+            raise reader.SyntaxError(msg) from None
+        except compiler.CompilerException as e:
+            msg = "".join(format_exception(e, compiler.CompilerException, e.__traceback__))
+            raise compiler.CompilerException(msg, phase=e.phase, filename=e.filename) from None
+        except Exception as e:
+            msg = "".join(format_exception(e, Exception, e.__traceback__))
+            raise Exception(msg) from None
+
+
+class BasilispKernel(IPythonKernel):
+    implementation = 'basilisp-kernel'
+    implementation_version = '1.0'
+    language_info = {
+        'name': 'clojure',
+        'mimetype': 'text/x-clojure',
+        'file_extension': '.lpy',
+    }
+    banner = "Basilisp: a Clojure-compatible(-ish) Lisp dialect in Python"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.imported = False
+        runtime.set_current_ns("user")
+        with runtime.ns_bindings("user") as ns:
+            ns.refer_all(core_ns)
+
+
+    def do_complete(self, acode, cursor_pos):
+        code = acode[:cursor_pos]
+        words = re.split(_DELIMITED_WORD_PATTERN, code)
+
+        last_word = words[-1]
+        completions = list(runtime.repl_completions(last_word)) or ()
+
+        return {"status" : "ok",
+                "matches" : completions,
+                "cursor_start" : cursor_pos-len(last_word),
+                "cursor_end" : cursor_pos,
+                "metadata" : dict()
+                }
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        bas_code = f'basilisp_kernel.kernel.do_execute({repr(code)})'
+        if not self.imported:
+            bas_code = f'import basilisp_kernel\n{bas_code}'
+            self.imported = True
+        return super().do_execute(code=bas_code, silent=silent, store_history=store_history,
+                                  user_expressions=user_expressions, allow_stdin=allow_stdin)
+    
