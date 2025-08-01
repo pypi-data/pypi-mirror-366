f7549f28b6d1140eb7ff2c15c5f0e4a73f34e70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:05:22 +0000

    Add package mozilla-django-oidc-db/0.24.0/mozilla_django_oidc_db-0.24.0.tar.gz

---
+++ packages/mozilla-django-oidc-db/mozilla_django_oidc_db-0.24.0.tar.gz/mozilla_django_oidc_db-0.24.0/tests/test_models.py
@@ -0,0 +1,8 @@
+from django.utils.translation import gettext as _
+
+from mozilla_django_oidc_db.fields import ClaimFieldDefault
+
+
+def test_claim_field_default_equality():
+    assert ClaimFieldDefault("foo", "bar") == ClaimFieldDefault("foo", "bar")
+    assert ClaimFieldDefault("foo", "bar") != ClaimFieldDefault("bar", "foo")
