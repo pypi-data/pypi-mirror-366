c47990acf29c6607b8c9b1e15bed37eeedb9e868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:06:41 +0000

    Add package speedbuild/0.1.3/speedbuild-0.1.3.tar.gz

---
+++ packages/speedbuild/speedbuild-0.1.3.tar.gz/speedbuild-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="speedbuild",
+    version="0.1.3",
+    description="Extracts, adapts, and deploys battle-tested features from existing codebases to new projects—complete with all dependencies, configurations, and framework integrations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Required for README.md
+    packages=find_packages(),
+    install_requires=[
+        "django",
+        "pyyaml",
+        "openai",
+        "requests",
+        "websockets"
+    ],
+    entry_points={
+        'console_scripts': [
+            'speedbuild=speedbuild.sb:start',
+        ],
+    },
+)
