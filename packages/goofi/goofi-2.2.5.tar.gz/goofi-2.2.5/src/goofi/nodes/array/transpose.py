b568fe0e2f291ad14c395a2b25236e39714732d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:06:29 +0000

    Add package goofi/2.2.5/goofi-2.2.5.tar.gz

---
+++ packages/goofi/goofi-2.2.5.tar.gz/goofi-2.2.5/src/goofi/nodes/array/transpose.py
@@ -0,0 +1,37 @@
+from goofi.data import Data, DataType
+from goofi.node import Node
+
+
+class Transpose(Node):
+    def config_input_slots():
+        return {"array": DataType.ARRAY}
+
+    def config_output_slots():
+        return {"out": DataType.ARRAY}
+
+    def config_params():
+        return {}
+
+    def process(self, array: Data):
+        if array is None or array.data is None:
+            return None
+
+        if array.data.ndim == 1:
+            # If the input is a 1D array, add an extra dimension
+            array.data = array.data.reshape(-1, 1)
+
+        if array.data.ndim != 2:
+            # TODO: support n-dimensional arrays
+            raise ValueError("Data must be 2D (TODO: support n-dimensional arrays).")
+
+        result = array.data.T
+
+        # transpose channel names
+        ch_names = {}
+        if "dim0" in array.meta["channels"]:
+            ch_names["dim1"] = array.meta["channels"]["dim0"]
+        if "dim1" in array.meta["channels"]:
+            ch_names["dim0"] = array.meta["channels"]["dim1"]
+        array.meta["channels"] = ch_names
+
+        return {"out": (result, array.meta)}
