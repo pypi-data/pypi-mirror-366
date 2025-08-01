436c7afa9a88e91ab84acc9412909088801e78d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 11:05:34 +0000

    Add package pypearl/0.5.1/pypearl-0.5.1-cp312-cp312-macosx_10_9_universal2.whl

---
+++ packages/pypearl/pypearl-0.5.1-cp312-cp312-macosx_10_9_universal2.whl/pypearl/__init__.py
@@ -0,0 +1,4 @@
+# pypearl/__init__.py
+from ._pypearl import add, ArrayD1, ArrayD2, ArrayI2, Layer, ReLU, Softmax, CCE, SGD, Model, breed_models, copy_model
+
+__all__ = ["add", "ArrayD1", "ArrayD2", "ArrayI2", "Layer", "ReLU", "Softmax", "CCE", "SGD", "Model", "breed_models", "copy_model"]
