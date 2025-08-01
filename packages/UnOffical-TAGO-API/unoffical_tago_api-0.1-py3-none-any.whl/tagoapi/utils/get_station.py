bd17358707391f047c49855c027106530869011d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:04:49 +0000

    Add package UnOffical-TAGO-API/0.1/unoffical_tago_api-0.1-py3-none-any.whl

---
+++ packages/UnOffical-TAGO-API/unoffical_tago_api-0.1-py3-none-any.whl/tagoapi/utils/get_station.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from tagoapi.models import Station
+from .cache import Cache
+
+def csv_to_dict(csvfile, encoding):
+    loaded_csv = pd.read_csv(csvfile, encoding=encoding)
+    data = loaded_csv.to_dict(orient='records')
+    return data
+
+
+def get_station(keyword) -> list[Station]:
+    result = []
+    for station in station_list:
+        if  keyword in station["정류장명"]:
+            result.append(
+                Station.from_dict({
+                    "nodeid": station["정류장번호"],
+                    "nodenm": station["정류장명"],
+                    "nodeno": station["모바일단축번호"],
+                    "gpslati": station["위도"],
+                    "gpslong": station["경도"],
+                    "citycode": station["도시코드"]
+                })
+            )
+    return result
+
+cache = Cache("caches/station.pkl")
+station_list = cache.get("stations_2025_06_15.csv")
+if not station_list:
+    station_list = csv_to_dict("tagoapi/csv/stations_2025_06_15.csv", 'cp949')
+    cache.save("stations_2025_06_15.csv", station_list)
+
