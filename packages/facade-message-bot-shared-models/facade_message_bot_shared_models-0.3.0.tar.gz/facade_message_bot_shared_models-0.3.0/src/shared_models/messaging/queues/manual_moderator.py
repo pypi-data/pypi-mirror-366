751866113bc5695bb0b1b4e9b6ff60cf077b698d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 09:07:31 +0000

    Add package facade-message-bot-shared-models/0.3.0/facade_message_bot_shared_models-0.3.0.tar.gz

---
+++ packages/facade-message-bot-shared-models/facade_message_bot_shared_models-0.3.0.tar.gz/facade_message_bot_shared_models-0.3.0/src/shared_models/messaging/queues/manual_moderator.py
@@ -0,0 +1,24 @@
+from faststream.rabbit import RabbitQueue, QueueType
+from faststream.rabbit.schemas.queue import QuorumQueueArgs
+
+
+manual_moderator_queue = RabbitQueue(
+    "moderator.manual",
+    queue_type=QueueType.QUORUM,
+    durable=True,
+    declare=True,
+    arguments=QuorumQueueArgs(
+        {
+            "x-dead-letter-exchange": "dlx",
+            "x-dead-letter-routing-key": "dlx.moderator.manual",
+            "x-dead-letter-strategy": "at-least-once",
+        }
+    ),
+)
+
+manual_moderator_dlx_queue = RabbitQueue(
+    "dlx.moderator.manual",
+    queue_type=QueueType.QUORUM,
+    durable=True,
+    declare=True,
+)
---
+++ packages/facade-message-bot-shared-models/facade_message_bot_shared_models-0.3.0.tar.gz/facade_message_bot_shared_models-0.3.0/src/shared_models/messaging/queues/vision.py
@@ -0,0 +1,24 @@
+from faststream.rabbit import RabbitQueue, QueueType
+from faststream.rabbit.schemas.queue import QuorumQueueArgs
+
+
+vision_notification_queue = RabbitQueue(
+    "vision.notification",
+    queue_type=QueueType.QUORUM,
+    durable=True,
+    declare=True,
+    arguments=QuorumQueueArgs(
+        {
+            "x-dead-letter-exchange": "dlx",
+            "x-dead-letter-routing-key": "dlx.vision.notification",
+            "x-dead-letter-strategy": "at-least-once",
+        }
+    ),
+)
+
+vision_notification_dlx_queue = RabbitQueue(
+    "dlx.vision.notification",
+    queue_type=QueueType.QUORUM,
+    durable=True,
+    declare=True,
+)
