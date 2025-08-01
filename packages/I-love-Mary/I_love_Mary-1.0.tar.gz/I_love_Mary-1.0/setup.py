02cbcee93a568e96d55bd42c3dad56f869cc84be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:07:27 +0000

    Add package I-love-Mary/1.0/I_love_Mary-1.0.tar.gz

---
+++ packages/I-love-Mary/I_love_Mary-1.0.tar.gz/I_love_Mary-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name		 = 'I_love_Mary',
+        version 	 = '1.0',
+        py_modules   = ['I_love_Mary'],
+        author	 	 = 'Balboa',
+        author_email = 'herewww@outlook.com',
+        url			 = '',
+        description	 = 'I-love-Mary',
+     )
