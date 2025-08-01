01b311bf02cf9ea37fc3c6a72fc26f194621db1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:07:42 +0000

    Add package inocloudreve/0.1.2/inocloudreve-0.1.2-py3-none-any.whl

---
+++ packages/inocloudreve/inocloudreve-0.1.2-py3-none-any.whl/CloudreveClient/session/generate_token_util.py
@@ -0,0 +1,18 @@
+import jwt
+
+def decode_token(
+    self,
+    master_key: str,
+    access_token: str
+) -> dict:
+    decoded = jwt.decode(
+        access_token,
+        master_key,
+        algorithms=["HS256"],
+        options={"verify_exp": False, "verify_nbf": False}
+    )
+    return {
+        "success": True,
+        "token": decoded
+    }
+    
