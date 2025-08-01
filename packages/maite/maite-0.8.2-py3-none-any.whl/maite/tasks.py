deb0e712b535ece7c038e96fab79d904628d9a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 07:10:25 +0000

    Add package maite/0.8.2/maite-0.8.2-py3-none-any.whl

---
+++ packages/maite/maite-0.8.2-py3-none-any.whl/maite/tasks.py
@@ -0,0 +1,8 @@
+# Copyright 2024, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014)
+# SPDX-License-Identifier: MIT
+
+
+from maite._internals.tasks.generic import evaluate, evaluate_from_predictions, predict
+
+__all__ = ["evaluate", "evaluate_from_predictions", "predict"]
