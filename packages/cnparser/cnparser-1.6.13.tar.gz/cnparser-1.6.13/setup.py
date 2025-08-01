b111b85cf601452d7550a98ebc3b91849233ab6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:50 +0000

    Add package cnparser/1.6.13/cnparser-1.6.13.tar.gz

---
+++ packages/cnparser/cnparser-1.6.13.tar.gz/cnparser-1.6.13/setup.py
@@ -0,0 +1,20 @@
+''' setup.py
+'''
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cnparser',
+    version='1.6.13',
+    author='new-village',
+    url='https://github.com/new-village/cnparser',
+    description='cnparser is a parser library of Corporate Number Publication Site data.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license = 'Apache-2.0 license',
+    install_requires=['requests', 'bs4', 'pandas', 'pandarallel', 'pykakasi'],
+    packages=find_packages(),
+    package_data={'': ['config/*.json']},
+)
