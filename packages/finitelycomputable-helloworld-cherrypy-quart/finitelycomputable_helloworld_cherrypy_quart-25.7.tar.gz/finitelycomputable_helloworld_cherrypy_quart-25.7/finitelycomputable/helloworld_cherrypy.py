196a72e3bbc850034576e1eebe6dc902589dd9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:04:38 +0000

    Add package finitelycomputable-helloworld-cherrypy-quart/25.7/finitelycomputable_helloworld_cherrypy_quart-25.7.tar.gz

---
+++ packages/finitelycomputable-helloworld-cherrypy-quart/finitelycomputable_helloworld_cherrypy_quart-25.7.tar.gz/finitelycomputable_helloworld_cherrypy_quart-25.7/finitelycomputable/helloworld_cherrypy.py
@@ -0,0 +1,34 @@
+import asyncio
+import cherrypy
+from os import environ
+
+from finitelycomputable import helloworld_quart
+
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "CherryPy adapts, " + asyncio.run(helloworld_quart.hello_world())
+
+
+def setup_server():
+    # because CherryPy and only Cherrypy needs to instantiate its own singletons
+    cherrypy.tree.__module__ = __name__
+    cherrypy.tree.mount(HelloWorld(), base_path, {'/': {}})
+    return cherrypy.tree
+
+base_path = environ.get('BASE_PATH', '')
+application = setup_server()
+
+
+def run():
+    from sys import argv, exit, stderr
+    if len(argv) < 2 or argv[1] != 'run':
+        stderr.write(f'usage: {argv[0]} run [port]\n')
+        exit(1)
+    try:
+        cherrypy.config.update({'server.socket_port': int(argv[2])})
+    except IndexError:
+        pass
+    cherrypy.engine.start()
+    cherrypy.engine.block()
