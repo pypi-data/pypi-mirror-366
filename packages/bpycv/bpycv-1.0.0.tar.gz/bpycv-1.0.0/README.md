d613db4bf8c5083f981a096150158077b6a47227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:07:14 +0000

    Add package bpycv/1.0.0/bpycv-1.0.0.tar.gz

---
+++ packages/bpycv/bpycv-1.0.0.tar.gz/bpycv-1.0.0/README.md
@@ -0,0 +1,138 @@
+# `bpycv`: Computer Vision and Deep Learning Utils for Blender
+
+### Contents: [Features](#-features) | [Install](#-install) | [Demo](#-demo) | [Tips](#-tips) 
+
+![0](https://user-images.githubusercontent.com/10448025/115022704-55937980-9ef0-11eb-952e-c85eb5fad4b8.jpg)      
+*Figure.1 Render instance annoatation, RGB image and depth*
+
+## ▮ Features
+ - [x] Render annotations for semantic segmentation, instance segmentation and panoptic segmentation 
+ - [x] Generate 6DoF pose ground truth
+ - [x] Render depth ground truth
+ - [x] Pre-defined domain randomization: 
+    - [Light and background](https://github.com/DIYer22/bpycv_example_data/tree/main/background_and_light), automatic download from [HDRI Haven](https://hdrihaven.com/hdris/)
+    - [Distractors](https://arxiv.org/pdf/1804.06516) from [ShapeNet](https://shapenet.org/) (e.g. vase, painting, pallet in Figure.1)
+    - Textures from [Texture Haven](https://texturehaven.com/textures/)
+ - [x] Easy installation and demo running
+ - [x] Docker support: `docker run -v /tmp:/tmp diyer22/bpycv` (see [Dockerfile](Dockerfile))
+ - [x] A Python Codebase for building synthetic datasets (see [example/ycb_demo.py](example/ycb_demo.py))
+ - [x] Conversion to [Cityscapes annotation format](https://github.com/DIYer22/bpycv/issues/38)
+ - [x] Easy development and debugging due to:
+    - No complicated packaging
+    - Use of Blender's native API and calling methods
+
+**News:** [We win 🥈2nd place in IROS 2020 Open Cloud Robot Table Organization Challenge (OCRTOC)](https://github.com/DIYer22/bpycv/issues/15)
+
+## ▮ Install
+`bpycv` support Blender 2.9, 3.x, 4.x
+
+1. Download and install Blender [here](https://www.blender.org/download/).
+
+2. Open Blender dir in terminal and run install script:
+
+```bash
+# For Windows user: ensure powershell has administrator permission
+
+# Ensure pip: equl to /blender-path/3.xx/python/bin/python3.10 -m ensurepip
+./blender -b --python-expr "from subprocess import sys,call;call([sys.executable,'-m','ensurepip'])"
+# Update pip toolchain
+./blender -b --python-expr "from subprocess import sys,call;call([sys.executable]+'-m pip install -U pip setuptools wheel'.split())"
+# pip install bpycv
+./blender -b --python-expr "from subprocess import sys,call;call([sys.executable]+'-m pip install -U bpycv'.split())"
+# Check bpycv ready
+./blender -b -E CYCLES --python-expr "import bpycv,cv2;d=bpycv.render_data();bpycv.tree(d);cv2.imwrite('/tmp/try_bpycv_vis(inst-rgb-depth).jpg', d.vis()[...,::-1])"
+```
+
+## ▮ Demo
+#### 1. Fast Instance Segmentation and Depth Demo
+Copy-paste this code to `Scripting/Text Editor` and click `Run Script` button(or `Alt+P`)
+```python
+import cv2
+import bpy
+import bpycv
+import random
+import numpy as np
+
+# remove all MESH objects
+[bpy.data.objects.remove(obj) for obj in bpy.data.objects if obj.type == "MESH"]
+
+for index in range(1, 20):
+    # create cube and sphere as instance at random location
+    location = [random.uniform(-2, 2) for _ in range(3)]
+    if index % 2:
+        bpy.ops.mesh.primitive_cube_add(size=0.5, location=location)
+        categories_id = 1
+    else:
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=0.5, location=location)
+        categories_id = 2
+    obj = bpy.context.active_object
+    # set each instance a unique inst_id, which is used to generate instance annotation.
+    obj["inst_id"] = categories_id * 1000 + index
+
+# render image, instance annoatation and depth in one line code
+result = bpycv.render_data()
+# result["ycb_meta"] is 6d pose GT
+
+# save result
+cv2.imwrite(
+    "demo-rgb.jpg", result["image"][..., ::-1]
+)  # transfer RGB image to opencv's BGR
+
+# save instance map as 16 bit png
+cv2.imwrite("demo-inst.png", np.uint16(result["inst"]))
+# the value of each pixel represents the inst_id of the object
+
+# convert depth units from meters to millimeters
+depth_in_mm = result["depth"] * 1000
+cv2.imwrite("demo-depth.png", np.uint16(depth_in_mm))  # save as 16bit png
+
+# visualization instance mask, RGB, depth for human
+cv2.imwrite("demo-vis(inst_rgb_depth).jpg", result.vis()[..., ::-1])
+print(f"Saving vis image to: 'demo-vis(inst_rgb_depth).jpg'")
+```
+Open `./demo-vis(inst_rgb_depth).jpg`:   
+
+![demo-vis(inst_rgb_depth)](https://user-images.githubusercontent.com/10448025/115022679-4ad8e480-9ef0-11eb-9a42-cdfbf7e9d2ae.jpg)
+
+#### 2. YCB Demo
+
+```shell
+mkdir ycb_demo
+cd ycb_demo/
+
+# prepare code and example data
+git clone https://github.com/DIYer22/bpycv
+git clone https://github.com/DIYer22/bpycv_example_data
+
+cd bpycv/example/
+
+blender -b -P ycb_demo.py
+
+cd dataset/vis/
+ls .  # visualize result here
+# 0.jpg
+```
+Open visualize result `ycb_demo/bpycv/example/dataset/vis/0.jpg`:   
+![0](https://user-images.githubusercontent.com/10448025/115022704-55937980-9ef0-11eb-952e-c85eb5fad4b8.jpg)    
+*instance_map | RGB | depth*
+
+[example/ycb_demo.py](example/ycb_demo.py) Inculding:
+- Domain randomization for background, light and distractor (from ShapeNet)
+- Codebase for building synthetic datasets base on YCB dataset
+
+#### 3. 6DoF Pose Demo
+Generate and visualize 6DoF pose GT: [example/6d_pose_demo.py](example/6d_pose_demo.py)
+
+<img src="https://user-images.githubusercontent.com/10448025/74708759-5e3ee000-5258-11ea-8849-0174c34d507c.png" style="width:300px">
+
+
+## ▮ Tips
+ > Blender may can't direct load `.obj` or `.dea` file from YCB and ShapeNet dataset.  
+ > It's better to transefer and format using [`meshlabserver`](https://github.com/cnr-isti-vclab/meshlab/releases) by run `meshlabserver -i raw.obj -o for_blender.obj -m wt`
+
+<br>
+<br>
+<div align="center">
+
+**[suggestion](https://github.com/DIYer22/bpycv/issues) and pull request are welcome** 😊
+</div>
---
+++ packages/bpycv/bpycv-1.0.0.tar.gz/bpycv-1.0.0/bpycv/exr_image_parser.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: DIYer22@github
+@mail: ylxx@live.com
+Created on Thu Dec 26 18:15:47 2019
+"""
+import boxx
+from boxx import *
+from boxx import greyToRgb, histEqualize, inpkg, np, pathjoin, savenp
+
+with inpkg():
+    from .pseudo_color import heatmap_to_pseudo_color
+    from .utils import encode_inst_id
+    from .camera_utils import get_cam_intrinsic
+
+import os
+import struct
+
+import bpy
+import cv2
+import minexr
+import scipy.io
+
+
+def depth_of_point_to_depth(depth_of_point, K):
+    """
+    # https://blender.stackexchange.com/questions/130970/cycles-generates-distorted-depth
+    CYCLES's depth is distance to camera original point
+    EEVEE's dpeth is XYZ's Z, same to opencv
+    """
+    h, w = depth_of_point.shape
+    xyzs_1m = (
+        np.pad(
+            np.mgrid[:h, :w][::-1].reshape(2, -1), ((0, 1), (0, 0)), constant_values=1
+        ).T
+        @ np.linalg.inv(K).T
+    )
+    xyzs = (
+        xyzs_1m
+        * depth_of_point.flatten()[:, None]
+        / np.linalg.norm(xyzs_1m, axis=1, keepdims=True)
+    )
+    depth = xyzs[:, 2].reshape(h, w)
+    return depth
+
+
+class ExrReader(minexr.reader.MinExrReader):
+    def _read_image(self):
+        """
+        Override original _read_image, since that one assumes ims are float16 but we use float32 here
+        """
+        H, C, W = self.shape
+
+        dtype = self.channel_types[0]
+        DS = np.dtype(dtype).itemsize
+        SOFF = 8 + DS * W * C
+        strides = (SOFF, DS * W, DS)
+        nbytes = SOFF * H
+
+        self.fp.seek(self.first_offset, 0)
+        image = np.frombuffer(self.fp.read(nbytes), dtype=dtype, count=-1, offset=8)
+        self.image = np.lib.stride_tricks.as_strided(image, (H, C, W), strides)
+
+    def _read_header(self):
+        """
+        Override original _read_header, since that one doesn't allow for cycles' long attribute names
+        """
+        self.fp.seek(0)
+        buf = minexr.buffer.BufferReader(self.fp.read(10000))
+
+        # Magic and version and info bits
+        magic, version, b2, b3, b4 = struct.unpack("<iB3B", buf.read(8))
+        assert magic == 20000630, "Not an OpenEXR file."
+        assert b3 == b4 == 0, "Not a single-part scan line file."
+        assert b2 in (0, 4), "Not a single-part scan line file."
+
+        # Header attributes
+        self.attrs = self._read_header_attrs(buf)
+
+        # Parse channels and datawindow
+        self.compr = self._parse_compression(self.attrs)
+        self.channel_names, self.channel_types = self._parse_channels(self.attrs)
+        self.channel_map = {cn: i for i, cn in enumerate(self.channel_names)}
+        H, W = self._parse_data_window(self.attrs)
+        self.shape = (H, len(self.channel_names), W)
+        self.first_offset = self._read_first_offset(buf)
+
+        # Assert our assumptions
+        assert self.compr == 0x00, "Compression not supported."
+        assert len(set(self.channel_types)) <= 1, "All channel types must be equal."
+
+
+class ExrImage:
+    LIMIT_DEPTH = 6e4
+
+    def __init__(self, exr_path, K=None):
+        with open(exr_path, "rb") as fp:
+            self.reader = ExrReader(fp)
+        self.by_cycles = any(
+            [key for key in self.reader.attrs if key.startswith("cycles.")]
+        )
+        if self.by_cycles and K is None:  # CYCLES has different depth
+            K = get_cam_intrinsic()
+        self.K = K
+
+    def get_rgb(self):
+        # Try different possible RGB channel names for multilayer EXR compatibility
+        rgb_prefixes = ["ViewLayer.Combined.", "RenderLayer.Combined.", "Combined.", ""]
+        for prefix in rgb_prefixes:
+            channels = [f"{prefix}R", f"{prefix}G", f"{prefix}B"]
+            if all(ch in self.reader.channel_map for ch in channels):
+                return self.reader.select(channels).copy()
+        
+        # Fallback to basic RGB if available
+        if all(ch in self.reader.channel_map for ch in ["R", "G", "B"]):
+            return self.reader.select(["R", "G", "B"]).copy()
+        
+        print(f"Warning: No RGB channels found. Available channels: {self.reader.channel_names}")
+        h, w = self.reader.shape[0], self.reader.shape[2]
+        return np.zeros((h, 3, w), dtype=np.float32)
+
+    def get_rgba(self):
+        # Try different possible RGBA channel names for multilayer EXR compatibility  
+        rgba_prefixes = ["ViewLayer.Combined.", "RenderLayer.Combined.", "Combined.", ""]
+        for prefix in rgba_prefixes:
+            channels = [f"{prefix}R", f"{prefix}G", f"{prefix}B", f"{prefix}A"]
+            if all(ch in self.reader.channel_map for ch in channels):
+                return self.reader.select(channels).copy()
+        
+        # Fallback to basic RGBA if available
+        if all(ch in self.reader.channel_map for ch in ["R", "G", "B", "A"]):
+            return self.reader.select(["R", "G", "B", "A"]).copy()
+            
+        print(f"Warning: No RGBA channels found. Available channels: {self.reader.channel_names}")
+        h, w = self.reader.shape[0], self.reader.shape[2]
+        return np.zeros((h, 4, w), dtype=np.float32)
+
+    def get_raw_depth(self):
+        # Try different possible depth channel names for compatibility with different Blender versions
+        # In Blender 4.5 multilayer EXR, depth is typically "ViewLayer.Depth.Z" or similar
+        possible_depth_channels = [
+            "ViewLayer.Depth.Z", 
+            "ViewLayer.Z", 
+            "Depth.Z",
+            "Z",
+            "Depth",
+            "RenderLayer.Depth.Z",
+            "RenderLayer.Z"
+        ]
+        depth_channel = None
+        
+        for channel in possible_depth_channels:
+            if channel in self.reader.channel_map:
+                depth_channel = channel
+                break
+        
+        if depth_channel is None:
+            print(f"Warning: No depth channel found. Available channels: {self.reader.channel_names}")
+            print(f"Channel map: {self.reader.channel_map}")
+            # Return a dummy depth array with zeros
+            h, w = self.reader.shape[0], self.reader.shape[2]
+            return np.zeros((h, w), dtype=np.float32)
+        
+        raw_depth = self.reader.select([depth_channel]).copy().squeeze()
+        if self.by_cycles:
+            raw_depth = depth_of_point_to_depth(raw_depth, self.K)
+        return raw_depth
+
+    def get_pseudo_color(self):
+        depth = self.get_raw_depth()
+        limit_mask = depth < self.LIMIT_DEPTH
+        depth = depth * limit_mask
+        depth = depth / depth.max()
+        depth[~limit_mask] = 1.1
+        depth = 1 - depth
+        return heatmap_to_pseudo_color(depth)
+
+    def get_depth(self):
+        # turn inf depth to 0
+        depth = self.get_raw_depth()
+        limit_mask = depth < self.LIMIT_DEPTH
+        depth = depth * limit_mask
+        return depth
+
+    def get_inst(self):
+        rgb = self.get_rgb()
+        inst = encode_inst_id.rgb_to_id(rgb)
+
+        # if world.use_nodes is False, Blender will set background as a gray (0.05087609, 0.05087609, 0.05087609)
+        gray_background_mask = (rgb[..., 0] != 0) & (rgb[..., 0] != 1)
+        inst[gray_background_mask] = -1
+        return inst
+
+
+class ImageWithAnnotation(dict):
+    def __init__(self, image=None, exr=None, **kv):
+        super().__init__(**kv)
+        if image is not None:
+            self["image"] = image
+        self["inst"] = exr.get_inst()
+        self["depth"] = exr.get_depth()
+        self["_raw_exr"] = exr
+
+    def __getattribute__(self, key):
+        if key in self:
+            return self[key]
+        return dict.__getattribute__(self, key)
+
+    def vis(self):
+        vis_list = []  # RGB float 0.~1.
+
+        def vis_inst(inst):
+            unique, _idxs = np.unique(inst, return_inverse=True)
+            idxs = _idxs.reshape(inst.shape)
+            return boxx.mapping_array(
+                idxs, boxx.getDefaultColorList(len(unique), includeBackGround=True)
+            )
+
+        if "inst" in self:
+            inst_vis = vis_inst(self["inst"])
+            vis_list.append(inst_vis)
+
+        image = self.get("image", self.get("image1"))
+        if image is not None:
+            vis_list.append(image[..., :3] / 255.0)
+
+        depth_vis = self["_raw_exr"].get_pseudo_color()
+        vis_list.append(depth_vis)
+        vis = (np.concatenate(vis_list, 1) * 255).astype(np.uint8)
+        return vis
+
+    def save(
+        self, dataset_dir="dataset", fname="0", save_blend=False, no_sub_dir=False
+    ):
+        fname = str(fname)
+        os.makedirs(dataset_dir, exist_ok=True)
+
+        def split_by_datatype_prefix(fname, type_name):
+            sub_dir = pathjoin(dataset_dir, type_name)
+            os.makedirs(sub_dir, exist_ok=True)
+            return sub_dir + "/" + fname
+
+        no_sub_dir_prefix = lambda fname, type_name: pathjoin(
+            dataset_dir, f"{fname}~{type_name}"
+        )
+        get_prefix = no_sub_dir_prefix if no_sub_dir else split_by_datatype_prefix
+
+        if self.get("inst") is not None:
+            inst_prefix = get_prefix(fname, "instance_map")
+            inst_path = inst_prefix + ".png"
+            cv2.imwrite(inst_path, self["inst"].clip(0).astype(np.uint16))
+        if self.get("depth") is not None:
+            depth_prefix = get_prefix(fname, "depth")
+            depth_path = depth_prefix
+            if self["depth"].dtype == np.uint16:
+                cv2.imwrite(depth_path + ".png", self["depth"])
+            else:
+                savenp(depth_path, self["depth"].astype(np.float16))
+        if (
+            self.get("image", self.get("image1")) is not None
+            and self.get("inst") is not None
+            and self.get("depth") is not None
+        ):
+            vis_prefix = get_prefix(fname, "vis")
+            vis_path = vis_prefix + ".jpg"
+            cv2.imwrite(vis_path, self.vis()[..., ::-1])
+        if self.get("ycb_6d_pose") is not None:
+            pose_prefix = get_prefix(fname, "ycb_6d_pose")
+            pose_path = pose_prefix + ".mat"
+            scipy.io.savemat(pose_path, self["ycb_6d_pose"])
+        if save_blend:
+            blend_prefix = get_prefix(fname, "blend")
+            blend_path = blend_prefix + ".blend"
+            bpy.ops.wm.save_mainfile(filepath=os.path.abspath(blend_path))
+        # save image at last for unstable compute enviroment
+        def save_rgb_image(key):
+            if self.get(key) is not None:
+                image_prefix = get_prefix(fname, key)
+                image_path = image_prefix + ".jpg"
+                cv2.imwrite(image_path, self[key][..., ::-1])
+
+        save_rgb_image("image")
+        save_rgb_image("image1")
+        save_rgb_image("image2")
+
+
+def parser_exr(exr_path):
+    exr = ExrImage(exr_path)
+    return exr
+
+
+def test_parser_exr(exr_path="../tmp_exrs/cycles.exr"):
+    return parser_exr(exr_path)
+
+
+if __name__ == "__main__":
+    from boxx import imread, show
+
+    exr_path = "../example/tmp_exr/inst.exr"
+    exr = parser_exr(exr_path)
+    inst = exr.get_inst()
+    png = np.uint8(boxx.mapping_array(inst % 20, boxx.getDefaultColorList(20)) * 255)
+
+    ann = ImageWithAnnotation(png, exr)
+    vis = ann.vis()
+    show - vis
