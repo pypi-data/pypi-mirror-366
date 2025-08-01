a9b970c3bb4d282abffd8c47f3b544ad502cc6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:04:48 +0000

    Add package python-byzatic-commons/0.1.6/python_byzatic_commons-0.1.6-py3-none-any.whl

---
+++ packages/python-byzatic-commons/python_byzatic_commons-0.1.6-py3-none-any.whl/python_byzatic_commons/__init__.py
@@ -0,0 +1,25 @@
+#
+#
+#
+import python_byzatic_commons.crud
+import python_byzatic_commons.exceptions
+import python_byzatic_commons.filereaders
+import python_byzatic_commons.flattener
+import python_byzatic_commons.import_modules_factory
+import python_byzatic_commons.in_memory_storages
+import python_byzatic_commons.logging_manager
+import python_byzatic_commons.random_string_generator
+import python_byzatic_commons.singleton
+
+__version__ = "0.1.6"
+__all__ = [
+    'crud',
+    'exceptions',
+    'filereaders',
+    'flattener',
+    'import_modules_factory',
+    'in_memory_storages',
+    'logging_manager',
+    'random_string_generator',
+    'singleton'
+]
