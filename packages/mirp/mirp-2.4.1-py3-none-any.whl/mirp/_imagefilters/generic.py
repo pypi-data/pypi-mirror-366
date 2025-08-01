247ceb44b319fc56da7370216ef6af82ec4dd47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:04:51 +0000

    Add package mirp/2.4.1/mirp-2.4.1-py3-none-any.whl

---
+++ packages/mirp/mirp-2.4.1-py3-none-any.whl/mirp/_imagefilters/generic.py
@@ -0,0 +1,20 @@
+from mirp.settings.generic import SettingsClass
+from mirp._images.generic_image import GenericImage
+
+
+class GenericFilter:
+
+    def __init__(self, image: GenericImage, settings: SettingsClass, name: str):
+        # In-slice (2D) or 3D filtering
+        self.separate_slices = image.separate_slices
+
+        # Even though most currently implemented filters are IBSI-compliant, set value to False to avoid surprises in
+        # the future.
+        self.ibsi_compliant: bool = False
+        self.ibsi_id: None | str = None
+
+    def generate_object(self):
+        raise NotImplementedError("_generate_object method should be defined in the subclasses")
+
+    def transform(self, image: GenericImage):
+        raise NotImplementedError("transform method should be defined in the subclasses.")
