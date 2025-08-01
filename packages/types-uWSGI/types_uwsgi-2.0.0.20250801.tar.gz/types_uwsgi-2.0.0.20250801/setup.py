0209e4b88830d6e71fda227bf94481e3ce558984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:05:02 +0000

    Add package types-uWSGI/2.0.0.20250801/types_uwsgi-2.0.0.20250801.tar.gz

---
+++ packages/types-uWSGI/types_uwsgi-2.0.0.20250801.tar.gz/types_uwsgi-2.0.0.20250801/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'uwsgi-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed'], 'uwsgidecorators-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed']})
