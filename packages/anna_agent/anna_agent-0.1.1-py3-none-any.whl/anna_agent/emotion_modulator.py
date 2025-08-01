2e27d44f2c31a11e5e11642bf4b88daa80bba21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:05:05 +0000

    Add package anna_agent/0.1.1/anna_agent-0.1.1-py3-none-any.whl

---
+++ packages/anna_agent/anna_agent-0.1.1-py3-none-any.whl/anna_agent/emotion_modulator.py
@@ -0,0 +1,89 @@
+from random import randint
+from .emotion_pertuber import perturb_state
+from .backbone import get_emotion_client
+from .common.registry import registry
+import json
+
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "emotion_inference",
+            "description": "根据profile和对话记录，推理下一句情绪",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "emotion": {
+                        "type": "string",
+                        "enum": [
+                            "admiration",
+                            "amusement",
+                            "anger",
+                            "annoyance",
+                            "approval",
+                            "caring",
+                            "confusion",
+                            "curiosity",
+                            "desire",
+                            "disappointment",
+                            "disapproval",
+                            "disgust",
+                            "embarrassment",
+                            "excitement",
+                            "fear",
+                            "gratitude",
+                            "grief",
+                            "joy",
+                            "love",
+                            "nervousness",
+                            "optimism",
+                            "pride",
+                            "realization",
+                            "relief",
+                            "remorse",
+                            "sadness",
+                            "surprise",
+                            "neutral",
+                        ],
+                        "description": "推理出的情绪类别，必须是GoEmotions定义的27种情绪之一。",
+                    }
+                },
+                "required": ["emotion"],
+            },
+        },
+    }
+]
+
+
+
+def emotion_inferencer(profile, conversation):
+    client = get_emotion_client()
+    patient_info = f"### 患者信息\n年龄：{profile['age']}\n性别：{profile['gender']}\n职业：{profile['occupation']}\n婚姻状况：{profile['martial_status']}\n症状：{profile['symptoms']}"
+    dialogue_history = "\n".join(
+        [f"{conv['role']}: {conv['content']}" for conv in conversation]
+    )
+    response = client.chat.completions.create(
+        model=registry.get("anna_engine_config").emotion_model_name,
+        messages=[
+            {
+                "role": "user",
+                "content": f"### 任务\n根据患者情况及咨访对话历史记录推测患者下一句话最可能的情绪。\n{patient_info}\n### 对话记录\n{dialogue_history}",
+            }
+        ],
+        tools=tools,
+        tool_choice={"type": "function", "function": {"name": "emotion_inference"}},
+    )
+    emotion = json.loads(response.choices[0].message.tool_calls[0].function.arguments)[
+        "emotion"
+    ]
+    return emotion
+
+
+def emotion_modulation(profile, conversation):
+    indicator = randint(0, 100)
+    emotion = emotion_inferencer(profile, conversation)
+    if indicator > 90:
+        return perturb_state(emotion)
+    else:
+        return emotion
---
+++ packages/anna_agent/anna_agent-0.1.1-py3-none-any.whl/anna_agent/ms_patient.py
@@ -0,0 +1,151 @@
+from .backbone import get_counselor_client, get_openai_client
+from .common.registry import registry
+from .fill_scales import fill_scales, fill_scales_previous
+from .event_trigger import event_trigger, situationalising_events
+from .emotion_modulator import emotion_modulation
+from .querier import query, is_need
+from .complaint_elicitor import switch_complaint, transform_chain
+from .complaint_chain import gen_complaint_chain
+from .short_term_memory import summarize_scale_changes
+from .style_analyzer import analyze_style
+import random
+import logging
+from .anna_agent_template import prompt_template
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# 控制台打印
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+
+logger.addHandler(handler)
+
+
+class MsPatient:
+    def __init__(self, portrait: dict, report: dict, previous_conversations: list):
+        self.configuration = {}
+        self.portrait = portrait  # age, gender, occupation, maritial_status, symptom
+        # self.profile = {key:self.portrait[key] for key in self.portrait if key != "symptom"}          # profile不包含症状symptom
+        self.configuration["gender"] = self.portrait["gender"]
+        self.configuration["age"] = self.portrait["age"]
+        self.configuration["occupation"] = self.portrait["occupation"]
+        self.configuration["marriage"] = self.portrait["martial_status"]
+        self.report = report
+        self.previous_conversations = previous_conversations
+        # 填写之前疗程的量表
+        self.p_bdi, self.p_ghq, self.p_sass = fill_scales_previous(
+            self.portrait, self.report
+        )
+        self.conversation = []  # Conversation存储咨访记录
+        self.messages = []  # Messages存储LLM的消息列表
+        # 生成主诉认知变化链
+        self.complaint_chain = gen_complaint_chain(self.portrait)
+        # 生成近期事件
+        self.event = event_trigger(self.portrait)
+        # 总结短期记忆-事件
+        self.situation = situationalising_events(self.portrait)
+        self.configuration["situation"] = self.situation
+        # 分析说话风格
+        self.style = analyze_style(self.portrait, self.previous_conversations)
+        self.configuration["style"] = self.style
+        self.configuration[
+            "status"
+        ] = ""  # 先置状态为空，后续会根据量表分析结果进行更新
+        seeker_utterances = [
+            utterance["content"]
+            for utterance in self.previous_conversations
+            if utterance["role"] == "Seeker"
+        ]
+        self.configuration["statement"] = random.choices(seeker_utterances, k=3)
+        # 填写当前量表
+        # bdi = ["B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B","B"]
+        # ghq = ["C","C","B","B","D","C","B","C","B","B","D","B","C","B","D","B","C","B","D","B","C","B","B","B","D","B","B","B"]
+        # sass = ["B","B","C","B","C","B","B","B","B","B","C","B","C","B","C","B","B","B","B","B","B"]
+        # self.bdi, self.ghq, self.sass = bdi, ghq, sass
+
+        self.bdi, self.ghq, self.sass = fill_scales(
+            prompt_template.format(**self.configuration)
+        )
+        scales = {
+            "p_bdi": self.p_bdi,
+            "p_ghq": self.p_ghq,
+            "p_sass": self.p_sass,
+            "bdi": self.bdi,
+            "ghq": self.ghq,
+            "sass": self.sass,
+        }
+        # 分析近期状态
+        self.status = summarize_scale_changes(scales)
+        self.configuration["status"] = self.status
+        # 选取对话样例
+        self.system = prompt_template.format(**self.configuration)
+        self.chain_index = 1
+        self.client = get_openai_client()
+
+    def chat(self, message):
+        # 更新消息列表
+        self.conversation.append({"role": "Counselor", "content": message})
+        self.messages.append({"role": "user", "content": message})
+        try:
+            # 初始化本次对话的状态
+            emotion = emotion_modulation(self.portrait, self.conversation)
+            self.chain_index = switch_complaint(
+                self.complaint_chain, self.chain_index, self.conversation
+            )
+            logger.info(f"complaint_chain: {self.complaint_chain}")
+            complaint = transform_chain(self.complaint_chain)[self.chain_index]
+            # 判断是否涉及前疗程内容
+            if is_need(message):
+                # 生成前疗程内容
+                sup_information = query(
+                    message, self.previous_conversations, self.report
+                )
+
+                # 生成回复
+                messages = (
+                    [{"role": "system", "content": self.system}]
+                    + self.messages
+                    + [
+                        {
+                            "role": "user",
+                            "content": f"当前的情绪状态是：{emotion}，当前的主诉是：{complaint}，涉及到之前疗程的信息是：{sup_information}",
+                        }
+                    ]
+                )
+                print(messages)
+
+                response = self.client.chat.completions.create(
+                    model=registry.get("anna_engine_config").model_name,
+                    messages=messages,
+                )
+            else:
+                # 生成回复
+                messages = (
+                    [{"role": "system", "content": self.system}]
+                    + self.messages
+                    + [
+                        {
+                            "role": "user",
+                            "content": f"当前的情绪状态是：{emotion}，当前的主诉是：{complaint}",
+                        }
+                    ]
+                )
+                print(messages)
+                response = self.client.chat.completions.create(
+                    model=registry.get("anna_engine_config").model_name,
+                    messages=messages,
+                )
+
+            # 更新消息列表
+            self.conversation.append(
+                {"role": "Seeker", "content": response.choices[0].message.content}
+            )
+            self.messages.append(
+                {"role": "assistant", "content": response.choices[0].message.content}
+            )
+            return response.choices[0].message.content
+        except Exception as err:
+            logger.error("Exception", err)  
+
+            return ""
