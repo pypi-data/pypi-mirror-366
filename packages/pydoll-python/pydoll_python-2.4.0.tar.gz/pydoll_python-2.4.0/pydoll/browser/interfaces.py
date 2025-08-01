5e1520906a48a2bc20540c954c18a61c43f0e446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:16 +0000

    Add package pydoll-python/2.4.0/pydoll_python-2.4.0.tar.gz

---
+++ packages/pydoll-python/pydoll_python-2.4.0.tar.gz/pydoll_python-2.4.0/pydoll/browser/interfaces.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class Options(ABC):
+    @property
+    @abstractmethod
+    def arguments(self) -> list[str]:
+        pass
+
+    @property
+    @abstractmethod
+    def binary_location(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def start_timeout(self) -> int:
+        pass
+
+    @abstractmethod
+    def add_argument(self, argument: str):
+        pass
+
+    @property
+    @abstractmethod
+    def browser_preferences(self) -> dict:
+        pass
+
+
+class BrowserOptionsManager(ABC):
+    @abstractmethod
+    def initialize_options(self) -> Options:
+        pass
+
+    @abstractmethod
+    def add_default_arguments(self):
+        pass
---
+++ packages/pydoll-python/pydoll_python-2.4.0.tar.gz/pydoll_python-2.4.0/pydoll/protocol/browser/types.py
@@ -0,0 +1,13 @@
+from typing_extensions import NotRequired, TypedDict
+
+from pydoll.constants import WindowState
+
+
+class WindowBoundsDict(TypedDict):
+    """Structure for window bounds parameters."""
+
+    windowState: WindowState
+    width: NotRequired[int]
+    height: NotRequired[int]
+    x: NotRequired[int]
+    y: NotRequired[int]
