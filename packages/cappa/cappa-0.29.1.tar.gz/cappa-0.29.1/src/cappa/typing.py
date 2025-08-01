850f6549c10ca1f9146b6e7045ff35c64ea0eb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 07:13:41 +0000

    Add package cappa/0.29.1/cappa-0.29.1.tar.gz

---
+++ packages/cappa/cappa-0.29.1.tar.gz/cappa-0.29.1/src/cappa/typing.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+
+import enum
+import inspect
+import sys
+import typing
+from dataclasses import dataclass
+from types import MethodType
+from typing import Any, Protocol, TypeVar
+
+import typing_extensions
+from typing_extensions import assert_never
+
+from cappa.type_view import TypeView
+
+
+class DocType(Protocol):
+    documentation: str
+
+
+try:
+    from typing_extensions import Doc as TypingExtensionsDoc
+
+    Doc: type[DocType] = TypingExtensionsDoc
+except ImportError:  # pragma: no cover
+
+    @dataclass
+    class InternalDoc:
+        documentation: str
+
+    Doc = InternalDoc
+
+
+__all__ = [
+    "Doc",
+    "T",
+    "assert_never",
+    "assert_type",
+    "detect_choices",
+    "find_annotations",
+]
+
+
+T = TypeVar("T")
+
+
+def find_annotations(type_view: TypeView[Any], kind: type[T]) -> list[T]:
+    result: list[T] = []
+    for annotation in type_view.metadata:
+        if isinstance(annotation, kind):
+            result.append(annotation)
+
+        if isinstance(annotation, type) and issubclass(annotation, kind):
+            result.append(annotation())
+
+    return result
+
+
+def assert_type(value: Any, typ: type[T]) -> T:
+    assert isinstance(value, typ), value
+    return value
+
+
+def detect_choices(type_view: TypeView[Any]) -> list[str] | None:
+    if type_view.is_optional:
+        type_view = type_view.strip_optional()
+
+    if type_view.is_subclass_of(enum.Enum):
+        return [v.value for v in type_view.annotation]
+
+    if type_view.is_subclass_of((list, set)) or type_view.is_variadic_tuple:
+        type_view = type_view.inner_types[0]
+
+    if type_view.is_union:
+        if all(t.is_literal for t in type_view.inner_types):
+            return [str(t.args[0]) for t in type_view.inner_types]
+
+    if type_view.is_literal:
+        return [str(t) for t in type_view.args]
+
+    return None
+
+
+def get_method_class(fn: MethodType) -> type:
+    return inspect._findclass(fn)  # type: ignore
+
+
+if sys.version_info < (3, 12):
+
+    def is_type_alias(type_view: TypeView[Any]) -> bool:
+        type_alias_type: type[Any] | None = (
+            typing_extensions.TypeAliasType
+            if hasattr(typing_extensions, "TypeAliasType")
+            else None
+        )
+        return (
+            isinstance(type_view.annotation, type_alias_type)
+            if type_alias_type is not None
+            else False
+        )
+
+else:
+
+    def is_type_alias(type_view: TypeView[Any]) -> bool:
+        backport_type_alias_type = (
+            typing_extensions.TypeAliasType
+            if hasattr(typing_extensions, "TypeAliasType")
+            else None
+        )
+        type_alias_types = tuple(
+            t for t in (typing.TypeAliasType, backport_type_alias_type) if t is not None
+        )
+        return isinstance(type_view.annotation, type_alias_types)
---
+++ packages/cappa/cappa-0.29.1.tar.gz/cappa-0.29.1/tests/arg/test_destructured.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+import enum
+from dataclasses import dataclass
+
+import pytest
+from typing_extensions import Annotated
+
+import cappa
+from cappa.subcommand import Subcommands
+from tests.utils import parse
+
+
+class Flavor(enum.Enum):
+    vanilla = "vanilla"
+    chocolate = "chocolate"
+
+
+@dataclass
+class Attributes:
+    color: Annotated[str, cappa.Arg(long="attribute.color")]
+    flavors: Annotated[list[Flavor], cappa.Arg(value_name="attribute.flavor")]
+    something: Annotated[int, cappa.Arg(short="-p")]
+    custom_parse: Annotated[str, cappa.Arg(long="foo", parse=lambda _: "always this")]  # pyright: ignore
+    custom_action: Annotated[int, cappa.Arg(long="bar", action=lambda: 42)]  # pyright: ignore
+    optional: Annotated[int, cappa.Arg(long="baz")] = 4
+
+
+@dataclass
+class Args:
+    attrs: Annotated[Attributes, cappa.Arg.destructure()]
+
+
+def test_destructured():
+    test = parse(
+        Args,
+        "--attribute.color=red",
+        "chocolate",
+        "vanilla",
+        "-p4",
+        "--foo=whatever",
+        "--bar=0",
+    )
+    assert test == Args(
+        attrs=Attributes(
+            "red",
+            [Flavor.chocolate, Flavor.vanilla],
+            something=4,
+            custom_parse="always this",
+            custom_action=42,
+            optional=4,
+        )
+    )
+
+
+@dataclass
+class OptionalArgs:
+    attrs: Annotated[Attributes | None, cappa.Arg.destructure()]
+
+
+def test_destructured_optional():
+    with pytest.raises(ValueError) as e:
+        parse(OptionalArgs)
+    assert (
+        str(e.value)
+        == "Destructured arguments currently only support singular concrete types."
+    )
+
+
+@dataclass
+class InvalidSubcommandSub:
+    sub: Subcommands[Attributes]
+
+
+@dataclass
+class InvalidSubcommand:
+    attrs: Annotated[InvalidSubcommandSub, cappa.Arg.destructure()]
+
+
+def test_invalid_subcommand():
+    with pytest.raises(ValueError) as e:
+        parse(InvalidSubcommand)
+    assert (
+        str(e.value)
+        == "Subcommands are unsupported in the context of a destructured argument"
+    )
+
+
+@dataclass
+class CollisionSub:
+    field1: str = ""
+    field2: Annotated[str, cappa.Arg(long=True)] = ""
+
+
+@dataclass
+class Collision:
+    sub: Annotated[CollisionSub, cappa.Arg.destructure()]
+    field1: int = 0
+    field2: int = 0
+
+
+def test_destructured_collision():
+    result = parse(Collision, "1")
+    assert result == Collision(sub=CollisionSub("1", ""), field1=0)
+
+    result = parse(Collision, "1", "2")
+    assert result == Collision(sub=CollisionSub("1", ""), field1=2)
+
+    result = parse(Collision, "--field2=-1", "1", "2", "3")
+    assert result == Collision(sub=CollisionSub("1", "-1"), field1=2, field2=3)
