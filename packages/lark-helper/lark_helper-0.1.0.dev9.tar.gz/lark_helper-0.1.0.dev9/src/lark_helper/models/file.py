72e029b5965d532c3510130ce8101a846294af22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:04:50 +0000

    Add package lark-helper/0.1.0.dev9/lark_helper-0.1.0.dev9.tar.gz

---
+++ packages/lark-helper/lark_helper-0.1.0.dev9.tar.gz/lark_helper-0.1.0.dev9/src/lark_helper/models/file.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class ImportTaskResult(BaseModel):
+    job_status: int
+    token: str | None = None
+    url: str | None = None
+    job_error_msg: str | None = None
+
+
+class TmpDownloadUrl(BaseModel):
+    file_token: str
+    tmp_download_url: str
