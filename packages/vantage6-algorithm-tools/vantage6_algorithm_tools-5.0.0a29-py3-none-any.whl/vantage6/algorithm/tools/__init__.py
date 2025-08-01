881b533504adf6755c096620645f4fe3b95d7d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:06:40 +0000

    Add package vantage6-algorithm-tools/5.0.0a29/vantage6_algorithm_tools-5.0.0a29-py3-none-any.whl

---
+++ packages/vantage6-algorithm-tools/vantage6_algorithm_tools-5.0.0a29-py3-none-any.whl/vantage6/algorithm/tools/__init__.py
@@ -0,0 +1,15 @@
+"""Algorithm tools support the development of algorithms for the vantage6 platform."""
+
+from vantage6.common.enum import StrEnumBase
+
+# make sure the version is available
+from vantage6.algorithm.client._version import __version__  # noqa: F401
+
+
+class DecoratorStepType(StrEnumBase):
+    """Type of the decorator step"""
+
+    DATA_EXTRACTION = "data_extraction"
+    PREPROCESSING = "preprocessing"
+    FEDERATED = "federated"
+    CENTRAL = "central"
