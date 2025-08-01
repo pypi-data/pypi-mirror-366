2317dc1ed9ea9ff73881443345caa6a567cdb631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:06 +0000

    Add package finitelycomputable-helloworld-flask-morepath/25.7/finitelycomputable_helloworld_flask_morepath-25.7-py3-none-any.whl

---
+++ packages/finitelycomputable-helloworld-flask-morepath/finitelycomputable_helloworld_flask_morepath-25.7-py3-none-any.whl/finitelycomputable/helloworld_flask.py
@@ -0,0 +1,36 @@
+import flask
+from os import environ
+from posixpath import join
+
+from finitelycomputable import helloworld_morepath
+
+
+class Flask(flask.Flask):
+    '''this will have __module__ == finitelycomputable.helloworld_flask'''
+class Blueprint(flask.Blueprint):
+    '''this will have __module__ == finitelycomputable.helloworld_flask'''
+
+
+application = Flask(__name__)
+blueprint = Blueprint('helloworld', __name__)
+base_path = join('/', environ.get('BASE_PATH', ''))
+model = helloworld_morepath.Root()
+
+
+@blueprint.route('/')
+def hello_world():
+    return "Flask adapts, " + \
+            helloworld_morepath.hello_world(model, flask.request)
+
+application.register_blueprint(blueprint, url_prefix = base_path)
+
+def run():
+    from sys import argv, exit, stderr
+    if len(argv) < 2 or argv[1] != 'run':
+        stderr.write(f'usage: {argv[0]} run [port]\n')
+        exit(1)
+    try:
+        port=int(argv[2])
+    except IndexError:
+        port=8080
+    application.run(port=port)
