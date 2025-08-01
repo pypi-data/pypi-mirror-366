4fe8dc4703774b6578ea238568a987166e6ee5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:07:51 +0000

    Add package gitads-pypi/0.1.5/gitads_pypi-0.1.5-py3-none-any.whl

---
+++ packages/gitads-pypi/gitads_pypi-0.1.5-py3-none-any.whl/gitads_pypi/__init__.py
@@ -0,0 +1,11 @@
+"""
+gitads-pypi: A simple Hello World PyPI package
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+from .main import hello_world
+
+__all__ = ["hello_world"]
