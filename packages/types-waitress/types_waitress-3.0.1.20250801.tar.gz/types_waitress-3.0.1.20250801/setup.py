7b7a99cdf7cd63df9edc6c1f40e02db00eb4be36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:05:02 +0000

    Add package types-waitress/3.0.1.20250801/types_waitress-3.0.1.20250801.tar.gz

---
+++ packages/types-waitress/types_waitress-3.0.1.20250801.tar.gz/types_waitress-3.0.1.20250801/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'waitress-stubs': ['__init__.pyi', 'adjustments.pyi', 'buffers.pyi', 'channel.pyi', 'compat.pyi', 'parser.pyi', 'proxy_headers.pyi', 'receiver.pyi', 'rfc7230.pyi', 'runner.pyi', 'server.pyi', 'task.pyi', 'trigger.pyi', 'utilities.pyi', 'wasyncore.pyi', 'METADATA.toml', 'py.typed']})
