b26d2a9b86e92392328a880b1828f5036fac78d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:05:18 +0000

    Add package Tigor/0.0.6/tigor-0.0.6.tar.gz

---
+++ packages/Tigor/tigor-0.0.6.tar.gz/tigor-0.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='Tigor',
+  version='0.0.6',
+  #author='Tigrarururu',
+  #author_email='m777k02@yandex.ru',
+  #description='This library is designed to shorten the course as much as possible and avoid difficulties with various problems.',
+  #long_description=readme(),
+  #long_description_content_type='text/markdown',
+  #url='https://github.com/Tigrarururu',
+  #packages=find_packages(),
+  #install_requires=['requests>=2.25.1'],
+  #classifiers=[
+  #  'Programming Language :: Python :: 3.13',
+  #  'License :: OSI Approved :: MIT License',
+  #  'Operating System :: Microsoft :: Windows'
+  #],
+  #keywords='files speedfiles ',
+  #project_urls={
+  #  'GitHub': 'https://github.com/Tigrarururu'
+  #},
+  #python_requires='>=3.6'
+)
