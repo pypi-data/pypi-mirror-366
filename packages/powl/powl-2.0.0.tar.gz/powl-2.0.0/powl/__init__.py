5c5bff42cc032c4a5177b6a8c0d640ff4cac39d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 07:10:16 +0000

    Add package powl/2.0.0/powl-2.0.0.tar.gz

---
+++ packages/powl/powl-2.0.0.tar.gz/powl-2.0.0/powl/__init__.py
@@ -0,0 +1,4 @@
+from powl.main import import_event_log, discover, view, view_net, save_visualization, save_visualization_net, convert_to_petri_net, convert_to_bpmn, discover_from_partially_ordered_log
+
+__name__ = 'powl'
+__version__ = '2.0.0'
