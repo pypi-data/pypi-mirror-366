696e91d8f06b81fd2d915cd204758ba6da7f9816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 08:05:40 +0000

    Add package invenio-alma/0.13.4/invenio_alma-0.13.4-py2.py3-none-any.whl

---
+++ packages/invenio-alma/invenio_alma-0.13.4-py2.py3-none-any.whl/invenio_alma/config.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2025 Graz University of Technology.
+#
+# invenio-alma is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module to connect InvenioRDM to Alma."""
+
+ALMA_API_KEY = ""
+"""Default value for the Alma API key.
+
+The implemenation assumes that with the key it is only allowed to
+create records within Institution Zone (IZ). The Network Zone (NZ)
+would need higher permissions.
+
+With the IZ key it is only possible to change 98X fields within an
+existing record, but it is possible to create a totally new record.
+
+This value should be set on a place which is not under DVCS control.
+"""
+
+ALMA_API_HOST = ""
+"""Default value for the Alma API host.
+
+This value should be set on a place which is not under DVCS control.
+"""
+
+ALMA_REPOSITORY_RECORDS_IMPORT_FUNCS = {}
+"""Function to import a record from alma into the repository."""
+
+ALMA_ALMA_RECORDS_CREATE_AGGREGATORS = {}
+"""List of aggregators with following signature: aggregator() -> list[marc_id]."""
+
+ALMA_ALMA_RECORDS_CREATE_FUNCS = {}
+"""The function to create record in alma."""
+
+ALMA_REPOSITORY_RECORDS_UPDATE_AGGREGATORS = {}
+"""List of aggregators with following signature: aggregator() -> list[tuple[marc_id, alma_id]]."""
+
+ALMA_REPOSITORY_RECORDS_UPDATE_FUNCS = {}
+"""This is a callable to make the update process dependend on the workflow."""
+
+ALMA_USER_EMAIL = ""
+"""This is the email adress of the alma user in the repository."""
+
+ALMA_ERROR_MAIL_SENDER = ""
+"""This is the error mail sender."""
+
+ALMA_ERROR_MAIL_RECIPIENTS = []
+"""This is a list of recipients who should get the error message."""
+
+ALMA_CELERY_BEAT_SCHEDULE = {}
+"""Celery beat schedule for the theses import.
+
+This should be set on the InvenioRDM instance invenio.cfg file.
+"""
+
+ALMA_SRU_DOMAIN = ""
+"""SRU domain.
+
+This is the domain of the institution Alma SRU service.
+"""
+
+ALMA_SRU_INSTITUTION_CODE = ""
+"""SRU institution code.
+
+This is the code that acts as a reference for the institution in Alma.
+"""
