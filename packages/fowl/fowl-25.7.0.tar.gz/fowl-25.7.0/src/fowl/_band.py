4042e5627dbd5ed08bddc8aa4619561617e3e1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:49 +0000

    Add package fowl/25.7.0/fowl-25.7.0.tar.gz

---
+++ packages/fowl/fowl-25.7.0.tar.gz/fowl-25.7.0/src/fowl/_band.py
@@ -0,0 +1,17 @@
+
+
+#
+# testing some ideas around storing + visualizing "bandwidth" data ... maybe any time-series data
+#
+# a while ago i played with "mutli-time-scale" things, e.g. say a
+# 2-section one where the "oldest" section is 10x more time (or
+# whatever). So e.g. 10 data-points in a row visually "collect" into a
+# single roll-up (e.g. average .. or min/max) before proceeding
+# through the second one.
+#
+# ...need a lot of history to "see" that though, so having a test-driver for this is good.
+#
+# ...for the rust thing, a full "fake fowl, that always opens a
+# connection on ports you open" or something would be cool.
+#
+
