e14bcfc51aaf0b4aab9ff75dd0a44783c0d44fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 03:40:16 +0000

    Add package contact-test/0.1.46/contact_test-0.1.46-py3-none-any.whl

---
+++ packages/contact-test/contact_test-0.1.46-py3-none-any.whl/contact_test/__init__.py
@@ -0,0 +1,13 @@
+__author__ = "CONTACT Software GmbH"
+__email__ = "ptm-team@contact-software.com"
+
+def hello():
+    """A simple hello function for testing."""
+    return "Hello from dummy test package!"
+
+def main():
+    """Main function that can be called from command line."""
+    print(hello())
+
+if __name__ == "__main__":
+    main()
