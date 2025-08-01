8898e68ab5915818b856f3aa2bb1a0a26c628512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:05:02 +0000

    Add package types-keyboard/0.13.2.20250801/types_keyboard-0.13.2.20250801.tar.gz

---
+++ packages/types-keyboard/types_keyboard-0.13.2.20250801.tar.gz/types_keyboard-0.13.2.20250801/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'keyboard-stubs': ['__init__.pyi', '_canonical_names.pyi', '_generic.pyi', '_keyboard_event.pyi', '_mouse_event.pyi', 'mouse.pyi', 'METADATA.toml', 'py.typed']})
