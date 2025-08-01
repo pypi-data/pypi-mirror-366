fd88e5d27fd6d2257598b45bd28eba49d7d0ad43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 07:14:16 +0000

    Add package arcade-confluence/2.0.0/arcade_confluence-2.0.0-py3-none-any.whl

---
+++ packages/arcade-confluence/arcade_confluence-2.0.0-py3-none-any.whl/arcade_confluence/tools/cloud.py
@@ -0,0 +1,18 @@
+import asyncio
+from typing import Annotated
+
+import httpx
+from arcade_tdk import ToolContext, tool
+from arcade_tdk.auth import Atlassian
+
+from arcade_confluence.utils import get_atlassian_clouds
+
+
+# Note: The read:page:confluence scope is required to run a dummy API check to
+# determine whether the auth token is authorized for a given Atlassian Cloud.
+@tool(requires_auth=Atlassian(scopes=["read:page:confluence"]))
+async def get_available_atlassian_clouds(
+    context: ToolContext,
+) -> Annotated[dict[str, list[dict[str, str]]], "Available Atlassian Clouds"]:
+    """Get available Atlassian Clouds."""
+    return {"clouds": await get_atlassian_clouds(context)}
