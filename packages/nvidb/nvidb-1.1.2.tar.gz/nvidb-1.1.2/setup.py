554775b5488cd8e446e92d69e40f5a8e13d50141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:04:52 +0000

    Add package nvidb/1.1.2/nvidb-1.1.2.tar.gz

---
+++ packages/nvidb/nvidb-1.1.2.tar.gz/nvidb-1.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='nvidb',
+    version='1.1.2',
+    packages=find_packages(),
+    url="https://github.com/FanBB2333/nvidb/",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'pandas',
+        'paramiko',
+        'pytest',
+        'PyYAML',
+        'setuptools',
+        'termcolor',
+        'nvidia_ml_py',
+        'blessed'
+    ],
+    entry_points={
+        'console_scripts': [
+            'nvidb=nvidb.test.run:main',
+        ],
+    },
+)
