40732155342f2dae5c54db652155fa38518d59ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:05:02 +0000

    Add package types-colorama/0.4.15.20250801/types_colorama-0.4.15.20250801.tar.gz

---
+++ packages/types-colorama/types_colorama-0.4.15.20250801.tar.gz/types_colorama-0.4.15.20250801/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'colorama-stubs': ['__init__.pyi', 'ansi.pyi', 'ansitowin32.pyi', 'initialise.pyi', 'win32.pyi', 'winterm.pyi', 'METADATA.toml', 'py.typed']})
