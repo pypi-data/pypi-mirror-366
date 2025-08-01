509d7b4a6a0d77ff98bd17e083cfe6ff9342c85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:04:58 +0000

    Add package banguard-api/0.1.3/banguard_api-0.1.3.tar.gz

---
+++ packages/banguard-api/banguard_api-0.1.3.tar.gz/banguard_api-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='banguard-api',
+    version='0.1.3',
+    author='Zyrafaq',
+    author_email='contact@zyrafaq.com',
+    description='Python API wrapper for BanGuard API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/zyrafaq/BanGuard-API/',
+    packages=find_packages(),
+    license='GPL-3.0',
+    install_requires=[
+        'requests>=2.25.1',
+        'pydantic>=1.8.2',
+        'aiohttp>=3.11.2',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
