b85d95fc0adb6d1118669ba12c081e0cfdc3796d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 05:13:39 +0000

    Add package mqttconsumer/0.3.0/mqttconsumer-0.3.0.tar.gz

---
+++ packages/mqttconsumer/mqttconsumer-0.3.0.tar.gz/mqttconsumer-0.3.0/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This code facilitates the use of the MQTT protocol in analytics-related applications.
+It is based on [Eclipse Paho™ MQTT Python Client](https://github.com/eclipse/paho.mqtt.python).
---
+++ packages/mqttconsumer/mqttconsumer-0.3.0.tar.gz/mqttconsumer-0.3.0/mqttconsumer/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for mqttcomsumer."""
+
+__author__ = """Martin Trat, Simon Fritz"""
+__email__ = 'martin.trat@gmail.com, '
+__license__ = 'MIT'
+__version__ = '0.3.0'
+
+from .mqtt_consumer import MQTTConsumer
