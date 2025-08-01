de2f801a17aa1f849c5e57d8916e8b130c27b759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:07:22 +0000

    Add package gitads-test-package/0.0.2/gitads_test_package-0.0.2.tar.gz

---
+++ packages/gitads-test-package/gitads_test_package-0.0.2.tar.gz/gitads_test_package-0.0.2/README.md
@@ -0,0 +1,11 @@
+# GitAds Test Package
+
+A quick Python package to test GitAds integration on PyPI.
+
+## Usage
+
+```python
+from gitads_test_package import hello_gitads
+
+hello_gitads()
+<!-- GitAds-Verify: EQ36GV6LXYL24NEWOCXZI4RC9TVUIUXX -->
---
+++ packages/gitads-test-package/gitads_test_package-0.0.2.tar.gz/gitads_test_package-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gitads_test_package",
+    version="0.0.2",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple PyPI package to test GitAds integration.",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/yourusername/gitads_test_package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+)
