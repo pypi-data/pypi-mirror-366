0afec94901795974b2152ee69b0e007cf4068d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:04:50 +0000

    Add package lark-helper/0.1.0.dev9/lark_helper-0.1.0.dev9-py3-none-any.whl

---
+++ packages/lark-helper/lark_helper-0.1.0.dev9-py3-none-any.whl/lark_helper/exception.py
@@ -0,0 +1,10 @@
+class LarkResponseError(Exception):
+    """lark api response error"""
+
+    pass
+
+
+class ImportTaskError(Exception):
+    def __init__(self, job_status: int, job_error_msg: str):
+        self.job_status = job_status
+        self.job_error_msg = job_error_msg
