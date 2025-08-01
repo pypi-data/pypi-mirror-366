b4b1bfcce77420dd9159b829fafd9424a163105f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:06:42 +0000

    Add package arthur-common/2.1.53/arthur_common-2.1.53.tar.gz

---
+++ packages/arthur-common/arthur_common-2.1.53.tar.gz/arthur_common-2.1.53/src/arthur_common/models/datasets.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class ModelProblemType(str, Enum):
+    REGRESSION = "regression"
+    BINARY_CLASSIFICATION = "binary_classification"
+    ARTHUR_SHIELD = "arthur_shield"
+    CUSTOM = "custom"
+    MULTICLASS_CLASSIFICATION = "multiclass_classification"
+
+
+class DatasetFileType(str, Enum):
+    JSON = "json"
+    CSV = "csv"
+    PARQUET = "parquet"
+
+
+class DatasetJoinKind(str, Enum):
+    INNER = "inner"
+    LEFT_OUTER = "left_outer"
+    OUTER = "outer"
+    RIGHT_OUTER = "right_outer"
