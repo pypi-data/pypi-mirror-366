2c8efb8970e2cf954c19cdfa0128a4cc274a2191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:06:55 +0000

    Add package render50/9.2.10/render50-9.2.10.tar.gz

---
+++ packages/render50/render50-9.2.10.tar.gz/render50-9.2.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    author="CS50",
+    author_email="sysadmins@cs50.harvard.edu",
+    classifiers=[
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+        "Topic :: Utilities"
+    ],
+    description="This is render50, with which you can render source code as PDFs.",
+    install_requires=["backports.shutil_which", "braceexpand", "beautifulsoup4", "natsort", "Pygments>=2.7.1", "pypdf>=3.17.1", "requests", "six>=1.10.0", "termcolor", "WeasyPrint"],
+    keywords=["render", "render50"],
+    license="GPLv3",
+    long_description_content_type="text/markdown",
+    name="render50",
+    python_requires=">=3.9",
+    scripts=["render50"],
+    url="https://github.com/cs50/render50",
+    version="9.2.10"
+)
