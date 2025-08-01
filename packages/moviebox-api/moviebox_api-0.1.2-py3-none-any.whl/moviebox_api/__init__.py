8c5d7eeb1cb5b7b571713a010d7219b2f5c16a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:06:55 +0000

    Add package moviebox-api/0.1.2/moviebox_api-0.1.2-py3-none-any.whl

---
+++ packages/moviebox-api/moviebox_api-0.1.2-py3-none-any.whl/moviebox_api/__init__.py
@@ -0,0 +1,63 @@
+"""
+This package allows you to download movies
+and tv series from moviebox.ph and its mirror hosts.
+
+Right from performing `search` query down to downloading
+it in your desired quality.
+
+For instance:
+
+```python
+from moviebox_api import Auto
+
+async def main():
+    auto = Auto()
+    movie_saved_to, subtitle_saved_to = await auto.run("Avatar")
+    print(movie_saved_to, subtitle_saved_to, sep="\n")
+    # Output
+    # /.../Avatar - 1080P.mp4
+    # /.../Avatar - English.srt
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
+
+```
+"""
+
+from importlib import metadata
+import logging
+
+try:
+    __version__ = metadata.version("moviebox-api")
+except metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
+
+__author__ = "Smartwa"
+__repo__ = "https://github.com/Simatwa/moviebox-api"
+
+logger = logging.getLogger(__name__)
+
+from moviebox_api.core import Homepage, Search
+from moviebox_api.requests import Session
+from moviebox_api.download import (
+    MediaFileDownloader,
+    CaptionFileDownloader,
+    DownloadableMovieFilesDetail,
+    DownloadableSeriesFilesDetail,
+    resolve_media_file_to_be_downloaded,
+)
+from moviebox_api.extra.movies import Auto
+
+__all__ = [
+    "Auto",
+    "Homepage",
+    "Search",
+    "Session",
+    "MediaFileDownloader",
+    "CaptionFileDownloader",
+    "DownloadableMovieFilesDetail",
+    "DownloadableSeriesFilesDetail",
+    "resolve_media_file_to_be_downloaded",
+]
---
+++ packages/moviebox-api/moviebox_api-0.1.2-py3-none-any.whl/moviebox_api/constants.py
@@ -0,0 +1,110 @@
+"""This module store constant variables"""
+
+import os
+import typing as t
+from moviebox_api import logger
+from enum import IntEnum, StrEnum
+
+mirror_hosts = (
+    "moviebox.ng",
+    "h5.aoneroom.com",
+    "movieboxapp.in",
+    "moviebox.pk",
+    "moviebox.ph",
+    "moviebox.id",
+)
+"""Mirror domains/subdomains of Moviebox"""
+
+ENVIRONMENT_HOST_KEY = "MOVIEBOX_API_HOST"
+"""User declares host to use as environment variable using this key"""
+
+selected_host = (
+    os.getenv(ENVIRONMENT_HOST_KEY) or mirror_hosts[0]
+)  # TODO: Choose the right value based on working status
+"""Host adress only with protocol"""
+
+host_protocol = "https"
+"""Host protocol i.e http/https"""
+
+host_url = f"{host_protocol}://{selected_host}/"
+"""Complete host adress with protocol"""
+
+logger.info(f"Moviebox host url - {host_url}")
+
+default_request_headers = {
+    "X-Client-Info": '{"timezone":"Africa/Nairobi"}',  # TODO: Set this value dynamically.
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0",
+    "Referer": host_url,  # "https://moviebox.ng/movies/titanic-kGoZgiDdff?id=206379412718240440&scene&page_from=search_detail&type=%2Fmovie%2Fdetail",
+    "Host": selected_host,
+    # "X-Source": "",
+}
+"""For general http requests other than download"""
+
+download_request_headers = {
+    "Accept": "*/*",  # "video/webm,video/ogg,video/*;q=0.9,application/ogg;q=0.7,audio/*;q=0.6,*/*;q=0.5",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0",
+    "Origin": selected_host,
+    "Referer": host_url,
+}
+"""For media and subtitle files download requests"""
+
+
+downloadQualitiesType: t.TypeAlias = t.Literal[
+    "WORST", "BEST", "360P", "480P", "720P", "1080P"
+]  # TODO: Add more qualities
+
+DOWNLOAD_QUALITIES = (
+    "WORST",
+    "BEST",
+    "360P",
+    "480P",
+    "720P",
+    "1080P",
+)  # TODO: Add more qualities
+
+
+DEFAULT_CAPTION_LANGUAGE = "English"
+
+DEFAULT_SHORT_CAPTION_LANGUAGE = "en"
+
+
+class SubjectType(IntEnum):
+    """Content types mapped to their integer representatives"""
+
+    ALL = 0
+    """Both Movies, series and music contents"""
+    MOVIES = 1
+    """Movies content only"""
+    TV_SERIES = 2
+    """TV Series content only"""
+    MUSIC = 6
+    """Music contents only"""
+
+    @classmethod
+    def map(cls) -> dict[str, int]:
+        """Content-type names mapped to their int representatives"""
+        resp = {}
+        for entry in cls:
+            resp[entry.name] = entry.value
+        return resp
+
+
+class DownloadMode(StrEnum):
+    START = "start"
+    RESUME = "resume"
+    AUTO = "auto"
+
+    @classmethod
+    def map(cls) -> dict[str, str]:
+        """Modes mapped to their string representatives"""
+        resp = {}
+        for entry in cls:
+            resp[entry.name] = entry.value
+        return resp
+
+    @classmethod
+    def map_cls(cls):
+        """Names in lower case mapped to their values"""
+        return {"start": cls.START, "resume": cls.RESUME, "auto": cls.AUTO}
