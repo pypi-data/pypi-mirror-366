d5b6686cf49e8ae296b22b6f5da3fdb862091d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:04:41 +0000

    Add package irispy2/3.4.2/irispy2-3.4.2.tar.gz

---
+++ packages/irispy2/irispy2-3.4.2.tar.gz/irispy2-3.4.2/irispy2/bot/_internal/iris.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass
+import requests
+from loguru import logger
+import typing as t
+import base64
+
+
+@dataclass
+class IrisRequest:
+    msg: str
+    room: str
+    sender: str
+    raw: dict
+
+
+class IrisAPI:
+    def __init__(self, iris_endpoint: str):
+        self.iris_endpoint = iris_endpoint
+
+    def __parse(self, res: requests.Response) -> dict:
+        try:
+            data: dict = res.json()
+        except Exception:
+            raise Exception(f"Iris 응답 JSON 파싱 오류: {res.text}")
+
+        if not 200 <= res.status_code <= 299:
+            logger.debug(f"Iris 오류: {res}")
+            raise Exception(f"Iris 오류: {data.get('message', '알 수 없는 오류')}")
+
+        return data
+
+    def reply(self, room_id: int, msg: str):
+        res = requests.post(
+            f"{self.iris_endpoint}/reply",
+            json={"type": "text", "room": str(room_id), "data": str(msg)},
+        )
+        return self.__parse(res)
+
+    def reply_media(
+        self,
+        room_id: int,
+        type: t.Literal["IMAGE"],
+        files: list[bytes],
+    ):
+        if type != "IMAGE":
+            raise Exception("지원하지 않는 타입입니다")
+
+        res = requests.post(
+            f"{self.iris_endpoint}/reply",
+            json={
+                "type": "image_multiple",
+                "room": str(room_id),
+                "data": list(map(lambda v: base64.b64encode(v).decode(), files)),
+            },
+        )
+        return self.__parse(res)
+
+    def decrypt(self, enc: int, b64_ciphertext: str, user_id: int) -> str | None:
+        res = requests.post(
+            f"{self.iris_endpoint}/decrypt",
+            json={"enc": enc, "b64_ciphertext": b64_ciphertext, "user_id": user_id},
+        )
+
+        res = self.__parse(res)
+        return res.get("plain_text")
+
+    def query(self, query: str, bind: list[t.Any] | None = None) -> list[dict]:
+        res = requests.post(
+            f"{self.iris_endpoint}/query", json={"query": query, "bind": bind or []}
+        )
+        res = self.__parse(res)
+        return res.get("data", [])
+
+    def get_info(self):
+        res = requests.get(f"{self.iris_endpoint}/config")
+        return self.__parse(res)
---
+++ packages/irispy2/irispy2-3.4.2.tar.gz/irispy2-3.4.2/irispy2/bot/models.py
@@ -0,0 +1,98 @@
+from dataclasses import dataclass
+import io
+import traceback
+import typing as t
+from irispy2.bot._internal.iris import IrisAPI
+from loguru import logger
+
+
+@dataclass
+class Message:
+    id: int
+    type: int
+    msg: str
+    attachment: str
+    v: dict
+
+
+@dataclass
+class Room:
+    id: int
+    name: str
+
+
+@dataclass
+class User:
+    id: int
+    name: str
+
+
+@dataclass(init=False)
+class ChatContext:
+    room: Room
+    sender: User
+    message: Message
+    raw: dict
+
+    def __init__(
+        self,
+        room: Room,
+        sender: User,
+        message: Message,
+        raw: dict,
+        api: IrisAPI,
+        enabled_invisible_prefix: bool,
+    ):
+        self.__api = api
+        self.room = room
+        self.sender = sender
+        self.message = message
+        self.raw = raw
+        self.enabled_invisible_prefix = enabled_invisible_prefix
+
+    def reply(self, message: str, room_id=None):
+        if room_id is None:
+            room_id = self.room.id
+
+        if self.enabled_invisible_prefix:
+            message = "\u200b" + message
+
+        try:
+            self.__api.reply(room_id, message)
+        except Exception as e:
+            logger.error(f"reply 오류: {e}")
+
+    def reply_media(
+        self,
+        mediaType: t.Literal["IMAGE"],
+        files: list[t.IO[bytes] | bytes],
+        room_id=None,
+    ):
+        if room_id is None:
+            room_id = self.room.id
+
+        if mediaType != "IMAGE":
+            raise Exception("지원하지 않는 타입입니다")
+
+        def convert(e: t.IO[bytes] | bytes):
+            if isinstance(e, io.BufferedIOBase):
+                return e.read()
+            elif isinstance(e, bytes):
+                return e
+            else:
+                raise Exception(f"알 수 없는 타입입니다 {type(e)}")
+
+        try:
+            mfiles: list[bytes] = list(map(convert, files))
+            self.__api.reply_media(room_id, mediaType, mfiles)
+        except Exception as e:
+            traceback.print_exc()
+            logger.error(f"reply_media 오류: {e}")
+
+
+@dataclass
+class ErrorContext:
+    event: str
+    func: t.Callable
+    exception: Exception
+    args: list[t.Any]
