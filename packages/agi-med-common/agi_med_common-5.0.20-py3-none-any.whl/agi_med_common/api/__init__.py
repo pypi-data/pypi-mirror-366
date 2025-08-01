ce7e4ce534b4b26f5840af3e2c1d25dd63dc60f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 08:06:27 +0000

    Add package agi-med-common/5.0.20/agi_med_common-5.0.20-py3-none-any.whl

---
+++ packages/agi-med-common/agi_med_common-5.0.20-py3-none-any.whl/agi_med_common/api/__init__.py
@@ -0,0 +1,7 @@
+from .chat_manager_api import ChatManagerAPI
+from .content_interpreter_api import ContentInterpreterAPI, Interpretation, ResourceId
+from .content_interpreter_remote_api import ContentInterpreterRemoteAPI
+from .text_generator_api import TextGeneratorAPI
+from .text_processor_api import TextProcessorAPI
+from .classifier_api import ClassifierAPI
+from .critic_api import CriticAPI
