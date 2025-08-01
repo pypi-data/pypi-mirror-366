25258d26b64c2542376ee5cf286bd26230868df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:53 +0000

    Add package btype/0.1.7/btype-0.1.7-py3-none-any.whl

---
+++ packages/btype/btype-0.1.7-py3-none-any.whl/btype/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2021 by Phase Advanced Sensor Systems, Inc.
+from .btype import (int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t,
+                    int64_t, uint64_t, float32_t, float64_t, Array, CString,
+                    Struct)
+
+
+__all__ = ['Array',
+           'CString',
+           'Struct',
+
+           'float32_t',
+           'float64_t',
+           'int8_t',
+           'uint8_t',
+           'int16_t',
+           'uint16_t',
+           'int32_t',
+           'uint32_t',
+           'int64_t',
+           'uint64_t',
+           ]
