ab7181b0c45013b564a4bff4a288eecffb2895c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:07 +0000

    Add package finitelycomputable-helloworld-cherrypy-falcon/25.7/finitelycomputable_helloworld_cherrypy_falcon-25.7.tar.gz

---
+++ packages/finitelycomputable-helloworld-cherrypy-falcon/finitelycomputable_helloworld_cherrypy_falcon-25.7.tar.gz/finitelycomputable_helloworld_cherrypy_falcon-25.7/finitelycomputable/helloworld_cherrypy.py
@@ -0,0 +1,38 @@
+import cherrypy
+from os import environ
+from falcon import Response
+
+from finitelycomputable import helloworld_falcon
+
+
+class HelloWorld(object):
+    impl = helloworld_falcon.HelloWorld()
+
+    @cherrypy.expose
+    def index(self):
+        resp = Response()
+        self.impl.on_get(cherrypy.request, resp)
+        return "CherryPy adapts, " + resp.text
+
+
+def setup_server():
+    # because CherryPy and only Cherrypy needs to instantiate its own singletons
+    cherrypy.tree.__module__ = __name__
+    cherrypy.tree.mount(HelloWorld(), base_path, {'/': {}})
+    return cherrypy.tree
+
+base_path = environ.get('BASE_PATH', '')
+application = setup_server()
+
+
+def run():
+    from sys import argv, exit, stderr
+    if len(argv) < 2 or argv[1] != 'run':
+        stderr.write(f'usage: {argv[0]} run [port]\n')
+        exit(1)
+    try:
+        cherrypy.config.update({'server.socket_port': int(argv[2])})
+    except IndexError:
+        pass
+    cherrypy.engine.start()
+    cherrypy.engine.block()
