fa5597f42bd591ac964560595e5401352d16b933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:06:41 +0000

    Add package glgrpa/0.0.86/glgrpa-0.0.86-py3-none-any.whl

---
+++ packages/glgrpa/glgrpa-0.0.86-py3-none-any.whl/glgrpa/__init__.py
@@ -0,0 +1,15 @@
+from .Chrome import Chrome
+from .Windows import Windows
+from .Terminal import Terminal
+from .ARCA import ARCA
+from .Excel import Excel
+from .Email import Email
+from .Sharepoint import Sharepoint
+from .SAP import SAP
+from .ControlEjecucion import ControlEjecucion
+
+# Aplicativos ARCA
+from .aplicativos.CartasDePorteElectronicas import CartasDePorteElectronicas
+
+# Transacciones
+from .transacciones.OB08 import OB08
