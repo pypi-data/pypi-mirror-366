9bc9708c7083e5436a4036cfc643c2d45628cd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 11:05:44 +0000

    Add package samplehc/0.4.0/samplehc-0.4.0-py3-none-any.whl

---
+++ packages/samplehc/samplehc-0.4.0-py3-none-any.whl/samplehc/types/v2/task_retrieve_response.py
@@ -0,0 +1,18 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, Optional
+from typing_extensions import Literal
+
+from ..._models import BaseModel
+
+__all__ = ["TaskRetrieveResponse"]
+
+
+class TaskRetrieveResponse(BaseModel):
+    id: str
+
+    output: Optional[Dict[str, object]] = None
+
+    state: Optional[Dict[str, object]] = None
+
+    status: Literal["running", "waiting-dependencies", "suspended", "completed", "failed", "cancelled"]
