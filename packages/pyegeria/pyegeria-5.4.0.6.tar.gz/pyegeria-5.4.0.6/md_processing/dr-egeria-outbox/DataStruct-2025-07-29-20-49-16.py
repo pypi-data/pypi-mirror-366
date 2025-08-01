fcc3a2a46a60eddbee8cb4480668c8fabda6522a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 06:07:41 +0000

    Add package pyegeria/5.4.0.6/pyegeria-5.4.0.6.tar.gz

---
+++ packages/pyegeria/pyegeria-5.4.0.6.tar.gz/pyegeria-5.4.0.6/md_processing/dr-egeria-outbox/DataStruct-2025-07-29-20-49-16.py
@@ -0,0 +1,8 @@
+[
+    {
+        "Display Name": "",
+        "Qualified Name": " [DataStruct::TBDF-Incoming Weekly Measurement Data](#3e4bc9a1-2661-4d24-a753-b26060c61e34) ",
+        "Category": "",
+        "Description": "This describes the weekly measurement data for each patient for the Teddy Bear drop foot clinical trial."
+    }
+]
