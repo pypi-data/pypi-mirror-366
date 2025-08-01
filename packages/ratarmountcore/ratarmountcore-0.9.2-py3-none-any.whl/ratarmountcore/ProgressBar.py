98d7cb4185a155d326fe51037895d6b2879811f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:07:25 +0000

    Add package ratarmountcore/0.9.2/ratarmountcore-0.9.2-py3-none-any.whl

---
+++ packages/ratarmountcore/ratarmountcore-0.9.2-py3-none-any.whl/ratarmountcore/ProgressBar.py
@@ -0,0 +1,44 @@
+import time
+
+
+class ProgressBar:
+    """Simple progress bar which keeps track of changes and prints the progress and a time estimate."""
+
+    def __init__(self, maxValue: float):
+        # Do not use thread_time here even if it is twice as fast because it does not count the time the thread
+        # has been sleeping, which it does when it waits for I/O. This made the times incorrect for slow HDD accesses!
+        self._get_time = time.time
+        self.maxValue = maxValue
+        self.lastUpdateTime = self._get_time()
+        self.lastUpdateValue = 0.0
+        self.updateInterval = 2.0  # seconds
+        self.creationTime = self._get_time()
+
+    def update(self, value: float) -> None:
+        """Should be called whenever the monitored value changes. The progress bar is updated accordingly."""
+        if self.lastUpdateTime is not None and (self._get_time() - self.lastUpdateTime) < self.updateInterval:
+            return
+
+        # Use whole interval since start to estimate time
+        percent = value / self.maxValue if self.maxValue != 0 else 1.0
+        totalTime = self._get_time() - self.creationTime
+        eta1 = int(totalTime / percent - totalTime if percent != 0 else 0)
+        # Use only a shorter window interval to estimate time.
+        # Accounts better for higher speeds in beginning, e.g., caused by caching effects.
+        # However, this estimate might vary a lot while the other one stabilizes after some time!
+        if value == self.lastUpdateValue:
+            eta2 = 99999 * 60
+        else:
+            eta2 = int(
+                (self._get_time() - self.lastUpdateTime) / (value - self.lastUpdateValue) * (self.maxValue - value)
+            )
+        print(
+            f"Position {value} of {self.maxValue} ({percent * 100.0:.2f}%). "
+            f"Remaining time: {eta2 // 60} min {eta2 % 60} s (current rate), "
+            f"{eta1 // 60} min {eta1 % 60} s (average rate). "
+            f"Spent time: {int(totalTime) // 60} min {int(totalTime) % 60} s",
+            flush=True,
+        )
+
+        self.lastUpdateTime = self._get_time()
+        self.lastUpdateValue = value
---
+++ packages/ratarmountcore/ratarmountcore-0.9.2-py3-none-any.whl/ratarmountcore/mountsource/compositing/subvolumes.py
@@ -0,0 +1,142 @@
+import os
+from typing import IO, Dict, Iterable, List, Optional, Tuple, Union
+
+from ratarmountcore.mountsource import FileInfo, MountSource, create_root_file_info
+from ratarmountcore.utils import overrides
+
+
+class SubvolumesMountSource(MountSource):
+    def __init__(self, mountSources: Dict[str, MountSource], printDebug: int = 0) -> None:
+        """
+        mountSources : List of mount sources to mount as subfolders.
+        """
+        self.mountSources: Dict[str, MountSource] = mountSources
+        self.printDebug = printDebug
+
+        for name in self.mountSources:
+            if not name:
+                raise ValueError("Mount source names may not be empty!")
+            if '/' in name:
+                raise ValueError(f"Mount source names may not contain slashes! ({name})")
+
+        self.rootFileInfo = create_root_file_info(userdata=[None])
+
+    def _find_mount_source(self, path: str) -> Optional[Tuple[str, str]]:
+        path = path.strip('/')
+        subvolume, subpath = path.split('/', maxsplit=1) if '/' in path else [path, ""]
+        return (subvolume, '/' + subpath) if subvolume in self.mountSources else None
+
+    @overrides(MountSource)
+    def is_immutable(self) -> bool:
+        return all(m.is_immutable() for m in self.mountSources.values())
+
+    @overrides(MountSource)
+    def lookup(self, path: str, fileVersion: int = 0) -> Optional[FileInfo]:
+        path = path.strip('/')
+        if not path:
+            return self.rootFileInfo.clone()
+
+        result = self._find_mount_source(path)
+        if result is None:
+            return None
+        subvolume, subpath = result
+
+        fileInfo = self.mountSources[subvolume].lookup(subpath, fileVersion=fileVersion)
+        if isinstance(fileInfo, FileInfo):
+            fileInfo.userdata.append(subvolume)
+            return fileInfo
+
+        return None
+
+    @overrides(MountSource)
+    def versions(self, path: str) -> int:
+        path = path.strip('/')
+        if not path:
+            return 1
+
+        result = self._find_mount_source(path)
+        if result is None:
+            return 0
+        subvolume, subpath = result
+
+        return self.mountSources[subvolume].versions(subpath)
+
+    def _list(self, path: str, onlyMode: bool):
+        path = path.strip('/')
+        if not path:
+            return dict.fromkeys(self.mountSources.keys(), self.rootFileInfo.mode if onlyMode else self.rootFileInfo)
+
+        result = self._find_mount_source(path)
+        if result is None:
+            return None
+        subvolume, subpath = result
+
+        return (
+            self.mountSources[subvolume].list_mode(subpath) if onlyMode else self.mountSources[subvolume].list(subpath)
+        )
+
+    @overrides(MountSource)
+    def list(self, path: str) -> Optional[Union[Iterable[str], Dict[str, FileInfo]]]:
+        return self._list(path, onlyMode=False)
+
+    @overrides(MountSource)
+    def list_mode(self, path: str) -> Optional[Union[Iterable[str], Dict[str, int]]]:
+        return self._list(path, onlyMode=True)
+
+    @overrides(MountSource)
+    def open(self, fileInfo: FileInfo, buffering=-1) -> IO[bytes]:
+        subvolume = fileInfo.userdata.pop()
+        if subvolume is None:
+            raise ValueError(f"Found subvolume is None for fileInfo: {fileInfo}")
+        try:
+            return self.mountSources[subvolume].open(fileInfo, buffering=buffering)
+        finally:
+            fileInfo.userdata.append(subvolume)
+
+    @overrides(MountSource)
+    def read(self, fileInfo: FileInfo, size: int, offset: int) -> bytes:
+        subvolume = fileInfo.userdata.pop()
+        if subvolume is None:
+            raise ValueError(f"Found subvolume is None for fileInfo: {fileInfo}")
+        try:
+            return self.mountSources[subvolume].read(fileInfo, size, offset)
+        finally:
+            fileInfo.userdata.append(subvolume)
+
+    @overrides(MountSource)
+    def list_xattr(self, fileInfo: FileInfo) -> List[str]:
+        subvolume = fileInfo.userdata.pop()
+        try:
+            return [] if subvolume is None else self.mountSources[subvolume].list_xattr(fileInfo)
+        finally:
+            fileInfo.userdata.append(subvolume)
+
+    @overrides(MountSource)
+    def get_xattr(self, fileInfo: FileInfo, key: str) -> Optional[bytes]:
+        subvolume = fileInfo.userdata.pop()
+        try:
+            return None if subvolume is None else self.mountSources[subvolume].get_xattr(fileInfo, key)
+        finally:
+            fileInfo.userdata.append(subvolume)
+
+    @overrides(MountSource)
+    def get_mount_source(self, fileInfo: FileInfo) -> Tuple[str, MountSource, FileInfo]:
+        sourceFileInfo = fileInfo.clone()
+        subvolume = sourceFileInfo.userdata.pop()
+
+        if subvolume is None or subvolume not in self.mountSources:
+            return '/', self, fileInfo
+        mountSource = self.mountSources[subvolume]
+
+        subpath, subMountSource, subFileInfo = mountSource.get_mount_source(sourceFileInfo)
+        return os.path.normpath(f"/{subvolume}/{subpath}"), subMountSource, subFileInfo
+
+    @overrides(MountSource)
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        for mountSource in self.mountSources.values():
+            mountSource.__exit__(exception_type, exception_value, exception_traceback)
+
+    def join_threads(self):
+        for mountSource in self.mountSources:
+            if hasattr(mountSource, 'join_threads'):
+                mountSource.join_threads()
