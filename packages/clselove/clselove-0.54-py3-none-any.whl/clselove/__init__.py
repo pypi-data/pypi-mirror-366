4bc636a4124ba2359707aaabbd9f0c36d87a3923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 08:06:20 +0000

    Add package clselove/0.54/clselove-0.54-py3-none-any.whl

---
+++ packages/clselove/clselove-0.54-py3-none-any.whl/clselove/__init__.py
@@ -0,0 +1,19 @@
+
+from .clselove import (
+    auto,
+    get_phone,
+    get_sms,
+    scr,
+    totel_temp,
+    geetest,
+    se,
+    na_em,
+    cap_se,
+    web,
+    se_chr,
+    sign,
+    cap_ph
+)
+
+__version__ = "0.6"
+__author__ = "CodeMaster"
