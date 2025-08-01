466b36d15630f0a7587e27699cf3a952b00deb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 08:05:52 +0000

    Add package typed-api-response/1.0.0/typed_api_response-1.0.0-py3-none-any.whl

---
+++ packages/typed-api-response/typed_api_response-1.0.0-py3-none-any.whl/typed_api_response/errors.py
@@ -0,0 +1,10 @@
+
+
+class ApiResponseBuilderError(Exception):
+    """Raised when the ApiResponseBuilder fails to build the response"""
+    pass
+
+
+class ApiPayloadBuilderError(Exception):
+    """Raised when the ApiPayloadBuilder fails to build the payload"""
+    pass
