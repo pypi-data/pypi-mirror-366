9878a76c110f8c33020e235980c6f86b5b9f66f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 05:13:18 +0000

    Add package power-grid-model/1.11.38/power_grid_model-1.11.38.tar.gz

---
+++ packages/power-grid-model/power_grid_model-1.11.38.tar.gz/power_grid_model-1.11.38/power_grid_model_c/power_grid_model/include/power_grid_model/main_core/calculation_info.hpp
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+#include "../common/calculation_info.hpp"
+#include "../common/common.hpp"
+#include "../common/timer.hpp"
+
+namespace power_grid_model::main_core {
+
+inline CalculationInfo merge_calculation_info(std::vector<CalculationInfo> const& infos) {
+    CalculationInfo result;
+
+    auto const key = Timer::make_key(2226, "Max number of iterations");
+    for (auto const& info : infos) {
+        for (auto const& [k, v] : info) {
+            if (k == key) {
+                result[k] = std::max(result[k], v);
+            } else {
+                result[k] += v;
+            }
+        }
+    }
+
+    return result;
+}
+
+} // namespace power_grid_model::main_core
