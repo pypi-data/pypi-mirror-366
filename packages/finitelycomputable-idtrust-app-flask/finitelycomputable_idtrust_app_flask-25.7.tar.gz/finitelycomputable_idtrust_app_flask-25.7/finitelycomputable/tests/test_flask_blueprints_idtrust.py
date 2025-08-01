79f638c989cd048f79534fee56b51fe975726f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 15:04:39 +0000

    Add package finitelycomputable-idtrust-app-flask/25.7/finitelycomputable_idtrust_app_flask-25.7.tar.gz

---
+++ packages/finitelycomputable-idtrust-app-flask/finitelycomputable_idtrust_app_flask-25.7.tar.gz/finitelycomputable_idtrust_app_flask-25.7/finitelycomputable/tests/test_flask_blueprints_idtrust.py
@@ -0,0 +1,61 @@
+import re
+import pytest
+
+from finitelycomputable import flask_blueprints
+
+
+@pytest.fixture
+def client():
+        flask_blueprints.application.config['TESTING'] = True
+
+        with flask_blueprints.application.test_client() as client:
+            yield client
+
+
+def test_idtrust_in_index(client):
+    response = client.get('/')
+    assert 200 == response.status_code
+    assert re.search('finitelycomputable-idtrust-app-flask', response.text)
+    assert re.search('finitelycomputable.flask_blueprints', response.text)
+
+def test_idtrust_in_env_info(client):
+    response = client.get('/env_info/')
+    assert 200 == response.status_code
+    assert re.search('finitelycomputable.idtrust_flask', response.text)
+    assert re.search('finitelycomputable.flask_blueprints', response.text)
+
+def test_identification_of_trust_home_blind(client):
+    response = client.get('/identification_of_trust')
+    assert 200 == response.status_code
+    assert re.search(
+        '<button type="submit" name="user_intent" value="Trust">Trust</button>',
+        response.text
+    )
+    assert re.search(
+        '<button type="submit" name="user_intent" value="Distrust">Distrust</button>',
+        response.text
+    )
+    assert re.search(
+        '<a href="/identification_of_trust/choose_miscommunication">',
+        response.text)
+
+def test_identification_of_trust_home_reveal(client):
+    response = client.get(
+            '/identification_of_trust/choose_miscommunication')
+    assert 200 == response.status_code
+    assert re.search(
+        '<button type="submit" name="user_intent" value="Trust">Trust</button>',
+        response.text
+    )
+    assert re.search(
+        '<button type="submit" name="user_intent" value="Distrust">Distrust</button>',
+        response.text
+    )
+    assert re.search(
+        '<input type="range" min="0" max="1" .* name="user_miscommunication"/>',
+        response.text
+    )
+    assert re.search(
+        '<input type="range" min="0" max="1" .* name="foil_miscommunication"/>',
+        response.text
+    )
