dc295f0c8ef4342bb073e91a4698ce158901a3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 11:05:30 +0000

    Add package runwayml/3.8.0/runwayml-3.8.0-py3-none-any.whl

---
+++ packages/runwayml/runwayml-3.8.0-py3-none-any.whl/runwayml/resources/__init__.py
@@ -0,0 +1,89 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .tasks import (
+    TasksResource,
+    AsyncTasksResource,
+    TasksResourceWithRawResponse,
+    AsyncTasksResourceWithRawResponse,
+    TasksResourceWithStreamingResponse,
+    AsyncTasksResourceWithStreamingResponse,
+)
+from .organization import (
+    OrganizationResource,
+    AsyncOrganizationResource,
+    OrganizationResourceWithRawResponse,
+    AsyncOrganizationResourceWithRawResponse,
+    OrganizationResourceWithStreamingResponse,
+    AsyncOrganizationResourceWithStreamingResponse,
+)
+from .text_to_image import (
+    TextToImageResource,
+    AsyncTextToImageResource,
+    TextToImageResourceWithRawResponse,
+    AsyncTextToImageResourceWithRawResponse,
+    TextToImageResourceWithStreamingResponse,
+    AsyncTextToImageResourceWithStreamingResponse,
+)
+from .video_upscale import (
+    VideoUpscaleResource,
+    AsyncVideoUpscaleResource,
+    VideoUpscaleResourceWithRawResponse,
+    AsyncVideoUpscaleResourceWithRawResponse,
+    VideoUpscaleResourceWithStreamingResponse,
+    AsyncVideoUpscaleResourceWithStreamingResponse,
+)
+from .image_to_video import (
+    ImageToVideoResource,
+    AsyncImageToVideoResource,
+    ImageToVideoResourceWithRawResponse,
+    AsyncImageToVideoResourceWithRawResponse,
+    ImageToVideoResourceWithStreamingResponse,
+    AsyncImageToVideoResourceWithStreamingResponse,
+)
+from .character_performance import (
+    CharacterPerformanceResource,
+    AsyncCharacterPerformanceResource,
+    CharacterPerformanceResourceWithRawResponse,
+    AsyncCharacterPerformanceResourceWithRawResponse,
+    CharacterPerformanceResourceWithStreamingResponse,
+    AsyncCharacterPerformanceResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "TasksResource",
+    "AsyncTasksResource",
+    "TasksResourceWithRawResponse",
+    "AsyncTasksResourceWithRawResponse",
+    "TasksResourceWithStreamingResponse",
+    "AsyncTasksResourceWithStreamingResponse",
+    "ImageToVideoResource",
+    "AsyncImageToVideoResource",
+    "ImageToVideoResourceWithRawResponse",
+    "AsyncImageToVideoResourceWithRawResponse",
+    "ImageToVideoResourceWithStreamingResponse",
+    "AsyncImageToVideoResourceWithStreamingResponse",
+    "TextToImageResource",
+    "AsyncTextToImageResource",
+    "TextToImageResourceWithRawResponse",
+    "AsyncTextToImageResourceWithRawResponse",
+    "TextToImageResourceWithStreamingResponse",
+    "AsyncTextToImageResourceWithStreamingResponse",
+    "VideoUpscaleResource",
+    "AsyncVideoUpscaleResource",
+    "VideoUpscaleResourceWithRawResponse",
+    "AsyncVideoUpscaleResourceWithRawResponse",
+    "VideoUpscaleResourceWithStreamingResponse",
+    "AsyncVideoUpscaleResourceWithStreamingResponse",
+    "CharacterPerformanceResource",
+    "AsyncCharacterPerformanceResource",
+    "CharacterPerformanceResourceWithRawResponse",
+    "AsyncCharacterPerformanceResourceWithRawResponse",
+    "CharacterPerformanceResourceWithStreamingResponse",
+    "AsyncCharacterPerformanceResourceWithStreamingResponse",
+    "OrganizationResource",
+    "AsyncOrganizationResource",
+    "OrganizationResourceWithRawResponse",
+    "AsyncOrganizationResourceWithRawResponse",
+    "OrganizationResourceWithStreamingResponse",
+    "AsyncOrganizationResourceWithStreamingResponse",
+]
