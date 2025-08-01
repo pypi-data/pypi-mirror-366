2b0b8315f6db63bcc381848c49dce54220ee5905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:04:49 +0000

    Add package xztools/0.1/xztools-0.1.tar.gz

---
+++ packages/xztools/xztools-0.1.tar.gz/xztools-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='xztools',
+    version='0.1',
+    packages=['xztools'],
+    description='A fun library to generate random compliments!',
+    author='Her Name',
+    author_email='her_email@example.com',
+)
---
+++ packages/xztools/xztools-0.1.tar.gz/xztools-0.1/xztools/my_functions.py
@@ -0,0 +1,4 @@
+def random_compliment():
+    compliments = ["You're awesome!", "You light up the room!", "You're a coding star!"]
+    import random
+    return random.choice(compliments)
