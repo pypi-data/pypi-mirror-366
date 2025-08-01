8be144a3db3ab52ba6139692e3be3a2f140e351c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 14:06:51 +0000

    Add package ecpet/2.0.0b32/ecpet-2.0.0b32.tar.gz

---
+++ packages/ecpet/ecpet-2.0.0b32.tar.gz/ecpet-2.0.0b32/ecpet/_version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module stub that keeps the one centraliced variable that gives
+the current version number.
+
+It is read by multiple build scripts, too.
+"""
+
+__title__ = 'ecpet'
+__release__ = '2.0.0-beta32'
+
