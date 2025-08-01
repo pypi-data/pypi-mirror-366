1efa211680a040318ab79206c6fe50d95f8023be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 08:06:04 +0000

    Add package nominal-api/0.799.0/nominal_api-0.799.0.tar.gz

---
+++ packages/nominal-api/nominal_api-0.799.0.tar.gz/nominal_api-0.799.0/nominal_api/security_api_workspace/__init__.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+from .._impl import (
+    security_api_workspace_RemoveType as RemoveType,
+    security_api_workspace_UpdateOrRemoveWorkspaceDisplayName as UpdateOrRemoveWorkspaceDisplayName,
+    security_api_workspace_UpdateOrRemoveWorkspaceDisplayNameVisitor as UpdateOrRemoveWorkspaceDisplayNameVisitor,
+    security_api_workspace_UpdateOrRemoveWorkspaceSymbol as UpdateOrRemoveWorkspaceSymbol,
+    security_api_workspace_UpdateOrRemoveWorkspaceSymbolVisitor as UpdateOrRemoveWorkspaceSymbolVisitor,
+    security_api_workspace_UpdateWorkspaceRequest as UpdateWorkspaceRequest,
+    security_api_workspace_Workspace as Workspace,
+    security_api_workspace_WorkspaceService as WorkspaceService,
+    security_api_workspace_WorkspaceSymbol as WorkspaceSymbol,
+    security_api_workspace_WorkspaceSymbolVisitor as WorkspaceSymbolVisitor,
+)
+
+__all__ = [
+    'RemoveType',
+    'UpdateOrRemoveWorkspaceDisplayName',
+    'UpdateOrRemoveWorkspaceDisplayNameVisitor',
+    'UpdateOrRemoveWorkspaceSymbol',
+    'UpdateOrRemoveWorkspaceSymbolVisitor',
+    'UpdateWorkspaceRequest',
+    'Workspace',
+    'WorkspaceSymbol',
+    'WorkspaceSymbolVisitor',
+    'WorkspaceService',
+]
+
