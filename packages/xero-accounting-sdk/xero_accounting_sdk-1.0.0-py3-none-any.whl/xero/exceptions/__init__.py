722b517d60d1212b8f78d27fc9a3d7d82e6d5fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-01 16:05:11 +0000

    Add package xero-accounting-sdk/1.0.0/xero_accounting_sdk-1.0.0-py3-none-any.whl

---
+++ packages/xero-accounting-sdk/xero_accounting_sdk-1.0.0-py3-none-any.whl/xero/exceptions/__init__.py
@@ -0,0 +1,6 @@
+__all__ = [
+    'api_exception',
+    'validation_error_some_data_was_incorrect_returns_response_of_type_error_exception',
+    'a_failed_request_due_to_validation_error_exception',
+    'o_auth_provider_exception',
+]
