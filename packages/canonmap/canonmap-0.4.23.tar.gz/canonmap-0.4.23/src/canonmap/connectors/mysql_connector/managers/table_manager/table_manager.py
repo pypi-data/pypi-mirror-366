3a6f4667168968a130e7aa854308f94a2d07e5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:05:50 +0000

    Add package canonmap/0.4.23/canonmap-0.4.23.tar.gz

---
+++ packages/canonmap/canonmap-0.4.23.tar.gz/canonmap-0.4.23/src/canonmap/connectors/mysql_connector/managers/table_manager/table_manager.py
@@ -0,0 +1,29 @@
+# src/canonmap/connectors/mysql_connector/managers/table_manager/table_manager.py
+
+import logging
+import pandas as pd
+from pathlib import Path
+
+from canonmap.connectors.mysql_connector.mysql_connector import MySQLConnector
+from canonmap.connectors.mysql_connector.managers.table_manager.validators.requests import CreateTableRequest, DropTableRequest
+from canonmap.connectors.mysql_connector.managers.table_manager.utils.create_table import create_table_util
+from canonmap.connectors.mysql_connector.managers.table_manager.utils.create_table_from_data import create_table_from_data_util
+from canonmap.connectors.mysql_connector.managers.table_manager.utils.drop_table import drop_table_util
+from canonmap.connectors.mysql_connector.managers.table_manager.utils.split_large_table import split_large_table_util, get_table_split_recommendation
+
+logger = logging.getLogger(__name__)
+
+
+class TableManager:
+    def __init__(self, connector: MySQLConnector):
+        self.connector = connector
+
+    def create_table(self, create_table_request: CreateTableRequest) -> dict:
+        if create_table_request.data is not None:
+            return create_table_from_data_util(create_table_request, self.connector, create_table_request.data)
+        else:
+            return create_table_util(create_table_request, self.connector)
+    
+    def drop_table(self, drop_table_request: DropTableRequest) -> None:
+        return drop_table_util(drop_table_request, self.connector)
+        
