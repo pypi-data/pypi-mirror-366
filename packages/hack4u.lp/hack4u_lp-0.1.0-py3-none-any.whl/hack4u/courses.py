d51bfb69892e1daa3417eadad13ea2fc93eb23a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:08:55 +0000

    Add package hack4u.lp/0.1.0/hack4u_lp-0.1.0-py3-none-any.whl

---
+++ packages/hack4u.lp/hack4u_lp-0.1.0-py3-none-any.whl/hack4u/courses.py
@@ -0,0 +1,28 @@
+class Course:
+    def __init__(self, name, duration, link):
+        self.name = name
+        self.duration = duration
+        self.link = link
+
+    def __repr__(self):
+        return f"{self.name} [{self.duration} horas] ({self.link})"
+
+
+courses = [
+    Course("Introducción a Linux", 15, "https://hack4u.io/cursos/introduccion-a-linux/"),
+    Course("Personalización de entorno en Linux", 3, "https://hack4u.io/cursos/personalizacion-de-entorno-en-linux/"),
+    Course("Introducción al Hacking", 53, "https://hack4u.io/cursos/introduccion-al-hacking/"),
+    Course("Python Ofensivo", 35, "https://hack4u.io/cursos/python-ofensivo/"),
+    Course("Hacking Web", 51, "https://hack4u.io/cursos/hacking-web/")
+]
+
+def list_courses():
+    for course in courses:
+        print(course)
+
+def search_course_by_name(name):
+    for course in courses:
+        if course.name == name:
+            return course 
+
+    return None
