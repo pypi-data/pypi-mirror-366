1f6d52cb8cb94d6797c66f3eba6cc9a39379cabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:45 +0000

    Add package tclogger/1.7/tclogger-1.7-py3-none-any.whl

---
+++ packages/tclogger/tclogger-1.7-py3-none-any.whl/tclogger/types.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from typing import Union
+
+KeyType = Union[str, int]
+KeysType = Union[KeyType, list[KeyType]]
+
+PathType = Union[str, Path]
+PathsType = Union[PathType, list[PathType]]
+
+StrsType = Union[str, list[str]]
