3f7d0e79e0b3f40f014462fdc554828e24f8e3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:43 +0000

    Add package yta-video-manim/0.0.5/yta_video_manim-0.0.5.tar.gz

---
+++ packages/yta-video-manim/yta_video_manim-0.0.5.tar.gz/yta_video_manim-0.0.5/src/yta_video_manim/settings.py
@@ -0,0 +1,10 @@
+MIN_ANIMATION_DURATION = 1
+"""
+The minimum number of seconds we accept as a
+parameter to build a manim animation.
+"""
+MAX_ANIMATION_DURATION = 120
+"""
+The maximum number of seconds we accept as a
+parameter to build a manim animation.
+"""
