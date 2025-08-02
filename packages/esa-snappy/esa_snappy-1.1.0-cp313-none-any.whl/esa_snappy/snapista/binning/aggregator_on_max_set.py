099a821adedd87530ad639ebb69735c23ed4636f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:35 +0000

    Add package esa-snappy/1.1.0/esa_snappy-1.1.0-cp313-none-any.whl

---
+++ packages/esa-snappy/esa_snappy-1.1.0-cp313-none-any.whl/esa_snappy/snapista/binning/aggregator_on_max_set.py
@@ -0,0 +1,47 @@
+from attrs import asdict, define, field
+import lxml.etree as etree
+
+
+@define
+class AggregatorOnMaxSet(object):
+
+    type = field(init=False, default="ON_MAX_SET")
+    targetName = field()
+    onMaxVarName = field()
+    setVarNames = field(default=None)
+
+    def __str__(self):
+
+        self.type = "ON_MAX_SET"
+
+        return self.__repr__()
+
+    def __repr__(self):
+
+        self.type = "ON_MAX_SET"
+
+        return "AggregatorOnMaxSet({})".format(
+            ", ".join(
+                ["{}='{}'".format(key, value) for key, value in self.to_dict().items()]
+            )
+        )
+
+    def to_dict(self):
+
+        self.type = "ON_MAX_SET"
+
+        return asdict(self)
+
+    def to_xml(self):
+
+        self.type = "ON_MAX_SET"
+
+        root = etree.Element("aggregator")
+
+        for key, value in self.to_dict().items():
+
+            elem = etree.SubElement(root, key)
+
+            elem.text = str(value)
+
+        return root
