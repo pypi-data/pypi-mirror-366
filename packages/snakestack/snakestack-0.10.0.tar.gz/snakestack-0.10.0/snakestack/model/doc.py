65468288bd9166babc65ab7c98f4c6a3676809ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:40 +0000

    Add package snakestack/0.10.0/snakestack-0.10.0.tar.gz

---
+++ packages/snakestack/snakestack-0.10.0.tar.gz/snakestack-0.10.0/snakestack/model/doc.py
@@ -0,0 +1,87 @@
+from http import HTTPStatus
+from typing import Any, Type
+
+from pydantic import BaseModel
+
+from .error import ErrorModel
+
+ResponseDoc = dict[int | str, dict[str, Any]]
+
+
+def make_standard_response(
+    status_code: int,
+    *,
+    model: Type[BaseModel],
+    example: dict[str, Any],
+    content_type: str = "application/json",
+    override_description: str | None = None,
+) -> ResponseDoc:
+    status = HTTPStatus(status_code)
+    if not any([
+        status.is_success,
+        status.is_redirection,
+        status.is_informational,
+        status.is_server_error,
+        status.is_server_error]
+    ):
+        raise ValueError(f"Unsupported status code: {status_code}")
+    result = {
+        "model": model,
+        "description": override_description or status.description,
+        "content": {
+            content_type: {
+                "example": example
+            }
+        },
+    }
+
+    if status_code == 204:
+        result.pop("model")
+        result.pop("content")
+
+    return {
+        status_code: result
+    }
+
+
+def make_standard_error_response(
+    status_code: int,
+    *,
+    model: Type[BaseModel] | None = None,
+    content_type: str = "application/json",
+    example: dict[str, Any] | None = None,
+    override_description: str | None = None,
+) -> ResponseDoc:
+    """
+    Gera uma resposta OpenAPI com modelo e exemplo opcional.
+    """
+    model = model or ErrorModel
+    example = example or ErrorModel(detail="Error").model_dump()
+    return make_standard_response(
+        status_code=status_code,
+        model=model or ErrorModel,
+        override_description=override_description,
+        example=example,
+        content_type=content_type
+    )
+
+
+def make_standard_success_response(
+    status_code: int,
+    model: Type[BaseModel],
+    *,
+    description: str | None = None,
+    example: dict[str, Any] | None = None,
+    content_type: str = "application/json"
+) -> ResponseDoc:
+    """
+    Gera uma resposta OpenAPI com modelo e exemplo opcional.
+    """
+    example = example or {"foo": "bar"}
+    return make_standard_response(
+        status_code=status_code,
+        model=model,
+        override_description=description,
+        example=example,
+        content_type=content_type
+    )
