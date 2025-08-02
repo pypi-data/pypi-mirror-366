e40860b236c6d90b77faa8fedd67e208ce8159be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:05:27 +0000

    Add package count-pdf-page/0.1.0/count_pdf_page-0.1.0.tar.gz

---
+++ packages/count-pdf-page/count_pdf_page-0.1.0.tar.gz/count_pdf_page-0.1.0/src/count_pdf_page/__init__.py
@@ -0,0 +1,15 @@
+"""
+Count PDF Page - A Python package for counting pages in PDF files.
+
+This package provides utilities to count pages in PDF files and generate
+markdown reports with detailed statistics.
+"""
+
+__version__ = "0.1.0"
+__author__ = "AaronOET"
+__email__ = "aaronhsu@mail.ntou.edu.tw"
+
+from .core import count_pdf_pages, generate_markdown_report
+from .cli import main
+
+__all__ = ["count_pdf_pages", "generate_markdown_report", "main"]
