a30e71ba66e317eb867d75cd8a7f12397f26794d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:16:17 +0000

    Add package elluminate/0.5.2/elluminate-0.5.2.tar.gz

---
+++ packages/elluminate/elluminate-0.5.2.tar.gz/elluminate-0.5.2/elluminate/schemas/base.py
@@ -0,0 +1,14 @@
+from typing import Generic, Sequence, TypeVar
+
+from pydantic import BaseModel
+
+# Generic type variable for the result type
+TResult = TypeVar("TResult", bound=BaseModel | Sequence[BaseModel])
+
+
+class BatchCreateStatus(BaseModel, Generic[TResult]):
+    status: str
+    result: list[TResult] | None
+    percent: int
+    progress_msg: str | None = None
+    error_msg: str | None = None
