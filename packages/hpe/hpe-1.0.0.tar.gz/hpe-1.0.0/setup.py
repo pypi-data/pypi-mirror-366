e2f80ef21de01d0e448c9503983ac775e1a414c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:37 +0000

    Add package hpe/1.0.0/hpe-1.0.0.tar.gz

---
+++ packages/hpe/hpe-1.0.0.tar.gz/hpe-1.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hpe",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "customtkinter==5.2.2",
+        "darkdetect==0.8.0",
+        "llvmlite==0.44.0",
+        "numba==0.61.2",
+        "numpy==2.2.6",
+        "packaging==25.0",
+        "pillow==11.3.0",
+        "pygame==2.6.1",
+        "PyOpenGL==3.1.9",
+        "pyopengltk==0.0.4",
+        "toml==0.10.2",
+        "trimesh==4.7.1"
+    ],
+    entry_points={
+        "console_scripts": [
+            "hpe=hpe.cli:main",
+        ],
+    },
+    author="XO Aria, S.U.P.E",
+    author_email="hf18950@gmail.com, hamedsheygh@gmail.com",
+    description="Hamid Py Engine",
+    license="MIT",
+    package_data={
+        "hpe": ["code/hpe.py"],
+    },
+    python_requires=">=3.8",
+)
