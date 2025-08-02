47ad001b384532115d9feb2e87ddeb169b59ff94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:55 +0000

    Add package collibra-connector/1.0.10rc0/collibra_connector-1.0.10rc0-py3-none-any.whl

---
+++ packages/collibra-connector/collibra_connector-1.0.10rc0-py3-none-any.whl/collibra_connector/api/__init__.py
@@ -0,0 +1,17 @@
+from .Asset import Asset
+from .Base import BaseAPI
+from .Community import Community
+from .Domain import Domain
+from .User import User
+from .Responsibility import Responsibility
+from .Workflow import Workflow
+from .Metadata import Metadata
+from .Comment import Comment
+from .Relation import Relation
+from .Exceptions import (
+    CollibraAPIError,
+    UnauthorizedError,
+    ForbiddenError,
+    NotFoundError,
+    ServerError
+)
