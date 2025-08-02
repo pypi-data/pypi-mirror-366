c0bbfc25cb5544d40e21814837264789c124acb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:05:38 +0000

    Add package spyndex/0.7.1/spyndex-0.7.1-py3-none-any.whl

---
+++ packages/spyndex/spyndex-0.7.1-py3-none-any.whl/spyndex/__init__.py
@@ -0,0 +1,9 @@
+"""spyndex - Awesome Spectral Indices in Python"""
+
+__version__ = "0.7.1"
+__author__ = "David Montero Loaiza <dml.mont@gmail.com>"
+__all__ = []
+
+from . import datasets, plot
+from .axioms import bands, constants, indices
+from .spyndex import *
