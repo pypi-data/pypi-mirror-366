6eb6c397e87d1e719513dc53e5712ca112944b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:15:53 +0000

    Add package spVIPES/0.1.0/spvipes-0.1.0.tar.gz

---
+++ packages/spVIPES/spvipes-0.1.0.tar.gz/spvipes-0.1.0/README.md
@@ -0,0 +1,80 @@
+# spVIPES
+
+[![Tests][badge-tests]][link-tests]
+
+<!-- [![Documentation][badge-docs]][link-docs] -->
+
+[badge-tests]: https://img.shields.io/github/actions/workflow/status/nrclaudio/spVIPES/test.yaml?branch=main
+[link-tests]: https://github.com/nrclaudio/spVIPES/actions/workflows/test.yml
+
+<!-- [badge-docs]: https://img.shields.io/readthedocs/spVIPES -->
+
+Shared-private Variational Inference with Product of Experts and Supervision
+
+## Getting started
+
+Please refer to the [documentation][link-docs]. In particular, the
+
+-   [API documentation][link-api].
+
+## Installation
+
+You need to have Python 3.9 or newer installed on your system.
+
+### PyTorch installation
+
+We strongly recommend using spVIPES with GPU acceleration. In Linux, check your NVIDIA drivers running:
+
+```bash
+nvidia-smi
+```
+
+You can then install a compatible PyTorch version from https://pytorch.org/get-started/previous-versions/. For example if your CUDA drivers are version 11.3, you should install PyTorch v1.12.1 with the following command:
+
+```bash
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+```
+
+Make sure your torch installation can see your CUDA device to take full advantage of GPU acceleration by running:
+
+```python
+import torch
+
+torch.cuda.is_available()
+```
+
+This should return `True` if everything is installed correctly.
+
+### spVIPES installation
+
+To install spVIPES:
+
+1. Install the latest release of `spVIPES` from `PyPI <https://pypi.org/project/spVIPES/>`\_:
+
+```bash
+pip install spVIPES
+```
+
+2. Install the latest development version:
+
+```bash
+pip install git+https://github.com/nrclaudio/spVIPES.git@main
+```
+
+## Tutorials
+
+To get started, please refer to the [basic spVIPES tutorial](docs/notebooks/Tutorial.ipynb).
+
+## Contact
+
+If you found a bug, please use the [issue tracker][issue-tracker].
+
+## Citation
+
+> t.b.a
+
+[scverse-discourse]: https://discourse.scverse.org/
+[issue-tracker]: https://github.com/nrclaudio/spVIPES/issues
+[changelog]: https://spVIPES.readthedocs.io/latest/changelog.html
+[link-docs]: https://spVIPES.readthedocs.io
+[link-api]: https://spVIPES.readthedocs.io/latest/api.html
---
+++ packages/spVIPES/spvipes-0.1.0.tar.gz/spvipes-0.1.0/src/spVIPES/data/prepare_adatas.py
@@ -0,0 +1,60 @@
+from typing import Optional, Union
+
+import anndata as ad
+import numpy as np
+
+
+def prepare_adatas(
+    adatas: dict[str, ad.AnnData],
+    layers: Optional[list[list[Union[str, None]]]] = None,
+):
+    """Concatenate all the input anndata objects.
+    :param adatas:
+        Dict of AnnData objects
+    """
+    groups_obs_names = []
+    groups_obs = {}
+    groups_lengths = {}
+    groups_var_names = {}  # Changed to dictionary
+    groups_mapping = {}
+    if len(adatas) != 2:
+        raise ValueError("Currently only 2 groups are supported")
+
+    for i, (groups, adata) in enumerate(adatas.items()):
+        if adata is not None:
+            groups_lengths[i] = adata.shape[1]
+            groups_obs_names.append(adata.obs_names)
+            if groups_obs.get(groups, None) is None:
+                groups_obs[groups] = adata.obs.copy()
+                groups_obs[groups].loc[:, "group"] = groups
+
+            else:
+                cols_to_use = adata.obs.columns.difference(groups_obs[groups].columns)
+                groups_obs[groups] = groups_obs[groups].join(adata.obs[cols_to_use])
+              # Store var_names for each group
+            adata.obs["groups"] = groups
+            adata.var_names = f"{groups}_" + adata.var_names
+            groups_var_names[groups] = adata.var_names
+            groups_mapping[i] = groups
+
+    multigroups_adata = ad.concat(adatas, join="outer", label="groups", index_unique="-")
+    multigroups_adata.uns["groups_var_indices"] = [
+        np.where(multigroups_adata.var_names.str.startswith(k))[0] for k in adatas.keys()
+    ]
+    multigroups_adata.uns["groups_obs_indices"] = [
+        np.where(multigroups_adata.obs["groups"].str.startswith(k))[0] for k in adatas.keys()
+    ]
+    multigroups_adata.uns["groups_obs_names"] = groups_obs_names
+    multigroups_adata.uns["groups_obs"] = groups_obs
+    multigroups_adata.uns["groups_lengths"] = groups_lengths
+    multigroups_adata.uns["groups_var_names"] = groups_var_names
+    multigroups_adata.uns["groups_mapping"] = groups_mapping
+    
+    # Create indices column
+    indices = []
+    for group, group_indices in zip(adatas.keys(), multigroups_adata.uns["groups_obs_indices"]):
+        group_size = len(group_indices)
+        indices.extend(np.arange(group_size, dtype=np.int32))
+    multigroups_adata.obs["indices"] = indices
+
+    return multigroups_adata
