9d95546a39702c8e2fa39b24ad8a1c9d69872837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:15:44 +0000

    Add package assonant/2.4.5/assonant-2.4.5-py3-none-any.whl

---
+++ packages/assonant/assonant-2.4.5-py3-none-any.whl/assonant/naming_standards/experimental_technique.py
@@ -0,0 +1,47 @@
+"""Expermiental Technique names definitions."""
+
+from enum import Enum
+
+
+class ExperimentalTechniquesAcronyms(Enum):
+    """Experimental Technique acronyms standards."""
+
+    ARPES = "ARPES"
+    CD = "CD"
+    CT = "CT"
+    PDF = "PDF"
+    PEEM = "PEEM"
+    RIXS = "RIXS"
+    SAXS = "SAXS"
+    USAXS = "USAXS"
+    WAXS = "WAXS"
+    XAS = "XAS"
+    XES = "XES"
+    XEOL = "XEOL"
+    XMCD = "XMCD"
+    XPS = "XPS"
+    XPCS = "XPCS"
+    XRD = "XRD"
+    XRF = "XRF"
+
+
+class ExperimentalTechniquesFullNames(Enum):
+    """Experimental Technique full name standards."""
+
+    ARPES = "Angle-Resolved Photoemission Spectroscopy"
+    CD = "Circular Dichroism Spectroscopy"
+    CT = "Computed Tomography"
+    PDF = "Pair Distribution Function"
+    PEEM = "Photoemission Electron Microscopy"
+    RIXS = "Resonant Inelastic X-ray Scattering"
+    SAXS = "Small Angle X-ray Scattering"
+    USAXS = "Ultra-Small Angle X-ray Scattering"
+    WAXS = "Wide-Angle X-ray Scattering"
+    XAS = "X-ray Absortion Spectroscopy"
+    XES = "X-ray Emission Spectroscopy"
+    XEOL = "X-ray Excited Optical Luminescence"
+    XMCD = "X-ray Magnetic Circular Dichroism"
+    XPS = "X-ray Photonelectron Spectroscopy"
+    XPCS = "X-ray Photon Correlation Spectroscopy"
+    XRD = "X-ray Diffraction"
+    XRF = "X-ray Fluorescence"
