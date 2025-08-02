40b2af44f7a9b48dac64bf41e6be70db766dc8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:37 +0000

    Add package xyzzy3/0.1.0/xyzzy3-0.1.0.tar.gz

---
+++ packages/xyzzy3/xyzzy3-0.1.0.tar.gz/xyzzy3-0.1.0/README.md
@@ -0,0 +1,5 @@
+﻿# xyzzy3
+
+This is a placeholder package to reserve the name xyzzy3 and prevent dependency confusion attacks.
+
+Do not install or use this package.
---
+++ packages/xyzzy3/xyzzy3-0.1.0.tar.gz/xyzzy3-0.1.0/setup.py
@@ -0,0 +1,13 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="xyzzy3",
+    version="0.1.0",
+    description="Reserved package name to prevent dependency confusion attacks.",
+    long_description="This package exists only to reserve the name 'xyzzy3'.",
+    long_description_content_type="text/plain",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
