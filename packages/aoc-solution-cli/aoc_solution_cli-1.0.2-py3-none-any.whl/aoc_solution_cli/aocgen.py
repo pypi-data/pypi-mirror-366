5d55461676e3b17d5cc797ef1c86c5ab1a248190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:16:03 +0000

    Add package aoc-solution-cli/1.0.2/aoc_solution_cli-1.0.2-py3-none-any.whl

---
+++ packages/aoc-solution-cli/aoc_solution_cli-1.0.2-py3-none-any.whl/aoc_solution_cli/aocgen.py
@@ -0,0 +1,26 @@
+# Copyright (C) taitep <taitep@taitep.se>
+#
+# This file is licensed under the GNU LGPL v3.
+# See <https://www.gnu.org/licenses/>
+
+import click
+from .recent_problem_saver import get_recent, day_to_module
+
+@click.command
+@click.option('--day', type=int)
+@click.option('--year', type=int)
+def aocgen(day, year):
+    day, year = get_recent(day, year)
+    file = f"{day_to_module(day, year)}.py"
+    with open(file, 'w') as f:
+        f.write(f'''\
+def solution_a(data):
+    pass
+
+def solution_b(data):
+    pass
+''')
+
+if __name__ == '__main__':
+    aocgen()
+
---
+++ packages/aoc-solution-cli/aoc_solution_cli-1.0.2-py3-none-any.whl/aoc_solution_cli/recent_problem_saver.py
@@ -0,0 +1,33 @@
+# Copyright (C) taitep <taitep@taitep.se>
+#
+# This file is licensed under the GNU LGPL v3.
+# See <https://www.gnu.org/licenses/>
+
+import collections
+import aocd
+
+AoCDay = collections.namedtuple('AoCDay', ['day', 'year'])
+
+def get_recent(day=None, year=None, file='.recent_problem'):
+    try:
+        with open(file, 'r') as f:
+                saved_year, saved_day = tuple([int(i) for i in f.read().strip().split(' ')])
+    except FileNotFoundError:
+        saved_day, saved_year = None, None
+    
+    if not day: day = saved_day
+    if not year: year = saved_year
+
+    if not day or not year:
+        aocd_day, aocd_year = aocd.get_day_and_year()
+        if not day: day = aocd_day
+        if not year: year = aocd_year
+
+    if (day, year) != (saved_day, saved_year):
+        with open(file, 'w') as f:
+            f.write(f"{year} {day}")
+    
+    return AoCDay(day, year)
+
+
+day_to_module = lambda day, year: f'aoc_{year}_{day}'
