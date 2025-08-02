ebdaef6fbda402c13e37e2eab3b1c77929d2c45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 14:11:48 +0000

    Add package FernetKeyVault/1.0.1/fernetkeyvault-1.0.1.tar.gz

---
+++ packages/FernetKeyVault/fernetkeyvault-1.0.1.tar.gz/fernetkeyvault-1.0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Setup script for the FernetKeyVault package.
+"""
+
+from setuptools import setup, find_packages
+
+# Read the content of README.md for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="FernetKeyVault",
+    version="1.0.1",
+    author="Rajarajan Veerichetty",
+    author_email="rajarajan.v@gmail.com",
+    description="A simple Python SQLite3-based key-value storage vault",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kvcrajan/FernetKeyVault",
+    project_urls={
+        "Bug Tracker": "https://github.com/kvcrajan/FernetKeyVault/issues",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
