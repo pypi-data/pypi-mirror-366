f177a6253e1fdeaa5fdbd52496ccabc1db37a056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 16:06:17 +0000

    Add package yta-video-manim/0.0.8/yta_video_manim-0.0.8.tar.gz

---
+++ packages/yta-video-manim/yta_video_manim-0.0.8.tar.gz/yta_video_manim-0.0.8/src/yta_video_manim/animations/classes/test/test_timeline_manim_animation.py
@@ -0,0 +1,126 @@
+"""
+This class is to make complex testings.
+"""
+from yta_video_manim.animations.base_manim_animation import BaseManimAnimation
+from yta_video_manim.animations.base_manim_animation_wrapper import BaseManimAnimationWrapper
+from yta_constants.manim import ManimRenderer, ManimAnimationType
+from yta_programming.output import Output
+from yta_constants.file import FileExtension
+from manim import *
+from typing import Union
+
+
+class TestTimelineManimAnimationWrapper(BaseManimAnimationWrapper):
+    """
+    Using a timeline. Just testing.
+    """
+
+    __test__ = False
+
+    def __init__(
+        self,
+    ):
+        # TODO: This parameter validation could be done
+        # by our specific parameter validator (under
+        # construction yet)
+        exception_messages = []
+
+        if len(exception_messages) > 0:
+            raise Exception('\n'.join([exception_message for exception_message in exception_messages]))
+        
+        super().__init__(TestTimelineManimAnimationGenerator, ManimAnimationType.GENERAL)
+
+class TestTimelineManimAnimationGenerator(BaseManimAnimation):
+
+    __test__ = False
+
+    def construct(self):
+        """
+        This method is called by manim when executed by shell and
+        will call the scene animation render method to be processed
+        and generated.
+        """
+        self.animate()
+
+    def generate(
+        self,
+        parameters: dict,
+        output_filename: Union[str, None] = None
+    ):
+        """
+        Build the manim animation video and stores it
+        locally as the provided 'output_filename', 
+        returning the final output filename.
+
+        This method will call the '.animate()' method
+        to build the scene with the instructions that
+        are written in that method.
+        """
+        output_filename = Output.get_filename(output_filename, FileExtension.MOV)
+
+        return super().generate(
+            parameters,
+            renderer = ManimRenderer.CAIRO,
+            output_filename = output_filename
+        )
+    
+    def animate(self):
+        """
+        This code will generate the manim animation and belongs to the
+        Scene manim object.
+        """
+        from yta_video_manim.animations.timeline import ManimTimeline, ManimAnimationOnTimeline
+
+        timeline = ManimTimeline()
+        square = Square().shift(LEFT * 2)
+        square.generate_target()
+        square.target.shift(RIGHT * 4)
+        animation_one = ManimAnimationOnTimeline(1, 3, MoveToTarget(square))
+
+        circle = Circle().shift(RIGHT * 2)
+        circle.generate_target()
+        circle.target.scale(1.5)
+        animation_two = ManimAnimationOnTimeline(2, 6, MoveToTarget(circle))
+
+        timeline.add_animation(animation_one).add_animation(animation_two)
+        self.play(AnimationGroup(*timeline.compiled_animations, lag_ratio = 0))
+
+        
+
+        return
+
+        square = Square().shift(LEFT * 2)
+        circle = Circle().shift(RIGHT * 2)
+
+        self.add(square, circle)
+
+        square.generate_target()
+        square.target.shift(RIGHT * 4)
+
+        circle.generate_target()
+        circle.target.scale(1.5)
+
+        animation_one = ManimAnimationOnTimeline(1, 3, MoveToTarget(square))
+        animation_two = ManimAnimationOnTimeline(2, 4, MoveToTarget(circle))
+
+        animations_on_timeline = [
+            animation_one,
+            animation_two
+        ]
+
+        timeline_end_time = max(
+            animation_on_timeline.t_end
+            for animation_on_timeline in animations_on_timeline
+        )
+        
+        animations = []
+        for animation_on_timeline in animations_on_timeline:
+            #self.compile_animations
+            full_animation = Succession(
+                Wait(animation_on_timeline.t_start),
+                animation_on_timeline.animation.set_run_time(animation_on_timeline.duration),
+                Wait(timeline_end_time - animation_on_timeline.t_end),
+            )
+            animations.append(full_animation)
+
+        self.play(AnimationGroup(*animations, lag_ratio = 0))
---
+++ packages/yta-video-manim/yta_video_manim-0.0.8.tar.gz/yta_video_manim-0.0.8/src/yta_video_manim/animations/timeline/__init__.py
@@ -0,0 +1,113 @@
+from yta_validation.parameter import ParameterValidator
+from manim import Wait, Succession
+
+
+# TODO: This class is for testing, but if
+# definitive, it must me moved to another
+# module
+class ManimAnimationOnTimeline:
+    """
+    Class to wrap the information about an animation
+    that will be set in a timeline to handle when it
+    must be played.
+    """
+
+    @property
+    def duration(
+        self
+    ) -> float:
+        """
+        The duration of the animation.
+        """
+        return self.t_end - self.t_start
+
+    def __init__(
+        self,
+        t_start: float,
+        t_end: float,
+        animation: any
+    ):
+        ParameterValidator.validate_mandatory_positive_number('t_start', t_start, do_include_zero = True)
+        ParameterValidator.validate_mandatory_positive_number('t_end', t_end, do_include_zero = False)
+
+        self.t_start: float = t_start
+        """
+        The time moment (in seconds) in which the animation
+        must start being played.
+        """
+        self.t_end: float = t_end
+        """
+        The time moment (in seconds) in which the animation
+        must stop being played.
+        """
+        self.animation = animation
+
+"""
+Example of code using it:
+
+circle = Circle().shift(RIGHT * 2)
+self.add(circle)
+circle.generate_target()
+circle.target.scale(1.5)
+animation_one = ManimAnimationOnTimeline(1, 3, MoveToTarget(square))
+"""
+
+class ManimTimeline:
+    """
+    Class to represent the timeline of a whole
+    animation in Manim, able to store different
+    mobjects and animations on it with their
+    time moments and peculiarities.
+    """
+
+    @property
+    def t_end(
+        self
+    ) -> float:
+        """
+        The time moment (in seconds) in which the last
+        animation of the timeline must stop being played.
+        """
+        return max(
+            animation.t_end
+            for animation in self.animations
+        )
+    
+    @property
+    def compiled_animations(
+        self
+    ) -> list[Succession]:
+        """
+        A list of Succession instances that are the
+        different animations prepared to be played
+        with the next code:
+        
+        - `self.play(AnimationGroup(*animations, lag_ratio = 0))`
+        """
+        return [
+            Succession(
+                Wait(anim.t_start),
+                anim.animation.set_run_time(anim.duration),
+                Wait(self.t_end - anim.t_end),
+            )
+            for anim in self.animations
+        ]
+
+    def __init__(
+        self
+    ):
+        self.animations: list[ManimAnimationOnTimeline] = []
+
+    def add_animation(
+        self,
+        animation: ManimAnimationOnTimeline
+    ) -> 'ManimTimeline':
+        """
+        Add the provided 'animation' to the internal
+        list of animations that must be displayed.
+        """
+        ParameterValidator.validate_mandatory_instance_of('animation', animation, ManimAnimationOnTimeline)
+
+        self.animations.append(animation)
+
+        return self
