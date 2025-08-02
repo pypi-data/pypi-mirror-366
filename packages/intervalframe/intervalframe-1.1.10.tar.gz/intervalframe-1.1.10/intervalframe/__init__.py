4101011d9b6d4090f425b282db474b20aac755e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:25 +0000

    Add package intervalframe/1.1.10/intervalframe-1.1.10.tar.gz

---
+++ packages/intervalframe/intervalframe-1.1.10.tar.gz/intervalframe-1.1.10/intervalframe/__init__.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from .core.IntervalFrame import IntervalFrame
+from .core.IntervalSeries import IntervalSeries
+from .write import write_h5
+from .read import read_h5, read_text
+
+# This is extracted automatically by the top-level setup.py.
+__version__ = '1.1.10'
+__author__ = "Kyle S. Smith"
+
+
+__doc__ = """\
+API
+======
+
+Basic class
+-----------
+
+.. autosummary::
+   :toctree: .
+   
+   IntervalFrame
+   IntervalSeries
+    
+"""
