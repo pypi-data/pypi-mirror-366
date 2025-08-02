e9ceb08a36d1be1862a34dd826cdbcb854d4fbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 15:05:22 +0000

    Add package visivo/1.0.70/visivo-1.0.70-py3-none-any.whl

---
+++ packages/visivo/visivo-1.0.70-py3-none-any.whl/visivo/commands/init.py
@@ -0,0 +1,18 @@
+import click
+from visivo.commands.options import project_dir
+
+
+@click.command()
+@project_dir
+def init(project_dir):
+    """
+    Enables a quick set up by writing your source & api credentials to an env file.
+    """
+    from visivo.logger.logger import Logger
+
+    Logger.instance().debug("Init")
+
+    from visivo.commands.init_phase import init_phase
+
+    init_phase(project_dir)
+    Logger.instance().success("Done")
