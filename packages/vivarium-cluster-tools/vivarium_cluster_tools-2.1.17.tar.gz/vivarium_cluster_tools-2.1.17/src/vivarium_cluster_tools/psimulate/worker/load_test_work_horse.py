e26ddcb89e99b7e395c1d8de9f0419d9c0daf7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:06:04 +0000

    Add package vivarium-cluster-tools/2.1.17/vivarium_cluster_tools-2.1.17.tar.gz

---
+++ packages/vivarium-cluster-tools/vivarium_cluster_tools-2.1.17.tar.gz/vivarium_cluster_tools-2.1.17/src/vivarium_cluster_tools/psimulate/worker/load_test_work_horse.py
@@ -0,0 +1,80 @@
+# mypy: ignore-errors
+"""
+================
+Load Test Worker
+================
+
+RQ worker executable for doing load testing.
+
+"""
+import time
+from traceback import format_exc
+
+import numpy as np
+import pandas as pd
+from loguru import logger
+from rq import get_current_job
+from vivarium.framework.randomness import get_hash
+
+from vivarium_cluster_tools.psimulate.environment import ENV_VARIABLES
+from vivarium_cluster_tools.psimulate.jobs import JobParameters
+
+LOAD_TEST_WORK_HORSE_IMPORT_PATH = f"{__name__}.work_horse"
+
+
+def get_psimulate_test_dict():
+    return {
+        "sleep": {
+            "function": sleep_test,
+            "peak_memory": 1,
+            "max_runtime": "00:05:00",
+        },
+        "large_results": {
+            "function": large_results_test,
+            "peak_memory": 2,
+            "max_runtime": "00:30:00",
+        },
+    }
+
+
+def sleep_test(job_parameters: JobParameters) -> pd.DataFrame:
+    min_sleep_time = 5
+    max_sleep_time = 60
+    rs = np.random.RandomState(seed=get_hash(f"sleep_test_{job_parameters.random_seed}"))
+    sleep_time = rs.randint(min_sleep_time, max_sleep_time)
+
+    logger.info(f"Sleeping for {sleep_time}s.")
+    time.sleep(sleep_time)
+    logger.info(f"Sleep test successful.")
+    return pd.DataFrame(
+        {"sleep_time": sleep_time}, index=pd.Index([job_parameters.random_seed], name="seed")
+    )
+
+
+def large_results_test(job_parameters: JobParameters) -> pd.DataFrame:
+    time.sleep(30)
+    np.random.seed(seed=get_hash(f"large_results_test_{job_parameters.random_seed}"))
+    return pd.DataFrame(np.random.random(10_000_000).reshape((1_000_000, 10)))
+
+
+def work_horse(job_parameters: dict) -> pd.DataFrame:
+    node = f"{ENV_VARIABLES.HOSTNAME.value}"
+    job = f"{ENV_VARIABLES.JOB_ID.value}:{ENV_VARIABLES.TASK_ID.value}"
+
+    job_parameters = JobParameters(**job_parameters)
+
+    test_type = job_parameters.extras["test_type"]
+    test_runner = get_psimulate_test_dict()[test_type]["function"]
+
+    logger.info(f"Launching new job {job} on {node}")
+    logger.info(f"Starting job: {job_parameters}")
+    try:
+        return test_runner(job_parameters)
+    except Exception:
+        logger.exception("Unhandled exception in worker")
+        job = get_current_job()
+        job.meta["root_exception"] = format_exc()
+        job.save_meta()
+        raise
+    finally:
+        logger.info(f"Exiting job: {job_parameters}")
---
+++ packages/vivarium-cluster-tools/vivarium_cluster_tools-2.1.17.tar.gz/vivarium_cluster_tools-2.1.17/tests/psimulate/redis_dbs/test_registry.py
@@ -0,0 +1,74 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from vivarium_cluster_tools.psimulate.redis_dbs.registry import RegistryManager
+
+
+@pytest.mark.parametrize("num_queues", [1, 7, 10, 19, 37])
+@pytest.mark.parametrize("num_jobs", [1, 10, 42, 1337, 2023])
+def test_allocate_jobs(mocker, num_queues, num_jobs):
+    # mock the QueueManager class
+    mocker.patch("vivarium_cluster_tools.psimulate.redis_dbs.registry.QueueManager")
+    queues = [(f"queue_{i}", i) for i in range(num_queues)]
+    manager = RegistryManager(queues, num_jobs, 0)
+    # create a list of dummy jobs based on a list of integers
+    jobs = [{"job": i} for i in range(num_jobs)]
+    # make an array of all the jobs in every queue
+    jobs_by_queue = list(manager.allocate_jobs(jobs))
+    # Check that each "layer" of jobs has strictly smaller-priority jobs than the next
+    largest_queue_size = max(len(queue) for queue in jobs_by_queue)
+    for i in range(largest_queue_size - 1):
+        this_layer = {queue[i]["job"] for queue in jobs_by_queue if len(queue) > i}
+        next_layer = {queue[i + 1]["job"] for queue in jobs_by_queue if len(queue) > i + 1}
+        assert max(this_layer) < min(next_layer)
+
+
+def create_mock_job(job_id, job_parameters):
+    mock_job = MagicMock()
+    mock_job.id = job_id
+    mock_job.kwargs = {"job_parameters": job_parameters}
+    return mock_job
+
+
+@pytest.fixture
+def mock_registry_manager():
+    # Mock the QueueManager used within RegistryManager
+    with patch(
+        "vivarium_cluster_tools.psimulate.redis_dbs.registry.QueueManager"
+    ) as MockQueueManager:
+        # Create mock jobs for each QueueManager
+        mock_jobs_list_1 = [
+            create_mock_job(1, "parameters1"),
+            create_mock_job(2, "parameters2"),
+        ]
+        mock_jobs_list_2 = [
+            create_mock_job(3, "parameters3"),
+            create_mock_job(4, "parameters4"),
+        ]
+
+        # Mock instances of QueueManager and their _queue.jobs to return the mock jobs
+        mock_queue_manager_instance_1 = MagicMock()
+        mock_queue_manager_instance_1._queue.jobs = mock_jobs_list_1
+
+        mock_queue_manager_instance_2 = MagicMock()
+        mock_queue_manager_instance_2._queue.jobs = mock_jobs_list_2
+
+        # Assuming RegistryManager initializes two QueueManagers internally
+        MockQueueManager.side_effect = [
+            mock_queue_manager_instance_1,
+            mock_queue_manager_instance_2,
+        ]
+
+        # Initialize your RegistryManager with mocked QueueManagers
+        manager = RegistryManager([("queue_0", 0), ("queue_1", 0)], 4, 0)
+
+        return manager
+
+
+def test_registry_manager_get_params_by_job(mock_registry_manager):
+    params_by_job = mock_registry_manager.get_params_by_job()
+    assert params_by_job[1] == "parameters1"
+    assert params_by_job[2] == "parameters2"
+    assert params_by_job[3] == "parameters3"
+    assert params_by_job[4] == "parameters4"
