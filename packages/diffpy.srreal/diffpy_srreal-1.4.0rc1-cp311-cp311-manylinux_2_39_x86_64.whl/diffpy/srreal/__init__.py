a55247b947957f81d4ead89ef7cd4022be7bfedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 14:12:18 +0000

    Add package diffpy.srreal/1.4.0rc1/diffpy_srreal-1.4.0rc1-cp311-cp311-manylinux_2_39_x86_64.whl

---
+++ packages/diffpy.srreal/diffpy_srreal-1.4.0rc1-cp311-cp311-manylinux_2_39_x86_64.whl/diffpy/srreal/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2025 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.srreal/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+"""Tools for real space structure analysis."""
+
+# package version
+from diffpy.srreal.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
