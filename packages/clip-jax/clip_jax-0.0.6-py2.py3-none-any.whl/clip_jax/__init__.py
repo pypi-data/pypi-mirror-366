05fb8081bae61667128edc4e4c779953c5bf616e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:05:36 +0000

    Add package clip-jax/0.0.6/clip_jax-0.0.6-py2.py3-none-any.whl

---
+++ packages/clip-jax/clip_jax-0.0.6-py2.py3-none-any.whl/clip_jax/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.6"
+
+from .modeling import (
+    CLIPModel,
+    CLIPTextModelForFineTuning,
+    CLIPVisionModelForFineTuning,
+    CLIPVisionModelForImageClassification,
+)
---
+++ packages/clip-jax/clip_jax-0.0.6-py2.py3-none-any.whl/clip_jax/utils.py
@@ -0,0 +1,50 @@
+import json
+from dataclasses import fields
+
+import flax
+import fsspec
+import jax
+from flax import linen as nn
+
+from .wandb_utils import maybe_use_artifact
+
+
+def with_logical_constraint(x, c, use_constraint=False):
+    if use_constraint:
+        return nn.with_logical_constraint(x, c)
+    else:
+        return x
+
+
+def load_config(config_path):
+    """Load config from file."""
+    with maybe_use_artifact(config_path, "config.json") as json_path:
+        with fsspec.open(json_path, "r") as f:
+            config = json.load(f)
+    return config
+
+
+def count_params(pytree):
+    return sum([x.size for x in jax.tree_util.tree_leaves(pytree)])
+
+
+def asdict(model):
+    excluded_keys = ["maxtext_mesh", "maxtext_args", "parent", "name"]
+    return {f.name: getattr(model, f.name) for f in fields(model) if f.name not in excluded_keys}
+
+
+# from maxtext
+def unbox_logicallypartioned(boxed_pytree):
+    """Unboxes the flax.LogicallyPartitioned pieces
+
+    Args:
+      boxed_pytree: a pytree that includes LogicallyPartitioned
+        leaves.
+    Returns:
+      a pytree where all all LogicallyPartitioned leaves have been unboxed.
+    """
+    return jax.tree_util.tree_map(
+        lambda x: x.unbox() if isinstance(x, flax.linen.spmd.LogicallyPartitioned) else x,
+        boxed_pytree,
+        is_leaf=lambda k: isinstance(k, flax.linen.spmd.LogicallyPartitioned),
+    )
