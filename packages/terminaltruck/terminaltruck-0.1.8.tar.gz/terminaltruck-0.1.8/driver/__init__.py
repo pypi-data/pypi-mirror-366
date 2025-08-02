a2ec8c419b4bce442468c49a188eb932de989d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 15:05:29 +0000

    Add package terminaltruck/0.1.8/terminaltruck-0.1.8.tar.gz

---
+++ packages/terminaltruck/terminaltruck-0.1.8.tar.gz/terminaltruck-0.1.8/driver/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.8"
+
+from . import driver
+from .driver import Driver
---
+++ packages/terminaltruck/terminaltruck-0.1.8.tar.gz/terminaltruck-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terminaltruck",
+    version="0.1.8",
+    author="Vitek",
+    author_email="cheeseqwertycheese@gmail.com",
+    description="Game about trucking",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Viktor640266/driver",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    packages = find_packages(),
+    entry_points={
+        'console_scripts': ['truck = driver.driver:run'],
+    },
+    )
