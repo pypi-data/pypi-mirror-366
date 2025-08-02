3ac75267f80e80b1b9fd16b0b6e5923c36b397b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:40 +0000

    Add package rdvt-s3storage/0.1.0/rdvt_s3storage-0.1.0.tar.gz

---
+++ packages/rdvt-s3storage/rdvt_s3storage-0.1.0.tar.gz/rdvt_s3storage-0.1.0/README.md
@@ -0,0 +1,5 @@
+﻿# rdvt-s3storage
+
+This is a placeholder package to reserve the name dvt-s3storage and prevent dependency confusion attacks.
+
+Do not install or use this package.
---
+++ packages/rdvt-s3storage/rdvt_s3storage-0.1.0.tar.gz/rdvt_s3storage-0.1.0/setup.py
@@ -0,0 +1,13 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="rdvt-s3storage",
+    version="0.1.0",
+    description="Reserved package to prevent dependency confusion.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
