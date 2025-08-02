db485a4e90d0273895109190e869c830d6285e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:05:44 +0000

    Add package ob-metaflow-stubs/6.0.5.1/ob-metaflow-stubs-6.0.5.1.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.5.1.tar.gz/ob-metaflow-stubs-6.0.5.1/metaflow-stubs/plugins/argo/__init__.pyi
@@ -0,0 +1,13 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.8.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-01T20:12:28.702385                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import argo_events as argo_events
+from . import argo_workflows_decorator as argo_workflows_decorator
+from . import argo_workflows_deployer as argo_workflows_deployer
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.5.1.tar.gz/ob-metaflow-stubs-6.0.5.1/metaflow-stubs/plugins/pypi/pypi_environment.pyi
@@ -0,0 +1,18 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.8.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-08-01T20:12:28.729320                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.plugins.pypi.conda_environment
+
+from .conda_environment import CondaEnvironment as CondaEnvironment
+
+class PyPIEnvironment(metaflow.plugins.pypi.conda_environment.CondaEnvironment, metaclass=type):
+    ...
+
