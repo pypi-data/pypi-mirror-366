2583df2443b73102ee38b712c0dd1bbd931071f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:08:53 +0000

    Add package automagik-hive/0.1.0a36/automagik_hive-0.1.0a36.tar.gz

---
+++ packages/automagik-hive/automagik_hive-0.1.0a36.tar.gz/automagik_hive-0.1.0a36/lib/utils/calculator.py
@@ -0,0 +1,9 @@
+"""Calculator utilities for basic math operations."""
+
+def add(a: int, b: int) -> int:
+    """Add two numbers."""
+    return a + b
+
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers."""
+    return a * b
