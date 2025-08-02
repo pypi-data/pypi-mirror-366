c43b61ebc31ce52189dbd7facde1750e1585b48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:08:48 +0000

    Add package lambda-cloud-cli/0.1.3/lambda_cloud_cli-0.1.3.tar.gz

---
+++ packages/lambda-cloud-cli/lambda_cloud_cli-0.1.3.tar.gz/lambda_cloud_cli-0.1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='lambda-cloud-cli',
+    version='0.1.3',
+    description='CLI tool for managing Lambda Cloud resources',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Taylor Gautreaux',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/lambda-cli',
+    license='MIT',
+    py_modules=['lambda_cli', 'lambda_api_client', 'config'],
+    install_requires=[
+    'typer',
+    'requests',
+    'rich',
+    'shellingham',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lambda-cli=lambda_cli:app',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
+
