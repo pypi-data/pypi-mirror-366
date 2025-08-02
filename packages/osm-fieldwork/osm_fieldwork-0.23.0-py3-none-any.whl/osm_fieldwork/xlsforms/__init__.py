be94e504a239e10d1c8c21dffe47ff5fb003b0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:59 +0000

    Add package osm-fieldwork/0.23.0/osm_fieldwork-0.23.0-py3-none-any.whl

---
+++ packages/osm-fieldwork/osm_fieldwork-0.23.0-py3-none-any.whl/osm_fieldwork/xlsforms/__init__.py
@@ -0,0 +1,9 @@
+"""Index of XLSForm file paths."""
+
+import os
+
+xlsforms_path = os.path.dirname(os.path.abspath(__file__))
+
+buildings = f"{xlsforms_path}/buildings.xls"
+healthcare = f"{xlsforms_path}/health.xls"
+highways = f"{xlsforms_path}/highways.xls"
