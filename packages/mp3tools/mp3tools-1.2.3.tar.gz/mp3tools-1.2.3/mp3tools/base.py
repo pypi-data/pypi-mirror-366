011c91f57eaf169184298ca9c1c7616bf0ec64e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 14:11:38 +0000

    Add package mp3tools/1.2.3/mp3tools-1.2.3.tar.gz

---
+++ packages/mp3tools/mp3tools-1.2.3.tar.gz/mp3tools-1.2.3/mp3tools/base.py
@@ -0,0 +1,17 @@
+"""
+mp3tools base module.
+
+This is the principal module of the mp3tools project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "mp3tools"
---
+++ packages/mp3tools/mp3tools-1.2.3.tar.gz/mp3tools-1.2.3/tests/test_base.py
@@ -0,0 +1,5 @@
+from mp3tools.base import NAME
+
+
+def test_base():
+    assert NAME == "mp3tools"
