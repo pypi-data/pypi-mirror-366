ca8f2562061af3a94036401950881aa44e051316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:16:14 +0000

    Add package sm-dag-compiler/1.0.0/sm_dag_compiler-1.0.0-py3-none-any.whl

---
+++ packages/sm-dag-compiler/sm_dag_compiler-1.0.0-py3-none-any.whl/sm_dag_compiler/__version__.py
@@ -0,0 +1,9 @@
+"""Version information for AutoPipe."""
+
+__version__ = "1.0.0"
+__title__ = "autopipe"
+__description__ = "Automatic SageMaker Pipeline Generation from DAG Specifications"
+__author__ = "Tianpei Xie"
+__author_email__ = "tianpei.x.luke@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/TianpeiLuke/autopipe"
