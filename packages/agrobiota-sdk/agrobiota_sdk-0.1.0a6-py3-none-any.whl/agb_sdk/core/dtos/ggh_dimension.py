800fc38219adc5e1ad7cb065aa31dd57bbdbe5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:32 +0000

    Add package agrobiota-sdk/0.1.0a6/agrobiota_sdk-0.1.0a6-py3-none-any.whl

---
+++ packages/agrobiota-sdk/agrobiota_sdk-0.1.0a6-py3-none-any.whl/agb_sdk/core/dtos/ggh_dimension.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class GGHDimension(Enum):
+    Biodiversity = "biodiversity"
+    BiologicalAgents = "biological-agents"
+    BiologicalFertility = "biological-fertility"
+    PhytosanitaryRisk = "phytosanitary-risk"
