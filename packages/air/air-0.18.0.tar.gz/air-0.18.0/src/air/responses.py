5e079f0bb8c409fdd581a67ee53e0162559bece2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 15:05:48 +0000

    Add package air/0.18.0/air-0.18.0.tar.gz

---
+++ packages/air/air-0.18.0.tar.gz/air-0.18.0/src/air/responses.py
@@ -0,0 +1,46 @@
+import importlib
+from typing import Any
+
+from fastapi import Response
+from starlette.responses import FileResponse as FileResponse  # noqa
+from starlette.responses import HTMLResponse as HTMLResponse  # noqa
+from starlette.responses import JSONResponse as JSONResponse  # noqa
+from starlette.responses import PlainTextResponse as PlainTextResponse  # noqa
+from starlette.responses import RedirectResponse as RedirectResponse  # noqa
+from starlette.responses import StreamingResponse as StreamingResponse  # noqa
+
+
+def dict_to_ft_component(d):
+    children_raw = d.get("_children", ())
+    children = tuple(
+        dict_to_ft_component(c) if isinstance(c, dict) else c for c in children_raw
+    )
+    module = importlib.import_module(d["_module"])
+    obj = getattr(module, d["_name"])
+    return obj(*children, **d.get("_attrs", {}))
+
+
+class TagResponse(Response):
+    """Response class to handle air.tags.Tags."""
+
+    media_type = "text/html; charset=utf-8"
+
+    def render(self, content: Any) -> bytes:
+        """Render Tag elements to bytes of HTML."""
+        if isinstance(content, dict):
+            content = dict_to_ft_component(content)
+        return content.render().encode("utf-8")
+
+
+class AirResponse(Response):
+    """Response class to handle air.tags.Tags or HTML (from Jinja2)."""
+
+    media_type = "text/html; charset=utf-8"
+
+    def render(self, content: Any) -> bytes:
+        """Render Tag elements to bytes of HTML."""
+        if isinstance(content, str):
+            return content.encode("utf-8")
+        if isinstance(content, dict):
+            content = dict_to_ft_component(content)
+        return content.render().encode("utf-8")
