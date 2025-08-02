5e01e8e1672a0e594f8e8680ca8163b57dc782cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:36 +0000

    Add package esa-snappy/1.1.0/esa_snappy-1.1.0.tar.gz

---
+++ packages/esa-snappy/esa_snappy-1.1.0.tar.gz/esa_snappy-1.1.0/esa_snappy/snapista/binning/aggregator_avg_outlier.py
@@ -0,0 +1,46 @@
+import lxml.etree as etree
+from attrs import asdict, define, field
+
+
+@define
+class AggregatorAvgOutlier(object):
+
+    type = field(init=False, default="AVG_OUTLIER")
+    varName = field()
+    targetName = field()
+
+    def __str__(self):
+
+        self.type = "AVG_OUTLIER"
+
+        return self.__repr__()
+
+    def __repr__(self):
+
+        self.type = "AVG_OUTLIER"
+
+        return "AggregatorAvgOutlier({})".format(
+            ", ".join(
+                ["{}='{}'".format(key, value) for key, value in self.to_dict().items()]
+            )
+        )
+
+    def to_dict(self):
+
+        self.type = "AVG_OUTLIER"
+
+        return asdict(self)
+
+    def to_xml(self):
+
+        self.type = "AVG_OUTLIER"
+
+        root = etree.Element("aggregator")
+
+        for key, value in self.to_dict().items():
+
+            elem = etree.SubElement(root, key)
+
+            elem.text = str(value)
+
+        return root
---
+++ packages/esa-snappy/esa_snappy-1.1.0.tar.gz/esa_snappy-1.1.0/esa_snappy/snapista/binning/aggregator_percentile.py
@@ -0,0 +1,56 @@
+from attrs import asdict, define, field
+import lxml.etree as etree
+
+
+@define
+class AggregatorPercentile(object):
+
+    type = field(init=False, default="PERCENTILE")
+    varName = field()
+    targetName = field()
+    percentage = field(default=90)
+
+    @percentage.validator
+    def _check_type(self, attribute, value):
+
+        if not (isinstance(value, int)):
+            raise ValueError("percentage is an int")
+
+        if int(value) < 0 or int(value > 100):
+            raise ValueError("percentage value is in range [0.0, 100.0]")
+
+    def __str__(self):
+
+        self.type = "PERCENTILE"
+
+        return self.__repr__()
+
+    def __repr__(self):
+
+        self.type = "PERCENTILE"
+
+        return "AggregatorPercentile({})".format(
+            ", ".join(
+                ["{}='{}'".format(key, value) for key, value in self.to_dict().items()]
+            )
+        )
+
+    def to_dict(self):
+
+        self.type = "PERCENTILE"
+
+        return asdict(self)
+
+    def to_xml(self):
+
+        self.type = "PERCENTILE"
+
+        root = etree.Element("aggregator")
+
+        for key, value in self.to_dict().items():
+
+            elem = etree.SubElement(root, key)
+
+            elem.text = str(value)
+
+        return root
