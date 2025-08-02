0880839792a1388500dff5b9b665aa563bbd8672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:05:41 +0000

    Add package kernel/0.8.3/kernel-0.8.3-py3-none-any.whl

---
+++ packages/kernel/kernel-0.8.3-py3-none-any.whl/kernel/resources/browsers/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .replays import (
+    ReplaysResource,
+    AsyncReplaysResource,
+    ReplaysResourceWithRawResponse,
+    AsyncReplaysResourceWithRawResponse,
+    ReplaysResourceWithStreamingResponse,
+    AsyncReplaysResourceWithStreamingResponse,
+)
+from .browsers import (
+    BrowsersResource,
+    AsyncBrowsersResource,
+    BrowsersResourceWithRawResponse,
+    AsyncBrowsersResourceWithRawResponse,
+    BrowsersResourceWithStreamingResponse,
+    AsyncBrowsersResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ReplaysResource",
+    "AsyncReplaysResource",
+    "ReplaysResourceWithRawResponse",
+    "AsyncReplaysResourceWithRawResponse",
+    "ReplaysResourceWithStreamingResponse",
+    "AsyncReplaysResourceWithStreamingResponse",
+    "BrowsersResource",
+    "AsyncBrowsersResource",
+    "BrowsersResourceWithRawResponse",
+    "AsyncBrowsersResourceWithRawResponse",
+    "BrowsersResourceWithStreamingResponse",
+    "AsyncBrowsersResourceWithStreamingResponse",
+]
