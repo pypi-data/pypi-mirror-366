ddc16b19c7d491d9400d8c2c2c30eccaa3b36b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 15:05:43 +0000

    Add package pyllamaui/0.0.1/pyllamaui-0.0.1.tar.gz

---
+++ packages/pyllamaui/pyllamaui-0.0.1.tar.gz/pyllamaui-0.0.1/README.md
@@ -0,0 +1,4 @@
+# PyllamaUI
+
+This is a placeholder package to reserve the name **pyllamaui** on PyPI.  
+Real content and code will be published soon.
---
+++ packages/pyllamaui/pyllamaui-0.0.1.tar.gz/pyllamaui-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pyllamaui',
+    version='0.0.1',
+    description='Reserved package name for PyllamaUI',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Bhuvanesh M',
+    author_email='bhuvaneshm.developer@gmail.com',
+    url='https://github.com/bhuvaneshm-dev/pyllamaui',
+    packages=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.6',
+)
