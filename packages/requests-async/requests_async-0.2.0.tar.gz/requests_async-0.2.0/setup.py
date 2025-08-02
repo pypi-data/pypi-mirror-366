89719a0ac5605498b9605fa6a2343563273675c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 16:06:13 +0000

    Add package requests-async/0.2.0/requests_async-0.2.0.tar.gz

---
+++ packages/requests-async/requests_async-0.2.0.tar.gz/requests_async-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="requests-async",  # Replace with your package name
+    version="0.2.0",
+    author="taisui",
+    author_email="tais00@qq.com",
+    description="An asynchronous HTTP request library based on httpx.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/taisuii/requests-async",  # Replace with your project URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "httpx",
+        "loguru"
+    ],
+)
