27c2b9f7901817b05441a7ce5fcaec168dabe8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:15:31 +0000

    Add package localstack-extension-tailscale/0.1.0/localstack_extension_tailscale-0.1.0-py3-none-any.whl

---
+++ packages/localstack-extension-tailscale/localstack_extension_tailscale-0.1.0-py3-none-any.whl/localstack_extension_tailscale/extension.py
@@ -0,0 +1,52 @@
+from collections.abc import Iterable
+import logging
+import socket
+from typing import final
+
+from localstack import config
+from localstack.extensions.api import Extension
+from localstack.utils.run import FuncThread
+from localstack_extension_tailscale.container import TailscaleContainer
+
+logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger("localstack.extension.tailscale")
+
+
+@final
+class LocalStackTailscale(Extension):
+    name: str = "localstack-extension-tailscale"
+    requirements = []
+    log_printer: FuncThread | None
+
+    def __init__(self):
+        self.tailscale_container = TailscaleContainer()
+        self.container_id: str | None = None
+        self.log_printer = None
+
+    def on_extension_load(self):
+        if config.DEBUG:
+            level = logging.DEBUG
+        else:
+            level = logging.INFO
+        LOG.setLevel(level)
+
+    def on_platform_ready(self):
+        LOG.info("%s: localstack is running", self.name)
+
+        # if we are not running in docker then exit
+        if not config.is_in_docker:
+            LOG.warning("not running as a docker container")
+            return
+
+        self.start_sidecar_container()
+
+    def on_platform_shutdown(self):
+        self.stop_sidecar_container()
+
+    def start_sidecar_container(self):
+        # get the container name
+        localstack_container_id = socket.gethostname()
+        self.tailscale_container.start(localstack_container_id)
+
+    def stop_sidecar_container(self):
+        self.tailscale_container.stop()
