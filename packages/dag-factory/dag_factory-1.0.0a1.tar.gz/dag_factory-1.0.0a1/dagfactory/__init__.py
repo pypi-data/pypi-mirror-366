c7e1be73163798247c208a5f1b8b789438a073ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:16:13 +0000

    Add package dag-factory/1.0.0a1/dag_factory-1.0.0a1.tar.gz

---
+++ packages/dag-factory/dag_factory-1.0.0a1.tar.gz/dag_factory-1.0.0a1/dagfactory/__init__.py
@@ -0,0 +1,9 @@
+"""Modules and methods to export for easier access"""
+
+from .dagfactory import DagFactory, load_yaml_dags
+
+__version__ = "1.0.0a1"
+__all__ = [
+    "DagFactory",
+    "load_yaml_dags",
+]
