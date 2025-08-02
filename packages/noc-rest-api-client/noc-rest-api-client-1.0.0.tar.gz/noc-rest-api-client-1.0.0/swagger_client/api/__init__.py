877c414293002586ad1a47707b5eda2c2ca28c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:16:17 +0000

    Add package noc-rest-api-client/1.0.0/noc-rest-api-client-1.0.0.tar.gz

---
+++ packages/noc-rest-api-client/noc-rest-api-client-1.0.0.tar.gz/noc-rest-api-client-1.0.0/swagger_client/api/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.accumulated_values_api import AccumulatedValuesApi
+from swagger_client.api.aircraft_api import AircraftApi
+from swagger_client.api.airport_api import AirportApi
+from swagger_client.api.com_channel_api import ComChannelApi
+from swagger_client.api.configuration_data_api import ConfigurationDataApi
+from swagger_client.api.configuration_management_api import ConfigurationManagementApi
+from swagger_client.api.crew_api import CrewApi
+from swagger_client.api.flight_api import FlightApi
+from swagger_client.api.hotel_api import HotelApi
+from swagger_client.api.human_resource_api import HumanResourceApi
+from swagger_client.api.maintenance_api import MaintenanceApi
+from swagger_client.api.pairing_api import PairingApi
+from swagger_client.api.roster_api import RosterApi
+from swagger_client.api.schedule_api import ScheduleApi
+from swagger_client.api.turn_around_rule_api import TurnAroundRuleApi
+from swagger_client.api.user_api import UserApi
