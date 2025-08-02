94a4d5e3ada26723aa91ff7fa58fe70029a93fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:35 +0000

    Add package aiopathlib/0.6.0/aiopathlib-0.6.0.tar.gz

---
+++ packages/aiopathlib/aiopathlib-0.6.0.tar.gz/aiopathlib-0.6.0/README.md
@@ -0,0 +1,205 @@
+aiopathlib: Pathlib support for asyncio
+=======================================
+
+[![image](https://img.shields.io/pypi/v/aiopathlib.svg)](https://pypi.org/project/aiopathlib/)
+[![image](https://img.shields.io/pypi/pyversions/aiopathlib.svg)](https://pypi.org/project/aiopathlib/)
+[![image](https://img.shields.io/pypi/l/aiopathlib.svg)](https://pypi.org/project/aiopathlib/)
+[![image](https://img.shields.io/codecov/c/github/waketzheng/aiopathlib/master.svg)](https://codecov.io/github/waketzheng/aiopathlib?branch=master)
+[![image](https://img.shields.io/badge/code%20style-pep8-green.svg)](https://www.python.org/dev/peps/pep-0008/)
+
+> If you are not using aiofiles, `anyio.Path` is another choice:
+
+Install:
+```bash
+pip install anyio
+```
+Examples:
+```py
+from datetime import datetime
+from anyio import Path
+
+filepath = Path(__file__)
+another = filepath.parent / 'sub_dirpath' / 'filename.ext'
+
+if await another.exists():
+    content = await another.read_bytes()
+elif not await another.parent.exists():
+    await another.parent.mkdir(parents=True)
+else:
+    await another.write_bytes(b'1')
+
+# glob/stat/remove
+async for p in filepath.parent.glob('*'):
+    if await p.is_file():
+        stat = await p.stat()
+        create_time = datetime.fromtimestamp(stat.st_ctime)
+        update_time = datetime.fromtimestamp(stat.st_mtime)
+        await p.unlink()  # remove file
+        print(f'{p} created at: {create_time}, modified at: {update_time}, removed at: {datetime.now()}')
+```
+See more at: https://github.com/agronholm/anyio
+
+------
+**aiopathlib** is written in Python, for handling local
+disk files in asyncio applications.
+
+Base on [aiofiles](https://github.com/Tinche/aiofiles) and just like pathlib, but use await.
+
+```py
+with open('filename', 'w') as fp:
+    fp.write('My file contents')
+
+text = await aiopathlib.AsyncPath('filename').read_text()
+print(text)
+'My file contents'
+
+content = await aiopathlib.AsyncPath(Path('filename')).read_bytes()
+print(content)
+b'My file contents'
+```
+
+Asynchronous interface to create folder.
+
+```py
+from aiopathlib import AsyncPath
+
+apath = AsyncPath('dirname/subpath')
+if not await apath.exists():
+    await apath.mkdir(parents=True)
+```
+
+
+Features
+--------
+
+- a file API very similar to Python's standard package `pathlib`, blocking API
+- support for buffered and unbuffered binary files, and buffered text files
+- support for ``async``/``await`` (:PEP:`492`) constructs
+
+
+Installation
+------------
+
+To install aiopathlib, simply:
+
+
+```bash
+$ pip install aiopathlib
+```
+
+
+Usage
+-----
+These functions are awaitable
+
+* ``read_text``
+* ``read_bytes``
+* ``read_json``
+* ``write_text``
+* ``write_bytes``
+* ``write_json``
+* ``mkdir``
+* ``touch``
+* ``exists``
+* ``rename``
+* ``unlink``
+* ``rmdir``
+* ``remove``
+* ``stat``
+* ``lstat``
+* ``is_file``
+* ``is_dir``
+* ``is_symlink``
+* ``is_fifo``
+* ``is_mount``
+* ``is_block_device``
+* ``is_char_device``
+* ``is_socket``
+
+Example
+-------
+Some common using cases:
+
+```
+from pashlib import Path
+from aiopathlib import AsyncPath
+
+filename = 'test.json'
+ap = AsyncPath(filename)
+p = Path(filename)
+assert (await ap.exists()) == p.exists() == False
+await ap.touch()  # Create a empty file
+assert (await ap.is_file()) == p.is_file() == True
+assert (await ap.is_dir()) == p.is_dir() == False
+assert (await ap.is_symlink()) == p.is_symlink() == False
+for func in ('is_fifo', 'is_mount', 'is_block_device', 'is_char_device', 'is_socket'):
+    assert (await getattr(ap, func)()) == getattr(p, func)()
+d = {'key': 'value'}
+await ap.write_json(d)  # == p.write_text(json.dumps(d))
+assert (await ap.read_json()) == d  # == json.loads(p.read_text())
+assert (await ap.read_bytes()) == p.read_bytes()  # b'{"key": "value"}'
+assert (await ap.stat()) == p.stat()
+assert (await ap.lstat()) == p.lstat()
+ap = await ap.rename('test_dir')  # == AsyncPath(p.rename('test_dir'))
+await ap.remove()  # == await ap.unlink() == p.unlink()
+await ap.mkdir()  # == p.mkdir()
+
+# Synchronization functions
+[Path(i) for i in ap.glob('*')] == list(p.glob('*'))
+[Path(i) for i in ap.rglob('*')] == list(p.rglob('*'))
+ap / 'filename' == ap.joinpath('filename') == AsyncPath(f'{ap}/filename')
+str(AsyncPath('string-or-Path-or-AsyncPath')) == str(Path('string-or-Path-or-AsyncPath'))
+ap.stem == p.stem
+ap.suffix == p.suffix
+Path(ap.with_name('xxx')) == p.with_name('xxx')
+Path(ap.parent) == p.parent
+Path(ap.resolve()) == p.resolve()
+...
+```
+
+
+History
+-------
+
+#### 0.3.1 (2022-02-20)
+
+- Return content size after write local file
+- Upgrade dependencies
+
+#### 0.3.0 (2021-12-16)
+
+- Support Python3.7
+- Clear `dev_requirements.txt` to be only package name and version
+
+#### 0.2.3 (2021-10-16)
+
+- Make `touch` pass test for py39.
+- Remove support for pypy3 from docs.
+
+#### 0.2.2 (2021-09-20)
+
+- Make `touch`/`stat`/`is_file`/... be awaitable.
+- Use `super().__new__` for initial.
+
+#### 0.2.0 (2021-08-29)
+
+- Make `AsyncPath` be subclass of `pathlib.Path`.
+- Add github action to show test coverage.
+
+#### 0.1.3 (2021-08-28)
+
+- Add makefile.
+- Test all functions.
+- Fix rename method error.
+- Support sync pathlib methods.
+
+#### 0.1.0 (2021-06-14)
+
+- Introduced a changelog.
+- Publish at gitee.
+
+
+Contributing
+------------
+
+Contributions are very welcome.
---
+++ packages/aiopathlib/aiopathlib-0.6.0.tar.gz/aiopathlib-0.6.0/tests/test_async.py
@@ -0,0 +1,176 @@
+"""Tests for asyncio's os module."""
+
+import contextlib
+import json
+import os
+from os.path import dirname, exists, isdir, join
+from pathlib import Path
+
+import aiofiles.os
+import pytest
+
+from aiopathlib import AsyncPath
+
+
+@pytest.mark.asyncio
+async def test_stat_lstat_is_sth():
+    """Test the stat call."""
+    filename = join(dirname(__file__), "resources", "test_file1.txt")
+
+    stat_res = await AsyncPath(filename).stat()
+
+    assert stat_res.st_size == 10
+    p = Path(filename)
+    assert stat_res == p.stat()
+    ln = p.with_name(p.name + ".ln")
+    aln = AsyncPath(ln)
+    await aln.remove(True)
+    ln.symlink_to(p)
+    assert (await aln.stat()) == stat_res
+    lstat_res = await AsyncPath(ln).lstat()
+    assert lstat_res != stat_res
+    assert lstat_res.st_size != stat_res.st_size
+    assert await aln.is_symlink()
+    assert await aln.is_file()
+    assert not await aln.is_dir()
+    assert await aln.parent.is_dir()
+    await aln.remove(True)
+    assert not await aln.is_symlink()
+    assert not await aln.is_file()
+    assert not await aln.is_dir()
+    assert not await aln.is_mount()
+    assert not await aln.is_socket()
+    assert await AsyncPath("/").is_mount()
+    assert await AsyncPath("/dev").is_mount()
+    assert not await aln.is_char_device()
+    char_path = list(Path("/dev").glob("tty*"))[0]
+    assert await AsyncPath(char_path).is_char_device()
+    assert not await aln.is_block_device()
+    block_paths = list(Path("/dev").glob("loop*"))
+    block_path = block_paths[0] if block_paths else list(Path("/dev").glob("disk*"))[0]
+    assert await AsyncPath(block_path).is_block_device()
+    assert not await aln.is_fifo()
+    ap = AsyncPath("fifo-test.foo")
+    await ap.remove(True)
+    # await ap.touch()
+    os.mkfifo(ap)
+    assert await ap.is_fifo()
+    await ap.remove(True)
+
+
+@pytest.mark.asyncio
+async def test_touch():
+    ap = AsyncPath("touch-test.foo")
+    await ap.remove(True)
+    await ap.touch()
+    assert (await ap.stat()).st_mode in [33204, 33188]
+    assert await ap.exists()
+    with pytest.raises(FileExistsError):
+        await ap.touch(exist_ok=False)
+    await ap.remove(True)
+    await ap.touch(mode=511)
+    assert (await ap.stat()).st_mode != 33188
+    if hasattr(ap, "_raise_closed"):
+        ap._closed = True  # type:ignore
+        with pytest.raises(ValueError):
+            await ap.touch()
+    await ap.remove(True)
+
+
+@pytest.mark.asyncio
+async def test_remove():
+    """Test the remove call."""
+    filename = join(dirname(__file__), "resources", "test_file2.txt")
+    with open(filename, "w") as f:
+        f.write("Test file for remove call")
+
+    assert exists(filename)
+    await AsyncPath(filename).remove()
+    assert exists(filename) is False
+    with pytest.raises(FileNotFoundError):
+        await AsyncPath("not_exist_file").remove()
+    await AsyncPath("not_exist_file").remove(True)
+    folder = AsyncPath("folder_for_test")
+    with contextlib.suppress(FileNotFoundError):
+        os.rmdir(folder)
+    await folder.mkdir()
+    with pytest.raises((PermissionError, IsADirectoryError)):
+        await folder.unlink()
+    await folder.remove()
+    assert not await folder.exists()
+
+
+@pytest.mark.asyncio
+async def test_mkdir_and_rmdir():
+    """Test the mkdir and rmdir call."""
+    directory = join(dirname(__file__), "resources", "test_dir")
+    ap = AsyncPath(directory)
+    if await ap.exists():
+        if await ap.is_dir():
+            await ap.rmdir()
+        else:
+            await ap.remove()
+    await AsyncPath(directory).mkdir()
+    assert isdir(directory)
+    with pytest.raises(FileExistsError):
+        await AsyncPath(directory).mkdir()
+    assert (await ap.mkdir(exist_ok=True)) is None  # type:ignore
+    await AsyncPath(directory).rmdir()
+    assert exists(directory) is False
+
+    sub_dir = join(directory, "sub_dir")
+    with pytest.raises(FileNotFoundError):
+        await AsyncPath(sub_dir).mkdir()
+    await AsyncPath(sub_dir).mkdir(parents=True)
+    assert isdir(sub_dir)
+    await AsyncPath(sub_dir).rmdir()
+    await AsyncPath(directory).rmdir()
+    assert exists(sub_dir) is False
+
+
+@pytest.mark.asyncio
+async def test_exists():
+    """Test the exists call."""
+    assert await AsyncPath(__file__).exists()
+    not_exist_file = join(dirname(__file__), "not_exist")
+    assert not await AsyncPath(not_exist_file).exists()
+
+
+@pytest.mark.asyncio
+async def test_rename():
+    """Test the rename call."""
+    old_filename = join(dirname(__file__), "resources", "test_file1.txt")
+    new_filename = join(dirname(__file__), "resources", "test_file2.txt")
+    await aiofiles.os.rename(old_filename, new_filename)
+    assert exists(old_filename) is False and exists(new_filename)
+    await aiofiles.os.rename(new_filename, old_filename)
+    assert exists(old_filename) and exists(new_filename) is False
+
+
+@pytest.mark.asyncio
+async def test_write():
+    """Test the write call."""
+    filename = join(dirname(__file__), "resources", "test_write.txt")
+    size = await AsyncPath(filename).write_bytes(b"1")
+    assert Path(filename).read_bytes() == b"1"
+    assert size == 1
+    size = await AsyncPath(filename).write_text("22")
+    assert Path(filename).read_text() == "22"
+    assert size == 2
+    data = {"key": 3}
+    size = await AsyncPath(filename).write_json(data)
+    content = json.dumps(data)
+    assert Path(filename).read_text() == content
+    assert await AsyncPath(filename).read_json() == data
+    assert size == len(content)
+    await AsyncPath(filename).async_write(b"4")
+    assert Path(filename).read_bytes() == b"4"
+    assert await AsyncPath(filename).read_bytes() == b"4"
+    assert await AsyncPath(filename).read_text() == "4"
+    assert exists(filename)
+    new_name = filename.replace(".txt", ".json")
+    await AsyncPath(filename).rename(new_name)
+    assert not exists(filename)
+    assert await AsyncPath(Path(new_name)).exists()
+    await AsyncPath(new_name).unlink()
+    assert not exists(new_name)
