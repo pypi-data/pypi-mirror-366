f866a20fe9ec602fd6971871ee81db7cf2d0e631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:08:31 +0000

    Add package napari-phasors/0.0.5/napari_phasors-0.0.5.tar.gz

---
+++ packages/napari-phasors/napari_phasors-0.0.5.tar.gz/napari_phasors-0.0.5/src/napari_phasors/__init__.py
@@ -0,0 +1,29 @@
+__version__ = "0.0.5"
+
+from ._reader import napari_get_reader
+from ._sample_data import (
+    convallaria_FLIM_sample_data,
+    embryo_FLIM_sample_data,
+    paramecium_HSI_sample_data,
+)
+from ._widget import (
+    CalibrationWidget,
+    LifetimeWidget,
+    PhasorTransform,
+    WriterWidget,
+)
+from ._writer import write_ome_tiff
+from .plotter import PlotterWidget
+
+__all__ = (
+    "napari_get_reader",
+    "write_ome_tiff",
+    "convallaria_FLIM_sample_data",
+    "embryo_FLIM_sample_data",
+    "paramecium_HSI_sample_data",
+    "PhasorTransform",
+    "PlotterWidget",
+    "CalibrationWidget",
+    "WriterWidget",
+    "LifetimeWidget",
+)
