cde784daa9749089c690aaee0936366a70a443fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:08:25 +0000

    Add package hyperspell/0.20.0/hyperspell-0.20.0-py3-none-any.whl

---
+++ packages/hyperspell/hyperspell-0.20.0-py3-none-any.whl/hyperspell/types/memory_status.py
@@ -0,0 +1,65 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["MemoryStatus"]
+
+
+class MemoryStatus(BaseModel):
+    id: int
+    """Deprecated: refer to documents by source and resource_id instead"""
+
+    resource_id: str
+
+    source: Literal[
+        "collections",
+        "vault",
+        "web_crawler",
+        "notion",
+        "slack",
+        "google_calendar",
+        "reddit",
+        "box",
+        "google_drive",
+        "airtable",
+        "algolia",
+        "amplitude",
+        "asana",
+        "ashby",
+        "bamboohr",
+        "basecamp",
+        "bubbles",
+        "calendly",
+        "confluence",
+        "clickup",
+        "datadog",
+        "deel",
+        "discord",
+        "dropbox",
+        "exa",
+        "facebook",
+        "front",
+        "github",
+        "gitlab",
+        "google_docs",
+        "google_mail",
+        "google_sheet",
+        "hubspot",
+        "jira",
+        "linear",
+        "microsoft_teams",
+        "mixpanel",
+        "monday",
+        "outlook",
+        "perplexity",
+        "rippling",
+        "salesforce",
+        "segment",
+        "todoist",
+        "twitter",
+        "zoom",
+    ]
+
+    status: Literal["pending", "processing", "completed", "failed"]
