fe440aa48dc022753727f71dc7b8b8ab29f2a7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:09:10 +0000

    Add package MLAMA/0.1.124/mlama-0.1.124.tar.gz

---
+++ packages/MLAMA/mlama-0.1.124.tar.gz/mlama-0.1.124/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MLAMA",
+    version="0.1.124",
+    description="MLAMA test package by Johra Moosa",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Johra Moosa",
+    author_email="your_email@example.com",  # Replace with your email
+    url="https://github.com/yourusername/testbyjohra",  # Optional, replace with your GitHub repo if you have one
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
