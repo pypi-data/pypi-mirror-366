242d2046ad5bfd532dba875c2332731fa6259d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 15:05:53 +0000

    Add package flati/0.2.0/flati-0.2.0.tar.gz

---
+++ packages/flati/flati-0.2.0.tar.gz/flati-0.2.0/flati/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""Flati - Flatten nested iterable object
+
+Author:
+    Yukino Ikegami
+
+Lisence:
+    MIT License
+
+Usage:
+    >>> import flati
+    >>> iterable = [(1, 2, 3), (4, (5, 6))]
+    >>> list(flati.flatten(iterable))
+    [1, 2, 3, 4, 5, 6]
+    >>> iterable = [('abc'), ('def', ('g', 'hi'))]
+    >>> list(flati.flatten(iterable, ignore=(str,)))
+    ['abc', 'def', 'g', 'hi']
+"""
+from sys import version_info
+if version_info < (3,):
+    from . import py2
+    flatten = py2.flatten
+else:
+    from . import py3
+    flatten = py3.flatten
+
+VERSION = (0, 2, 0)
+__version__ = '0.2.0'
+__license__ = 'MIT License'
