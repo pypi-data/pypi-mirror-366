73051167be907943c1c15c48bdfd779530133c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 15:05:42 +0000

    Add package mflux-mcp/0.0.1/mflux_mcp-0.0.1-py3-none-any.whl

---
+++ packages/mflux-mcp/mflux_mcp-0.0.1-py3-none-any.whl/mflux_mcp/cli.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def main():
+    print("hello world - this library is coming soon to enable MCP on MLX-powered Flux image generation")
+
+
+if __name__ == "__main__":
+    main()
