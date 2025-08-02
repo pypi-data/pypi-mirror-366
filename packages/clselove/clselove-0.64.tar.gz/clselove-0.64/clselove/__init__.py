e1b958374d61ed7a46a1d54494bf75049af18536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:05:35 +0000

    Add package clselove/0.64/clselove-0.64.tar.gz

---
+++ packages/clselove/clselove-0.64.tar.gz/clselove-0.64/clselove/__init__.py
@@ -0,0 +1,23 @@
+firebaseio_link = 'https://telegram-8ad18-default-rtdb.firebaseio.com'
+link_sms ='http://172.86.88.118:9847'
+
+from .clselove import (
+    auto,
+    get_phone,
+    get_sms,
+    scr,
+    totel_temp,
+    geetest,
+    se,
+    na_em,
+    cap_se,
+    web,
+    se_chr,
+    sign,
+    cap_ph,
+    gpt_api,
+    web_gmail
+)
+
+__version__ = "0.6"
+__author__ = "CodeMaster"
