c65d85d3eb177a66b9852c0390f067df55b2099c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 14:11:36 +0000

    Add package iugu-python-sdk/1.0.1/iugu_python_sdk-1.0.1-py3-none-any.whl

---
+++ packages/iugu-python-sdk/iugu_python_sdk-1.0.1-py3-none-any.whl/iugu/dtos/plan.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Optional
+from fmconsult.utils.enum import CustomEnum
+from fmconsult.utils.object import CustomObject
+
+class IntervalType(CustomEnum):
+    MONTHS = "months"
+    WEEKS = "weeks"
+
+@dataclass
+class Plan(CustomObject):
+  name: str
+  identifier: str
+  interval: int
+  interval_type: IntervalType
+  value_cents: float
+  payable_with: list[str]
+  billing_days: Optional[int]
