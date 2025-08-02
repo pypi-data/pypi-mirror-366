a651577eef3fe0dc3e1d52a32461053dd17f8852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 14:11:41 +0000

    Add package common-expression-language/0.4.1/common_expression_language-0.4.1.tar.gz

---
+++ packages/common-expression-language/common_expression_language-0.4.1.tar.gz/common_expression_language-0.4.1/tests/test_functions.py
@@ -0,0 +1,17 @@
+import cel
+import pytest
+
+
+def test_custom_function():
+    def custom_function(a, b):
+        return a + b
+
+    assert cel.evaluate("custom_function(1, 2)", {"custom_function": custom_function}) == 3
+
+
+def test_readme_custom_function_example():
+    def is_adult(age):
+        return age > 21
+
+    assert not cel.evaluate("is_adult(age)", {"is_adult": is_adult, "age": 18})
+    assert cel.evaluate("is_adult(age)", {"is_adult": is_adult, "age": 32})
