bb9e64d5764451daa8b4d65c0bf362471968b8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:29 +0000

    Add package panels/0.5.0/panels-0.5.0-cp38-cp38-win_amd64.whl

---
+++ packages/panels/panels-0.5.0-cp38-cp38-win_amd64.whl/panels/version.py
@@ -0,0 +1,13 @@
+# This file is generated automatically by the setup.py
+short_version = '0.5.0'
+version = '0.5.0'
+full_version = '0.5.0'
+svn_revision = ''
+isreleased = True
+__year__ = '2025'
+if isreleased:
+    __version__ = version
+else:
+    __version__ = full_version
+    version = full_version
+
