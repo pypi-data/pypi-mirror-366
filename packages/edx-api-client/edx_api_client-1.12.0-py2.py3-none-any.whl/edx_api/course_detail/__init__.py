e4fda364fb5673c160122ab6e716253919982d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 03:32:42 +0000

    Add package edx-api-client/1.12.0/edx_api_client-1.12.0-py2.py3-none-any.whl

---
+++ packages/edx-api-client/edx_api_client-1.12.0-py2.py3-none-any.whl/edx_api/course_detail/__init__.py
@@ -0,0 +1,81 @@
+"""Course Detail API"""
+from urllib.parse import urljoin
+
+from .models import CourseDetail, CourseMode
+
+
+# pylint: disable=too-few-public-methods
+class CourseDetails:
+    """
+    API Client to interface with the course detail API.
+    """
+
+    def __init__(self, requester, base_url):
+        self._requester = requester
+        self._base_url = base_url
+
+    def get_detail(self, course_id, username=None):
+        """
+        Fetches course details.
+
+        Args:
+            course_id (str): An edx course id.
+
+        Returns:
+            CourseDetail
+        """
+        # the request is done on behalf of the current logged in user
+        # this only works if COURSE_ABOUT_VISIBILITIY_PERMISSION is not
+        # set to staff, otherwise you need to pass in a username with
+        # permissions.
+        if not username:
+            resp = self._requester.get(
+                urljoin(
+                    self._base_url,
+                    f"/api/courses/v1/courses/{course_id}",
+                )
+            )
+        else:
+            resp = self._requester.get(
+                urljoin(
+                    self._base_url,
+                    f"/api/courses/v1/courses/{course_id}/?username={username}"
+                )
+            )
+
+        resp.raise_for_status()
+
+        return CourseDetail(resp.json())
+
+
+class CourseModes:
+    """
+    API Client to interface with the course modes API.
+    """
+
+    def __init__(self, requester, base_url):
+        self._requester = requester
+        self._base_url = base_url
+
+    def get_mode(self, course_id):
+        """
+        Fetches course mode details.
+
+        Args:
+            course_id (str): An edx course id.
+
+        Returns:
+            List of CourseMode
+        """
+        resp = self._requester.get(
+            urljoin(
+                self._base_url,
+                f"/api/course_modes/v1/courses/{course_id}",
+            )
+        )
+
+        resp.raise_for_status()
+        course_mode_list = []
+        for course_mode_json in resp.json():
+            course_mode_list.append(CourseMode(course_mode_json))
+        return course_mode_list
