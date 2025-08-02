11327dfb3503faf342bb84ad85167d36145107cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:06:06 +0000

    Add package aenet-gpr/1.8.2/aenet-gpr-1.8.2.tar.gz

---
+++ packages/aenet-gpr/aenet-gpr-1.8.2.tar.gz/aenet-gpr-1.8.2/aenet_gpr/__init__.py
@@ -0,0 +1,15 @@
+"""
+aenet-gpr: A Python package for Gaussian Process Regression (GPR) surrogate modeling
+to augment energy data for GPR-ANN potential training
+"""
+from .inout import inout_process, input_parameter, io_print, read_input
+from .src import gpr_batch, gpr_iterative, pytorch_kernel, pytorch_kerneltypes, calculator
+from .util import additional_data, param_optimization, prepare_data, reference_data
+from .tool import acquisition, aidneb, ase_tool, trainingset
+
+
+__version__ = "1.8.2"
+__all__ = ["inout", "inout_process", "input_parameter", "io_print", "read_input",
+           "src", "gpr_batch", "gpr_iterative", "pytorch_kernel", "pytorch_kerneltypes", "calculator",
+           "util", "additional_data", "param_optimization", "prepare_data", "reference_data",
+           "tool", "acquisition", "aidneb", "ase_tool", "trainingset"]
