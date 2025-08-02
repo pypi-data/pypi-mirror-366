f043a88bef1e2db1bd0b0a4db337b6b630210d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:09:02 +0000

    Add package clselove/0.61/clselove-0.61.tar.gz

---
+++ packages/clselove/clselove-0.61.tar.gz/clselove-0.61/clselove/__init__.py
@@ -0,0 +1,23 @@
+
+from .clselove import (
+    firebaseio_link,
+    link_sms,
+    auto,
+    get_phone,
+    get_sms,
+    scr,
+    totel_temp,
+    geetest,
+    se,
+    na_em,
+    cap_se,
+    web,
+    se_chr,
+    sign,
+    cap_ph,
+    gpt_api,
+    web_gmail
+)
+
+__version__ = "0.6"
+__author__ = "CodeMaster"
