4dc6a56556db2441bb5dd441cf3043bd3ee5cdea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 04:15:25 +0000

    Add package ltc-client/0.2.28/ltc_client-0.2.28-py3-none-any.whl

---
+++ packages/ltc-client/ltc_client-0.2.28-py3-none-any.whl/ltc_client/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# from ltc_client.worker import StandardWorker, DefaultIdLogFilter, HostnameFilter
+from ltc_client.api import Api, NameQuantityPair, Quantity, Unit, Log
+from ltc_client.helpers import Machine, Job, Material, decode
+
+__title__ = "TINARM - Node creation tool for TAE workers"
+__version__ = "0.2.28"
+__author__ = "Martin West, Chris Wallis"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2023 Tin Arm Engineering Ltd."
