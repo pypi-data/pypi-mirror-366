80b74b40b73c6d4faa03cab29251b6cf03322922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 15:05:42 +0000

    Add package nifty-ls/1.1.0/nifty_ls-1.1.0-cp314-cp314t-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/nifty-ls/nifty_ls-1.1.0-cp314-cp314t-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/nifty_ls/astropy.py
@@ -0,0 +1,71 @@
+"""
+Hooks for astropy's Lomb-Scargle implementation.  The hooks are installed in __init__.py.
+"""
+
+from __future__ import annotations
+
+from .core import lombscargle
+
+
+def lombscargle_fastnifty(
+    t,
+    y,
+    dy,
+    f0,
+    df,
+    Nf,
+    center_data=True,
+    fit_mean=True,
+    normalization='standard',
+    **lombscargle_kwargs,
+):
+    """
+    Usually one will want to use the finufft or cufinufft backends, rather than a brute-force backend.
+    Brute-force is probably only faster for many small NUFFTs, for which one needs to use the native
+    nifty-ls interface anyway (because astropy does not support batching).
+    """
+
+    return lombscargle(
+        t,
+        y,
+        dy,
+        fmin=f0,
+        fmax=f0 + df * (Nf - 1),
+        Nf=Nf,
+        center_data=center_data,
+        fit_mean=fit_mean,
+        normalization=normalization,
+        **lombscargle_kwargs,
+    ).power
+
+
+def lombscargle_fastnifty_chi2(
+    t,
+    y,
+    dy,
+    f0,
+    df,
+    Nf,
+    center_data=True,
+    fit_mean=True,
+    normalization='standard',
+    nterms=1,
+    **lombscargle_kwargs,
+):
+    """
+    Use the finufft_chi2 or cufinufft_chi2 backends
+    """
+
+    return lombscargle(
+        t,
+        y,
+        dy,
+        fmin=f0,
+        fmax=f0 + df * (Nf - 1),
+        Nf=Nf,
+        center_data=center_data,
+        fit_mean=fit_mean,
+        normalization=normalization,
+        nterms=nterms,
+        **lombscargle_kwargs,
+    ).power
