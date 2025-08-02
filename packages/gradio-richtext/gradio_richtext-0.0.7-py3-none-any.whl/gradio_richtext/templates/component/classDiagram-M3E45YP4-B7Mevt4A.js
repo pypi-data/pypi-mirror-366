3b207da98aa31d76b437b80a5deca8a2080a426d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:09:00 +0000

    Add package gradio-richtext/0.0.7/gradio_richtext-0.0.7-py3-none-any.whl

---
+++ packages/gradio-richtext/gradio_richtext-0.0.7-py3-none-any.whl/gradio_richtext/templates/component/classDiagram-M3E45YP4-B7Mevt4A.js
@@ -0,0 +1,16 @@
+import { s as r, c as s, a as e, C as t } from "./chunk-SZ463SBG-CvH-t2Z9.js";
+import { _ as l } from "./mermaid.core-16sd0E5l.js";
+var d = {
+  parser: e,
+  get db() {
+    return new t();
+  },
+  renderer: s,
+  styles: r,
+  init: /* @__PURE__ */ l((a) => {
+    a.class || (a.class = {}), a.class.arrowMarkerAbsolute = a.arrowMarkerAbsolute;
+  }, "init")
+};
+export {
+  d as diagram
+};
---
+++ packages/gradio-richtext/gradio_richtext-0.0.7-py3-none-any.whl/gradio_richtext/templates/component/clone-BxcQx72S.js
@@ -0,0 +1,8 @@
+import { b as r } from "./_baseUniq-DWUea4Wd.js";
+var e = 4;
+function a(o) {
+  return r(o, e);
+}
+export {
+  a as c
+};
