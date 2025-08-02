6f40075be57e3cff955347fb0d4083bee30b02d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 15:05:39 +0000

    Add package tterp-cores/0.2.7/tterp_cores-0.2.7.tar.gz

---
+++ packages/tterp-cores/tterp_cores-0.2.7.tar.gz/tterp_cores-0.2.7/cores/utils/status_codes.py
@@ -0,0 +1,10 @@
+class StatusCode:
+    """
+    Lớp này chứa các mã trạng thái HTTP.
+    Tài liệu chính thức: https://tools.ietf.org/html/rfc7231#section-6
+    """
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    CONFLICT = 409
