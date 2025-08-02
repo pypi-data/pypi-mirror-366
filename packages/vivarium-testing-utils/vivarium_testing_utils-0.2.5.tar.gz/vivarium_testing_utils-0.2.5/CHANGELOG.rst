2536727ceaae1592317bc664a057e94718953968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:08:53 +0000

    Add package vivarium-testing-utils/0.2.5/vivarium_testing_utils-0.2.5.tar.gz

---
+++ packages/vivarium-testing-utils/vivarium_testing_utils-0.2.5.tar.gz/vivarium_testing_utils-0.2.5/CHANGELOG.rst
@@ -0,0 +1,36 @@
+**0.2.5 - 08/01/25**
+
+ - Use vivarium_dependencies for common setup constraints
+
+**0.2.4 - 07/25/25**
+
+ - Feature: Support new environment creation via 'make build-env'
+
+**0.2.3 - 07/16/25**
+
+ - Support pinning of vivarium_build_utils; pin vivarium_build_utils>=1.1.0,<2.0.0
+
+**0.2.2 - 05/27/25**
+
+ - Update pandas stubs package pin
+
+**0.2.1 - 02/05/24**
+
+ - Add python versions json
+
+**0.2.0 - 11/21/24**
+
+ - Drop support for Python 3.9
+
+**0.1.2 - 10/31/24**
+
+ - Add mypy type checking
+ - Add unit tests for FuzzyChecker
+
+**0.1.1 - 10/14/24**
+
+ - Make name an optional parameter to fuzzy_assert_proportion
+
+**0.1.0 - 03/01/24**
+
+ - Repository creation
---
+++ packages/vivarium-testing-utils/vivarium_testing_utils-0.2.5.tar.gz/vivarium_testing_utils-0.2.5/setup.py
@@ -0,0 +1,121 @@
+import json
+import sys
+
+from packaging.version import parse
+from setuptools import find_packages, setup
+
+with open("python_versions.json", "r") as f:
+    supported_python_versions = json.load(f)
+
+python_versions = [parse(v) for v in supported_python_versions]
+min_version = min(python_versions)
+max_version = max(python_versions)
+if not (
+    min_version <= parse(".".join([str(v) for v in sys.version_info[:2]])) <= max_version
+):
+    py_version = ".".join([str(v) for v in sys.version_info[:3]])
+    # NOTE: Python 3.5 does not support f-strings
+    error = (
+        "\n--------------------------------------------\n"
+        "Error: Vivarium Testing Utils runs under python {min_version}-{max_version}.\n"
+        "You are running python {py_version}.\n".format(
+            min_version=min_version.base_version,
+            max_version=max_version.base_version,
+            py_version=py_version,
+        )
+        + "--------------------------------------------\n"
+    )
+    print(error, file=sys.stderr)
+    sys.exit(1)
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+if __name__ == "__main__":
+    base_dir = Path(__file__).parent
+    src_dir = base_dir / "src"
+
+    about = {}
+    with (src_dir / "vivarium_testing_utils" / "__about__.py").open() as f:
+        exec(f.read(), about)
+
+    with (base_dir / "README.rst").open() as f:
+        long_description = f.read()
+
+    install_requirements = [
+        "vivarium_dependencies[pandas,numpy,pyyaml,scipy,click,tables,loguru,networkx]",
+        "vivarium_build_utils>=2.0.1,<3.0.0",
+        # Type stubs
+        "types-setuptools",
+    ]
+
+    setup_requires = ["setuptools_scm"]
+
+    interactive_requirements = [
+        "vivarium_dependencies[interactive]",
+    ]
+
+    test_requirements = [
+        "vivarium_dependencies[pytest]",
+    ]
+
+    doc_requirements = [
+        "vivarium_dependencies[sphinx,sphinx-click,ipython,matplotlib,docutils]",
+    ]
+
+    lint_requirements = [
+        "vivarium_dependencies[lint]",
+    ]
+
+    setup(
+        name=about["__title__"],
+        description=about["__summary__"],
+        long_description=long_description,
+        license=about["__license__"],
+        url=about["__uri__"],
+        author=about["__author__"],
+        author_email=about["__email__"],
+        classifiers=[
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+            "Natural Language :: English",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX",
+            "Operating System :: POSIX :: BSD",
+            "Operating System :: POSIX :: Linux",
+            "Operating System :: Microsoft :: Windows",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Education",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Artificial Life",
+            "Topic :: Scientific/Engineering :: Mathematics",
+            "Topic :: Scientific/Engineering :: Medical Science Apps.",
+            "Topic :: Scientific/Engineering :: Physics",
+            "Topic :: Software Development :: Libraries",
+        ],
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
+        include_package_data=True,
+        install_requires=install_requirements,
+        tests_require=test_requirements,
+        extras_require={
+            "docs": doc_requirements,
+            "test": test_requirements,
+            "interactive": interactive_requirements,
+            "dev": doc_requirements
+            + test_requirements
+            + interactive_requirements
+            + lint_requirements,
+        },
+        zip_safe=False,
+        use_scm_version={
+            "write_to": "src/vivarium_testing_utils/_version.py",
+            "write_to_template": '__version__ = "{version}"\n',
+            "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+        },
+        setup_requires=setup_requires,
+    )
