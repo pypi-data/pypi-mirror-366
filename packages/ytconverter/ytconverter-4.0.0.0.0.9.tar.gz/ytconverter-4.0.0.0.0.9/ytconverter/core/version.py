6e68d5f55bf271262d1ce2c519060023b2c5f821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 05:08:56 +0000

    Add package ytconverter/4.0.0.0.0.9/ytconverter-4.0.0.0.0.9.tar.gz

---
+++ packages/ytconverter/ytconverter-4.0.0.0.0.9.tar.gz/ytconverter-4.0.0.0.0.9/ytconverter/core/version.py
@@ -0,0 +1,15 @@
+import httpx
+
+from ytconverter.config import load_local_version
+from ytconverter.constants import PYPI_VERSION_URL
+
+
+def check_version():
+    try:
+        remote = httpx.get(PYPI_VERSION_URL, timeout=3).json()["info"]["version"]
+    except Exception as e:
+        print(e)
+        exit()
+        return None, None
+    local, version_type = load_local_version()
+    return local, remote
