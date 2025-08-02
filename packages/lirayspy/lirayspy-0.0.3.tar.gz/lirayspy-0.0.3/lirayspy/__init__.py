10b2e3bed25c376237410ac7b7c2ab54aca44bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 06:05:33 +0000

    Add package lirayspy/0.0.3/lirayspy-0.0.3.tar.gz

---
+++ packages/lirayspy/lirayspy-0.0.3.tar.gz/lirayspy-0.0.3/lirayspy/__init__.py
@@ -0,0 +1,17 @@
+"""LiRAYS API Python Client.
+
+A Python client library for the LiRAYS API - Fiber Network Design.
+
+This library provides a comprehensive interface to interact with the
+LiRAYS API.
+"""
+
+from .client import LiRAYSApiClient, ExecStatus
+
+__author__ = "LiRAYS API Client"
+__email__ = "support@lirays.com"
+
+__all__ = [
+    "LiRAYSApiClient",
+    "ExecStatus",
+]
