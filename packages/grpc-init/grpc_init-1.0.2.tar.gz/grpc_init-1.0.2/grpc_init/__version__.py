e98397114146ce9dc67b0f4d3529132a42a93285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-02 16:06:17 +0000

    Add package grpc-init/1.0.2/grpc_init-1.0.2.tar.gz

---
+++ packages/grpc-init/grpc_init-1.0.2.tar.gz/grpc_init-1.0.2/grpc_init/__version__.py
@@ -0,0 +1,8 @@
+"""
+This package contains the grpc-init package.
+
+Author: Achraf MATAICH <achraf.mataich@outlook.com>
+"""
+
+from importlib.metadata import version as get_version
+__version__ = get_version("grpc-init")
