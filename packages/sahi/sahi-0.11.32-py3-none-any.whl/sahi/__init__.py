cf8128d388cf688c04fa08541b3b187f708b0498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:38 +0000

    Add package sahi/0.11.32/sahi-0.11.32-py3-none-any.whl

---
+++ packages/sahi/sahi-0.11.32-py3-none-any.whl/sahi/__init__.py
@@ -0,0 +1,13 @@
+import importlib.metadata as importlib_metadata
+
+try:
+    # This will read version from pyproject.toml
+    __version__ = importlib_metadata.version(__package__ or __name__)
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "development"
+
+
+from sahi.annotation import BoundingBox, Category, Mask
+from sahi.auto_model import AutoDetectionModel
+from sahi.models.base import DetectionModel
+from sahi.prediction import ObjectPrediction
