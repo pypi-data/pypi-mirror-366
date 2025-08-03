6089079c9183197f0173d10fc2cb15834928954e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 15:05:56 +0000

    Add package jageocoder/2.1.10/jageocoder-2.1.10.tar.gz

---
+++ packages/jageocoder/jageocoder-2.1.10.tar.gz/jageocoder-2.1.10/jageocoder/dataset.py
@@ -0,0 +1,60 @@
+from logging import getLogger
+from pathlib import Path
+from typing import Any, Dict, Optional
+
+from PortableTab import BaseTable
+
+
+logger = getLogger(__name__)
+
+
+class Dataset(BaseTable):
+    """
+    Dataset metadata.
+
+    Attributes
+    ----------
+    id : int
+        The unique identifier.
+        This value also indicates priority assigned to this dataset.
+        Smaller value indicates higher priority.
+    title : str
+        The title of this dataset.
+    url : str
+        The URL where the dataset is distributed.
+    """
+
+    __tablename__ = 'dataset'
+    __schema__ = """
+        struct Dataset {
+            id @0 :UInt8;
+            title @1 :Text;
+            url @2 :Text;
+        }
+        """
+    __record_type__ = "Dataset"
+
+    def __init__(self, db_dir: Path) -> None:
+        super().__init__(db_dir=db_dir)
+        self._map: Optional[Dict[int, Any]] = None
+
+    def load_records(self) -> Dict[int, Any]:
+        dataset_map = {}
+        for i in range(self.count_records()):
+            record = self.get_record(pos=i, as_dict=True)
+            dataset_map[record["id"]] = record
+
+        self.unload()
+        return dataset_map
+
+    def get(self, id: int) -> Any:
+        if self._map is None:
+            self._map = self.load_records()
+
+        return self._map[id]
+
+    def get_all(self) -> Optional[Dict[int, Any]]:
+        if self._map is None:
+            self._map = self.load_records()
+
+        return self._map
