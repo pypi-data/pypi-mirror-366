a0a540d62977f9283a422508b116fda88839a66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:55 +0000

    Add package wyzeapy/0.5.29/wyzeapy-0.5.29-py3-none-any.whl

---
+++ packages/wyzeapy/wyzeapy-0.5.29-py3-none-any.whl/wyzeapy/exceptions.py
@@ -0,0 +1,36 @@
+#  Copyright (c) 2021. Mulliken, LLC - All Rights Reserved
+#  You may use, distribute and modify this code under the terms
+#  of the attached license. You should have received a copy of
+#  the license with this file. If not, please write to:
+#  katie@mulliken.net to receive a copy
+"""
+Custom exception classes for Wyzeapy API interactions.
+"""
+
+
+class ActionNotSupported(Exception):
+    """Raised when an unsupported action is requested for a device type."""
+
+    def __init__(self, device_type: str):
+        message = f"The action specified is not supported by device type: {device_type}"
+        super().__init__(message)
+
+
+class ParameterError(Exception):
+    """Raised when invalid parameters are provided to an API call."""
+
+
+class AccessTokenError(Exception):
+    """Raised when the access token is invalid or has expired."""
+
+
+class LoginError(Exception):
+    """Raised during authentication or login failures."""
+
+
+class UnknownApiError(Exception):
+    """Raised for unexpected or generic API errors."""
+
+
+class TwoFactorAuthenticationEnabled(Exception):
+    """Raised when two-factor authentication is required for login."""
