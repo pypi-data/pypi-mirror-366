be42d399d623eb8ccf544a8f4aba76fcdb3a82cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:19 +0000

    Add package koinmal/0.3/koinmal-0.3.tar.gz

---
+++ packages/koinmal/koinmal-0.3.tar.gz/koinmal-0.3/koinmal/__init__.py
@@ -0,0 +1,3 @@
+class koin:
+    def all(x=None,y=None):
+        return x+y
---
+++ packages/koinmal/koinmal-0.3.tar.gz/koinmal-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="koinmal",
+    version="0.3",
+    description="maths integration",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Bedirhan",
+    author_email="bedirhan.oytpass@gmail.com",
+    url="",
+    packages=["koinmal"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    keywords=["maths","entegration"],
+    project_urls={},
+)
