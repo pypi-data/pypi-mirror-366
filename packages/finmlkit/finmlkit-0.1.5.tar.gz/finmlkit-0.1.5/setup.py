c6f06649d566faec4803a8ff669df766ad5acbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:36 +0000

    Add package finmlkit/0.1.5/finmlkit-0.1.5.tar.gz

---
+++ packages/finmlkit/finmlkit-0.1.5.tar.gz/finmlkit-0.1.5/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="finmlkit",
+    version="0.1.5",
+    description="Financial ML toolkit",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Dániel Terbe",
+    url="https://github.com/quantscious/finmlkit",
+    license="MIT",
+    packages=find_packages(exclude=("tests", "docs")),
+    python_requires=">=3.9",
+    install_requires=requirements,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
