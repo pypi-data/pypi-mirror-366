c63dbbf0adc85b4409dd961cf2b8fe2eaf961e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:52 +0000

    Add package rb-commons/0.7.9/rb_commons-0.7.9-py3-none-any.whl

---
+++ packages/rb-commons/rb_commons-0.7.9-py3-none-any.whl/rb_commons/orm/enum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class OriginType(str, Enum):
+    MANUAL = "MANUAL"
+    ETL = "ETL"
+    API = "API"
+    DEFAULT = "DEFAULT"
+
+class MediaSource(str, Enum):
+    ROBO = "ROBO"
+    BILLZ = "BILLZ"
+    BITO = "BITO"
