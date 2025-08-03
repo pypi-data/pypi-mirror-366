138288914a2ecf8cb0b23dc69d7fd0280684455c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:51 +0000

    Add package OpenPartsLibrary/0.1.0/openpartslibrary-0.1.0.tar.gz

---
+++ packages/OpenPartsLibrary/openpartslibrary-0.1.0.tar.gz/openpartslibrary-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='OpenPartsLibrary',
+    version='0.1.0',    
+    description='Python library for creating a database of hardware components for manufacturing',
+    url='https://github.com/alekssadowski95/OpenPartsLibrary',
+    author='Aleksander Sadowski',
+    author_email='aleksander.sadowski@alsado.de',
+    license='MIT',
+    packages=['openpartslibrary'],
+    install_requires=[''],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3'
+    ],
+)
