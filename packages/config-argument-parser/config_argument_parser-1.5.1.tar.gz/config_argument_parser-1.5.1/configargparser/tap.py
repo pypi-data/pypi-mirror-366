5fae5b7f0f00dda45aec74c3f26b33d671fb9e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:56 +0000

    Add package config-argument-parser/1.5.1/config_argument_parser-1.5.1.tar.gz

---
+++ packages/config-argument-parser/config_argument_parser-1.5.1.tar.gz/config_argument_parser-1.5.1/configargparser/tap.py
@@ -0,0 +1,100 @@
+"""A module for building command-line interface from dataclass."""
+
+import argparse
+import inspect
+
+
+class TypeArgumentParser:
+    """Parser parsing and updating a dataclass object.
+
+    Attributes:
+        parser: An `~argparse.ArgumentParser`.
+        defaults: A `dict` contains the default arguments.
+        args: A `dict` contains the parsed arguments.
+        help: A `dict` contains the help messages.
+    """
+
+    def __init__(self):
+        """Initialize TypeArgumentParser."""
+        self._init_parser()
+        self.defaults = {}
+        self.args = {}
+        self.help = {}
+
+    def _init_parser(self):
+        self.parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+
+    def _read_obj(self, obj: object):
+        """Read and parse the attributes of a dataclass object.
+
+        Convert attributes to :attr:`defaults` and parse the comments into :attr:`help`.
+        """
+        source_lines, _ = inspect.getsourcelines(type(obj))
+        self.defaults = obj.__dict__.copy()
+        msg_lst = []
+        args_iter = iter(self.defaults.keys())
+        for line in source_lines:
+            if line.strip().startswith(("@", "class ")):
+                pass
+            elif line.strip().startswith("#"):
+                msg = line.lstrip(" #").strip()
+                msg_lst.append(msg)
+            else:
+                key = next(args_iter)
+                # A non-whitespace string is needed to show the default in help.
+                self.help[key] = " ".join(msg_lst) if msg_lst else str(key)
+                msg_lst = []
+
+    def _add_arguments(self, shorts=""):
+        """Add arguments to parser according to the default.
+
+        Args:
+            shorts: A sequence of short option letters for the leading options.
+        """
+        boolean_to_action = {True: "store_false", False: "store_true"}
+        for i, (option, value) in enumerate(self.defaults.items()):
+            flags = [f"--{option.replace('_', '-')}"]
+            if i < len(shorts):
+                flags.insert(0, f"-{shorts[i]}")
+            if isinstance(value, bool):
+                self.parser.add_argument(
+                    *flags,
+                    action=boolean_to_action[value],
+                    help=self.help[option],
+                )
+            else:
+                self.parser.add_argument(
+                    *flags, default=value, type=type(value), help=self.help[option]
+                )
+
+    def _parse_args(self, args=None):
+        """Convert argument strings to dictionary :attr:`args`.
+
+        Return a `dict` containing arguments.
+        """
+        namespace = self.parser.parse_args(args)
+        self.args = vars(namespace)
+        return self.args
+
+    def _change_obj(self, obj):
+        """Update object attributes."""
+        obj.__dict__.update(self.args)
+
+    def parse_obj(self, obj, args=None, *, shorts=""):
+        """Parse arguments and update object attributes.
+
+        Args:
+            obj: A `~dataclasses.dataclass` object with attributes as default arguments.
+            args: A list of strings to parse. The default is taken from `sys.argv`.
+            shorts: A sequence of short option letters for the leading options.
+
+        Returns:
+            A `dict` containing updated arguments.
+        """
+        self._read_obj(obj)
+        self._add_arguments(shorts=shorts)
+        self._parse_args(args=args)
+        self._change_obj(obj)
+        return self.args
---
+++ packages/config-argument-parser/config_argument_parser-1.5.1.tar.gz/config_argument_parser-1.5.1/tests/test_cap.py
@@ -0,0 +1,113 @@
+import os
+from tempfile import mkstemp
+
+from configargparser import cap
+
+conf_str = """[DEFAULT]
+# Help message of the first argument. Help is optional.
+a_string = 'abc'
+a_float = 1.23  # inline comments are omitted
+# Help can span multiple lines.
+# This is another line.
+a_boolean = False
+an_integer = 0
+"""
+
+
+class Args:
+    # Help message of the first argument. Help is optional.
+    a_string = "abc"
+    a_float = 1.23  # inline comments are omitted
+    # Help can span multiple lines.
+    # This is another line.
+    a_boolean = False
+    an_integer = 0
+
+
+class TestConfigArgumentParser:
+    parser = cap.ConfigArgumentParser()
+
+    def setup_method(self):
+        self.parser.read_string(conf_str)
+
+    def teardown_method(self):
+        self.parser._init_parser()
+
+    def test_read_file(self):
+        fd, fname = mkstemp()
+        with open(fname, "w") as fp:
+            fp.write(conf_str)
+        parser = cap.ConfigArgumentParser()
+        parser.read(fname)
+        assert parser.defaults == self.parser.defaults
+        assert parser.help == self.parser.help
+        os.close(fd)
+        os.remove(fname)
+
+    def test_read_py(self):
+        fd, fname = mkstemp(suffix=".py")
+        with open(fname, "w") as fp:
+            fp.write("# " + conf_str)
+        parser = cap.ConfigArgumentParser()
+        parser.read_py(fname)
+        assert parser.defaults == self.parser.defaults
+        assert parser.help == self.parser.help
+        os.close(fd)
+        os.remove(fname)
+
+    def test_parse_comments(self):
+        assert (
+            self.parser.help["a_string"]
+            == "Help message of the first argument. Help is optional."
+        )
+        assert self.parser.help["a_float"] == "a_float"
+        assert (
+            self.parser.help["a_boolean"]
+            == "Help can span multiple lines. This is another line."
+        )
+
+    def test_parse_args_default(self):
+        self.parser._add_arguments()
+        self.parser._parse_args([])
+        assert (
+            self.parser.defaults
+            == self.parser.args
+            == {"a_string": "abc", "a_float": 1.23, "a_boolean": False, "an_integer": 0}
+        )
+
+    def test_parse_args_separate(self):
+        self.parser._add_arguments()
+        self.parser._parse_args("--a-float 1".split())
+        assert self.parser.args["a_float"] == 1.0
+        self.parser._parse_args(["--a-boolean"])
+        assert self.parser.args["a_boolean"]
+
+    def test_parse_args_short(self):
+        self.parser._add_arguments(shorts="sfb")
+        self.parser._parse_args("-b -f 1".split())
+        assert self.parser.args["a_float"] == 1.0
+        assert self.parser.args["a_boolean"]
+
+    def test_parse_args(self):
+        self.parser.parse_args("-b -f 1".split(), shorts="sfb")
+        assert self.parser.args["a_string"] == "abc"
+        assert self.parser.args["a_float"] == 1.0
+        assert self.parser.args["a_boolean"]
+        assert self.parser.args["an_integer"] == 0
+
+    def test_read_obj(self):
+        args = Args()
+        parser = cap.ConfigArgumentParser()
+        parser._read_obj(args)
+        assert parser.defaults == self.parser.defaults
+        assert parser.help == self.parser.help
+
+    def test_parse_obj(self):
+        args = Args()
+        self.parser.parse_obj(args, "-b -f 1".split(), shorts="sfb")
+        assert args.__dict__ == {
+            "a_string": "abc",
+            "a_float": 1.0,
+            "a_boolean": True,
+            "an_integer": 0,
+        }
