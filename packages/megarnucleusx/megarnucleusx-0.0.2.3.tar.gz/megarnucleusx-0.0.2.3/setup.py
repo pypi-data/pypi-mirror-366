c2dfffd8955f7c813c6bcead043007175aa46666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:58 +0000

    Add package megarnucleusx/0.0.2.3/megarnucleusx-0.0.2.3.tar.gz

---
+++ packages/megarnucleusx/megarnucleusx-0.0.2.3.tar.gz/megarnucleusx-0.0.2.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='megarnucleusx',
+    version='0.0.2.3',
+    description='IA modular sin dependencias externas',
+    author='TuNombre',
+    author_email='tu@email.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
