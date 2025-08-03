c60a29e3130c1b5e31f9fcbbaf60a701647472fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 15:06:16 +0000

    Add package waldiez-studio/0.5.8/waldiez_studio-0.5.8.tar.gz

---
+++ packages/waldiez-studio/waldiez_studio-0.5.8.tar.gz/waldiez_studio-0.5.8/waldiez_studio/static/frontend/assets/ansi-regex-BLuUz8pX.js
@@ -0,0 +1,1 @@
+function u({onlyFirst:u=!1}={}){const n=["[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?(?:\\u0007|\\u001B\\u005C|\\u009C))","(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))"].join("|");return new RegExp(n,u?void 0:"g")}export{u as a};
