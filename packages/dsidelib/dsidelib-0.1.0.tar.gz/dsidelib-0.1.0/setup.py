36180150cdc391b1ebf34be54fe55854e721fe7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 15:06:17 +0000

    Add package dsidelib/0.1.0/dsidelib-0.1.0.tar.gz

---
+++ packages/dsidelib/dsidelib-0.1.0.tar.gz/dsidelib-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dsidelib",
+    version="0.1.0",
+    packages=find_packages(),
+    author="fuqasha",
+    description="Simple example library with run function",
+    python_requires=">=3.6",
+)
