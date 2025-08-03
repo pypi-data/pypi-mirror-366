450593bdda95903056f648b4e3557bbf52792712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:25 +0000

    Add package fiasco/0.6.0/fiasco-0.6.0-py3-none-any.whl

---
+++ packages/fiasco/fiasco-0.6.0-py3-none-any.whl/fiasco/tests/test_base.py
@@ -0,0 +1,47 @@
+"""
+Test base ion functionality
+"""
+import pytest
+
+import fiasco
+
+from fiasco.util.exceptions import MissingIonError
+
+
+@pytest.mark.parametrize('ion_name', [
+    "fe 21",
+    "Fe 21",
+    "Iron 21",
+    "iron XXI",
+    "Fe xxi",
+    "Fe 20+",
+    (26, 21),
+    ("Fe", "XXI"),
+])
+def test_create_ion_input_formats(hdf5_dbase_root, ion_name):
+    ion = fiasco.base.IonBase(ion_name, hdf5_dbase_root=hdf5_dbase_root)
+    assert ion.element_name == 'iron'
+    assert ion.atomic_symbol == 'Fe'
+    assert ion.atomic_number == 26
+    assert ion.ionization_stage == 21
+    assert ion.charge_state == 20
+    assert ion.isoelectronic_sequence == 'C'
+    assert ion._ion_name == 'fe_21'
+    assert ion.ion_name_roman == 'Fe XXI'
+    assert ion.ionization_stage_roman == 'XXI'
+
+
+def test_create_invalid_ion_raises_missing_ion_error(hdf5_dbase_root):
+    with pytest.raises(MissingIonError):
+        fiasco.base.IonBase('hydrogen 3', hdf5_dbase_root=hdf5_dbase_root)
+
+
+@pytest.mark.parametrize(('ion_name', 'sequence_name'), [
+    ("Fe 21", "C"),
+    ("Fe 26", "H"),
+    ("Fe 25", "He"),
+    ("C 7", None),
+])
+def test_isoelectronic_sequence(ion_name, sequence_name, hdf5_dbase_root):
+    ion = fiasco.base.IonBase(ion_name, hdf5_dbase_root=hdf5_dbase_root)
+    assert ion.isoelectronic_sequence == sequence_name
