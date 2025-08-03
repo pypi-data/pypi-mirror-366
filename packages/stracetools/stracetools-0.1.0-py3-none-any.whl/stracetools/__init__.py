0f0b8e03003379d1a6299578f61713e446e5f32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:11 +0000

    Add package stracetools/0.1.0/stracetools-0.1.0-py3-none-any.whl

---
+++ packages/stracetools/stracetools-0.1.0-py3-none-any.whl/stracetools/__init__.py
@@ -0,0 +1,22 @@
+"""
+Strace Tools - A Python library for parsing and analyzing strace output
+"""
+
+__version__ = "0.1.0"
+__author__ = "Alex Jiakai Xu"
+__email__ = "jiakai.xu@columbia.edu"
+
+# Import main classes for easy access
+from .parser import TraceEventType, TraceEvent, StraceParser
+from .analyzer import ProcessInfo, SyscallStats, StraceAnalyzer
+from .visualizer import StraceVisualizer
+
+__all__ = [
+    "TraceEventType",
+    "TraceEvent",
+    "StraceParser",
+    "ProcessInfo",
+    "SyscallStats",
+    "StraceAnalyzer",
+    "StraceVisualizer"
+]
