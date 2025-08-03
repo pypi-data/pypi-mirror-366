9bf016856c428abc607613051e76c5475b94a196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 14:06:30 +0000

    Add package contextq/0.1.1/contextq-0.1.1.tar.gz

---
+++ packages/contextq/contextq-0.1.1.tar.gz/contextq-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="contextq",
+    version="0.1.1",
+    description="ContextQ: Context Based adjustments for LLMs with attention and quantization",
+    author="Ayan Jhunjhunwala",
+    author_email="ayanqwerty@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "transformers>=4.43",
+        "bitsandbytes>=0.43",
+        "datasets",
+        "accelerate",
+    ],
+    entry_points={
+        "console_scripts": [
+            "selective-gpt=contextq.main:main",
+        ],
+    }
+)
