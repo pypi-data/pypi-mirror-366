5d8e6340e53b116ab1f958d1ba9ac7eb527783f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 15:05:51 +0000

    Add package gc7-uuu5/1.0.0/gc7_uuu5-1.0.0.tar.gz

---
+++ packages/gc7-uuu5/gc7_uuu5-1.0.0.tar.gz/gc7_uuu5-1.0.0/src/gc7_uuu5/hello.py
@@ -0,0 +1,20 @@
+def hello():
+    from setuptools_scm import get_version
+
+    print("\nDernière version : " + get_version() + "\n" + ("-" * 71))
+    from dotenv import load_dotenv
+    import os
+
+    load_dotenv()  # Charge les variables depuis .env
+    # load_dotenv(override=True)
+    
+    gh_token = os.getenv("GH_TOKEN")
+    pypi_token = os.getenv("PYPI_TOKEN")
+
+    print(f"GH_TOKEN: {gh_token}\n\nPYPI_TOKEN: {pypi_token}\n")
+
+    return f"Salut les gens try automatic v4  !"
+
+
+if __name__ == "__main__":
+    print(hello())
