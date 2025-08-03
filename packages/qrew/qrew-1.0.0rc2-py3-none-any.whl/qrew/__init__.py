5a23fda6f958ea24e1dc79db08ec27efc226ec81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 14:07:34 +0000

    Add package qrew/1.0.0rc2/qrew-1.0.0rc2-py3-none-any.whl

---
+++ packages/qrew/qrew-1.0.0rc2-py3-none-any.whl/qrew/__init__.py
@@ -0,0 +1,36 @@
+"""
+Qrew - Automated loudspeaker measurement system using REW API
+
+This package provides a PyQt5-based GUI for automated speaker measurements
+through the Room EQ Wizard (REW) API.
+"""
+
+__version__ = "1.0.0rc2"
+__author__ = "Juan F. Loya"
+
+# Import main components for easier access
+from .main import main
+from .Qrew import MainWindow, shutdown_handler, wait_for_rew_qt
+from .Qrew_api_helper import check_rew_connection, initialize_rew_subscriptions
+from .Qrew_message_handlers import run_flask_server, stop_flask_server
+from .Qrew_styles import GLOBAL_STYLE, get_dark_palette, get_light_palette
+from .Qrew_messagebox import QrewMessageBox
+from . import Qrew_settings as qs
+from . import Qrew_common
+
+__all__ = [
+    "main",
+    "MainWindow",
+    "shutdown_handler",
+    "wait_for_rew_qt",
+    "check_rew_connection",
+    "initialize_rew_subscriptions",
+    "run_flask_server",
+    "stop_flask_server",
+    "GLOBAL_STYLE",
+    "get_dark_palette",
+    "get_light_palette",
+    "QrewMessageBox",
+    "qs",
+    "Qrew_common",
+]
