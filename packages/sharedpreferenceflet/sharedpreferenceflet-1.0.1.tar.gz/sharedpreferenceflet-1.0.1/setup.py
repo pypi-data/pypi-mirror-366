a0ac7e154e12a1dc036fc4f971fe2336df187332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:25 +0000

    Add package sharedpreferenceflet/1.0.1/sharedpreferenceflet-1.0.1.tar.gz

---
+++ packages/sharedpreferenceflet/sharedpreferenceflet-1.0.1.tar.gz/sharedpreferenceflet-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sharedpreferenceflet',
+    version='1.0.1',
+    description='Encrypted JSON-like Shared Preferences for Python',
+    author='Amir Louktila',
+    author_email='amir.louktila@example.com',
+    url='https://github.com/AmirLouktaila/SharedPreferenceFlet', 
+    packages=find_packages(),
+    install_requires=[
+        'cryptography>=3.4.7'
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
