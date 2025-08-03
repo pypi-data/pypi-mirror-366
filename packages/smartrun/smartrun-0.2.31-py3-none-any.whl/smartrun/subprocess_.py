6bb417e6b659d44d2d0bc4d75e013a154c8920ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 15:06:17 +0000

    Add package smartrun/0.2.31/smartrun-0.2.31-py3-none-any.whl

---
+++ packages/smartrun/smartrun-0.2.31-py3-none-any.whl/smartrun/subprocess_.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+import subprocess
+
+# smartrun
+from .options import Options
+from .runner_helpers import (
+    check_env_before,
+    NoActiveVirtualEnvironment,
+)
+from .utils import _ensure_pip
+from .envc.envc2 import EnvComplete
+from .utils import in_ci
+
+
+class NoActiveVirtualEnvironment(BaseException): ...
+
+
+from .utils import get_bin_path
+
+
+class SubprocessSmart:
+    """SubprocessSmart"""
+
+    def __init__(self, opts: Options):
+        self.opts = opts
+        self.check()
+        venv_path = self.get()
+        self.python_path = get_bin_path(venv_path, "python")
+        _ensure_pip(self.python_path)
+
+    def check(self):
+        env_check = check_env_before(self.opts)
+        if not env_check and not in_ci():
+            raise NoActiveVirtualEnvironment("Activate an environment")
+
+    def get(self):
+        env = EnvComplete()()
+        any_active = env.is_any_env_active()
+        if any_active:
+            return Path(env.get()["path"])
+        fallback = Path(".venv")
+        if fallback.exists():
+            return fallback.resolve()
+        raise NoActiveVirtualEnvironment("Activate an environment")
+
+    def run(self, params: list, verbose=False, return_output=False):
+        from .utils import is_verbose
+
+        verbose = is_verbose(verbose)
+        if self.opts.verbose:
+            verbose = True
+        params = [str(x) for x in params]
+        cmd = [str(self.python_path), *params]
+        if verbose:
+            print("Subprocess will run:", " ".join(cmd))
+        try:
+            result = subprocess.run(
+                cmd,
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+            if verbose:
+                print("[+]", result.stdout.strip())
+                print("[.]", result.stderr.strip())
+            return result if return_output else True
+        except subprocess.CalledProcessError as exc:
+            if verbose:
+                print("❌ Subprocess failed:")
+                print("STDOUT:", exc.stdout)
+                print("STDERR:", exc.stderr)
+            return False
