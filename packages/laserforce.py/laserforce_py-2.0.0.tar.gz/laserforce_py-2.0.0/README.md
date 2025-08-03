ca3c33d002854b046e05ef7ce8c209933693abda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 14:06:53 +0000

    Add package laserforce.py/2.0.0/laserforce_py-2.0.0.tar.gz

---
+++ packages/laserforce.py/laserforce_py-2.0.0.tar.gz/laserforce_py-2.0.0/README.md
@@ -0,0 +1,5 @@
+# laserforce.py
+[![Documentation Status](https://readthedocs.org/projects/laserforcepy/badge/?version=latest)](https://laserforcepy.readthedocs.io/en/latest/?badge=latest)
+[![MIT](https://img.shields.io/pypi/l/gd.py.svg)](https://opensource.org/licenses/MIT)
+
+A python package for Laserforce!
---
+++ packages/laserforce.py/laserforce_py-2.0.0.tar.gz/laserforce_py-2.0.0/laserforce/__init__.py
@@ -0,0 +1,10 @@
+"""
+Laserforce module for python
+"""
+
+from laserforce.objects.player import Player
+from laserforce.objects.site import Site
+from laserforce.objects.mission import Mission
+from laserforce.objects.achievement import Achievement
+from laserforce.objects.game_summary import GameSummary
+from laserforce.helpers import PlayerId
