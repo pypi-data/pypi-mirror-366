b4b826fc324ee8e6ec527e22a5a4675582c820e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:49 +0000

    Add package VibhanshGod-STT/0.1/vibhanshgod_stt-0.1.tar.gz

---
+++ packages/VibhanshGod-STT/vibhanshgod_stt-0.1.tar.gz/vibhanshgod_stt-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='VibhanshGod-STT',
+    version='0.1',
+    author='VibhanshGod',
+    author_email='vibhugpta3006@gmail.com',
+    description='this is speech to text program for J.A.R.V.I.S made by VibhanshGod',
+)
+packages = find_packages(),
+install_requirement = [
+    'selenium',
+    'webdriver_manager',
+]
