c5b78527eb9e86bb5ac29ff14c31c576c9d287b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 15:06:19 +0000

    Add package emota-loader/2.0.0/emota_loader-2.0.0-py3-none-any.whl

---
+++ packages/emota-loader/emota_loader-2.0.0-py3-none-any.whl/emota_loader/__init__.py
@@ -0,0 +1,12 @@
+from .dataset import EmoTaDataset
+from .utils import SENTENCES, EMOTION_LABELS, SPEAKERS, Speaker, Sentence, EmotionLabel
+
+__all__ = [
+    "EmoTaDataset",
+    "SENTENCES",
+    "EMOTION_LABELS",
+    "SPEAKERS",
+    "Speaker",
+    "Sentence",
+    "EmotionLabel",
+]
