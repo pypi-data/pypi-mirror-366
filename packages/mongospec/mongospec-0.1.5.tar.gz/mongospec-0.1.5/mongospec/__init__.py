251d1c3c3e85110308788ccdb566aa71e6c518f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:30 +0000

    Add package mongospec/0.1.5/mongospec-0.1.5.tar.gz

---
+++ packages/mongospec/mongospec-0.1.5.tar.gz/mongospec-0.1.5/mongospec/__init__.py
@@ -0,0 +1,34 @@
+# Core functionality
+from .core import (close, init)
+
+# Document models
+from .document import MongoDocument
+from .document.operations import (
+    AsyncDocumentCursor,
+    CountOperationsMixin,
+    DeleteOperationsMixin,
+    FindOperationsMixin,
+    InsertOperationsMixin,
+    UpdateOperationsMixin,
+)
+
+# Re-export commonly used dependency types
+from mongojet import IndexModel
+
+__all__ = [
+    # Core functionality
+    "init",
+    "close",
+
+    # Document models
+    "MongoDocument",
+    "AsyncDocumentCursor",
+    "CountOperationsMixin",
+    "DeleteOperationsMixin",
+    "FindOperationsMixin",
+    "InsertOperationsMixin",
+    "UpdateOperationsMixin",
+
+    # Public re-exports
+    "IndexModel",
+]
