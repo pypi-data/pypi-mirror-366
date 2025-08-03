fe168567bc806f4ec7859a946afc2bb72cc16348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 14:06:15 +0000

    Add package opda/0.8.0/opda-0.8.0-py3-none-any.whl

---
+++ packages/opda/opda-0.8.0-py3-none-any.whl/opda/exceptions.py
@@ -0,0 +1,13 @@
+"""Exceptions."""
+
+
+class IntegrationError(Exception):
+    """An exception for integration failures."""
+
+
+class NumericalError(Exception):
+    """An exception for numerical stability issues."""
+
+
+class OptimizationError(Exception):
+    """An exception for optimization failures."""
