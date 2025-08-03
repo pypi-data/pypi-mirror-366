af973186ad345e92cf1aefa7f50300fc60a76fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:51 +0000

    Add package fetchy-cli/1.4/fetchy_cli-1.4-py3-none-any.whl

---
+++ packages/fetchy-cli/fetchy_cli-1.4-py3-none-any.whl/fetchy_cli/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (c) 2024, espehon
+# License: https://www.gnu.org/licenses/gpl-3.0.html
---
+++ packages/fetchy-cli/fetchy_cli-1.4-py3-none-any.whl/fetchy_cli/__main__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, espehon
+# License: https://www.gnu.org/licenses/gpl-3.0.html
+
+import sys
+
+from fetchy import cli
+
+if __name__ == "__main__":
+    sys.exit(cli())
