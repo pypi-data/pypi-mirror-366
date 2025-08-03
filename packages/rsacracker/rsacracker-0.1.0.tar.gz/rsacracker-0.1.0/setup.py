813c7375af94572480a63e9753d56875f70b92a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 14:07:10 +0000

    Add package rsacracker/0.1.0/rsacracker-0.1.0.tar.gz

---
+++ packages/rsacracker/rsacracker-0.1.0.tar.gz/rsacracker-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rsacracker",
+    version="0.1.0",
+    author="neelanjan_manna",
+    author_email="igi.manna@gmail.com",
+    description="The RSA N Component Cracker is a desktop app that factors a given RSA modulus n into its prime components p and q, where n = p × q. ",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
