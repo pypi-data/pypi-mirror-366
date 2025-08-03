356c4a775be3385d90ce14abfdcff1bef1363644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:46 +0000

    Add package django-cte-stubs/1.1.0/django_cte_stubs-1.1.0.tar.gz

---
+++ packages/django-cte-stubs/django_cte_stubs-1.1.0.tar.gz/django_cte_stubs-1.1.0/django_cte-stubs/__init__.pyi
@@ -0,0 +1,4 @@
+from .cte import CTE as CTE
+from .cte import CTEManager as CTEManager
+from .cte import CTEQuerySet as CTEQuerySet
+from .cte import with_cte as with_cte
---
+++ packages/django-cte-stubs/django_cte_stubs-1.1.0.tar.gz/django_cte_stubs-1.1.0/django_cte-stubs/join.pyi
@@ -0,0 +1,43 @@
+from typing import Literal
+
+from django.db.backends.base.base import BaseDatabaseWrapper
+from django.db.models.sql.compiler import SQLCompiler
+from django.db.models.sql.where import WhereNode
+from typing_extensions import Self, TypeAlias, override
+
+_Param: TypeAlias = str | int
+_Params: TypeAlias = list[_Param] | tuple[_Param, ...]
+_AsSqlType: TypeAlias = tuple[str, _Params]
+
+class QJoin:
+    filtered_relation: None
+    parent_alias: str
+    table_name: str
+    table_alias: str
+    on_clause: WhereNode
+    join_type: Literal["INNER JOIN", "LEFT OUTER JOIN"]
+    nullable: bool
+
+    def __init__(
+        self,
+        parent_alias: str,
+        table_name: str,
+        table_alias: str,
+        on_clause: WhereNode,
+        join_type: Literal["INNER JOIN", "LEFT OUTER JOIN"] = ...,
+        nullable: bool | None = ...,
+    ) -> None: ...
+    @property
+    def identity(self) -> type[QJoin]: ...
+    @override
+    def __hash__(self) -> int: ...
+    @override
+    def __eq__(self, other: object) -> bool: ...
+    def equals(self, other: object) -> bool: ...
+    def as_sql(self, compiler: SQLCompiler, connection: BaseDatabaseWrapper) -> _AsSqlType: ...
+    def relabeled_clone(self, change_map: dict[str | None, str]) -> Self: ...
+
+    class join_field:  # noqa: N801
+        class related_model:  # noqa: N801
+            class _meta:  # noqa: N801
+                local_concrete_fields: tuple  # pyright: ignore[reportMissingTypeArgument]
