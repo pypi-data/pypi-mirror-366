64cda6b2e73790567187a55460566851add88573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:19 +0000

    Add package hello-greetings/1.1.0/hello_greetings-1.1.0-py3-none-any.whl

---
+++ packages/hello-greetings/hello_greetings-1.1.0-py3-none-any.whl/halo/__init__.py
@@ -0,0 +1,11 @@
+"""
+hello-greetings - A simple command line tool that greets the user.
+"""
+
+__version__ = "1.1.0"
+__author__ = "Aaron Hsu"
+__email__ = "aaronhsu@mail.ntou.edu.tw"
+
+from .cli import main
+
+__all__ = ["main"]
