7eacc69844fb0101a8cb6a1cd7ab1dbb834793dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:32 +0000

    Add package Prihops/0.3/prihops-0.3.tar.gz

---
+++ packages/Prihops/prihops-0.3.tar.gz/prihops-0.3/README.md
@@ -0,0 +1,19 @@
+# Prihops
+
+Prihops kütüphanesi Google drive den veri çekme ve ekleme olanagı sunarak veri saklamayı daha basit bir hâle getirir 
+
+## Kullanım
+
+- info() tarminale kütüphanenin versiyonunu verir
+- Control() drive dosyasındaki metin veya başlığı kontrol eder
+- delete() drive dosyasındaki metin veya başlığı siler
+- get() drive dosyasındaki metin veya başlığı çeker
+- give() drive dosyasına metin veya başlık ekler
+
+### Download 
+
+- pip install Prihops
+
+#### Telif Hakkı 
+
+Prihops kütüphanesi ile yapılan herhangi bir uygulamada kütüphanenin herhangi bir işlevine üçretlendirme uygulanamaz
---
+++ packages/Prihops/prihops-0.3.tar.gz/prihops-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Prihops",
+    version="0.3",
+    description="Google drive integration",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Bedirhan",
+    author_email="bedirhan.oytpass@gmail.com",
+    url="",
+    packages=find_packages(),
+    py_modules=["Prihops"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["tkinter","google.oauth2","googleapiclient.discovery","googleapiclient.http","PIL","io"],
+    keywords=["Google", "DRİVE","entegration"],
+    project_urls={},
+)
