d37dff8f2390720a5bde08bf6635235ee90e4b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 14:06:20 +0000

    Add package elliptical-distribution-toolkit/0.0.1/elliptical_distribution_toolkit-0.0.1-py3-none-any.whl

---
+++ packages/elliptical-distribution-toolkit/elliptical_distribution_toolkit-0.0.1-py3-none-any.whl/elliptical_distribution_toolkit/toolkit.py
@@ -0,0 +1,18 @@
+"""
+-------------------------------------------------------------------------------
+
+Simple module for the elliptical-distribution-toolkit package
+
+-------------------------------------------------------------------------------
+"""
+
+
+def print_toolkit_name():
+    """
+    Print the name of the toolkit to stdout.
+
+    Returns:
+        None
+    """
+    print("I am the elliptical-distribution-toolkit")
+
