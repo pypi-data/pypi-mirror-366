1ab5cb2326b205f11d43a875610025af15c90948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:46 +0000

    Add package Prihops/0.2/prihops-0.2.tar.gz

---
+++ packages/Prihops/prihops-0.2.tar.gz/prihops-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="Prihops",
+    version="0.2",
+    py_modules=["prihops"],
+    author="Bedirhan",
+    author_email="bedirhan.oytpass@gmail.com",
+    description="Google Drive data manager",
+    long_description=open("README.md",encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="Proprietary",
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: Other/Proprietary License",
+    "Operating System :: OS Independent",
+    ],
+   
+    
+)
