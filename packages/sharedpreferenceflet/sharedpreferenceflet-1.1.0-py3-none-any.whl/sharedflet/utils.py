133aa39abf06eca52c555efa233e1b3531486cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 03:38:37 +0000

    Add package sharedpreferenceflet/1.1.0/sharedpreferenceflet-1.1.0-py3-none-any.whl

---
+++ packages/sharedpreferenceflet/sharedpreferenceflet-1.1.0-py3-none-any.whl/sharedflet/utils.py
@@ -0,0 +1,31 @@
+from Crypto.Cipher import AES
+import os
+class cryptographyJson:
+    @staticmethod
+    def encrypt_file_advanced(file_path, key):
+        cipher = AES.new(key, AES.MODE_EAX)
+        with open(file_path, 'rb') as file:
+            data = file.read()
+
+        ciphertext, tag = cipher.encrypt_and_digest(data)
+
+        with open(file_path + '.lock', 'wb') as file:
+            file.write(cipher.nonce + tag + ciphertext)
+
+        os.remove(file_path)
+
+    @staticmethod
+    def decrypt_file(encrypted_path, key):
+        with open(encrypted_path, 'rb') as file:
+            nonce = file.read(16)
+            tag = file.read(16)
+            ciphertext = file.read()
+
+        cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+        data = cipher.decrypt_and_verify(ciphertext, tag)
+
+        original_path = encrypted_path[:-5]
+        with open(original_path, 'wb') as file:
+            file.write(data)
+
+        os.remove(encrypted_path)
