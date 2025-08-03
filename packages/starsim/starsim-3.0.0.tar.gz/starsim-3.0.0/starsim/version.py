b6e72337cd5fc6b5ab30bc888fdd4c41523d6af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 15:05:53 +0000

    Add package starsim/3.0.0/starsim-3.0.0.tar.gz

---
+++ packages/starsim/starsim-3.0.0.tar.gz/starsim-3.0.0/starsim/version.py
@@ -0,0 +1,9 @@
+"""
+Version and license information.
+"""
+
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__ = '3.0.0'
+__versiondate__ = '2025-08-02'
+__license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023-2025 by IDM'
