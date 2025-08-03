b67388d42f032446e3d54fa59611c70ee74d8b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:16 +0000

    Add package hello-greetings/1.0.0/hello_greetings-1.0.0-py3-none-any.whl

---
+++ packages/hello-greetings/hello_greetings-1.0.0-py3-none-any.whl/halo/cli.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+A simple command line tool that greets the user.
+"""
+
+import argparse
+import sys
+
+
+def main():
+    """Main function to handle command line arguments and greet the user."""
+    parser = argparse.ArgumentParser(
+        description='A simple greeting tool',
+        epilog='Example: halo "World"'
+    )
+    
+    parser.add_argument(
+        'name',
+        nargs='?',  # Makes the argument optional
+        help='The name or thing to greet',
+        default=None
+    )
+    
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version='halo 1.0.0'
+    )
+    
+    args = parser.parse_args()
+    
+    # If no name provided, ask for input
+    if args.name is None:
+        try:
+            name = input('Enter something: ')
+        except KeyboardInterrupt:
+            print("\nGoodbye!")
+            sys.exit(0)
+    else:
+        name = args.name
+    
+    # Print the greeting
+    print(f'hello {name}')
+
+
+if __name__ == '__main__':
+    main()
