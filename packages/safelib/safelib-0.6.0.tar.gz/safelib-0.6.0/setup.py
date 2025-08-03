cd793859d001348997c946e5a9bc8dc574dfad32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-03 04:24:59 +0000

    Add package safelib/0.6.0/safelib-0.6.0.tar.gz

---
+++ packages/safelib/safelib-0.6.0.tar.gz/safelib-0.6.0/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+
+from setuptools import setup
+
+readme_file = pathlib.Path(__file__).parent / "README.md"
+long_description = readme_file.read_text()
+
+setup(
+    name="safelib",
+    version="0.6.0",
+    description="An advanced library for safe importing and scoped imports management.",
+    author="Mert Sirakaya",
+    packages=["safelib"],
+    python_requires=">=3.10",
+    install_requires=[],
+    long_description=long_description,
+    maintainer="fswair",
+    maintainer_email="contact@tomris.dev",
+    long_description_content_type="text/markdown",
+    url="https://github.com/fswair/safelib",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
