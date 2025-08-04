2c5044ba44b659989510ffd3309d6ab64ad7aa6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 05:18:51 +0000

    Add package rapidstats/0.2.0/rapidstats-0.2.0-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/rapidstats/rapidstats-0.2.0-cp313-cp313-macosx_11_0_arm64.whl/rapidstats/__init__.py
@@ -0,0 +1,6 @@
+from rapidstats import metrics, polars, preprocessing, selection
+
+from ._bootstrap import Bootstrap
+from ._corr import correlation_matrix
+from ._distributions import norm
+from ._general import auc
