62b8a4ac06f3a1cabeb167b7b30fe59aa11c605f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:37:02 +0000

    Add package nester-mike/1.0.0/nester_mike-1.0.0.tar.gz

---
+++ packages/nester-mike/nester_mike-1.0.0.tar.gz/nester_mike-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_mike',
+        version = '1.0.0',
+        py_modules = ['nester_mike'],
+        author = 'mike',
+        author_email = '',
+        url = '',
+        description = 'A simple printer of nested lists',
+        )
