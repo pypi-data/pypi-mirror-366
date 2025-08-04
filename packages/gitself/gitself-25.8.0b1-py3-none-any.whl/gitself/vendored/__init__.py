9f12f7ce8e6477a910beee5d1c4edbb89eb2461e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:36:42 +0000

    Add package gitself/25.8.0b1/gitself-25.8.0b1-py3-none-any.whl

---
+++ packages/gitself/gitself-25.8.0b1-py3-none-any.whl/gitself/vendored/__init__.py
@@ -0,0 +1,5 @@
+"""
+A collection of vendored modules. Code in here is unlikely to ever
+change, but also gitself would be very sad if you couldn't access it,
+so we include these libraries with our distributions.
+"""
