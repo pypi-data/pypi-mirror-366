4beb1d08278f9e42a1bcaac4e148edb7a5f2e1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:36:19 +0000

    Add package avatar-everywhere-cli/1.0.2/avatar_everywhere_cli-1.0.2.tar.gz

---
+++ packages/avatar-everywhere-cli/avatar_everywhere_cli-1.0.2.tar.gz/avatar_everywhere_cli-1.0.2/wallet/__init__.py
@@ -0,0 +1,7 @@
+"""
+Avatar Everywhere CLI - Wallet Module
+Handles NFT ownership verification on Polygon network
+"""
+
+# Note: This module contains JavaScript files for Node.js integration
+# The verify_owner.js file is called via subprocess from the Python CLI 
