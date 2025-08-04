d394e7e0a57f1280aad4ce941a3361815a456d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:09:59 +0000

    Add package fastapi-rich-logging/0.1.1/fastapi_rich_logging-0.1.1.tar.gz

---
+++ packages/fastapi-rich-logging/fastapi_rich_logging-0.1.1.tar.gz/fastapi_rich_logging-0.1.1/src/fastapi_rich_logging/__init__.py
@@ -0,0 +1,7 @@
+"""FastAPI Rich Logging - Beautiful request/response logging middleware."""
+
+from .middleware import RichLoggingMiddleware, SimpleRichLoggingMiddleware
+from .formatters import create_rich_formatter
+
+__version__ = "0.1.0"
+__all__ = ["RichLoggingMiddleware", "SimpleRichLoggingMiddleware", "create_rich_formatter"]
