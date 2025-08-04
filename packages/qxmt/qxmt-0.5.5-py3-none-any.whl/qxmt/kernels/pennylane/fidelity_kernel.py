5857ad2700879ffc22e429274559477bd1402cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 15:07:17 +0000

    Add package qxmt/0.5.5/qxmt-0.5.5-py3-none-any.whl

---
+++ packages/qxmt/qxmt-0.5.5-py3-none-any.whl/qxmt/kernels/pennylane/fidelity_kernel.py
@@ -0,0 +1,183 @@
+from typing import Callable, cast
+
+import numpy as np
+import pennylane as qml
+from pennylane.measurements.sample import SampleMP
+from pennylane.measurements.state import StateMP
+from rich.progress import track
+
+from qxmt.devices import BaseDevice
+from qxmt.exceptions import ModelSettingError
+from qxmt.feature_maps import BaseFeatureMap
+from qxmt.kernels import BaseKernel
+from qxmt.kernels.base import STATE_VECTOR_BLOCK_SIZE
+from qxmt.kernels.sampling import sample_results_to_probs
+
+
+class FidelityKernel(BaseKernel):
+    """Fidelity kernel class.
+    The fidelity kernel is a quantum kernel that computes the kernel value based on the fidelity
+    between two quantum states.
+
+    Args:
+        BaseKernel (_type_): base class of kernel
+
+    Examples:
+        >>> import numpy as np
+        >>> from qxmt.kernels.pennylane.fidelity_kernel import FidelityKernel
+        >>> from qxmt.feature_maps.pennylane.defaults import ZZFeatureMap
+        >>> from qxmt.configs import DeviceConfig
+        >>> from qxmt.devices.builder import DeviceBuilder
+        >>> config = DeviceConfig(
+        ...     platform="pennylane",
+        ...     name="default.qubit",
+        ...     n_qubits=2,
+        ...     shots=1024,
+        >>> )
+        >>> device = DeviceBuilder(config).build()
+        >>> feature_map = ZZFeatureMap(2, 2)
+        >>> kernel = FidelityKernel(device, feature_map)
+        >>> x1 = np.random.rand(2)
+        >>> x2 = np.random.rand(2)
+        >>> kernel.compute(x1, x2)
+        0.14
+    """
+
+    def __init__(
+        self,
+        device: BaseDevice,
+        feature_map: BaseFeatureMap | Callable[[np.ndarray], None],
+    ) -> None:
+        """Initialize the FidelityKernel class.
+
+        Args:
+            device (BaseDevice): device instance for quantum computation
+            feature_map (BaseFeatureMap | Callable[[np.ndarray], None]): feature map instance or function
+        """
+
+        super().__init__(device, feature_map)
+        self.qnode = None
+        self.state_memory = {}
+
+    def _initialize_qnode(self) -> None:
+        if (self.qnode is None) and (self.is_sampling):
+            self.qnode = qml.QNode(self._circuit, device=self.device.get_device(), cache=False, diff_method=None)
+        elif (self.qnode is None) and (not self.is_sampling):
+            self.qnode = qml.QNode(
+                self._circuit_state_vector, device=self.device.get_device(), cache=False, diff_method=None
+            )
+
+    def _circuit(self, x1: np.ndarray, x2: np.ndarray) -> SampleMP | list[SampleMP]:
+        if self.feature_map is None:
+            raise ModelSettingError("Feature map must be provided for FidelityKernel.")
+
+        self.feature_map(x1)
+        qml.adjoint(self.feature_map)(x2)  # type: ignore
+
+        if (self.is_sampling) and (self.device.is_amazon_device()):
+            # Amazon Braket does not support directry sample by computational basis
+            return [qml.sample(op=qml.PauliZ(wires=i)) for i in range(self.n_qubits)]
+        else:
+            return qml.sample(wires=range(self.n_qubits))
+
+    def _circuit_state_vector(self, x: np.ndarray) -> StateMP:
+        if self.feature_map is None:
+            raise ModelSettingError("Feature map must be provided for FidelityKernel.")
+
+        self.feature_map(x)
+
+        return qml.state()
+
+    def _compute_matrix_by_state_vector(
+        self,
+        x1_array: np.ndarray,
+        x2_array: np.ndarray,
+        bar_label: str = "",
+        show_progress: bool = True,
+        block_size: int = STATE_VECTOR_BLOCK_SIZE,
+    ) -> np.ndarray:
+        """Compute the kernel matrix based on the state vector.
+        This method is only available in the non-sampling mode.
+        Each kernel value computed by theoritically probability distribution by state vector.
+
+        Args:
+            x1_array (np.ndarray): numpy array representing the all data points (ex: Train data)
+            x2_array (np.ndarray): numpy array representing the all data points (ex: Train data, Test data)
+            bar_label (str): label for progress bar
+            show_progress (bool): flag for showing progress bar
+            block_size (int): block size for the batch computation
+
+        Returns:
+            np.ndarray: computed kernel matrix
+        """
+        self._initialize_qnode()
+        if self.qnode is None:
+            raise RuntimeError("QNode is not initialized.")
+
+        unique_inputs = set([tuple(x) for x in x1_array] + [tuple(x) for x in x2_array])
+        if show_progress:
+            bar_label = f" ({bar_label})" if bar_label else ""
+            iterator = track(unique_inputs, description=f"Computing Kernel Matrix{bar_label}")
+        else:
+            iterator = unique_inputs
+
+        # compute the state vector for each data point
+        for x_tuple in iterator:
+            if x_tuple not in self.state_memory:
+                self.state_memory[x_tuple] = self.qnode(np.array(x_tuple))
+
+        states1 = np.array([self.state_memory[tuple(x)] for x in x1_array])
+        states2 = np.array([self.state_memory[tuple(x)] for x in x2_array])
+
+        # batch compute the kernel matrix
+        n1 = len(states1)
+        n2 = len(states2)
+        kernel_matrix = np.zeros((n1, n2), dtype=np.float64)
+        for i_start in range(0, n1, block_size):
+            i_end = min(i_start + block_size, n1)
+            block1 = states1[i_start:i_end]
+            for j_start in range(0, n2, block_size):
+                j_end = min(j_start + block_size, n2)
+                block2 = states2[j_start:j_end]
+
+                inner_block = np.dot(block1, np.conj(block2.T))
+                kernel_block = np.abs(inner_block) ** 2
+                kernel_matrix[i_start:i_end, j_start:j_end] = kernel_block
+
+        return kernel_matrix
+
+    def _compute_by_sampling(self, x1: np.ndarray, x2: np.ndarray) -> tuple[float, np.ndarray]:
+        """Compute the fidelity kernel value between two data points.
+        This method is only available in the sampling mode.
+        Each kernel value computed by sampling the quantum circuit.
+
+        Args:
+            x1 (np.ndarray): numpy array representing the first data point
+            x2 (np.ndarray): numpy array representing the second data point
+
+        Returns:
+            tuple[float, np.ndarray]: fidelity kernel value and probability distribution
+        """
+        if not self.is_sampling:
+            raise ValueError("_compute_by_sampling method is only available in sampling mode.")
+
+        self._initialize_qnode()
+        if self.qnode is None:
+            raise RuntimeError("QNode is not initialized.")
+
+        if (self.is_sampling) and (self.device.is_amazon_device()):
+            result = self.qnode(x1, x2)
+            # PauliZ basis convert to computational basis (-1->1, 1->0)
+            binary_result = (np.array(result).T == -1).astype(int)
+            # convert the sample results to probability distribution
+            # shots must be over 0 when sampling mode
+            probs = sample_results_to_probs(binary_result, self.n_qubits, cast(int, self.device.shots))
+        else:
+            result = self.qnode(x1, x2)
+            # convert the sample results to probability distribution
+            # shots must be over 0 when sampling mode
+            probs = sample_results_to_probs(result, self.n_qubits, cast(int, self.device.shots))
+
+        kernel_value = probs[0]  # get |0..0> state probability
+
+        return kernel_value, probs
---
+++ packages/qxmt/qxmt-0.5.5-py3-none-any.whl/qxmt/kernels/pennylane/projected_kernel.py
@@ -0,0 +1,243 @@
+from typing import Callable, Literal, cast
+
+import numpy as np
+import pennylane as qml
+from pennylane.measurements.sample import SampleMP
+from pennylane.measurements.state import StateMP
+from rich.progress import track
+
+from qxmt.devices import BaseDevice
+from qxmt.exceptions import ModelSettingError
+from qxmt.feature_maps import BaseFeatureMap
+from qxmt.kernels import BaseKernel
+from qxmt.kernels.base import STATE_VECTOR_BLOCK_SIZE
+from qxmt.kernels.sampling import sample_results_to_probs
+
+
+class ProjectedKernel(BaseKernel):
+    """Projected kernel class.
+    The projected kernel is a quantum kernel that projects the quantum state to a specific basis
+    and computes the kernel value based on the projected measurement results.
+    Reference: https://www.nature.com/articles/s41467-021-22539-9
+
+    Args:
+        BaseKernel (_type_): base class of kernel
+
+    Examples:
+        >>> import numpy as np
+        >>> from qxmt.kernels.pennylane.projected_kernel import ProjectedKernel
+        >>> from qxmt.feature_maps.pennylane.defaults import ZZFeatureMap
+        >>> from qxmt.configs import DeviceConfig
+        >>> from qxmt.devices.builder import DeviceBuilder
+        >>> config = DeviceConfig(
+        ...     platform="pennylane",
+        ...     name="default.qubit",
+        ...     n_qubits=2,
+        ...     shots=1024,
+        >>> )
+        >>> device = DeviceBuilder(config).build()
+        >>> feature_map = ZZFeatureMap(2, 2)
+        >>> kernel = ProjectedKernel(device, feature_map)
+        >>> x1 = np.random.rand(2)
+        >>> x2 = np.random.rand(2)
+        >>> kernel.compute(x1, x2)
+        0.86
+    """
+
+    def __init__(
+        self,
+        device: BaseDevice,
+        feature_map: BaseFeatureMap | Callable[[np.ndarray], None],
+        gamma: float = 1.0,
+        projection: Literal["x", "y", "z"] = "z",
+    ) -> None:
+        """Initialize the ProjectedKernel class.
+
+        Args:
+            device (BaseDevice): device instance for quantum computation
+            feature_map (BaseFeatureMap | Callable[[np.ndarray], None]): feature map instance or function
+            gamma (float): gamma parameter for kernel computation
+            projection (str): projection method for kernel computation
+        """
+        if projection not in ["x", "y", "z"]:
+            raise ValueError("Projection method must be 'x', 'y', or 'z'.")
+
+        super().__init__(device, feature_map)
+        self.n_qubits = device.n_qubits
+        self.gamma = gamma
+        self.projection = projection
+        self.qnode = None
+        self.state_memory = {}
+
+    def _initialize_qnode(self) -> None:
+        if self.qnode is None:
+            self.qnode = qml.QNode(self._circuit, device=self.device.get_device(), cache=False)
+
+    def _process_measurement_results(self, results: list[float]) -> np.ndarray:
+        """Process the measurement results based on the projection method.
+
+        Args:
+            results (list[float]): projected measurement results for each qubit
+
+        Returns:
+            np.ndarray: processed measurement results
+        """
+        if self.projection == "xyz_sum":
+            sum_results: list[float] = []
+            idx_size = 3  # x, y, z
+            for i in range(0, self.n_qubits, idx_size):
+                sum_result: float = np.sum([result for result in results[i : i + idx_size]])
+                sum_results.append(sum_result)
+            projected_results = np.array(sum_results)
+        else:
+            projected_results = np.array(results)
+
+        return projected_results
+
+    def _calculate_expected_values_by_z(self, probs: np.ndarray, target_qubit: int) -> float:
+        mask = 1 << target_qubit
+        expval_z = 0.0
+        for i, prob in enumerate(probs):
+            if (i & mask) == 0:
+                expval_z += prob
+            else:
+                expval_z -= prob
+        return expval_z
+
+    def _calculate_expected_values(self, probs: np.ndarray) -> np.ndarray:
+        # calculate the expected values by Z basis for each qubit
+        # when the projection method is "x" or "y", apply Hadamard or RY gate in _circuit method
+        projected_exp_value = np.array([self._calculate_expected_values_by_z(probs, i) for i in range(self.n_qubits)])
+
+        return projected_exp_value
+
+    def _circuit(self, x: np.ndarray) -> SampleMP | list[SampleMP] | StateMP:
+        if self.feature_map is None:
+            raise ModelSettingError("Feature map must be provided for FidelityKernel.")
+
+        self.feature_map(x)
+
+        # apply projection operators for calculating the expected values by Z basis
+        if self.projection == "x":
+            for i in range(self.n_qubits):
+                qml.Hadamard(wires=i)
+        elif self.projection == "y":
+            for i in range(self.n_qubits):
+                qml.RY(qml.numpy.array(np.pi / 2), wires=i)
+
+        if (self.is_sampling) and (self.device.is_amazon_device()):
+            # Amazon Braket does not support directry sample by computational basis
+            return [qml.sample(op=qml.PauliZ(wires=i)) for i in range(self.n_qubits)]
+        elif self.is_sampling:
+            return qml.sample(wires=self.n_qubits)
+        else:
+            return qml.state()
+
+    def _compute_matrix_by_state_vector(
+        self,
+        x1_array: np.ndarray,
+        x2_array: np.ndarray,
+        bar_label: str = "",
+        show_progress: bool = True,
+        block_size: int = STATE_VECTOR_BLOCK_SIZE,
+    ) -> np.ndarray:
+        """Compute the kernel matrix based on the state vector.
+        This method is only available in the non-sampling mode.
+        Each kernel value computed by theoritically probability distribution by state vector.
+
+        Args:
+            x1_array (np.ndarray): numpy array representing the all data points (ex: Train data)
+            x2_array (np.ndarray): numpy array representing the all data points (ex: Train data, Test data)
+            bar_label (str): label for progress bar
+            show_progress (bool): flag for showing progress bar
+            block_size (int): block size for the batch computation
+
+        Returns:
+            np.ndarray: computed kernel matrix
+        """
+        self._initialize_qnode()
+        if self.qnode is None:
+            raise RuntimeError("QNode is not initialized.")
+
+        unique_inputs = set([tuple(x) for x in x1_array] + [tuple(x) for x in x2_array])
+        if show_progress:
+            bar_label = f" ({bar_label})" if bar_label else ""
+            iterator = track(unique_inputs, description=f"Computing Kernel Matrix{bar_label}")
+        else:
+            iterator = unique_inputs
+
+        # compute the state vector for each data point
+        for x_tuple in iterator:
+            if x_tuple not in self.state_memory:
+                x_state = self.qnode(np.array(x_tuple))
+                self.state_memory[x_tuple] = self._calculate_expected_values(np.abs(x_state) ** 2)
+
+        states1 = np.array([self.state_memory[tuple(x)] for x in x1_array])
+        states2 = np.array([self.state_memory[tuple(x)] for x in x2_array])
+
+        # batch compute the kernel matrix
+        n1 = len(states1)
+        n2 = len(states2)
+        kernel_matrix = np.zeros((n1, n2), dtype=np.float64)
+
+        for i_start in range(0, n1, block_size):
+            i_end = min(i_start + block_size, n1)
+            block1 = states1[i_start:i_end]
+            for j_start in range(0, n2, block_size):
+                j_end = min(j_start + block_size, n2)
+                block2 = states2[j_start:j_end]
+
+                # compute the squared Euclidean distance between blocks
+                # ||x - y||² = ||x||² + ||y||² - 2⟨x, y⟩
+                a_norm2 = np.sum(block1**2, axis=1).reshape(-1, 1)
+                b_norm2 = np.sum(block2**2, axis=1).reshape(1, -1)
+                cross_term = np.dot(block1, block2.T)
+                sq_dist = a_norm2 + b_norm2 - 2 * cross_term
+
+                # RBF kernel
+                kernel_block = np.exp(-self.gamma * sq_dist)
+                kernel_matrix[i_start:i_end, j_start:j_end] = kernel_block
+
+        return kernel_matrix
+
+    def _compute_by_sampling(self, x1: np.ndarray, x2: np.ndarray) -> tuple[float, np.ndarray]:
+        """Compute the projected kernel value between two data points.
+        This method is only available in the sampling mode.
+        Each kernel value computed by sampling the quantum circuit.
+
+        Args:
+            x1 (np.ndarray): numpy array representing the first data point
+            x2 (np.ndarray): numpy array representing the second data point
+
+        Returns:
+            tuple[float, np.ndarray]: projected kernel value and probability distribution
+        """
+        self._initialize_qnode()
+        if self.qnode is None:
+            raise RuntimeError("QNode is not initialized.")
+
+        x1_result = self.qnode(x1)
+        x2_result = self.qnode(x2)
+
+        if (self.is_sampling) and (self.device.is_amazon_device()):
+            # PauliZ basis convert to computational basis (-1->1, 1->0)
+            x1_binary_result = (np.array(x1_result).T == -1).astype(int)
+            x2_binary_result = (np.array(x2_result).T == -1).astype(int)
+            # convert the sample results to probability distribution
+            # shots must be over 0 when sampling mode
+            x1_probs = sample_results_to_probs(x1_binary_result, self.n_qubits, cast(int, self.device.shots))
+            x2_probs = sample_results_to_probs(x2_binary_result, self.n_qubits, cast(int, self.device.shots))
+        else:
+            # convert the sample results to probability distribution
+            # shots must be over 0 when sampling mode
+            x1_probs = sample_results_to_probs(x1_result, self.n_qubits, cast(int, self.device.shots))
+            x2_probs = sample_results_to_probs(x2_result, self.n_qubits, cast(int, self.device.shots))
+
+        # compute expected values for projection operators
+        x1_projected = self._calculate_expected_values(x1_probs)
+        x2_projected = self._calculate_expected_values(x2_probs)
+
+        # compute gaussian kernel value based on the projected measurement results
+        kernel_value = np.exp(-self.gamma * np.sum((x1_projected - x2_projected) ** 2))
+
+        return kernel_value, (x1_probs + x2_probs) / 2
