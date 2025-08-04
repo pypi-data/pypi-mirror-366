3a0ef789374c517e1ff4d3febb82083654fb0c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:37:00 +0000

    Add package tool2openai/0.1.6/tool2openai-0.1.6.tar.gz

---
+++ packages/tool2openai/tool2openai-0.1.6.tar.gz/tool2openai-0.1.6/src/tool2openai/__init__.py
@@ -0,0 +1,18 @@
+"""
+tool2openai - Библиотека для работы с OpenAI API
+"""
+
+# Импортируем основные функции и классы из вашего файла
+from .tool2openai import (
+    OpenAIClient,
+    Config,
+)
+
+__version__ = "0.1.6"
+__author__ = "Nehcy"
+
+# Опционально: определяем, что будет доступно при импорте *
+__all__ = [
+    "OpenAIClient",
+    "Config",
+]
