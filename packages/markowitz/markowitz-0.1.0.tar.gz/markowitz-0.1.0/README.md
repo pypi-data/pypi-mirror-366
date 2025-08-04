dccf2291957db6ad0723000360d2e1de32a3c202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 15:07:41 +0000

    Add package markowitz/0.1.0/markowitz-0.1.0.tar.gz

---
+++ packages/markowitz/markowitz-0.1.0.tar.gz/markowitz-0.1.0/README.md
@@ -0,0 +1,3 @@
+# markowitz
+
+Librería educativa para enseñar el modelo de Markowitz paso a paso con PyQt5 y datos de Yahoo Finance.
---
+++ packages/markowitz/markowitz-0.1.0.tar.gz/markowitz-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="markowitz",
+    version="0.1.0",
+    author="Jesús Colbert",
+    description="Librería educativa para enseñar el modelo de Markowitz paso a paso",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    install_requires=[
+        "pandas",
+        "numpy",
+        "yfinance",
+        "matplotlib",
+        "PyQt5",
+        "openpyxl"
+    ],
+)
