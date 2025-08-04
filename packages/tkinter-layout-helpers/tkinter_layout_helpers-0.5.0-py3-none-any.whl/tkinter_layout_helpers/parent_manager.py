c23819bc53c5fe48d5701f2431b8bc4feb5325b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 03:41:34 +0000

    Add package tkinter-layout-helpers/0.5.0/tkinter_layout_helpers-0.5.0-py3-none-any.whl

---
+++ packages/tkinter-layout-helpers/tkinter_layout_helpers-0.5.0-py3-none-any.whl/tkinter_layout_helpers/parent_manager.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import contextlib
+import tkinter as tk
+from typing import TYPE_CHECKING, TypeVar, Union
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+TParent = TypeVar("TParent", bound=Union[tk.Tk, tk.Toplevel, tk.Widget])
+
+
+class DefaultRootWrapper:  # pragma: no cover  # noqa: D101
+    @property
+    def default_root(self) -> tk.Widget:  # noqa: D102
+        return tk._default_root  # noqa: SLF001
+
+    @default_root.setter
+    def default_root(self, value: TParent) -> None:
+        tk._default_root = value  # noqa: SLF001
+
+
+_default_root_wrapper = DefaultRootWrapper()
+
+
+@contextlib.contextmanager
+def set_parent(parent: TParent) -> Generator[TParent, None, None]:
+    """
+    Set the parent widget for all widgets created within the `with` statement scope,
+    so you will not have to pass the parent for every created widget.
+
+    Args:
+        parent: parent widget
+
+    """
+    old_root = _default_root_wrapper.default_root
+    _default_root_wrapper.default_root = parent
+    try:
+        yield parent
+    finally:
+        _default_root_wrapper.default_root = old_root
