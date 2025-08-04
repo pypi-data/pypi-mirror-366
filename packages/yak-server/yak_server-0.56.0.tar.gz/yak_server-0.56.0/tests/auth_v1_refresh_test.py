28ddd6a172818734273a27d5331be3ddeb4fa1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:36:52 +0000

    Add package yak-server/0.56.0/yak_server-0.56.0.tar.gz

---
+++ packages/yak-server/yak_server-0.56.0.tar.gz/yak_server-0.56.0/tests/auth_v1_refresh_test.py
@@ -0,0 +1,116 @@
+import time
+from http import HTTPStatus
+from typing import TYPE_CHECKING
+
+from starlette.testclient import TestClient
+
+from testing.util import get_random_string
+
+if TYPE_CHECKING:
+    from fastapi import FastAPI
+
+
+def test_refresh_token_after_signup(app_with_valid_jwt_config: "FastAPI") -> None:
+    client = TestClient(app_with_valid_jwt_config)
+
+    response = client.post(
+        "/api/v1/users/signup",
+        json={
+            "name": get_random_string(10),
+            "first_name": get_random_string(10),
+            "last_name": get_random_string(10),
+            "password": get_random_string(150),
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    refresh_token = response.json()["result"]["refresh_token"]
+
+    # Try to refresh the token
+    response = client.post("/api/v1/users/refresh", json={"refresh_token": refresh_token})
+    assert response.status_code == HTTPStatus.CREATED
+
+    new_access_token = response.json()["result"]["access_token"]
+
+    response = client.get(
+        "/api/v1/bets",
+        headers={"Authorization": f"Bearer {new_access_token}"},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_refresh_token_after_login(app_with_valid_jwt_config: "FastAPI") -> None:
+    client = TestClient(app_with_valid_jwt_config)
+
+    name = get_random_string(10)
+    password = get_random_string(150)
+
+    response = client.post(
+        "/api/v1/users/signup",
+        json={
+            "name": name,
+            "first_name": get_random_string(10),
+            "last_name": get_random_string(10),
+            "password": password,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    refresh_token = response.json()["result"]["refresh_token"]
+
+    response = client.post(
+        "/api/v1/users/login",
+        json={
+            "name": name,
+            "password": password,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    # Try to refresh the token
+    response = client.post("/api/v1/users/refresh", json={"refresh_token": refresh_token})
+    assert response.status_code == HTTPStatus.CREATED
+
+    new_access_token = response.json()["result"]["access_token"]
+
+    response = client.get(
+        "/api/v1/bets",
+        headers={"Authorization": f"Bearer {new_access_token}"},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_refresh_token_expired(app_with_null_jwt_refresh_expiration_time: "FastAPI") -> None:
+    client = TestClient(app_with_null_jwt_refresh_expiration_time)
+
+    # Login to get the refresh token
+    response_signup = client.post(
+        "/api/v1/users/signup",
+        json={
+            "name": get_random_string(10),
+            "first_name": get_random_string(10),
+            "last_name": get_random_string(10),
+            "password": get_random_string(150),
+        },
+    )
+
+    assert response_signup.status_code == HTTPStatus.CREATED
+
+    refresh_token = response_signup.json()["result"]["refresh_token"]
+
+    time.sleep(5)  # Wait for the refresh token to expire
+
+    # Call the refresh endpoint with expired token:
+    response = client.post("/api/v1/users/refresh", json={"refresh_token": refresh_token})
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {
+        "ok": False,
+        "error_code": HTTPStatus.UNAUTHORIZED,
+        "description": "Expired access token, re-authentication required",
+    }
