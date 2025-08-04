0528d046feb8944177383b1d9eabdbe67fba6475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 05:18:51 +0000

    Add package thunderpy/1.0.0/thunderpy-1.0.0.tar.gz

---
+++ packages/thunderpy/thunderpy-1.0.0.tar.gz/thunderpy-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thunderpy",
+    version="1.0.0",
+    packages=find_packages(),
+    author="whyraze",
+    description="Testlibrarry",
+    python_requires=">=3.6",
+)
