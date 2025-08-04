231a369fcdc17d2c091b32a9f924f030da768162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 16:06:31 +0000

    Add package jia512/1.0.1/jia512-1.0.1.tar.gz

---
+++ packages/jia512/jia512-1.0.1.tar.gz/jia512-1.0.1/jia_encryption/__version__.py
@@ -0,0 +1,10 @@
+"""
+Jia Encryption Framework Version
+"""
+
+__version__ = "1.0.1"
+__author__ = "夏云龙"
+__email__ = "xiayunlong@example.com"
+__description__ = "轻量级加密解密框架，提供RSA-AES混合加密功能"
+__url__ = "https://github.com/xiayunlong/jia-encryption"
+__license__ = "MIT" 
