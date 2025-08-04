7c765054d26585c9d1964b0cd67ddd96776534c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 03:41:40 +0000

    Add package tkinter-layout-helpers/0.5.1/tkinter_layout_helpers-0.5.1.tar.gz

---
+++ packages/tkinter-layout-helpers/tkinter_layout_helpers-0.5.1.tar.gz/tkinter_layout_helpers-0.5.1/examples/parent_widget_management.py
@@ -0,0 +1,24 @@
+"""
+Specify a parent widget of another tkinter widget
+using a context manager
+"""
+
+import tkinter as tk
+
+from tkinter_layout_helpers.parent_manager import set_parent
+
+
+def on_button_press():
+    with set_parent(tk.Toplevel()) as dialog:
+        tk.Label(text="In the child window").pack()
+        dialog.wait_window()
+
+    tk.Label(text="In the main window again").pack()
+
+
+root = tk.Tk()
+
+tk.Label(text="In the main window").pack()
+tk.Button(text="Press me", command=on_button_press).pack()
+
+root.mainloop()
