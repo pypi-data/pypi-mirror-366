be9eab078186836ff5712e63ebd83cd29a6f100b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 15:07:26 +0000

    Add package current-controller-sdk/1.0.1/current_controller_sdk-1.0.1-py3-none-any.whl

---
+++ packages/current-controller-sdk/current_controller_sdk-1.0.1-py3-none-any.whl/current_controller_sdk/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+current_controller_sdk：电流控制器串口通信SDK
+"""
+from .core import CurrentControllerSDK
+
+__version__ = "1.0.0"
+__author__ = "hxp"
+__all__ = ["CurrentControllerSDK"]  # 公开接口
