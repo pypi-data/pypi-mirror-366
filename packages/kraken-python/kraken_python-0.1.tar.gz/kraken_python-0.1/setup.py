9f1cff1c30e60237c896e3c23005758120551b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 16:06:48 +0000

    Add package kraken-python/0.1/kraken_python-0.1.tar.gz

---
+++ packages/kraken-python/kraken_python-0.1.tar.gz/kraken_python-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kraken-python',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['pyTelegramBotAPI'],
+    author='Anonymous Developer',
+    description='Secure anonymous Telegram bot library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/NEFORCEO',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
