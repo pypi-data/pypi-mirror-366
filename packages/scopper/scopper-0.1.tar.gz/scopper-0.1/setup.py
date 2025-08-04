f97b95087956188ec37a787bda9177220ac2c0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:36:34 +0000

    Add package scopper/0.1/scopper-0.1.tar.gz

---
+++ packages/scopper/scopper-0.1.tar.gz/scopper-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scopper",
+    version="0.1",
+    packages=find_packages(),
+    author="Elijah Joseph",
+    description="Simple Scopper Library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
