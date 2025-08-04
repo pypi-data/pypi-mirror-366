3ce0194dfd1ce7f6e571da34c3dd08e5358fbcef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:09:44 +0000

    Add package irides/0.0.2/irides-0.0.2.tar.gz

---
+++ packages/irides/irides-0.0.2.tar.gz/irides-0.0.2/src/irides/__init__.py
@@ -0,0 +1,13 @@
+"""
+-------------------------------------------------------------------------------
+
+`irides` package top-level import
+
+-------------------------------------------------------------------------------
+"""
+
+# AUTO-GENERATED version
+__version__ = "0.0.2"
+
+# Import the toolkit function
+from .entry_point import get_name
