042178e946f47c13f6d213efeda2c18e404d0bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 03:41:32 +0000

    Add package qps-limit/1.2.7/qps_limit-1.2.7.tar.gz

---
+++ packages/qps-limit/qps_limit-1.2.7.tar.gz/qps_limit-1.2.7/qps_limit/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '1.2.7'
+
+from .limiter import Limiter
+from .run import async_batch_run, async_streaming_batch_run, batch_run, streaming_batch_run
+
+__all__ = [
+    Limiter,
+    async_batch_run,
+    batch_run,
+    async_streaming_batch_run,
+    streaming_batch_run
+]
