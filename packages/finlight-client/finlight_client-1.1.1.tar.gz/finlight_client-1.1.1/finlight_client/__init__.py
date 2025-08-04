15c03b7e62af2c301d9ea3edbbda235c0c5878f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:36:20 +0000

    Add package finlight-client/1.1.1/finlight_client-1.1.1.tar.gz

---
+++ packages/finlight-client/finlight_client-1.1.1.tar.gz/finlight_client-1.1.1/finlight_client/__init__.py
@@ -0,0 +1,14 @@
+from .api_client import ApiClient
+from .articles import ArticleService
+from .sources import SourcesService
+from .websocket_client import WebSocketClient
+from .models import ApiConfig
+
+
+class FinlightApi:
+    def __init__(self, config: ApiConfig):
+        self.config: ApiConfig = config or ApiConfig()
+        self.api_client = ApiClient(self.config)
+        self.articles = ArticleService(self.api_client)
+        self.websocket = WebSocketClient(self.config)
+        self.sources = SourcesService(self.api_client)
