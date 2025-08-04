bb7bb1b6e86fd58e6c8d899c53996209d3d315eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 05:18:51 +0000

    Add package OvertureMaestro/0.5.0/overturemaestro-0.5.0-py3-none-any.whl

---
+++ packages/OvertureMaestro/overturemaestro-0.5.0-py3-none-any.whl/overturemaestro/__init__.py
@@ -0,0 +1,41 @@
+"""
+OvertureMaestro.
+
+OvertureMaestro is a Python library used for downloading Overture Maps datasets with quality of life
+features.
+"""
+
+from overturemaestro.functions import (
+    convert_bounding_box_to_geodataframe,
+    convert_bounding_box_to_geodataframe_for_multiple_types,
+    convert_bounding_box_to_parquet,
+    convert_bounding_box_to_parquet_for_multiple_types,
+    convert_geometry_to_geodataframe,
+    convert_geometry_to_geodataframe_for_multiple_types,
+    convert_geometry_to_parquet,
+    convert_geometry_to_parquet_for_multiple_types,
+)
+from overturemaestro.geocode import geocode_to_geometry
+from overturemaestro.release_index import (
+    get_available_release_versions,
+    get_available_theme_type_pairs,
+    get_newest_release_version,
+)
+
+__app_name__ = "OvertureMaestro"
+__version__ = "0.5.0"
+
+__all__ = [
+    "convert_bounding_box_to_geodataframe",
+    "convert_bounding_box_to_geodataframe_for_multiple_types",
+    "convert_bounding_box_to_parquet",
+    "convert_bounding_box_to_parquet_for_multiple_types",
+    "convert_geometry_to_geodataframe",
+    "convert_geometry_to_geodataframe_for_multiple_types",
+    "convert_geometry_to_parquet",
+    "convert_geometry_to_parquet_for_multiple_types",
+    "geocode_to_geometry",
+    "get_available_release_versions",
+    "get_available_theme_type_pairs",
+    "get_newest_release_version",
+]
---
+++ packages/OvertureMaestro/overturemaestro-0.5.0-py3-none-any.whl/overturemaestro/_rich_progress.py
@@ -0,0 +1,115 @@
+from datetime import timedelta
+from typing import Any
+
+from rich import print as rprint
+from rich.console import Console
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    ProgressColumn,
+    SpinnerColumn,
+    Task,
+    Text,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
+from rq_geo_toolkit.rich_utils import FORCE_TERMINAL, VERBOSITY_MODE
+
+__all__ = [
+    "show_total_elapsed_time",
+    "FORCE_TERMINAL",
+    "VERBOSITY_MODE",
+    "TrackProgressSpinner",
+    "TrackProgressBar",
+]
+
+def show_total_elapsed_time(elapsed_seconds: float) -> None:
+    elapsed_time_formatted = str(timedelta(seconds=int(elapsed_seconds)))
+    rprint(f"Finished operation in [progress.elapsed]{elapsed_time_formatted}")
+
+
+class TrackProgressSpinner(Progress):  # type: ignore[misc]
+    def __init__(
+        self,
+        task_name: str,
+        verbosity_mode: VERBOSITY_MODE = "verbose",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            TextColumn("•"),
+            TimeElapsedColumn(),
+            refresh_per_second=1,
+            speed_estimate_period=3600,
+            transient=verbosity_mode == "transient",
+            disable=verbosity_mode == "silent",
+            console=Console(force_interactive=False, force_jupyter=False, force_terminal=True)
+            if FORCE_TERMINAL
+            else None,
+            **kwargs,
+        )
+        self.task_name = task_name
+
+    def __enter__(self):  # type: ignore
+        self.add_task(description=self.task_name, total=None)
+        self.start()
+
+    def __exit__(self, exc_type, exc_value, exc_tb):  # type: ignore
+        self.stop()
+
+
+class SpeedColumn(ProgressColumn):  # type: ignore[misc]
+    def render(self, task: "Task") -> Text:
+        if task.speed is None:
+            return Text("0 it/s")
+        elif task.speed >= 1:
+            return Text(f"{task.speed:.2f} it/s")
+        else:
+            return Text(f"{1/task.speed:.2f} s/it")  # noqa: FURB126
+
+
+class TrackProgressBar(Progress):  # type: ignore[misc]
+    def __init__(
+        self,
+        verbosity_mode: VERBOSITY_MODE = "verbose",
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            SpinnerColumn(),
+            TextColumn(
+                "[progress.description]{task.description}"
+                " [progress.percentage]{task.percentage:>3.0f}%"
+            ),
+            BarColumn(),
+            MofNCompleteColumn(),
+            TextColumn("•"),
+            TimeElapsedColumn(),
+            TextColumn("<"),
+            TimeRemainingColumn(),
+            TextColumn("•"),
+            SpeedColumn(),
+            refresh_per_second=1,
+            speed_estimate_period=3600,
+            transient=verbosity_mode == "transient",
+            disable=verbosity_mode == "silent",
+            console=Console(force_interactive=False, force_jupyter=False, force_terminal=True)
+            if FORCE_TERMINAL
+            else None,
+            **kwargs,
+        )
+        self.verbosity_mode = verbosity_mode
+
+    def print_progress(self) -> None:
+        if self.verbosity_mode != "silent":
+            current_task = self.tasks[0]
+            mofn = self.columns[3].render(current_task)
+            elapsed = self.columns[5].render(current_task)
+            remaining = self.columns[7].render(current_task)
+            speed = self.columns[9].render(current_task)
+            self.print(
+                f"{current_task.description} {current_task.percentage:>6.2f}% • "
+                f"{mofn} • {elapsed} < {remaining} • {speed}"
+            )
