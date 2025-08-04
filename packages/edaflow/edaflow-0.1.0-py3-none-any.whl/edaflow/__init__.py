7b7ee063b291b28d78d1c9a821dc1d554851b71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:36:17 +0000

    Add package edaflow/0.1.0/edaflow-0.1.0-py3-none-any.whl

---
+++ packages/edaflow/edaflow-0.1.0-py3-none-any.whl/edaflow/__init__.py
@@ -0,0 +1,27 @@
+"""
+edaflow - A Python package for exploratory data analysis workflows
+"""
+
+from .analysis import check_null_columns
+
+__version__ = "0.1.0"
+__author__ = "Evan Low"
+__email__ = "evan.low@illumetechnology.com"
+
+
+def hello():
+    """
+    A sample hello function to test the package installation.
+
+    Returns:
+        str: A greeting message
+    """
+    return "Hello from edaflow! Ready for exploratory data analysis."
+
+
+# Import main modules
+# from .visualization import *
+# from .preprocessing import *
+
+# Export main functions
+__all__ = ['hello', 'check_null_columns']
