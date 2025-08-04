4f84749490596dcb6c71e7e510944d9c6a0773d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:09:48 +0000

    Add package student-tools/0.1.6/student_tools-0.1.6.tar.gz

---
+++ packages/student-tools/student_tools-0.1.6.tar.gz/student_tools-0.1.6/README.md
@@ -0,0 +1,3 @@
+Package name: student_tools
+Install with: pip install student-tools
+Use with: import st
---
+++ packages/student-tools/student_tools-0.1.6.tar.gz/student_tools-0.1.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+here = Path(__file__).parent
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='student_tools',
+    packages=['st'],
+    version='0.1.6',
+    author='Error Dev',
+    author_email='3rr0r.d3v@gmail.com',
+    description='A collection of English, Math, and Utility tools, made for students.',
+    install_requires=[
+        'sympy'
+    ],
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
