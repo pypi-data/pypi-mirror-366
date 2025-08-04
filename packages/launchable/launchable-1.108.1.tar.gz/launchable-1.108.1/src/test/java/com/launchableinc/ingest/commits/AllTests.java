b083fe9ac4a12ac01af1041cac8ffc431657a490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:10:02 +0000

    Add package launchable/1.108.1/launchable-1.108.1.tar.gz

---
+++ packages/launchable/launchable-1.108.1.tar.gz/launchable-1.108.1/src/test/java/com/launchableinc/ingest/commits/AllTests.java
@@ -0,0 +1,14 @@
+package com.launchableinc.ingest.commits;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    CommitGraphCollectorTest.class,
+    CommitIngesterTest.class,
+    FileChunkStreamerTest.class,
+    SSLBypassTest.class
+})
+public class AllTests {}
