bcf7d14a4858378567b956a55bb12ecfac1c4a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 16:07:08 +0000

    Add package pycupra/0.1.12/pycupra-0.1.12-py3-none-any.whl

---
+++ packages/pycupra/pycupra-0.1.12-py3-none-any.whl/pycupra/__version__.py
@@ -0,0 +1,6 @@
+"""
+pycupra - A Python 3 library for interacting with the My Cupra/My Seat portal.
+
+For more details and documentation, visit the github page at https://github.com/WulfgarW/pycupra
+"""
+__version__ = "0.1.12"
