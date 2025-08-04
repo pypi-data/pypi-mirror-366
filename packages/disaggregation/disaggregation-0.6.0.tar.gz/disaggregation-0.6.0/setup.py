7a76c8d9de169ae9741d49b5ed77e51799536a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 14:09:42 +0000

    Add package disaggregation/0.6.0/disaggregation-0.6.0.tar.gz

---
+++ packages/disaggregation/disaggregation-0.6.0.tar.gz/disaggregation-0.6.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='disaggregation',
+    version='0.6.0',
+    description='Disaggregates in Pandas',
+    author='Nathaniel Rogalskyj',
+    author_email='nr282@cornell.edu',
+    packages=["disaggregation"],
+    install_requires=[
+        'requests',
+        'numpy>=1.20',
+        'pandas',
+        'scipy',
+        'coverage',
+        'matplotlib',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
