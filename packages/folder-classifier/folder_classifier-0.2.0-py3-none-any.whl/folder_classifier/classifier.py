bc6febb88be7a1b38682d0b6721f6b5205507534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 15:07:23 +0000

    Add package folder-classifier/0.2.0/folder_classifier-0.2.0-py3-none-any.whl

---
+++ packages/folder-classifier/folder_classifier-0.2.0-py3-none-any.whl/folder_classifier/classifier.py
@@ -0,0 +1,34 @@
+from typing import Tuple
+
+import numpy as np
+import torch
+from transformers import pipeline
+
+from folder_classifier.dto import Listing
+
+classifier = pipeline(
+    "zero-shot-classification",
+    model="MoritzLaurer/ModernBERT-large-zeroshot-v2.0",
+    torch_dtype=torch.bfloat16,
+    device="cuda"
+)
+
+candidate_labels = ["legal_matter", "other"]
+
+def predict(listing: Listing) -> Tuple[str, float]:
+    text = "\n".join(listing.items)
+    hypothesis_template = "This list of files is about {}"
+    prediction = classifier(
+        text,
+        candidate_labels,
+        hypothesis_template=hypothesis_template,
+        multi_label=False,
+    )
+    scores = np.array(prediction["scores"], dtype=float)
+    highest_ix = np.argmax(scores)
+    predicted_label = prediction["labels"][highest_ix]
+    confidence = float(scores[highest_ix])
+    prediction = "matter" if predicted_label == "legal_matter" else "other"
+    return prediction, confidence
+
+
