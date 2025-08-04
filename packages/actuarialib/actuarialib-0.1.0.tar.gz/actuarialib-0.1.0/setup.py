cf8885bbbeaf506701dd033d98af14981b45ca8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 03:41:53 +0000

    Add package actuarialib/0.1.0/actuarialib-0.1.0.tar.gz

---
+++ packages/actuarialib/actuarialib-0.1.0.tar.gz/actuarialib-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='actuarialib',
+    version='0.1.0',
+    description='Actuarial Specific Library(actuarial toolkit)',
+    author='Purusottam Prasad',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
