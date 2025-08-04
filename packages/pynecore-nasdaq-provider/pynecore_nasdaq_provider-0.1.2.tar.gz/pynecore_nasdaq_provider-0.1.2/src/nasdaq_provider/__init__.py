655cb5338ae85bd6bda4c497eef928374345ae15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 03:41:38 +0000

    Add package pynecore-nasdaq-provider/0.1.2/pynecore_nasdaq_provider-0.1.2.tar.gz

---
+++ packages/pynecore-nasdaq-provider/pynecore_nasdaq_provider-0.1.2.tar.gz/pynecore_nasdaq_provider-0.1.2/src/nasdaq_provider/__init__.py
@@ -0,0 +1,9 @@
+"""Nasdaq Provider Plugin for PyneCore
+
+This module provides data access for nasdaq through PyneCore's provider interface.
+"""
+
+from .provider import NasdaqProvider
+
+__version__ = "0.1.0"
+__all__ = ["NasdaqProvider"]
