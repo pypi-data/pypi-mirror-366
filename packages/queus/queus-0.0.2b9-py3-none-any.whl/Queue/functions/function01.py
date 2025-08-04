037d9d06c835eb7854519eaf5126e3845e561abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 04:36:41 +0000

    Add package queus/0.0.2b9/queus-0.0.2b9-py3-none-any.whl

---
+++ packages/queus/queus-0.0.2b9-py3-none-any.whl/Queue/functions/function01.py
@@ -0,0 +1,45 @@
+import asyncio
+from .function02 import Queues
+from ..exceptions import QueuedAlready
+#================================================================================================
+
+class Queue:
+
+    def __init__(self, **kwargs):
+        self.waiting = kwargs.get("wait", 1)
+        self.maximum = kwargs.get("maximum", 1)
+        self.storage = kwargs.get("storage", Queues)
+
+#================================================================================================
+
+    async def total(self):
+        return len(self.storage)
+
+    async def add(self, uid, priority=-1):
+        if uid in self.storage: raise QueuedAlready()
+        self.storage.append(uid) if priority == -1 else self.storage.insert(priority, uid)
+
+#================================================================================================
+
+    async def message(self, imog, text, button=None):
+        if len(self.storage) >= self.maximum:
+            try: await imog.edit(text=text, reply_markup=button)
+            except Exception: pass
+
+#================================================================================================
+    
+    async def remove(self, uid):
+        self.storage.remove(uid) if uid in self.storage else 0
+
+    async def position(self, uid):
+        return self.storage.index(uid) - self.maximum + 1 if uid in self.storage else 0
+
+#================================================================================================
+
+    async def queue(self, uid):
+        while uid in self.storage:
+            while uid in self.storage:
+                if self.storage.index(uid) >= self.maximum: await asyncio.sleep(self.waiting)
+                else: break
+
+#================================================================================================
