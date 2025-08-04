44a0dfd63fb45dcefce6fe292bcc433b1bf13ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-04 15:07:33 +0000

    Add package libcachesim/0.3.3.post2/libcachesim-0.3.3.post2.tar.gz

---
+++ packages/libcachesim/libcachesim-0.3.3.post2.tar.gz/libcachesim-0.3.3.post2/benchmark/simulation.py
@@ -0,0 +1,5 @@
+"""Benchmark the simulation performance of the library.
+
+This module contains benchmarks for various components of the library,
+including request processing times, memory usage, and overall throughput.
+"""
---
+++ packages/libcachesim/libcachesim-0.3.3.post2.tar.gz/libcachesim-0.3.3.post2/docs/src/en/faq.md
@@ -0,0 +1,5 @@
+# Frequently Asked Questions
+
+1.  How to resolve when pip install fails?
+
+See [installation](https://cachemon.github.io/libCacheSim-python/getting_started/installation/).
